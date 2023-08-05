5742866f2113552533a660b55c4874c47c0d469f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:02 +0000

    Add package aind-data-schema/0.13.22/aind-data-schema-0.13.22.tar.gz

---
+++ packages/aind-data-schema/aind-data-schema-0.13.22.tar.gz/aind-data-schema-0.13.22/tests/test_subject.py
@@ -0,0 +1,43 @@
+""" tests for Subject """
+
+import datetime
+import unittest
+
+import pydantic
+
+from aind_data_schema import Subject
+from aind_data_schema.subject import Housing, LightCycle, MgiAlleleId
+
+
+class SubjectTests(unittest.TestCase):
+    """tests for subject"""
+
+    def test_constructors(self):
+        """try building Subjects"""
+
+        with self.assertRaises(pydantic.ValidationError):
+            s = Subject()
+
+        now = datetime.datetime.now()
+
+        s = Subject(
+            species="Mus musculus",
+            subject_id="1234",
+            sex="Male",
+            date_of_birth=now.date(),
+            genotype="wt",
+            housing=Housing(
+                light_cycle=LightCycle(
+                    lights_on_time=now.time(),
+                    lights_off_time=now.time(),
+                ),
+                cage_id="543",
+            ),
+            mgi_allele_ids=[MgiAlleleId(mgi_id="12345", allele_name="adsf")],
+        )
+
+        assert s is not None
+
+
+if __name__ == "__main__":
+    unittest.main()
