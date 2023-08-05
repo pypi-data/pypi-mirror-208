f6ce5d4e6c9078543c4977b766f005c7bd03b2f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:39 +0000

    Add package tedana/0.0.13/tedana-0.0.13.tar.gz

---
+++ packages/tedana/tedana-0.0.13.tar.gz/tedana-0.0.13/tedana/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# emacs: -*- mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vi: set ft=python sts=4 ts=4 sw=4 et:
+"""
+tedana: A Python package for TE-dependent analysis of multi-echo data.
+"""
+
+import warnings
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+
+# cmp is not used, so ignore nipype-generated warnings
+warnings.filterwarnings("ignore", r"cmp not installed")
+
+del get_versions
