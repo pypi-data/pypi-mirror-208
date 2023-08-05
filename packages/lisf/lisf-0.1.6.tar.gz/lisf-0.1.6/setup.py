4f840e8042c7f6e7d033a1d880660f996b4cc50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:07 +0000

    Add package lisf/0.1.6/lisf-0.1.6.tar.gz

---
+++ packages/lisf/lisf-0.1.6.tar.gz/lisf-0.1.6/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['lisf']
+setup_kwargs = {
+    'name': 'lisf',
+    'version': '0.1.6',
+    'description': 'Lazy Instantiated Singleton Factory',
+    'long_description': 'None',
+    'author': 'imi',
+    'author_email': 'None',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
