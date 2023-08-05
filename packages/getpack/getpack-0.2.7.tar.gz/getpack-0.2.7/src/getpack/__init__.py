d05189c101b44001fbd9270427faca7e645bb2e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:44 +0000

    Add package getpack/0.2.7/getpack-0.2.7.tar.gz

---
+++ packages/getpack/getpack-0.2.7.tar.gz/getpack-0.2.7/src/getpack/__init__.py
@@ -0,0 +1,14 @@
+"""
+Declarative external resources with implicit deployment.
+
+This file is suitable to bundle with projects so they would bootstrap *getpack*
+and install it from pypi, then use full set of functions.
+
+
+Copyright (c) 2022 Konstantin Maslyuk. All rights reserved.
+
+This work is licensed under the terms of the MIT license.
+For a copy, see <https://opensource.org/licenses/MIT>.
+"""
+
+__version__ = '0.2.7'
