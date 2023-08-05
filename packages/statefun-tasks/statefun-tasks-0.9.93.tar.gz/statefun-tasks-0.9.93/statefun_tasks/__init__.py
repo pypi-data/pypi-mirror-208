686ccce994d88fc67c2c6fa75de49c6f7e9cf336
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:51 +0000

    Add package statefun-tasks/0.9.93/statefun-tasks-0.9.93.tar.gz

---
+++ packages/statefun-tasks/statefun-tasks-0.9.93.tar.gz/statefun-tasks-0.9.93/statefun_tasks/__init__.py
@@ -0,0 +1,27 @@
+# tasks API functions
+from statefun_tasks.task_builder import FlinkTasks
+
+
+# context
+from statefun_tasks.context import TaskContext
+
+
+# pipeline builder
+from statefun_tasks.pipeline_builder import PipelineBuilder, in_parallel
+
+
+# serialisation
+from statefun_tasks.protobuf import pack_any, unpack_any
+from statefun_tasks.serialisation import DefaultSerialiser
+
+
+# types
+from statefun_tasks.types import Task, Group, RetryPolicy, TaskAlreadyExistsException, TaskCancelledException, TasksException, \
+    PipelineInProgress, YieldTaskInvocation, MessageSizeExceeded
+    
+from statefun_tasks.type_helpers import flink_value_type_for, add_flink_value_type_for
+
+
+# protobuf message types
+from statefun_tasks.messages_pb2 import TaskRequest, TaskResult, TaskException, TaskActionRequest, TaskActionResult, \
+    TaskActionException, TaskAction, TaskStatus, TaskDeferral, PausedTask, Pipeline, ChildPipeline, Address, TaskInfo, DeferredTask
