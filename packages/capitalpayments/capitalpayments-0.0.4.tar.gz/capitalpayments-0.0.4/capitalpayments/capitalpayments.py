1776801d4db2be08699b27026e6960c209afec54
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:32 +0000

    Add package capitalpayments/0.0.4/capitalpayments-0.0.4.tar.gz

---
+++ packages/capitalpayments/capitalpayments-0.0.4.tar.gz/capitalpayments-0.0.4/capitalpayments/capitalpayments.py
@@ -0,0 +1,115 @@
+from url_manager import url_manager
+import requests
+
+class CapitalPaymentsSDK:
+    def __init__(self, api_key, api_secret):
+        self.api_key = api_key
+        self.api_secret = api_secret
+    def getLogin(self):
+        response = requests.get(
+            url_manager['login'], 
+            headers = {'Content-Type': 'application/json'},
+            auth = (self.api_key, self.api_secret)
+        )
+        return response.json()
+    def getAccount(self):
+        response = requests.get(
+            url_manager['get_account'], 
+            headers = {'Content-Type': 'application/json'},
+            auth = (self.api_key, self.api_secret)
+        )
+        return response.json()
+    def getBalance(self):
+        response = requests.get(
+            url_manager['get_balance'], 
+            headers = {'Content-Type': 'application/json'},
+            auth = (self.api_key, self.api_secret)
+        )
+        return response.json()
+    def getEnvironment(self):
+        response = requests.get(
+            url_manager['get_environment'], 
+            headers = {'Content-Type': 'application/json'},
+            auth = (self.api_key, self.api_secret)
+        )
+        return response.json()
+    def getMainWallet(self):
+        response = requests.get(
+            url_manager['get_main_wallet'], 
+            headers = {'Content-Type': 'application/json'},
+            auth = (self.api_key, self.api_secret)
+        )
+        return response.json()
+    def getWallets(self):
+        response = requests.get(
+            url_manager['get_wallets'], 
+            headers = {'Content-Type': 'application/json'},
+            auth = (self.api_key, self.api_secret)
+        )
+        return response.json()
+    def createInvoice(self,data):
+        response = requests.post(
+            url_manager['create_invoice'], 
+            headers = {'Content-Type': 'application/json'},
+            auth = (self.api_key, self.api_secret),
+            json = data
+        )
+        return response.json()
+    def cancelInvoice(self,data):
+        if data.get('invoice_id'):
+            response = requests.post(
+                url_manager['cancel_invoice'], 
+                headers = {'Content-Type': 'application/json'},
+                auth = (self.api_key, self.api_secret),
+                json = data
+            )
+            return response.json()
+        else: 
+            raise TypeError("Not invoice id")
+    def getInvoiceStatus(self,data):
+        if data.get('invoice_id'):
+            response = requests.post(
+                url_manager['get_invoice_status'], 
+                headers = {'Content-Type': 'application/json'},
+                auth = (self.api_key, self.api_secret),
+                json = data
+            )
+            return response.json()
+        else: 
+            raise TypeError("Not invoice id")
+    def createPayout(self,data):
+        if data.get('amount'):
+            if data.get('address'):
+                response = requests.post(
+                    url_manager['create_payout'], 
+                    headers = {'Content-Type': 'application/json'},
+                    auth = (self.api_key, self.api_secret),
+                    json = data
+                )
+                return response.json()
+            else: 
+                raise TypeError("Not address")
+        else: 
+            raise TypeError("Not ammount")
+    def cancelPayout(self,data):
+        if data.get('payout_id'):
+            response = requests.post(
+                url_manager['cancel_payout'], 
+                headers = {'Content-Type': 'application/json'},
+                auth = (self.api_key, self.api_secret),
+                json = data
+            )
+            return response.json()
+        else: 
+            raise TypeError("Not payout id")
+    def getPayoutStatus(self,data):
+        if data.get('payout_id'):
+            response = requests.post(
+                url_manager['get_payout_status'], 
+                headers = {'Content-Type': 'application/json'},
+                auth = (self.api_key, self.api_secret),
+                json = data
+            )
+            return response.json()
+        else: 
+            raise TypeError("Not payout id")
---
+++ packages/capitalpayments/capitalpayments-0.0.4.tar.gz/capitalpayments-0.0.4/capitalpayments/url_manager.py
@@ -0,0 +1,40 @@
+url_manager = {
+    'get_environment': 'http://localhost:8888/capitalpayments/app/api/getEnvironment',
+    'get_account': 'http://localhost:8888/capitalpayments/app/api/getAccount',
+    'login': 'http://localhost:8888/capitalpayments/app/api/login',
+
+    # invoices
+    'create_invoice': 'http://localhost:8888/capitalpayments/app/api/createInvoice',
+    'get_invoice_status': 'http://localhost:8888/capitalpayments/app/api/getInvoiceStatus',
+    'cancel_invoice': 'http://localhost:8888/capitalpayments/app/api/cancelInvoice',
+    
+    # payouts
+    'create_payout': 'http://localhost:8888/capitalpayments/app/api/createPayout',
+    'get_payout_status': 'http://localhost:8888/capitalpayments/app/api/getPayoutStatus',
+    'cancel_payout': 'http://localhost:8888/capitalpayments/app/api/cancelPayout',
+
+    # wallet
+    'get_balance': 'http://localhost:8888/capitalpayments/app/api/getBalance',
+    'get_main_wallet': 'http://localhost:8888/capitalpayments/app/api/getMainWallet',
+    'get_wallets': 'http://localhost:8888/capitalpayments/app/api/getWallets'
+}
+# url_manager = {
+#     'get_environment': 'https://www.capitalpayments.co/app/api/getEnvironment',
+#     'get_account': 'https://www.capitalpayments.co/app/api/getAccount',
+#     'login': 'https://www.capitalpayments.co/app/api/login',
+
+#     # invoices
+#     'create_invoice': 'https://www.capitalpayments.co/app/api/createInvoice',
+#     'get_invoice_status': 'https://www.capitalpayments.co/app/api/getInvoiceStatus',
+#     'cancel_invoice': 'https://www.capitalpayments.co/app/api/cancelInvoice',
+    
+#     # payouts
+#     'create_payout': 'https://www.capitalpayments.co/app/api/createPayout',
+#     'get_payout_status': 'https://www.capitalpayments.co/app/api/getPayoutStatus',
+#     'cancel_payout': 'https://www.capitalpayments.co/app/api/cancelPayout',
+
+#     # wallet
+#     'get_balance': 'https://www.capitalpayments.co/app/api/getBalance',
+#     'get_main_wallet': 'https://www.capitalpayments.co/app/api/getMainWallet',
+#     'get_wallets': 'https://www.capitalpayments.co/app/api/getWallets'
+# }
