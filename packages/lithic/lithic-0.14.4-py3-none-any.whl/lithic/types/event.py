08d3df65670416615fceff30d4fd7f6a82ef4fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:05 +0000

    Add package lithic/0.14.4/lithic-0.14.4-py3-none-any.whl

---
+++ packages/lithic/lithic-0.14.4-py3-none-any.whl/lithic/types/event.py
@@ -0,0 +1,30 @@
+# File generated from our OpenAPI spec by Stainless.
+
+from typing import Dict
+from datetime import datetime
+from typing_extensions import Literal
+
+from .._models import BaseModel
+
+__all__ = ["Event"]
+
+
+class Event(BaseModel):
+    created: datetime
+    """An RFC 3339 timestamp for when the event was created. UTC time zone.
+
+    If no timezone is specified, UTC will be used.
+    """
+
+    event_type: Literal["dispute.updated", "digital_wallet.tokenization_approval_request"]
+    """Event types:
+
+    - `dispute.updated` - A dispute has been updated.
+    - `digital_wallet.tokenization_approval_request` - Card network's request to
+      Lithic to activate a digital wallet token.
+    """
+
+    payload: Dict[str, object]
+
+    token: str
+    """Globally unique identifier."""
