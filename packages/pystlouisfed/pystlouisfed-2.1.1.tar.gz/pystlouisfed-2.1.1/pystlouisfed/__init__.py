5f5e601e7f0db237cc884286b66c0bd487e57b2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:44 +0000

    Add package pystlouisfed/2.1.1/pystlouisfed-2.1.1.tar.gz

---
+++ packages/pystlouisfed/pystlouisfed-2.1.1.tar.gz/pystlouisfed-2.1.1/pystlouisfed/__init__.py
@@ -0,0 +1,13 @@
+__version__ = '2.1.1'
+
+from pystlouisfed.client import FRED, ALFRED, GeoFRED, FRASER
+from pystlouisfed.enums import SortOrder, TagGroupID, FilterVariable, Seasonality, FilterValue, OrderBy, Unit, Frequency, AggregationMethod, OutputType, SearchType, RegionType, ShapeType
+from pystlouisfed.models import Release, Category, Shape, Source, Series, SeriesGroup
+
+__pdoc__ = {
+    'client.Client': False,
+    'client.URLFactory': False,
+    'models.Tag': False,
+    'models.ReleaseDate': False,
+    'models.Observation': False
+}
