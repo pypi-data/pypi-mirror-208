a71d5d283b396575a7f649b7f241d0e4717d94e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:47 +0000

    Add package eventiq/0.1.6/eventiq-0.1.6.tar.gz

---
+++ packages/eventiq/eventiq-0.1.6.tar.gz/eventiq-0.1.6/eventiq/models.py
@@ -0,0 +1,80 @@
+from datetime import datetime, timedelta
+from typing import Any, Dict, Generic, Optional
+
+from pydantic import Extra, Field, validator
+from pydantic.fields import PrivateAttr
+from pydantic.generics import GenericModel
+
+from .context import get_current_service
+from .message import Message
+from .types import ID, D
+from .utils import str_uuid
+from .utils.datetime import utc_now
+
+
+class CloudEvent(GenericModel, Generic[D]):
+    specversion: Optional[str] = "1.0"
+    content_type: str = Field("application/json", alias="datacontenttype")
+    id: ID = Field(default_factory=str_uuid)
+    time: datetime = Field(default_factory=utc_now)
+    topic: str = Field(..., alias="subject")
+    type: Optional[str] = None
+    source: Optional[str] = None
+    data: D
+    trace_ctx: Dict[str, str] = {}
+
+    _raw: Optional[Any] = PrivateAttr()
+
+    def __eq__(self, other):
+        if not isinstance(other, CloudEvent):
+            return False
+        return self.id == other.id
+
+    @validator("type", allow_reuse=True, always=True, pre=True)
+    def get_type_from_cls_name(cls, v) -> str:
+        return v or cls.__name__
+
+    @property
+    def raw(self) -> Message:
+        if self._raw is None:
+            raise AttributeError("raw property accessible only for incoming messages")
+        return self._raw
+
+    @property
+    def service(self):
+        return get_current_service()
+
+    @property
+    def log_context(self) -> Dict[str, Any]:
+        return {"message_id": str(self.id)}
+
+    def _set(self, name: str, value: Any):
+        object.__setattr__(self, name, value)
+
+    def set_source(self, value: str) -> None:
+        self._set("source", value)
+
+    def set_raw(self, value: Message):
+        self._set("_raw", value)
+
+    def dict(self, **kwargs: Any) -> Dict[str, Any]:
+        kwargs.setdefault("by_alias", True)
+        return super().dict(**kwargs)
+
+    @classmethod
+    def new(cls, obj: D, **kwargs: Any):
+        return cls(data=obj, **kwargs)
+
+    @property
+    def age(self) -> timedelta:
+        return utc_now() - self.time
+
+    def fail(self):
+        self.raw.fail()
+
+    class Config:
+        use_enum_values = True
+        allow_population_by_field_name = True
+        extra = Extra.allow
+        # events are immutable, however it's sometimes useful to set source or traceid for unpublished events
+        allow_mutation = False
---
+++ packages/eventiq/eventiq-0.1.6.tar.gz/eventiq-0.1.6/eventiq/service.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+
+import asyncio
+from typing import TYPE_CHECKING, Any, Callable
+
+from .consumer import Consumer, ConsumerGroup, ForwardResponse
+from .logger import LoggerMixin
+from .models import CloudEvent
+from .settings import ServiceSettings
+from .utils import generate_instance_id
+
+if TYPE_CHECKING:
+    from .broker import Broker
+    from .types import TagMeta
+
+
+class Service(LoggerMixin):
+    """Logical group of consumers. Provides group (queue) name and handles versioning"""
+
+    def __init__(
+        self,
+        name: str,
+        broker: Broker,
+        title: str | None = None,
+        version: str = "0.1.0",
+        description: str = "",
+        tags_metadata: list[TagMeta] | None = None,
+        instance_id_generator: Callable[[], str] | None = None,
+        **context: Any,
+    ):
+        self.broker = broker
+        self.name = name
+        self.title = title or name.title()
+        self.version = version
+        self.description = description
+        self.tags_metadata = tags_metadata or []
+        self.id = (instance_id_generator or generate_instance_id)()
+        self.consumer_group = ConsumerGroup()
+        self.context = context
+        # TODO: task gathering?
+        self._tasks: list[asyncio.Task] = []
+
+    def subscribe(
+        self,
+        topic: str,
+        *,
+        name: str | None = None,
+        timeout: int = 120,
+        dynamic: bool = False,
+        forward_response: ForwardResponse | None = None,
+        **options,
+    ):
+        return self.consumer_group.subscribe(
+            topic=topic,
+            name=name,
+            timeout=timeout,
+            dynamic=dynamic,
+            forward_response=forward_response,
+            **options,
+        )
+
+    def add_consumer(self, consumer: Consumer):
+        self.consumer_group.add_consumer(consumer)
+
+    def add_consumer_group(self, consumer_group: ConsumerGroup) -> None:
+        self.consumer_group.add_consumer_group(consumer_group)
+
+    async def publish(
+        self,
+        topic: str,
+        data: Any | None = None,
+        type_: type[CloudEvent] | str = "CloudEvent",
+        **kwargs,
+    ):
+        kwargs.setdefault("source", self.name)
+        return await self.broker.publish(topic, data, type_, **kwargs)
+
+    @property
+    def consumers(self):
+        return self.consumer_group.consumers
+
+    async def publish_event(self, message: CloudEvent, **kwargs):
+        if not message.source:
+            message.set_source(self.name)
+        return await self.broker.publish_event(message, **kwargs)
+
+    async def start(self):
+        await self.broker.connect()
+        await self.broker.dispatch_before("service_start", self)
+        for consumer in self.consumers.values():
+            asyncio.create_task(self.broker.start_consumer(self, consumer))
+        await self.broker.dispatch_after("service_start", self)
+
+    async def stop(self, *args, **kwargs):
+        await self.broker.dispatch_before("service_stop", self)
+        await self.broker.disconnect()
+        await self.broker.dispatch_after("service_stop", self)
+
+    def run(self, *args, **kwargs):
+        from .runner import ServiceRunner
+
+        runner = ServiceRunner([self])
+        runner.run(*args, **kwargs)
+
+    @classmethod
+    def from_settings(cls, settings: ServiceSettings, **kwargs: Any) -> Service:
+        kw = settings.dict(exclude={"broker_settings"})
+        kw.update(**kwargs)
+        broker = Broker.from_settings(settings.broker_settings)
+        return cls(broker=broker, **kw)
+
+    @classmethod
+    def from_env(cls, **kwargs: Any) -> Service:
+        return cls.from_settings(ServiceSettings(), **kwargs)
