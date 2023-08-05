3313235ec9604330311f94e247b61e72533e06e7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:03 +0000

    Add package Python-SPM/1.1/Python-SPM-1.1.tar.gz

---
+++ packages/Python-SPM/Python-SPM-1.1.tar.gz/Python-SPM-1.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='pi_spm',
+      version='1.0',
+      description='Python wrapper for Switch Path Manager',
+      url='',
+      author='Pickering Interfaces',
+      author_email='support@pickeringtest.com',
+      license='',
+      packages=['pi_spm'],
+      install_requires=[
+          'future',
+      ],
+      zip_safe=False)
