3327c3d075c097e9935d3dd208d83458375a6ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:11 +0000

    Add package glutton/0.0.1/glutton-0.0.1.tar.gz

---
+++ packages/glutton/glutton-0.0.1.tar.gz/glutton-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="glutton",
+    version="0.0.1",
+    author="glutton",
+    author_email="9239867+chaos-god@user.noreply.gitee.com",
+    description="glutton",
+    url="https://gitee.com/chaos-god/glutton.git",
+    packages=find_packages(),
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[
+        'pytest>=7.1.2',
+    ],
+    dependency_links=[
+    ],
+    extras_require={
+    }
+)
