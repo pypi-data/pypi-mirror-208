ded0d172a128306ca975fb26d78ee54052570a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:40 +0000

    Add package case43/0.1/case43-0.1-py3-none-any.whl

---
+++ packages/case43/case43-0.1-py3-none-any.whl/case43/__init__.py
@@ -0,0 +1,16 @@
+import firebase_admin
+import pandas as pd
+from firebase_admin import firestore,_apps,credentials
+def connect(json):
+    if not _apps:
+        cred=credentials.Certificate(json)
+        firebase_admin.initialize_app(cred)
+def selectallfromcollection(kalyan):
+    if _apps:
+        cursor=firestore.client()
+        stream=cursor.collection(kalyan).stream()
+        list=[]
+        for i in stream:
+            list.append(i.to_dict())
+        p=pd.DataFrame(list)
+        print(p)
