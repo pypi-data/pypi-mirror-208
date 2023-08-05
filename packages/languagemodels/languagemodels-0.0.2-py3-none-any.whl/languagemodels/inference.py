1f77ebe60ac25160b34307bc6e88ceb77ade2d6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:32 +0000

    Add package languagemodels/0.0.2/languagemodels-0.0.2-py3-none-any.whl

---
+++ packages/languagemodels/languagemodels-0.0.2-py3-none-any.whl/languagemodels/inference.py
@@ -0,0 +1,67 @@
+import requests
+import os
+from transformers import pipeline
+
+
+class InferenceException(Exception):
+    pass
+
+
+modelcache = {}
+
+
+def generate_ts(engine, prompt, max_tokens=200):
+    """Generates a single text response for a prompt from a textsynth server
+
+    The server and API key are provided as environment variables:
+
+    ts_server is the server such as http://localhost:8080
+    ts_key is the API key
+    """
+    apikey = os.environ.get("ts_key") or ""
+    server = os.environ.get("ts_server") or "https://api.textsynth.com"
+
+    response = requests.post(
+        f"{server}/v1/engines/{engine}/completions",
+        headers={"Authorization": f"Bearer {apikey}"},
+        json={"prompt": prompt, "max_tokens": max_tokens},
+    )
+    resp = response.json()
+    if "text" in resp:
+        return resp["text"]
+    else:
+        raise InferenceException(f"TextSynth error: {resp}")
+
+
+def generate_instruct(prompt, max_tokens=200, temperature=.1, repetition_penalty=1.2):
+    """Generates one completion for a prompt using an instruction-tuned model
+
+    This may use a local model, or it may make an API call to an external
+    model if API keys are available.
+    """
+    if os.environ.get("ts_key") or os.environ.get("ts_server"):
+        return generate_ts("flan_t5_xxl_q4", prompt, max_tokens)
+
+    generate = get_pipeline("text2text-generation", "google/flan-t5-large")
+
+    return generate(
+        prompt,
+        repetition_penalty=repetition_penalty,
+        temperature=temperature,
+        do_sample=temperature > .1,
+    )[0]["generated_text"]
+
+
+def get_pipeline(task, model):
+    """Gets a pipeline instance
+
+    This is thin wrapper around the pipeline constructor to provide caching
+    across calls.
+    """
+
+    if model not in modelcache:
+        modelcache[model] = pipeline(
+            task, model=model, model_kwargs={"low_cpu_mem_usage": True}
+        )
+
+    return modelcache[model]
