23b41a89892c8088ba5a1b42e12a588485482884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:41 +0000

    Add package catalyst-care/0.0.1/catalyst_care-0.0.1-py3-none-any.whl

---
+++ packages/catalyst-care/catalyst_care-0.0.1-py3-none-any.whl/python_workspace/workspace.py
@@ -0,0 +1,12 @@
+import math
+
+import django
+
+
+def say_hello(text: str) -> str:
+    return text
+
+
+sos = "fsfdsfdsdfsdfsdklskdkfdlksdflkafdlsfadllfdaldflkdsfaldfl;adsfl;sdafl;as"
+
+say_hello("bro")
