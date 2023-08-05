f21b41229263f54b483fafb13763b013d5ceb257
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:00 +0000

    Add package pandas-llm/0.0.3/pandas_llm-0.0.3-py3-none-any.whl

---
+++ packages/pandas-llm/pandas_llm-0.0.3-py3-none-any.whl/pandas_llm/example.py
@@ -0,0 +1,27 @@
+import os
+import pandas as pd
+from pandas_llm import PandasLLM
+
+# your key
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+
+# Data
+# Please note that these names, ages, and donations are randomly generated 
+# and do not correspond to real individuals or their donations.
+data = [('John Doe', 25, 50), 
+        ('Jane Smith', 38, 70),
+        ('Alex Johnson', 45, 80),
+        ('Jessica Brown', 60, 40),
+        ('Michael Davis', 22, 90),
+        ('Emily Wilson', 30, 60),
+        ('Daniel Taylor', 35, 75),
+        ('Sophia Moore', 40, 85),
+        ('David Thomas', 50, 65),
+        ('Olivia Jackson', 29, 55)]
+df = pd.DataFrame(data, columns=['name', 'age', 'donation'])
+
+conv_df = PandasLLM(data=df, config={ "openai_api_key":OPENAI_API_KEY})
+result = conv_df.prompt("What is the average donation of people older than 30 who donated more than $50?")
+
+print(f"Result ({type(result)}):\n {result}")
+
