35d09d4c280822c79aac15e3afc567df1077266b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:21:46 +0000

    Add package ardilla/0.0.5a0/ardilla-0.0.5a0-py3-none-any.whl

---
+++ packages/ardilla/ardilla-0.0.5a0-py3-none-any.whl/ardilla/asyncio/engine.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+import aiosqlite
+
+from .crud import AsyncCrud
+from ..models import M
+from ..engine import Engine
+
+from .abc import AbstractAsyncEngine
+
+
+class AsyncEngine(Engine, AbstractAsyncEngine):
+    async def connect(self) -> aiosqlite.Connection:
+        con = await aiosqlite.connect(self.path)
+        con.row_factory = aiosqlite.Row
+        return con
+
+    async def __aenter__(self) -> aiosqlite.Connection:
+        self.con = await self.connect()
+        return self.con
+
+    async def __aexit__(self, *_):
+        await self.con.close()
+
+    async def setup(self):
+        async with self as con:
+            await con.execute("PRAGMA foreign_keys = ON;")
+            for table in self.schemas:
+                await con.execute(table)
+            await con.commit()
+
+    def crud(self, Model: type[M]) -> AsyncCrud[M]:
+        return AsyncCrud(Model, self)
