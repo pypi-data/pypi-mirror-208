e263ff779188be5226d4130ffede39ac6d55fe44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:07 +0000

    Add package cardmasking/0.1.2/cardmasking-0.1.2-py3-none-any.whl

---
+++ packages/cardmasking/cardmasking-0.1.2-py3-none-any.whl/cardmasking/masking.py
@@ -0,0 +1,26 @@
+
+def cardmapping(card,cvv,ExpiryDate):
+    x = 0
+    card_num =""
+    cvv_num =""
+    expiry = ""
+    for i in card:
+        if x >3 and x<12:
+            card_num += "*"
+        else:
+            card_num+=i
+        x+=1
+
+    for i in cvv:
+        cvv_num += "*"
+
+    for i in ExpiryDate:
+        expiry += "*"
+
+    response = {
+        "card":card_num,
+        "cvv":cvv_num,
+        "expiry":expiry
+    }
+        
+    return response
