152c76c5216f566fb2e161e59e7dcf67917d9de9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:04 +0000

    Add package scsd/0.0.7/scsd-0.0.7.tar.gz

---
+++ packages/scsd/scsd-0.0.7.tar.gz/scsd-0.0.7/README.md
@@ -0,0 +1,42 @@
+# steamCloudSaveDownloader
+Download Save on Steam Cloud
+
+## Early Stage Prototype: DO NOT USE
+
+
+git hook to auto update version
+docker puid pgid
+docker mountable cron
+
+Logo, badges
+
+Rotation
+Specify Directory
+
+Help does not show full msg
+Disclaimer: Not affiliated with steam
+
+Rotation mode
+
+Show database mode
+
+Do not open remotestorage again or will logout???
+
+Running lock file to prevent re execute for cron
+
+# Notifier config files
+
+
+### DOcker
+Specify CronJob
+
+### Windows Version
+
+### CI
+- Gen executable for linux/windows
+
+### Misc
+- Banners
+
+### Logger
+100,000 Limit
---
+++ packages/scsd/scsd-0.0.7.tar.gz/scsd-0.0.7/steamCloudSaveDownloader/web.py
@@ -0,0 +1,95 @@
+from . import err
+from .err import err_enum
+from .parser import web_parser
+import requests
+import pickle
+import shutil
+import time
+import random
+
+g_dbg_mode = False
+g_web_acc_link = "https://store.steampowered.com/account/?l=english"
+g_web_link = "https://store.steampowered.com/account/remotestorage/?l=english"
+g_random_sleep_interval = (2, 5)
+
+def random_sleep(func):
+    def wrapper(*args, **kwargs):
+        if not g_dbg_mode:
+            time.sleep(random.randint(g_random_sleep_interval[0], g_random_sleep_interval[1]))
+        return func(*args, **kwargs)
+    return wrapper
+
+class web:
+    def __init__(self, cookie):
+        self.web_parser = web_parser()
+        self.session = requests.Session()
+        self.cookie_pkl = cookie
+        try:
+            with open(self.cookie_pkl, 'rb') as f:
+                self.session.cookies.update(pickle.load(f))
+        except:
+            raise err.err(err_enum.INVALID_COOKIE_FORMAT)
+
+        response = self.session.get(g_web_acc_link)
+
+    # Return list of {"Game": name, "Link", link}
+    @random_sleep
+    def get_list(self):
+        response = None
+
+        if (not g_dbg_mode):
+            response = self.session.get(g_web_link)
+            if (response.status_code != 200):
+                print(err.ERR_MSG[err_enum.ERR_CANNOT_RETRIEVE_LIST_MSG], file=sys.stderr)
+                exit(err_enum.ERR_CANNOT_RETRIEVE_LIST.value)
+
+        # --- Start of DBG Block ---
+        #with open('game_list_response.pkl', 'wb') as f:
+        #   pickle.dump(response, f)
+
+        if (g_dbg_mode):
+            with open('game_list_response.pkl', 'rb') as f:
+                response = pickle.load(f)
+        # --- End of DBG Block ---
+        return self.web_parser.parse_index(response.text)
+
+    @random_sleep
+    def _get_game_save(self, game_link:str):
+        response = None
+        if (not g_dbg_mode):
+            response = self.session.get(game_link)
+            if (response.status_code != 200):
+                err.err(ERR_CANNOT_RETRIEVE_GAME_FILES).print()
+                return (None, None)
+
+        # --- Start of DBG Block ---
+        #with open('game_file_response.pkl', 'wb') as f:
+        #    pickle.dump(response, f)
+
+        if g_dbg_mode:
+            with open('game_file_response.pkl', 'rb') as f:
+               response = pickle.load(f)
+        #--- End of DBG Block ---
+
+        return self.web_parser.parse_game_file(response.text)
+
+
+    def get_game_save(self, game_link:str):
+        # Some games have a lot of games with multiple pages
+        next_page_link = game_link
+        save_file_list = list()
+        while True:
+            partial_save_file_list, next_page_link = \
+                self._get_game_save(next_page_link)
+            save_file_list += partial_save_file_list
+            if g_dbg_mode or (next_page_link is None):
+                break
+        return save_file_list
+
+    @random_sleep
+    def download_game_save(self, link:str, store_location:str):
+        if g_dbg_mode:
+            return
+        with self.session.get(link, stream=True) as r:
+            with open(store_location, 'wb') as f:
+                    shutil.copyfileobj(r.raw, f)
