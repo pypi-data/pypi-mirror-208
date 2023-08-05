4a6cd4bd114fc1e79518567711bb951643274674
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:53 +0000

    Add package picodaqa/1.1.1/picodaqa-1.1.1-py2.py3-none-any.whl

---
+++ packages/picodaqa/picodaqa-1.1.1-py2.py3-none-any.whl/picodaqa/_version_info.py
@@ -0,0 +1,24 @@
+'''
+.. module:: _version_info
+   :platform: python 2.7, >=3.6
+   :synopsis: Version 1.0.0 of picodaq, rel. Mar. 22
+
+.. moduleauthor:: Guenter Quast <guenter.quast@online.de>
+'''
+
+major = 1
+minor = 1
+revision = 1
+
+def _get_version_tuple():
+  '''
+  version as a tuple
+  '''
+  return (major, minor, revision)
+
+def _get_version_string():
+  '''
+  version as a string
+  '''
+  return "%d.%d.%d" % _get_version_tuple()
+
