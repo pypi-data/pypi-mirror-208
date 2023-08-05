676c187a2f4afce628cba26284cbb622121a03d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:04 +0000

    Add package mocket/3.11.1/mocket-3.11.1.tar.gz

---
+++ packages/mocket/mocket-3.11.1.tar.gz/mocket-3.11.1/tests/main/test_https.py
@@ -0,0 +1,79 @@
+import io
+import json
+import os
+import tempfile
+from urllib.request import urlopen
+
+import pytest
+import requests
+
+from mocket import Mocket, Mocketizer, mocketize
+from mocket.mockhttp import Entry
+
+
+@pytest.fixture
+def response():
+    return {
+        "integer": 1,
+        "string": "asd",
+        "boolean": False,
+    }
+
+
+@mocketize
+def test_json(response):
+    url_to_mock = "https://testme.org/json"
+
+    Entry.single_register(
+        Entry.GET,
+        url_to_mock,
+        body=json.dumps(response),
+        headers={"content-type": "application/json"},
+    )
+
+    mocked_response = requests.get(url_to_mock).json()
+    assert response == mocked_response
+
+    mocked_response = json.loads(urlopen(url_to_mock).read().decode("utf-8"))
+    assert response == mocked_response
+
+
+@pytest.mark.skipif('os.getenv("SKIP_TRUE_HTTP", False)')
+def test_truesendall_with_recording_https():
+    with tempfile.TemporaryDirectory() as temp_dir:
+        with Mocketizer(truesocket_recording_dir=temp_dir):
+            url = "https://mockbin.org/ip"
+
+            requests.get(url, headers={"Accept": "application/json"})
+            resp = requests.get(url, headers={"Accept": "application/json"})
+            assert resp.status_code == 200
+
+            dump_filename = os.path.join(
+                Mocket.get_truesocket_recording_dir(),
+                Mocket.get_namespace() + ".json",
+            )
+            with io.open(dump_filename) as f:
+                responses = json.load(f)
+
+    assert len(responses["mockbin.org"]["443"].keys()) == 1
+
+
+@pytest.mark.skipif('os.getenv("SKIP_TRUE_HTTP", False)')
+def test_truesendall_after_mocket_session():
+    Mocket.enable()
+    Mocket.disable()
+
+    url = "https://mockbin.org/ip"
+    resp = requests.get(url)
+    assert resp.status_code == 200
+
+
+@pytest.mark.skipif('os.getenv("SKIP_TRUE_HTTP", False)')
+def test_real_request_session():
+    session = requests.Session()
+
+    url1 = "https://mockbin.org/ip"
+    url2 = "http://httpbin.org/headers"
+
+    with Mocketizer():
+        assert len(session.get(url1).content) < len(session.get(url2).content)
