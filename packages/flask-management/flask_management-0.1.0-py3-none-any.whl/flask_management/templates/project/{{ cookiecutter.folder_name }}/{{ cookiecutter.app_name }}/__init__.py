f2e8414e92ba0546c394085f1f259823037c1fff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:04 +0000

    Add package flask-management/0.1.0/flask_management-0.1.0-py3-none-any.whl

---
+++ packages/flask-management/flask_management-0.1.0-py3-none-any.whl/flask_management/templates/project/{{ cookiecutter.folder_name }}/{{ cookiecutter.app_name }}/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+
+from {{ cookiecutter.app_name }}.app_settings import AppSettings
+from {{ cookiecutter.app_name }}.signals import booting, after_boot
+
+from {{ cookiecutter.app_name }} import ext
+from {{ cookiecutter.app_name }} import models
+
+app = Flask(__name__)
+settings = AppSettings()
+
+app.config.from_object(settings)
+
+booting.send(app)
+after_boot.send(app)
