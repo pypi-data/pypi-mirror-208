3250be99c2e4db430daa64e1c2dc5e565f03e08e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:25 +0000

    Add package sysagi/0.0.1/sysagi-0.0.1.tar.gz

---
+++ packages/sysagi/sysagi-0.0.1.tar.gz/sysagi-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='sysagi',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='http://sysagi.github.io/',
+    author='SYSAGI',
+    author_email='',
+    packages=[],
+    classifiers=['Development Status :: 2 - Pre-Alpha'],
+)
