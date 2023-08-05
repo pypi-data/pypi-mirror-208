072e1b19daae3c57dc4cba315c148e26984ee73b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:45 +0000

    Add package metaindex/2.0.0/metaindex-2.0.0-py3-none-any.whl

---
+++ packages/metaindex/metaindex-2.0.0-py3-none-any.whl/metaindex/__init__.py
@@ -0,0 +1,6 @@
+from .cache import ThreadedCache
+from .cache import Cache
+from .cacheentry import CacheEntry
+from .configuration import Configuration
+from .indexer import IndexerBase, index_files, IndexerResult
+from .humanizer import humanize, register_humanizer
