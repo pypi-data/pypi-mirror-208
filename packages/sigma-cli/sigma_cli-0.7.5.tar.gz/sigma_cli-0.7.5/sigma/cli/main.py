1361701e4e2f9aebfd626071108909efe54ef840
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:02 +0000

    Add package sigma-cli/0.7.5/sigma_cli-0.7.5.tar.gz

---
+++ packages/sigma-cli/sigma_cli-0.7.5.tar.gz/sigma_cli-0.7.5/sigma/cli/main.py
@@ -0,0 +1,23 @@
+import imp
+import click
+from .list import list_group
+from .convert import convert
+from .check import check
+from .plugin import plugin_group
+from .analyze import analyze_group
+
+@click.group()
+def cli():
+    pass
+
+def main():
+    cli.add_command(analyze_group)
+    cli.add_command(plugin_group)
+    cli.add_command(list_group)
+    cli.add_command(convert)
+    cli.add_command(check)
+    cli()
+
+
+if __name__ == "__main__":
+    main()
