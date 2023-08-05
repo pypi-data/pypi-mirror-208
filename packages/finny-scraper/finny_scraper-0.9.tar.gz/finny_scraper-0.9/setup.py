09b9ebf8c910285b063aa3f0fd16e51963aca724
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:59 +0000

    Add package finny-scraper/0.9/finny_scraper-0.9.tar.gz

---
+++ packages/finny-scraper/finny_scraper-0.9.tar.gz/finny_scraper-0.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='finny_scraper',
+    version='0.9',
+    description='A package for scraping property information from Finny website',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/samueltanner/rfin_scraper',
+    packages=['finny_scraper'],
+    install_requires=[
+        'beautifulsoup4',
+        'requests'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+)
+
+# to push changes to pypi
+# python setup.py sdist bdist_wheel
+# twine upload dist/*
