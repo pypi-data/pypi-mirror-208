cdb7f8a0cd24242e1c0e7a501b49b45a626ef121
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:47 +0000

    Add package ckanext-toolbelt/0.3.11/ckanext_toolbelt-0.3.11-py3-none-any.whl

---
+++ packages/ckanext-toolbelt/ckanext_toolbelt-0.3.11-py3-none-any.whl/ckanext/toolbelt/utils/structures.py
@@ -0,0 +1,27 @@
+from __future__ import annotations
+
+import dataclasses
+from collections.abc import Collection
+from typing import Any, Generic, TypeVar
+
+T = TypeVar("T")
+
+
+@dataclasses.dataclass
+class Node(Generic[T]):
+    value: T
+    leaves: Collection[Node[T]] = ()
+    parent: Node | None = None
+    data: dict[str, Any] = dataclasses.field(default_factory=dict)
+
+    def __iter__(self):
+        return iter(self.leaves)
+
+    def __len__(self):
+        return len(self.leaves)
+
+    def __eq__(self, other: Any):
+        if not isinstance(other, Node):
+            return False
+
+        return self.value == other.value and self.leaves == other.leaves
