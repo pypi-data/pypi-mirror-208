08e6be1c874b95044127e2533a5a974741bd34a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:18:26 +0000

    Add package segment-studio/0.0.3/segment_studio-0.0.3.tar.gz

---
+++ packages/segment-studio/segment_studio-0.0.3.tar.gz/segment_studio-0.0.3/README.md
@@ -0,0 +1,28 @@
+
+# About
+
+Segment Studio is an application that allows users to send in batches of image files that will be automatically be segmented and return the masks for each image, from here the user is able to label the data, edit/clean up the masks and such. After the image processing in complete the user can extract the masks to use for the creation of their desired ML models.
+
+# Installation
+
+## Make sure you have upgraded version of pip
+
+Linux/MAC OS
+```
+python3 -m pip install --upgrade pip
+```
+
+## Install using pip
+```
+pip install segment-studio
+```
+
+## Open Python in terminal
+```
+python3
+```
+
+## Run the Editor
+```
+from segment_studio import segment_studio
+```
---
+++ packages/segment-studio/segment_studio-0.0.3.tar.gz/segment_studio-0.0.3/setup.py
@@ -0,0 +1,57 @@
+# [metadata]
+# name = segment_studio_test
+# version = 0.0.1
+# author = shanmukanaks
+# author_email = shanmukanaks@gmail.com
+# description = A small example package
+# long_description = file: README.md
+# long_description_content_type = text/markdown
+# url = https://github.com/shanmukanaks/Segment-Studio
+# project_urls =
+#     Bug Tracker = https://github.com/pypa/sampleproject/issues
+# classifiers =
+#     Programming Language :: Python :: 3
+#     License :: OSI Approved :: MIT License
+#     Operating System :: OS Independent
+
+# [options]
+# package_dir =
+#     = src
+# packages = find:
+# python_requires = >=3.6
+
+# [options.packages.find]
+# where = src
+
+from setuptools import setup
+
+# # read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='segment_studio',
+    version='0.0.3',
+    description='Segmentation Studio',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    classifiers=[
+    'Programming Language :: Python :: 3.9',
+    ],
+    keywords='object detection image editor segmention',
+    url='https://github.com/shanmukanaks/Segment-Studio.git',
+    author='shanmukanaks',
+    author_email='shanmukanaks@gmail.com',
+    # license='MIT',
+    # packages=['funniest'],
+    # install_requires=[
+    #     'markdown',
+    # ],
+    # test_suite='nose.collector',
+    # tests_require=['nose', 'nose-cover3'],
+    # entry_points={
+    #     'console_scripts': ['funniest-joke=funniest.command_line:main'],
+    # },
+    zip_safe=False
+)
