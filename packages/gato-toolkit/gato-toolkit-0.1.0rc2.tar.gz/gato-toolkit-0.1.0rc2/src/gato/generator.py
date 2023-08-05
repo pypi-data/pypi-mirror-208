b53887d2998e80fd6ddfd3c24efa75e0077eafcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:48 +0000

    Add package gato-toolkit/0.1.0rc2/gato-toolkit-0.1.0rc2.tar.gz

---
+++ packages/gato-toolkit/gato-toolkit-0.1.0rc2.tar.gz/gato-toolkit-0.1.0rc2/src/gato/generator.py
@@ -0,0 +1,37 @@
+import entity
+import llm
+
+
+class ScenarioGenerator:
+
+    def __init__(self, model: llm.LLM, prompt: entity.ScenarioPrompt):
+        self._model = model
+        self._prompt = prompt
+
+    def generate(self) -> entity.Scenario:
+        system_message = self._prompt.system_message
+        parameters = self._prompt.parameters.dict().items()
+        user_message = "\n".join([f"{name}: {param}" for name, param in parameters])
+
+        prompt = llm.ChatPrompt()
+        prompt.add_system_message(system_message)
+        prompt.add_user_message(user_message)
+        description = self._model.submit(prompt)
+        return entity.Scenario(description=description)
+
+
+class ActionGenerator:
+
+    def __init__(self, model: llm.LLM, prompt: entity.ActionPrompt):
+        self._model = model
+        self._prompt = prompt
+
+    def generate(self) -> entity.Action:
+        system_message = self._prompt.system_message
+        scenario_description = self._prompt.scenario.description
+
+        prompt = llm.ChatPrompt()
+        prompt.add_system_message(system_message)
+        prompt.add_user_message(scenario_description)
+        description = self._model.submit(prompt)
+        return entity.Action(description=description)
---
+++ packages/gato-toolkit/gato-toolkit-0.1.0rc2.tar.gz/gato-toolkit-0.1.0rc2/src/gato/service.py
@@ -0,0 +1,22 @@
+import entity
+import generator
+import llm
+import prompt
+
+
+class GatoService:
+    _scenario_prompt_factory = prompt.ScenarioPromptFactory()
+    _action_prompt_factory = prompt.ActionPromptFactory()
+
+    def __init__(self, model: llm.LLM):
+        self._model = model
+
+    def create_scenario(self) -> entity.Scenario:
+        scenario_prompt = self._scenario_prompt_factory.new()
+        scenario_generator = generator.ScenarioGenerator(self._model, scenario_prompt)
+        return scenario_generator.generate()
+
+    def create_action(self, scenario: entity.Scenario) -> entity.Action:
+        action_prompt = self._action_prompt_factory.new(scenario)
+        action_generator = generator.ActionGenerator(self._model, action_prompt)
+        return action_generator.generate()
