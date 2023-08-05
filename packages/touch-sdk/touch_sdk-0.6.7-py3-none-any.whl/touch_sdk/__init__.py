9eac16fe8a7e8a8cd0033ea854de0f3ee760267f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:46 +0000

    Add package touch-sdk/0.6.7/touch_sdk-0.6.7-py3-none-any.whl

---
+++ packages/touch-sdk/touch_sdk-0.6.7-py3-none-any.whl/touch_sdk/__init__.py
@@ -0,0 +1,12 @@
+from touch_sdk.watch import Watch
+
+__version__ = "0.6.7"
+
+__doc__ = """Connects to Doublepoint Touch SDK compatible Bluetooth devices
+
+You can download a controller app for Wear OS from
+https://play.google.com/store/apps/details?id=io.port6.watchbridge
+
+This module will connect to the Wear OS app via Bluetooth LE.
+
+See also https://doublepoint.com/product"""
