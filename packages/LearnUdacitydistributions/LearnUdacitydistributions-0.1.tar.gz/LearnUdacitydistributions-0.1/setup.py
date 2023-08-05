56fc20d0bde210214bf8d02553efd8bcb4244d17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:30 +0000

    Add package LearnUdacitydistributions/0.1/LearnUdacitydistributions-0.1.tar.gz

---
+++ packages/LearnUdacitydistributions/LearnUdacitydistributions-0.1.tar.gz/LearnUdacitydistributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='LearnUdacitydistributions',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions'],
+      author = 'Tobias Mueller',
+      author_email = 'tobias3m@googlemail.com',
+     zip_safe=False)
