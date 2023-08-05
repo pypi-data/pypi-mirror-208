01bc8e5f311c555e37e6424db78bb405a82ef407
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:24 +0000

    Add package pydio/0.4.1/pydio-0.4.1-py3-none-any.whl

---
+++ packages/pydio/pydio-0.4.1-py3-none-any.whl/pydio/__init__.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------------------
+# pydio/__init__.py
+#
+# Copyright (C) 2021 - 2023 Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
+#
+# This file is part of PyDio library and is released under the terms of the
+# MIT license: http://opensource.org/licenses/mit-license.php.
+#
+# See LICENSE.txt for details.
+# ---------------------------------------------------------------------------
+__released__ = 2021
+__author__ = 'Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>'
+__version__ = '0.4.1'
---
+++ packages/pydio/pydio-0.4.1-py3-none-any.whl/pydio/_compat.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------------------
+# pydio/_compat.py
+#
+# Copyright (C) 2021 - 2023 Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
+#
+# This file is part of PyDio library and is released under the terms of the
+# MIT license: http://opensource.org/licenses/mit-license.php.
+#
+# See LICENSE.txt for details.
+# ---------------------------------------------------------------------------
+from contextlib import AbstractAsyncContextManager
+
+__all__ = ['AbstractAsyncContextManager']
