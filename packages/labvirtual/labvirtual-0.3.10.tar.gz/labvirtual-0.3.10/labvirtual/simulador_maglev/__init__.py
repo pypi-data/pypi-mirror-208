25a3c1718b92029dd7edcb1efbd0ee7494fa8ca1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:40 +0000

    Add package labvirtual/0.3.10/labvirtual-0.3.10.tar.gz

---
+++ packages/labvirtual/labvirtual-0.3.10.tar.gz/labvirtual-0.3.10/labvirtual/simulador_maglev/__init__.py
@@ -0,0 +1,4 @@
+from .compensador import Compensador  # noqa: F401
+from .modelo_maglev import Maglev  # noqa: F401
+from .animacao import Simulacao  # noqa: F401
+from .grafico import Grafico  # noqa: F401
