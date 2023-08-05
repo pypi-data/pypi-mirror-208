83e1f59671874fad56d5166f3ca4b9d9349ea8e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:55 +0000

    Add package tinystream/0.0.7/tinystream-0.0.7.tar.gz

---
+++ packages/tinystream/tinystream-0.0.7.tar.gz/tinystream-0.0.7/README.md
@@ -0,0 +1,158 @@
+![Tests Status](https://github.com/mreiche/python-streams/actions/workflows/tests.yml/badge.svg)
+[![Code Coverage Status](https://codecov.io/github/mreiche/python-streams/branch/main/graph/badge.svg)](https://app.codecov.io/github/mreiche/python-streams)
+
+# tinystream / python-streams
+
+This is a simple and lightweight Streams API inspired by Java Streams with support for type hinting.
+
+This package is release as `tinystream` at pypi.
+
+## Basic API
+
+```python
+from tinystream import Stream
+
+stream = Stream.of([1, 2, 3, 4, 5]) # of_many(*), of_dict()
+
+stream \
+    .map(lambda x: x + 1) \       # flatmap(), peek(), map_key()
+    .filter(lambda x: x > 2) \    # filter_key()
+    .sorted(int, reverse=True) \  # sort()
+    .reverse() \                  # collect(), count()
+    .limit(2) \
+    .concat([4]) \
+    .sum()                        # reduce(), max(), min()
+```
+
+## Typehinting
+
+Since Python does not support typed lambdas, this library implements a workaround.
+
+```python
+from tinystream import Stream
+
+stream = Stream.of(["A", "B", "C"], str)
+```
+
+This is not necessary when typing is used:
+
+```python
+from tinystream import Stream
+from typing import List
+
+list: List[str] = ["A", "B", "C"]
+stream = Stream.of(list)
+```
+
+Type hinting the given type:
+
+```python
+from dataclasses import dataclass
+
+@dataclass
+class Node:
+    name: str
+    parent: "Node" = None
+```
+
+for lambdas:
+
+```python
+parent = Node(name="B")
+child = Node(name="A", parent=parent)
+
+stream = Stream.of([child])
+assert next(stream.map(lambda x: x.parent, typehint=Node)).name == "B"
+```
+
+This is not necessary when you pass a mapping function:
+```python
+def map_parent(n: Node):
+    return n.parent
+
+assert next(stream.map(map_parent)).name == "B"
+```
+
+### Typed dictionaries
+
+Dictionaries are streamed as `tuple(key, value)`
+
+```python
+children = {"a": Node(name="Child")} 
+stream = Stream.of_dict(children)
+for item in stream:
+    # item[0] is known as str
+    # item[1] is known as Node
+```
+
+This is the same like (but without known types):
+```python
+stream = Stream.of(children)
+```
+
+## End of stream
+
+Calling methods like `sum()`, `collect()`, `count()`... will end the stream.
+
+## More features
+
+### Filter by existing key
+```python
+items_with_name = Stream.of([child]).filter_key("name")
+```
+
+### Map object name attribute
+```python
+names = Stream.of([child]).map_key("name")
+```
+
+### Deep mapping of name attributes
+```python
+list = [
+   {"node": Node(name="Node A")},
+   {"node": Node(name="Node B")},
+   {"node": Node(name="Node C")},
+   {"node": Node(name="Node D")},
+]
+Stream.of(list).map_keys(("node", "name"))
+```
+
+### Collected join
+
+```python
+all_names = Stream.of([child]).map_key("name").join(", ")
+```
+
+## Comparison with other libraries
+
+There are a couple of other implementation to fulfill similar requirements.
+
+- https://github.com/alemazzo/Python-Java-Stream
+- https://github.com/ramsteak/streams
+- https://pypi.org/project/fluentpy
+- https://github.com/JaviOverflow/python-streams (*outdated*)
+- https://github.com/9seconds/streams/ (*outdated*)
+- https://github.com/tolsac/streampy (*outdated*)
+- Apache Spark
+
+## Run the tests
+
+```shell
+PYTHONPATH="." pytest --cov=tinystream -n 4 tests/
+```
+
+### Release update
+1. Update version in `setup.py`
+2. Package library
+    ```shell
+    python setup.py sdist
+    ```
+3. Publish library
+    ```shell
+    twine upload dist/tinystream-[version].tar.gz
+    ```
+
+## References
+
+- https://github.com/MichaelKim0407/tutorial-pip-package
+- https://packaging.python.org/en/latest/guides/making-a-pypi-friendly-readme/
---
+++ packages/tinystream/tinystream-0.0.7.tar.gz/tinystream-0.0.7/tinystream.py
@@ -0,0 +1,188 @@
+import functools
+import itertools
+import sys
+import warnings
+from typing import Iterable, TypeVar, Callable, List, Dict, Tuple, Iterator
+
+T = TypeVar("T")
+R = TypeVar("R")
+K = TypeVar("K")
+
+
+class Stream:
+
+    @staticmethod
+    def of(iterable: Iterable[T], typehint: T = None):
+        return IterableStream[T](iterable)
+
+    @staticmethod
+    def of_dict(source_dict: Dict[K, T]):
+        return IterableStream[Tuple[K, T]](source_dict)
+
+    @staticmethod
+    def of_many(typehint: T = None, *iterables):
+        return IterableStream[T]([]).concat(*iterables)
+
+
+class IterableStream(Iterator[T]):
+
+    def __next__(self) -> T | None:
+        try:
+            return next(self.__iterable)
+        except StopIteration as e:
+            return None
+
+    def __iter__(self) -> Iterator[T]:
+        return self.__iterable.__iter__()
+
+    def __normalize_iterator(self, iterable: Iterable[T]) -> Iterable[T]:
+        if isinstance(iterable, list):
+            return iter(iterable)
+        elif isinstance(iterable, dict):
+            return iter(iterable.items())
+        elif isinstance(iterable, str):
+            return iter(iterable)
+        else:
+            return iterable
+
+    def __init__(self, iterable: Iterable[T]):
+        self.__iterable = self.__normalize_iterator(iterable)
+        self.__collected: List[T] = None
+
+    def map(self, cb: Callable[[T], R], typehint: R = None):
+        return IterableStream[R](map(cb, self.__iterable))
+
+    def map_key(self, key: str | int, typehint: R = None) -> "IterableStream[R]":
+        def __map_key(x):
+            if isinstance(x, (list, dict, tuple)):
+                return x[key]
+            else:
+                return getattr(x, key)
+
+        return self.filter_key(key).map(__map_key, R)
+
+    def map_keys(self, keys: Iterable[str], typehint: R = None) -> "IterableStream[R]":
+        def _map_keys():
+            for val in self.__iterable:
+                for key in keys:
+                    if isinstance(val, (list, dict, tuple)) and key in val:
+                        val = val[key]
+                    elif hasattr(val, key):
+                        val = getattr(val, key)
+                    else:
+                        val = None
+                        break
+                if val:
+                    yield val
+
+        return IterableStream[R](_map_keys())
+
+    def filter(self, cb: Callable[[T, T], bool]):
+        return IterableStream[T](filter(cb, self.__iterable))
+
+    def filter_key(self, key: str | int, invert: bool = False):
+        def __filter_key(x):
+            if isinstance(x, (list, tuple)):
+                size = len(x)
+                if invert:
+                    return key >= size
+                else:
+                    return key < size
+            elif isinstance(x, (dict, Iterable)):
+                if invert:
+                    return key not in x
+                else:
+                    return key in x
+            else:
+                if invert:
+                    return not hasattr(x, key)
+                else:
+                    return hasattr(x, key)
+
+        return self.filter(__filter_key)
+
+    def flatmap(self, cb: Callable[[T], Iterable[R]] = None, typehint: R = None):
+
+        def __flatmap(f, xs):
+            return (y for ys in xs for y in f(ys))
+
+        def __flatten(xs):
+            return (y for ys in xs for y in ys)
+
+        if cb is not None:
+            return IterableStream[R](__flatmap(cb, self.__iterable))
+        else:
+            return IterableStream[R](__flatten(self.__iterable))
+
+    def peek(self, cb: Callable[[T], None]):
+        def __peek(x: T):
+            cb(x)
+            return x
+
+        return IterableStream[T](map(__peek, self.__iterable))
+
+    def sort(self, compare: Callable[[T, T], bool], reverse: bool = False):
+        key = functools.cmp_to_key(compare)
+        sort = sorted(self.__iterable, key=key, reverse=reverse)
+        return IterableStream[T](sort)
+
+    def sorted(self, key, reverse: bool = False):
+        sort = sorted(self.__iterable, key=key, reverse=reverse)
+        return IterableStream[T](sort)
+
+    def each(self) -> Iterable[T]:
+        warnings.warn("Use the stream as iterator instead", DeprecationWarning)
+        return self.__iterable
+
+    def next(self) -> T | None:
+        warnings.warn("Use next(stream) instead", DeprecationWarning)
+        return self.__next__()
+
+    def collect(self):
+        """Collects all items to a list and ends the stream"""
+        if not self.__collected:
+            self.__collected = list(self.__iterable)
+        return self.__collected
+
+    def join(self, separator: str) -> str:
+        """Joins the string to the elements and ends the stream"""
+        return separator.join(map(str, self.__iterable))
+
+    def count(self):
+        """Collects and counts all items and ends the stream"""
+        return len(self.collect())
+
+    def reverse(self):
+        copy = self.collect().copy()
+        copy.reverse()
+        return IterableStream[T](copy)
+
+    def reduce(self, cb: Callable[[T, T], R]) -> R:
+        try:
+            return functools.reduce(cb, self.__iterable)
+        except TypeError:
+            return None
+
+    def sum(self) -> T:
+        """Sums all numbers and ends the stream"""
+        return self.reduce(lambda x, y: x + y)
+
+    def max(self) -> T:
+        return self.reduce(lambda x, y: x if x > y else y)
+
+    def min(self) -> T:
+        return self.reduce(lambda x, y: x if x < y else y)
+
+    def limit(self, limit: int):
+        def __limit():
+            for i in range(limit):
+                yield self.next()
+
+        return IterableStream[T](__limit())
+
+    def concat(self, *iterables):
+        iterators = [self.__iterable]
+        for iterator in iterables:
+            iterators.append(self.__normalize_iterator(iterator))
+
+        return IterableStream[T](itertools.chain(*iterators))
