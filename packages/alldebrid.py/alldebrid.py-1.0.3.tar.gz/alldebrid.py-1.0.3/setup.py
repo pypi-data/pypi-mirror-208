c2c979cb84ecdbb4a2b6bb408b43afd2764b5b09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:07:06 +0000

    Add package alldebrid.py/1.0.3/alldebrid.py-1.0.3.tar.gz

---
+++ packages/alldebrid.py/alldebrid.py-1.0.3.tar.gz/alldebrid.py-1.0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='alldebrid.py',
+    version='1.0.3',
+    description='Wrapper for the alldebrid API.',
+    packages=['alldebrid'],
+    install_requires=[
+        'Requests==2.30.0'
+    ],
+    package_data={
+        '': ['README.md'],
+    },
+)
