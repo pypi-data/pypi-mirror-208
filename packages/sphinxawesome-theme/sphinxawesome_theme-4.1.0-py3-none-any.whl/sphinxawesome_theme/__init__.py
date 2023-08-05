715655cf5cc90601e0073a445164042060a71929
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:08 +0000

    Add package sphinxawesome-theme/4.1.0/sphinxawesome_theme-4.1.0-py3-none-any.whl

---
+++ packages/sphinxawesome-theme/sphinxawesome_theme-4.1.0-py3-none-any.whl/sphinxawesome_theme/__init__.py
@@ -0,0 +1,82 @@
+"""The Sphinx awesome theme as a Python package.
+
+:copyright: Copyright Kai Welke.
+:license: MIT, see LICENSE for details
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+from os import path
+from typing import Any, Dict
+
+from sphinx.application import Sphinx
+from sphinx.config import Config
+from sphinxcontrib.serializinghtml import JSONHTMLBuilder
+
+from . import jsonimpl
+
+try:
+    # obtain version from `pyproject.toml` via `importlib.metadata.version()`
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
+
+def post_config_setup(app: Sphinx, config: Config) -> None:
+    """Set up extensions if configuration is ready."""
+    if config.html_awesome_highlighting:
+        app.setup_extension("sphinxawesome_theme.highlighting")
+
+    if config.html_awesome_external_links:
+        app.setup_extension("sphinxawesome_theme.html_translator")
+
+    # The awesome code headers are handled in `postprocessing`
+    if config.html_awesome_postprocessing or config.html_awesome_code_headers:
+        app.setup_extension("sphinxawesome_theme.postprocess")
+
+    if config.html_awesome_docsearch:
+        app.setup_extension("sphinxawesome_theme.docsearch")
+
+    # Add the CSS overrides if we're using the `sphinx-design` extension
+    if "sphinx_design" in app.extensions:
+        app.add_css_file("awesome-sphinx-design.css", priority=900)
+
+
+def setup(app: "Sphinx") -> Dict[str, Any]:
+    """Register the theme and its extensions wih Sphinx."""
+    app.add_html_theme(
+        name="sphinxawesome_theme", theme_path=path.abspath(path.dirname(__file__))
+    )
+    app.add_config_value(
+        name="html_awesome_postprocessing", default=True, rebuild="html", types=(bool)
+    )
+    app.add_config_value(
+        name="html_awesome_highlighting", default=True, rebuild="html", types=(bool)
+    )
+    app.add_config_value(
+        name="html_awesome_external_links", default=False, rebuild="html", types=(bool)
+    )
+    app.add_config_value(
+        name="html_awesome_docsearch", default=False, rebuild="html", types=(bool)
+    )
+    app.add_config_value(
+        name="docsearch_config", default={}, rebuild="html", types=(dict)
+    )
+    app.add_config_value(
+        name="html_awesome_headerlinks", default=True, rebuild="html", types=(str)
+    )
+    app.add_config_value(
+        name="html_awesome_code_headers", default=True, rebuild="html", types=(str)
+    )
+
+    app.setup_extension("sphinxawesome_theme.jinja_functions")
+    app.connect("config-inited", post_config_setup)
+
+    JSONHTMLBuilder.out_suffix = ".json"
+    JSONHTMLBuilder.implementation = jsonimpl
+    JSONHTMLBuilder.indexer_format = jsonimpl
+
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
---
+++ packages/sphinxawesome-theme/sphinxawesome_theme-4.1.0-py3-none-any.whl/sphinxawesome_theme/postprocess.py
@@ -0,0 +1,213 @@
+"""Post-process the HTML produced by Sphinx.
+
+Some modifications can be done more easily on the finished HTML.
+
+This module defines a simple pipeline:
+
+1. Read all HTML files
+2. Parse them with `BeautifulSoup`
+3. Perform a chain of actions on the tree in place
+
+See the `_modify_html()` function for the list of
+transformations.
+
+Note: This file is not processed by Webpack; don't use Tailwind utility classes.
+They might not show up in the final CSS.
+
+:copyright: Copyright Kai Welke.
+:license: MIT, see LICENSE.
+"""
+
+import os
+import re
+from typing import Any, Dict, List, Optional
+
+from bs4 import BeautifulSoup, Comment
+from sphinx.application import Sphinx
+from sphinx.util import logging
+
+from . import __version__
+from .icons import ICONS
+
+logger = logging.getLogger(__name__)
+
+
+def _get_html_files(outdir: str) -> List[str]:
+    """Get a list of HTML files."""
+    html_list = []
+    for root, _, files in os.walk(outdir):
+        html_list.extend(
+            [os.path.join(root, file) for file in files if file.endswith(".html")]
+        )
+    return html_list
+
+
+def _collapsible_nav(tree: BeautifulSoup) -> None:
+    """Restructure the navigation links to make them collapsible.
+
+    First, all links in the navigation sidebar are wrapped in a ``div``.
+    This allows them to be 'block' and 'position relative' for the
+    'expand' icon to be positioned against.
+
+    Second, an icon is inserted right before the link.
+    Adding the icon as separate DOM element allows click events to be
+    captured separately between the icon and the link.
+    """
+    for link in tree.select(".nav-toc a"):
+        link["data-action"] = "click->sidebar#close"
+        # Don't add the nav-link class twice (#166)
+        if "nav-link" not in link.parent.get("class", []):
+            # First, all links should be wrapped in a div.nav-link
+            link.wrap(tree.new_tag("div", attrs={"class": "nav-link"}))
+            # Next, insert a span.expand before the link, if the #nav-link
+            # has any sibling elements (a ``ul`` in the navigation menu)
+            if link.parent.next_sibling:
+                # create the icon
+                svg = BeautifulSoup(ICONS["chevron_right"], "html.parser").svg
+                svg["tabindex"] = "0"
+                svg["height"] = "1.2rem"
+                svg["class"] = ["expand"]
+                svg["style"] = ["display: inline;"]
+                svg[
+                    "data-action"
+                ] = "click->sidebar#expand keydown->sidebar#expandKeyPressed"
+                link.insert_before(svg)
+
+
+def _expand_current(tree: BeautifulSoup) -> None:
+    """Add the ``.expanded`` class to li.current elements."""
+    for li in tree("li", class_="current"):
+        if "expanded" not in li.get("class", []):
+            li["class"] += ["expanded"]
+
+
+def _remove_empty_toctree(tree: BeautifulSoup) -> None:
+    """Remove empty toctree divs.
+
+    If you include a `toctree` with the `hidden` option,
+    an empty `div` is inserted. Remove them.
+    The empty `div` contains a single `end-of-line` character.
+    """
+    for div in tree("div", class_="toctree-wrapper"):
+        children = list(div.children)
+        if len(children) == 1 and not children[0].strip():
+            div.extract()
+
+
+def _headerlinks(tree: BeautifulSoup) -> None:
+    """Enhance the headerlink experience."""
+    for link in tree("a", class_="headerlink"):
+        link["data-controller"] = "clipboard"
+        link["data-action"] = "click->clipboard#copyHeaderLink"
+        link["aria-label"] = "Click to copy this link"
+        del link["title"]
+        link["class"].extend(["tooltipped", "tooltipped-ne"])
+
+
+def _code_controller(tree: BeautifulSoup) -> None:
+    """Add the controller attribute to code blocks."""
+    for block in tree("div", class_="highlight"):
+        block["data-controller"] = "code"
+
+
+def _external_links(tree: BeautifulSoup) -> None:
+    """Add `rel="nofollow noopener"` to external links.
+
+    The alternative was to copy `visit_reference` in the HTMLTranslator
+    and change literally one line.
+    """
+    for link in tree("a", class_="reference external"):
+        link["rel"] = "nofollow noopener"
+
+
+def _strip_comments(tree: BeautifulSoup) -> None:
+    """Remove HTML comments from documents."""
+    comments = tree.find_all(string=lambda text: isinstance(text, Comment))
+    for c in comments:
+        c.extract()
+
+
+def _code_headers(tree: BeautifulSoup) -> None:
+    """Add the programming language to a code block."""
+    # Find all "<div class="highlight-<LANG> notranslate>" blocks
+    pattern = re.compile("highlight-(.*) ")
+    for code_block in tree.find_all("div", class_=pattern):
+        hl_lang = None
+        # Get the highlight language
+        classes_string = " ".join(code_block.get("class", []))
+        match = pattern.search(classes_string)
+        if match:
+            hl_lang = match.group(1).replace("default", "python")
+
+        parent = code_block.parent
+
+        # Deal with code blocks with captions
+        if "literal-block-wrapper" in parent.get("class", []):
+            caption = parent.select(".code-block-caption")[0]
+            if caption:
+                span = tree.new_tag("span", attrs={"class": "code-lang"})
+                span.append(tree.new_string(hl_lang))
+                caption.insert(0, span)
+        else:
+            # Code block without captions, we need to wrap them first
+            wrapper = tree.new_tag("div", attrs={"class": "literal-block-wrapper"})
+            caption = tree.new_tag("div", attrs={"class": "code-block-caption"})
+            span = tree.new_tag("span", attrs={"class": "code-lang"})
+            span.append(tree.new_string(hl_lang))
+            caption.append(span)
+            code_block.wrap(wrapper)
+            wrapper.insert(0, caption)
+
+
+def _modify_html(html_filename: str, app: Sphinx) -> None:
+    """Modify a single HTML document.
+
+    1. The HTML document is parsed into a BeautifulSoup tree.
+    2. The modifications are performed in order and in place.
+    3. After these modifications, the HTML is written into a file,
+    overwriting the original file.
+    """
+    with open(html_filename, encoding="utf-8") as html:
+        tree = BeautifulSoup(html, "html.parser")
+
+    _expand_current(tree)
+    _collapsible_nav(tree)
+    _external_links(tree)
+    _remove_empty_toctree(tree)
+    if app.config.html_awesome_headerlinks:
+        _headerlinks(tree)
+    _code_controller(tree)
+    if app.config.html_awesome_code_headers:
+        _code_headers(tree)
+    _strip_comments(tree)
+
+    with open(html_filename, "w", encoding="utf-8") as out_file:
+        out_file.write(str(tree))
+
+
+def post_process_html(app: Sphinx, exc: Optional[Exception]) -> None:
+    """Perform modifications on the HTML after building.
+
+    This is an extra function, that gets a list from all HTML
+    files in the output directory, then runs the ``_modify_html``
+    function on each of them.
+    """
+    if app.builder is not None and app.builder.name not in ["html", "dirhtml"]:
+        return
+
+    if exc is None:
+        html_files = _get_html_files(app.outdir)
+
+        for doc in html_files:
+            _modify_html(doc, app)
+
+
+def setup(app: "Sphinx") -> Dict[str, Any]:
+    """Set this up as internal extension."""
+    app.connect("build-finished", post_process_html)
+
+    return {
+        "version": __version__,
+        "parallel_read_safe": True,
+        "parallel_write_safe": True,
+    }
