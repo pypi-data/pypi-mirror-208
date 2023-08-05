d53d397c04e1861fad133b75c9b715d61ac6aea1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:48 +0000

    Add package altimate-dataminion/0.0.1/altimate_dataminion-0.0.1-py3-none-any.whl

---
+++ packages/altimate-dataminion/altimate_dataminion-0.0.1-py3-none-any.whl/altimate_profiler/__init__.py
@@ -0,0 +1,4 @@
+from .main import get_metrics_profile, get_schema_profile, get_debug_data
+
+
+__all__ =  [ "get_metrics_profile", "get_schema_profile", "get_debug_data"]
---
+++ packages/altimate-dataminion/altimate_dataminion-0.0.1-py3-none-any.whl/altimate_profiler/exceptions.py
@@ -0,0 +1,74 @@
+class AltimateContractRegistryHTTPException(Exception):
+    """
+    Raised when the Altimate Contract Registry returns an error.
+    """
+
+    pass
+
+
+class AltimateContractRegistryConnectionError(Exception):
+    """
+    Raised when the connection to the Altimate Contract Registry fails.
+    """
+
+    pass
+
+
+class AltimateProfilerException(Exception):
+    """
+    Raised when the Altimate Profiler encounters an error.
+    """
+
+    pass
+
+
+class AltimateContractRegistryUnknownException(Exception):
+    """
+    Raised when an unknown exception occurs
+    """
+
+    pass
+
+
+class AltimateDataStoreNotSupported(Exception):
+    """
+    Raised when the data store is not supported
+    """
+
+    pass
+
+
+class AltimateDataStoreConnectionException(Exception):
+    """
+    Raised when the data store is not supported
+    """
+
+    pass
+
+
+class AltimateConfigNotFoundError(Exception):
+    """
+    Raised when the request to the Altimate Contract Registry fails.
+    """
+
+    pass
+
+
+class AltimateContractRegistryBadRequestError(Exception):
+    pass
+
+
+class AltimateUnsupportedCollectorTypeError(Exception):
+    pass
+
+
+class AltimateInvalidInputException(Exception):
+    pass
+
+
+class AltimateRegistryNotFoundError(Exception):
+    pass
+
+
+class AltimateJSONFileNotLoadingError(Exception):
+    pass
