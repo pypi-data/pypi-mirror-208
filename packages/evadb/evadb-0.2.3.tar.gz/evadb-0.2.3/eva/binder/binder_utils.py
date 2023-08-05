c1c19110c663857df5a9b211e1dba782883fe0c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:56 +0000

    Add package evadb/0.2.3/evadb-0.2.3.tar.gz

---
+++ packages/evadb/evadb-0.2.3.tar.gz/evadb-0.2.3/eva/binder/binder_utils.py
@@ -0,0 +1,134 @@
+# coding=utf-8
+# Copyright 2018-2022 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from __future__ import annotations
+
+import re
+from typing import TYPE_CHECKING, List
+
+from eva.catalog.catalog_type import TableType
+from eva.catalog.catalog_utils import is_string_col, is_video_table
+from eva.expression.function_expression import FunctionExpression
+from eva.parser.alias import Alias
+
+if TYPE_CHECKING:
+    from eva.binder.statement_binder_context import StatementBinderContext
+
+from eva.catalog.catalog_manager import CatalogManager
+from eva.catalog.models.table_catalog import TableCatalogEntry
+from eva.expression.tuple_value_expression import TupleValueExpression
+from eva.parser.table_ref import TableInfo, TableRef
+from eva.utils.logging_manager import logger
+
+
+class BinderError(Exception):
+    pass
+
+
+def bind_table_info(table_info: TableInfo) -> TableCatalogEntry:
+    """
+    Uses catalog to bind the table information .
+
+    Arguments:
+         table_info (TableInfo): table information obtained from SQL query
+
+    Returns:
+        TableCatalogEntry  -  corresponding table catalog entry for the input table info
+    """
+    catalog = CatalogManager()
+    obj = catalog.get_table_catalog_entry(
+        table_info.table_name,
+        table_info.database_name,
+    )
+
+    # Users should not be allowed to directly access or modify the SYSTEM tables, as
+    # doing so can lead to the corruption of other tables. These tables include
+    # metadata tables associated with unstructured data, such as the list of video
+    # files in the video table. Protecting these tables is crucial in order to maintain
+    # the integrity of the system.
+    if obj and obj.table_type == TableType.SYSTEM_STRUCTURED_DATA:
+        err_msg = (
+            "The query attempted to access or modify the internal table"
+            f"{table_info.table_name} of the system, but permission was denied."
+        )
+        logger.error(err_msg)
+        raise BinderError(err_msg)
+
+    if obj:
+        table_info.table_obj = obj
+    else:
+        error = "{} does not exist. Create the table using" " CREATE TABLE.".format(
+            table_info.table_name
+        )
+        logger.error(error)
+        raise BinderError(error)
+
+
+def extend_star(
+    binder_context: StatementBinderContext,
+) -> List[TupleValueExpression]:
+    col_objs = binder_context._get_all_alias_and_col_name()
+
+    target_list = list(
+        [
+            TupleValueExpression(col_name=col_name, table_alias=alias)
+            for alias, col_name in col_objs
+        ]
+    )
+    return target_list
+
+
+def check_groupby_pattern(groupby_string: str) -> None:
+    # match the pattern of group by clause (e.g., 16f or 8s)
+    pattern = re.search(r"^\d+[fs]$", groupby_string)
+    # if valid pattern
+    if not pattern:
+        err_msg = "Incorrect GROUP BY pattern: {}".format(groupby_string)
+        raise BinderError(err_msg)
+    match_string = pattern.group(0)
+    if not match_string[-1] == "f":
+        err_msg = "Only grouping by frames (f) is supported"
+        raise BinderError(err_msg)
+    # TODO ACTION condition on segment length?
+
+
+def check_table_object_is_video(table_ref: TableRef) -> None:
+    if not is_video_table(table_ref.table.table_obj):
+        err_msg = "GROUP BY only supported for video tables"
+        raise BinderError(err_msg)
+
+
+def check_column_name_is_string(col_ref) -> None:
+    if not is_string_col(col_ref.col_object):
+        err_msg = "LIKE only supported for string columns"
+        raise BinderError(err_msg)
+
+
+def resolve_alias_table_value_expression(node: FunctionExpression):
+    default_alias_name = node.name.lower()
+    default_output_col_aliases = [str(obj.name.lower()) for obj in node.output_objs]
+    if not node.alias:
+        node.alias = Alias(default_alias_name, default_output_col_aliases)
+    else:
+        if not len(node.alias.col_names):
+            node.alias = Alias(node.alias.alias_name, default_output_col_aliases)
+        else:
+            output_aliases = [
+                str(col_name.lower()) for col_name in node.alias.col_names
+            ]
+            node.alias = Alias(node.alias.alias_name, output_aliases)
+
+    assert len(node.alias.col_names) == len(
+        node.output_objs
+    ), f"""Expected {len(node.output_objs)} output columns for {node.alias.alias_name}, got {len(node.alias.col_names)}."""
---
+++ packages/evadb/evadb-0.2.3.tar.gz/evadb-0.2.3/test/integration_tests/test_similarity.py
@@ -0,0 +1,290 @@
+# coding=utf-8
+# Copyright 2018-2022 EVA
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+import unittest
+from test.util import create_sample_image, load_udfs_for_testing, shutdown_ray
+
+import numpy as np
+import pandas as pd
+import pytest
+
+from eva.catalog.catalog_manager import CatalogManager
+from eva.models.storage.batch import Batch
+from eva.server.command_handler import execute_query_fetch_all
+from eva.storage.storage_engine import StorageEngine
+
+
+@pytest.mark.notparallel
+class SimilarityTests(unittest.TestCase):
+    def setUp(self):
+        CatalogManager().reset()
+
+        # Prepare needed UDFs and data_col.
+        load_udfs_for_testing(mode="debug")
+        self.img_path = create_sample_image()
+
+        # Create base comparison table.
+        create_table_query = """CREATE TABLE IF NOT EXISTS testSimilarityTable
+                                  (data_col NDARRAY UINT8(3, ANYDIM, ANYDIM),
+                                   dummy INTEGER);"""
+        execute_query_fetch_all(create_table_query)
+
+        # Create feature table.
+        create_table_query = """CREATE TABLE IF NOT EXISTS testSimilarityFeatureTable
+                                  (feature_col NDARRAY FLOAT32(1, ANYDIM),
+                                   dummy INTEGER);"""
+        execute_query_fetch_all(create_table_query)
+
+        # Prepare injected data_col.
+        base_img = np.array(np.ones((3, 3, 3)), dtype=np.uint8)
+        base_img[0] -= 1
+        base_img[2] += 1
+
+        # id: 1 -> most dissimilar, id: 5 -> most similar
+        base_img += 4
+
+        # Inject data_col.
+        base_table_catalog_entry = CatalogManager().get_table_catalog_entry(
+            "testSimilarityTable"
+        )
+        feature_table_catalog_entry = CatalogManager().get_table_catalog_entry(
+            "testSimilarityFeatureTable"
+        )
+        storage_engine = StorageEngine.factory(base_table_catalog_entry)
+        for i in range(5):
+            storage_engine.write(
+                base_table_catalog_entry,
+                Batch(
+                    pd.DataFrame(
+                        [
+                            {
+                                "data_col": base_img,
+                                "dummy": i,
+                            }
+                        ]
+                    )
+                ),
+            )
+            storage_engine.write(
+                feature_table_catalog_entry,
+                Batch(
+                    pd.DataFrame(
+                        [
+                            {
+                                "feature_col": base_img.astype(np.float32).reshape(
+                                    1, -1
+                                ),
+                                "dummy": i,
+                            }
+                        ]
+                    )
+                ),
+            )
+            base_img -= 1
+
+    def tearDown(self):
+        shutdown_ray()
+
+        drop_table_query = "DROP TABLE testSimilarityTable;"
+        execute_query_fetch_all(drop_table_query)
+        drop_table_query = "DROP TABLE testSimilarityFeatureTable;"
+        execute_query_fetch_all(drop_table_query)
+
+    def test_similarity_should_work_in_order(self):
+        ###############################################
+        # Test case runs with UDF on raw input table. #
+        ###############################################
+
+        # Top 1 - assume table contains base data_col.
+        select_query = """SELECT data_col FROM testSimilarityTable
+                            ORDER BY Similarity(DummyFeatureExtractor(Open("{}")), DummyFeatureExtractor(data_col))
+                            LIMIT 1;""".format(
+            self.img_path
+        )
+        actual_batch = execute_query_fetch_all(select_query)
+
+        base_img = np.array(np.ones((3, 3, 3)), dtype=np.uint8)
+        base_img[0] -= 1
+        base_img[2] += 1
+
+        actual_open = actual_batch.frames["testsimilaritytable.data_col"].to_numpy()[0]
+        self.assertTrue(np.array_equal(actual_open, base_img))
+        # actual_distance = actual_batch.frames["similarity.distance"].to_numpy()[0]
+        # self.assertEqual(actual_distance, 0)
+
+        # Top 2 - assume table contains base data.
+        select_query = """SELECT data_col FROM testSimilarityTable
+                            ORDER BY Similarity(DummyFeatureExtractor(Open("{}")), DummyFeatureExtractor(data_col))
+                            LIMIT 2;""".format(
+            self.img_path
+        )
+        actual_batch = execute_query_fetch_all(select_query)
+
+        actual_open = actual_batch.frames["testsimilaritytable.data_col"].to_numpy()[0]
+        self.assertTrue(np.array_equal(actual_open, base_img))
+        actual_open = actual_batch.frames["testsimilaritytable.data_col"].to_numpy()[1]
+        self.assertTrue(np.array_equal(actual_open, base_img + 1))
+        # actual_distance = actual_batch.frames["similarity.distance"].to_numpy()[0]
+        # self.assertEqual(actual_distance, 0)
+        # actual_distance = actual_batch.frames["similarity.distance"].to_numpy()[1]
+        # self.assertEqual(actual_distance, 27)
+
+        ###########################################
+        # Test case runs on feature vector table. #
+        ###########################################
+
+        # Top 1 - assume table contains feature data.
+        select_query = """SELECT feature_col FROM testSimilarityFeatureTable
+                            ORDER BY Similarity(DummyFeatureExtractor(Open("{}")), feature_col)
+                            LIMIT 1;""".format(
+            self.img_path
+        )
+        actual_batch = execute_query_fetch_all(select_query)
+
+        base_img = np.array(np.ones((3, 3, 3)), dtype=np.uint8)
+        base_img[0] -= 1
+        base_img[2] += 1
+        base_img = base_img.astype(np.float32).reshape(1, -1)
+
+        actual_open = actual_batch.frames[
+            "testsimilarityfeaturetable.feature_col"
+        ].to_numpy()[0]
+        self.assertTrue(np.array_equal(actual_open, base_img))
+        # actual_distance = actual_batch.frames["similarity.distance"].to_numpy()[0]
+        # self.assertEqual(actual_distance, 0)
+
+        # Top 2 - assume table contains feature data.
+        select_query = """SELECT feature_col FROM testSimilarityFeatureTable
+                            ORDER BY Similarity(DummyFeatureExtractor(Open("{}")), feature_col)
+                            LIMIT 2;""".format(
+            self.img_path
+        )
+        actual_batch = execute_query_fetch_all(select_query)
+
+        actual_open = actual_batch.frames[
+            "testsimilarityfeaturetable.feature_col"
+        ].to_numpy()[0]
+        self.assertTrue(np.array_equal(actual_open, base_img))
+        actual_open = actual_batch.frames[
+            "testsimilarityfeaturetable.feature_col"
+        ].to_numpy()[1]
+        self.assertTrue(np.array_equal(actual_open, base_img + 1))
+        # actual_distance = actual_batch.frames["similarity.distance"].to_numpy()[0]
+        # self.assertEqual(actual_distance, 0)
+        # actual_distance = actual_batch.frames["similarity.distance"].to_numpy()[1]
+        # self.assertEqual(actual_distance, 27)
+
+    def test_should_do_faiss_index_scan(self):
+        ###########################################
+        # Test case runs on feature vector table. #
+        ###########################################
+
+        # Execution without index scan.
+        select_query = """SELECT feature_col FROM testSimilarityFeatureTable
+                            ORDER BY Similarity(DummyFeatureExtractor(Open("{}")), feature_col)
+                            LIMIT 3;""".format(
+            self.img_path
+        )
+        expected_batch = execute_query_fetch_all(select_query)
+
+        # Execution with index scan.
+        create_index_query = """CREATE INDEX testFaissIndexScanRewrite1
+                                    ON testSimilarityFeatureTable (feature_col)
+                                    USING HNSW;"""
+        execute_query_fetch_all(create_index_query)
+        select_query = """SELECT feature_col FROM testSimilarityFeatureTable
+                            ORDER BY Similarity(DummyFeatureExtractor(Open("{}")), feature_col)
+                            LIMIT 3;""".format(
+            self.img_path
+        )
+        explain_query = """EXPLAIN {}""".format(select_query)
+        explain_batch = execute_query_fetch_all(explain_query)
+        self.assertTrue("FaissIndexScan" in explain_batch.frames[0][0])
+        actual_batch = execute_query_fetch_all(select_query)
+
+        self.assertEqual(len(actual_batch), 3)
+        for i in range(3):
+            self.assertTrue(
+                np.array_equal(
+                    expected_batch.frames[
+                        "testsimilarityfeaturetable.feature_col"
+                    ].to_numpy()[i],
+                    actual_batch.frames[
+                        "testsimilarityfeaturetable.feature_col"
+                    ].to_numpy()[i],
+                )
+            )
+
+        ###############################################
+        # Test case runs with UDF on raw input table. #
+        ###############################################
+
+        # Execution without index scan.
+        select_query = """SELECT data_col FROM testSimilarityTable
+                            ORDER BY Similarity(DummyFeatureExtractor(Open("{}")), DummyFeatureExtractor(data_col))
+                            LIMIT 3;""".format(
+            self.img_path
+        )
+        expected_batch = execute_query_fetch_all(select_query)
+
+        # Execution with index scan.
+        create_index_query = """CREATE INDEX testFaissIndexScanRewrite2
+                                    ON testSimilarityTable (DummyFeatureExtractor(data_col))
+                                    USING HNSW;"""
+        execute_query_fetch_all(create_index_query)
+        select_query = """SELECT data_col FROM testSimilarityTable
+                            ORDER BY Similarity(DummyFeatureExtractor(Open("{}")), DummyFeatureExtractor(data_col))
+                            LIMIT 3;""".format(
+            self.img_path
+        )
+        explain_query = """EXPLAIN {}""".format(select_query)
+        explain_batch = execute_query_fetch_all(explain_query)
+        self.assertTrue("FaissIndexScan" in explain_batch.frames[0][0])
+        actual_batch = execute_query_fetch_all(select_query)
+
+        self.assertEqual(len(actual_batch), 3)
+        for i in range(3):
+            self.assertTrue(
+                np.array_equal(
+                    expected_batch.frames["testsimilaritytable.data_col"].to_numpy()[i],
+                    actual_batch.frames["testsimilaritytable.data_col"].to_numpy()[i],
+                )
+            )
+
+        # Cleanup
+        CatalogManager().drop_index_catalog_entry("testFaissIndexScanRewrite1")
+        CatalogManager().drop_index_catalog_entry("testFaissIndexScanRewrite2")
+
+    def test_should_not_do_faiss_index_scan_with_predicate(self):
+        # Execution with index scan.
+        create_index_query = """CREATE INDEX testFaissIndexScanRewrite
+                                    ON testSimilarityTable (DummyFeatureExtractor(data_col))
+                                    USING HNSW;"""
+        execute_query_fetch_all(create_index_query)
+
+        explain_query = """
+            EXPLAIN
+                SELECT data_col FROM testSimilarityTable WHERE dummy = 0
+                  ORDER BY Similarity(DummyFeatureExtractor(Open("{}")), DummyFeatureExtractor(data_col))
+                  LIMIT 3;
+        """.format(
+            "dummypath"
+        )
+        batch = execute_query_fetch_all(explain_query)
+
+        # Index scan should not be used.
+        self.assertFalse("FaissIndexScan" in batch.frames[0][0])
+
+        # Cleanup
+        CatalogManager().drop_index_catalog_entry("testFaissIndexScanRewrite")
