4aff071291ce9f0a3327c428177373984e3551e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:39 +0000

    Add package django-async-orm/0.1.12/django_async_orm-0.1.12-py3-none-any.whl

---
+++ packages/django-async-orm/django_async_orm-0.1.12-py3-none-any.whl/django_async_orm/manager.py
@@ -0,0 +1,7 @@
+from django.db.models.manager import BaseManager
+
+from django_async_orm.query import QuerySetAsync
+
+
+class AsyncManager(BaseManager.from_queryset(QuerySetAsync)):
+    pass
---
+++ packages/django-async-orm/django_async_orm-0.1.12-py3-none-any.whl/django_async_orm/utils.py
@@ -0,0 +1,35 @@
+from django_async_orm.manager import AsyncManager
+
+
+def mixin_async_manager_factory(model):
+    """
+    Creates a new type a mixin between the base manager and the async manager.
+
+    :param model:  A django model class
+    :type model:  models.Model
+    :return: A mixin type
+    :rtype: object
+    """
+
+    base_manager_cls = model.objects.__class__
+    if not base_manager_cls.__name__.startswith("MixinAsync"):
+        mixin_async_manager = type(
+            f"MixinAsync{base_manager_cls.__name__}",
+            (AsyncManager, base_manager_cls),
+            dict(),
+        )
+        return mixin_async_manager
+
+
+def patch_manager(model):
+    """
+    Patches django models to add async capabilities
+    :param model: A django model class
+    :type model: models.Model
+    :return: None
+    :rtype: None
+    """
+    async_manager_cls = mixin_async_manager_factory(model)
+    if async_manager_cls:
+        model.objects = async_manager_cls()
+        model.objects.model = model
