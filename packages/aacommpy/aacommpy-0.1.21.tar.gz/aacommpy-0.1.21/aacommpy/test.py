10becda6ba6888fe0a0a573a10fa49439e2414d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:00 +0000

    Add package aacommpy/0.1.21/aacommpy-0.1.21.tar.gz

---
+++ packages/aacommpy/aacommpy-0.1.21.tar.gz/aacommpy-0.1.21/aacommpy/test.py
@@ -0,0 +1,44 @@
+import clr
+import os
+import time
+
+path = os.path.join(os.path.dirname(__file__), 'aacommpyDownloader-main', 'Agito.AAComm.5.5.0', 'lib', 'net48', 'AAComm.dll')
+clr.AddReference(path)
+
+from AAComm import Services
+from AAComm import CommAPI
+from AAComm import Shared
+
+api = CommAPI()
+
+status = CommAPI.StartAACommServer()
+
+if status != "":
+    print(status)
+else:
+    # Access the static variable IsAACommServerRunning
+    is_running = CommAPI.IsAACommServerRunning
+    print(f"AACommServer is running: {is_running}")
+
+cData = Services.ConnectionData()
+cData.ControllerType = Shared.ProductTypes.AGM800_ID
+cData.CommChannelType = Shared.ChannelType.Ethernet
+cData.ET_IP_1 = 172
+cData.ET_IP_2 = 1
+cData.ET_IP_3 = 1
+cData.ET_IP_4 = 101
+cData.ET_Port = 5000
+
+res = api.Connect(cData)
+print(res)
+
+# from System import EventHandler
+# from AAComm.Services import AACommEventArgs
+
+# def on_reply_received(sender, event_args):
+#     print("Reply received:", event_args.Message)
+
+
+# OnReplyReceived = EventHandler[AACommEventArgs](on_reply_received)
+# msg = Services.AACommMessage(OnReplyReceived, "AMotorOn")
+
