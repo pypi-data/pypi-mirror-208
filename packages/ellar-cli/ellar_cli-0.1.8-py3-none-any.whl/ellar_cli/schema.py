89bea0f87819360747058318b54f93631646021b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:20 +0000

    Add package ellar-cli/0.1.8/ellar_cli-0.1.8-py3-none-any.whl

---
+++ packages/ellar-cli/ellar_cli-0.1.8-py3-none-any.whl/ellar_cli/schema.py
@@ -0,0 +1,44 @@
+import typing as t
+
+from ellar.common.serializer import Serializer
+from pydantic import Field
+
+
+class EllarPyProjectSerializer(Serializer):
+    project_name: str = Field(alias="project-name")
+    application: str = Field(alias="application")
+    config: str = Field(alias="config")
+    root_module: str = Field(alias="root-module")
+    apps_module: str = Field(alias="apps-module")
+
+
+class EllarScaffoldList(Serializer):
+    name: str
+    is_directory: bool = False
+    name_in_context: t.Optional[bool] = Field(default=None, alias="name-context")
+    files: t.Optional[t.List["EllarScaffoldList"]]
+
+
+EllarScaffoldList.update_forward_refs()
+
+
+class EllarScaffoldSchema(Serializer):
+    context: t.List[str]
+    files: t.List[EllarScaffoldList]
+
+    @classmethod
+    def schema_example(cls) -> "EllarScaffoldSchema":
+        return cls(
+            context=["project_name"],
+            files=[
+                dict(name="sample.ellar", is_directory=False),
+                dict(
+                    name="sample",
+                    files=[
+                        dict(
+                            dict(name="sample.ellar", is_directory=False),
+                        )
+                    ],
+                ),
+            ],
+        )
