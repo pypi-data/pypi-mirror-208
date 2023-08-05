3732b5e06dfecd6e39ece11449561a737b760b0f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:17:22 +0000

    Add package neurotune/0.2.5/neurotune-0.2.5.tar.gz

---
+++ packages/neurotune/neurotune-0.2.5.tar.gz/neurotune-0.2.5/setup.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+for line in open("neurotune/__init__.py"):
+    if line.startswith("__version__"):
+        version = line.split("=")[1].strip()[1:-1]
+
+setup(
+    name="neurotune",
+    version=version,
+    packages=["neurotune"],
+    author="Michael Vella, Padraig Gleeson",
+    author_email="mv333@cam.ac.uk, p.gleeson@gmail.com",
+    description="A Python library for optimising neuronal models",
+    license="BSD",
+    install_requires=["scipy", "inspyred", "pyelectro"],
+    url="https://github.com/NeuralEnsemble/neurotune",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: BSD License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Topic :: Scientific/Engineering",
+    ],
+)
