afbd58fe9dca3d5283b8299324f9f43974594415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:02 +0000

    Add package wagtailyoast/0.0.10/wagtailyoast-0.0.10-py3-none-any.whl

---
+++ packages/wagtailyoast/wagtailyoast-0.0.10-py3-none-any.whl/wagtailyoast/edit_handlers.py
@@ -0,0 +1,42 @@
+from django import forms
+from wagtail.admin.panels import ObjectList
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+
+
+class YoastPanel(ObjectList):
+    template = "wagtailyoast/edit_handlers/yoast_panel.html"
+
+    def __init__(self, keywords='keywords', title='seo_title',
+                 search_description='search_description', slug='slug',
+                 heading='Yoast', *args, **kwargs):
+        """
+        Panel used by a wagtail Page
+
+        :param keywords: Default keywords of the page.
+        :param title: 'Search Engine Friendly' title.
+        :param search_description: 'Search Engine Friendly' description.
+        :param slug: URL of the page.
+        :param heading: Heading of pannel
+        """
+        #  TODO: Test if fields exist
+
+        self.title_field = title
+        self.search_description = search_description
+        self.slug = slug
+
+        children = [
+            MultiFieldPanel([
+                FieldPanel(
+                    keywords,
+                    widget=forms.TextInput(attrs={'id': 'yoast_keywords'})
+                ),
+            ], heading="Page")
+        ]
+        super().__init__(children=children, heading=heading)
+
+    def clone_kwargs(self):
+        kwargs = super().clone_kwargs()
+        kwargs['title'] = self.title_field
+        kwargs['search_description'] = self.search_description
+        kwargs['slug'] = self.slug
+        return kwargs
