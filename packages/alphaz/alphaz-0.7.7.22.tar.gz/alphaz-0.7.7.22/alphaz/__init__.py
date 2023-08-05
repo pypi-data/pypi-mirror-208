b9d777643cd28e89ae7157079505b2fd7d60be09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:39 +0000

    Add package alphaz/0.7.7.22/alphaz-0.7.7.22.tar.gz

---
+++ packages/alphaz/alphaz-0.7.7.22.tar.gz/alphaz-0.7.7.22/alphaz/__init__.py
@@ -0,0 +1,27 @@
+import re, os, time
+from .libs.time_lib import timer
+from typing import (
+    Any,
+    Dict,
+    List,
+    Optional,
+    Union,
+    Callable,
+    Any,
+    Tuple,
+    OrderedDict,
+    Iterable,
+)
+from .models.main import (
+    AlphaException,
+    dataclass,
+    AlphaEnum,
+    AlphaDataclass,
+    AlphaClass,
+)
+from .models.main._enum import MappingMode
+
+from dataclasses import dataclass, field
+from collections import defaultdict, OrderedDict
+
+os.environ["ALPHAZ_DIR"] = os.path.dirname(__file__)
