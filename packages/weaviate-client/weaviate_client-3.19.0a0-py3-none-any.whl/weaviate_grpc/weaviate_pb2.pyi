0857269ccccadb4399c90168f2065d0ffb83dbdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:01 +0000

    Add package weaviate-client/3.19.0a0/weaviate_client-3.19.0a0-py3-none-any.whl

---
+++ packages/weaviate-client/weaviate_client-3.19.0a0-py3-none-any.whl/weaviate_grpc/weaviate_pb2.pyi
@@ -0,0 +1,101 @@
+from google.protobuf import struct_pb2 as _struct_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class NearObjectParams(_message.Message):
+    __slots__ = ["certainty", "distance", "id"]
+    CERTAINTY_FIELD_NUMBER: _ClassVar[int]
+    DISTANCE_FIELD_NUMBER: _ClassVar[int]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    certainty: float
+    distance: float
+    id: str
+    def __init__(self, id: _Optional[str] = ..., certainty: _Optional[float] = ..., distance: _Optional[float] = ...) -> None: ...
+
+class NearVectorParams(_message.Message):
+    __slots__ = ["certainty", "distance", "vector"]
+    CERTAINTY_FIELD_NUMBER: _ClassVar[int]
+    DISTANCE_FIELD_NUMBER: _ClassVar[int]
+    VECTOR_FIELD_NUMBER: _ClassVar[int]
+    certainty: float
+    distance: float
+    vector: _containers.RepeatedScalarFieldContainer[float]
+    def __init__(self, vector: _Optional[_Iterable[float]] = ..., certainty: _Optional[float] = ..., distance: _Optional[float] = ...) -> None: ...
+
+class Properties(_message.Message):
+    __slots__ = ["non_ref_properties", "ref_properties"]
+    NON_REF_PROPERTIES_FIELD_NUMBER: _ClassVar[int]
+    REF_PROPERTIES_FIELD_NUMBER: _ClassVar[int]
+    non_ref_properties: _containers.RepeatedScalarFieldContainer[str]
+    ref_properties: _containers.RepeatedCompositeFieldContainer[RefProperties]
+    def __init__(self, non_ref_properties: _Optional[_Iterable[str]] = ..., ref_properties: _Optional[_Iterable[_Union[RefProperties, _Mapping]]] = ...) -> None: ...
+
+class RefProperties(_message.Message):
+    __slots__ = ["linked_class", "linked_properties", "reference_property"]
+    LINKED_CLASS_FIELD_NUMBER: _ClassVar[int]
+    LINKED_PROPERTIES_FIELD_NUMBER: _ClassVar[int]
+    REFERENCE_PROPERTY_FIELD_NUMBER: _ClassVar[int]
+    linked_class: str
+    linked_properties: Properties
+    reference_property: str
+    def __init__(self, linked_class: _Optional[str] = ..., reference_property: _Optional[str] = ..., linked_properties: _Optional[_Union[Properties, _Mapping]] = ...) -> None: ...
+
+class ResultAdditionalProps(_message.Message):
+    __slots__ = ["id"]
+    ID_FIELD_NUMBER: _ClassVar[int]
+    id: str
+    def __init__(self, id: _Optional[str] = ...) -> None: ...
+
+class ResultProperties(_message.Message):
+    __slots__ = ["class_name", "non_ref_properties", "ref_props"]
+    CLASS_NAME_FIELD_NUMBER: _ClassVar[int]
+    NON_REF_PROPERTIES_FIELD_NUMBER: _ClassVar[int]
+    REF_PROPS_FIELD_NUMBER: _ClassVar[int]
+    class_name: str
+    non_ref_properties: _struct_pb2.Struct
+    ref_props: _containers.RepeatedCompositeFieldContainer[ReturnRefProperties]
+    def __init__(self, non_ref_properties: _Optional[_Union[_struct_pb2.Struct, _Mapping]] = ..., ref_props: _Optional[_Iterable[_Union[ReturnRefProperties, _Mapping]]] = ..., class_name: _Optional[str] = ...) -> None: ...
+
+class ReturnRefProperties(_message.Message):
+    __slots__ = ["prop_name", "properties"]
+    PROPERTIES_FIELD_NUMBER: _ClassVar[int]
+    PROP_NAME_FIELD_NUMBER: _ClassVar[int]
+    prop_name: str
+    properties: _containers.RepeatedCompositeFieldContainer[ResultProperties]
+    def __init__(self, properties: _Optional[_Iterable[_Union[ResultProperties, _Mapping]]] = ..., prop_name: _Optional[str] = ...) -> None: ...
+
+class SearchReply(_message.Message):
+    __slots__ = ["results", "took"]
+    RESULTS_FIELD_NUMBER: _ClassVar[int]
+    TOOK_FIELD_NUMBER: _ClassVar[int]
+    results: _containers.RepeatedCompositeFieldContainer[SearchResult]
+    took: float
+    def __init__(self, results: _Optional[_Iterable[_Union[SearchResult, _Mapping]]] = ..., took: _Optional[float] = ...) -> None: ...
+
+class SearchRequest(_message.Message):
+    __slots__ = ["additional_properties", "class_name", "limit", "near_object", "near_vector", "properties"]
+    ADDITIONAL_PROPERTIES_FIELD_NUMBER: _ClassVar[int]
+    CLASS_NAME_FIELD_NUMBER: _ClassVar[int]
+    LIMIT_FIELD_NUMBER: _ClassVar[int]
+    NEAR_OBJECT_FIELD_NUMBER: _ClassVar[int]
+    NEAR_VECTOR_FIELD_NUMBER: _ClassVar[int]
+    PROPERTIES_FIELD_NUMBER: _ClassVar[int]
+    additional_properties: _containers.RepeatedScalarFieldContainer[str]
+    class_name: str
+    limit: int
+    near_object: NearObjectParams
+    near_vector: NearVectorParams
+    properties: Properties
+    def __init__(self, class_name: _Optional[str] = ..., limit: _Optional[int] = ..., additional_properties: _Optional[_Iterable[str]] = ..., near_vector: _Optional[_Union[NearVectorParams, _Mapping]] = ..., near_object: _Optional[_Union[NearObjectParams, _Mapping]] = ..., properties: _Optional[_Union[Properties, _Mapping]] = ...) -> None: ...
+
+class SearchResult(_message.Message):
+    __slots__ = ["additional_properties", "properties"]
+    ADDITIONAL_PROPERTIES_FIELD_NUMBER: _ClassVar[int]
+    PROPERTIES_FIELD_NUMBER: _ClassVar[int]
+    additional_properties: ResultAdditionalProps
+    properties: ResultProperties
+    def __init__(self, properties: _Optional[_Union[ResultProperties, _Mapping]] = ..., additional_properties: _Optional[_Union[ResultAdditionalProps, _Mapping]] = ...) -> None: ...
