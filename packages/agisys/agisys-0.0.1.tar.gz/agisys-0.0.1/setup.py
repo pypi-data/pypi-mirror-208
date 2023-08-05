b2e66543486b577512cfcbc79381017cdb9c5c13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:23 +0000

    Add package agisys/0.0.1/agisys-0.0.1.tar.gz

---
+++ packages/agisys/agisys-0.0.1.tar.gz/agisys-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='agisys',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='http://agisys-ai.github.io/',
+    author='AGISYS',
+    author_email='agisys.ai@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 2 - Pre-Alpha'],
+)
