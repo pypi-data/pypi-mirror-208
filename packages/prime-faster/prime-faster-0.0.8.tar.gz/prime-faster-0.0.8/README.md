ede9484d3c439d4418303cbcb99b7637e892d771
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:52 +0000

    Add package prime-faster/0.0.8/prime-faster-0.0.8.tar.gz

---
+++ packages/prime-faster/prime-faster-0.0.8.tar.gz/prime-faster-0.0.8/README.md
@@ -0,0 +1,27 @@
+# prime-faster
+
+## Description
+
+This is a small program that finds all the prime numbers up to a given number faster than the naive algorithm.
+
+## Install
+
+```bash
+$ pip install prime-faster
+```
+
+## Usage
+
+```python
+import primefaster
+
+print(primefaster.is_prime(7))
+# True
+print(primefaster.get_prime(10))
+# [2, 3, 5, 7]
+```
+
+## License
+
+MIT License Copyright (c) 2022 Takahashi Akari <akaritakahashioss@gmail.com>
+
---
+++ packages/prime-faster/prime-faster-0.0.8.tar.gz/prime-faster-0.0.8/primefaster/primefaster.py
@@ -0,0 +1,40 @@
+# prime-faster - Faster Prime Number Generator
+# Author: Takahashi Akari <akaritakahashioss@gmail.com>
+# License: MIT License Copyright (c) 2022 Takahashi Akari <akaritakahashioss@gmail.com>
+# Version: 0.0.8
+# Date: 2022-08-08
+# Python: 3.10.6
+# Description: prime-faster - Faster Prime Number Generator
+
+import sqrt
+import numpy
+
+def is_prime(n):
+    if n < 2:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def get_prime(n):
+    if n < 2:
+        return []
+    if n == 2:
+        return [2]
+    if n == 3:
+        return [2, 3]
+    n += 1
+    sieve = numpy.ones(n // 3 + (n % 6 == 2), dtype=bool)
+    for i in range(1, int(sqrt(n)) // 3 + 1):
+        if sieve[i]:
+            k = 3 * i + 1 | 1
+            sieve[k * k // 3 :: 2 * k] = False
+            sieve[k * ( k - 2 * (i & 1 ) + 4) // 3 :: 2 * k] = False
+    np = numpy.r_[2, 3, ((3 * numpy.nonzero(sieve)[0][1:] + 1)|1)]
+    # return array
+    return np.tolist()
