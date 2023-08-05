e059d3eedd0b23ae5e1f15640344bca37bbab2f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:46 +0000

    Add package aws-fortuna/0.1.15/aws_fortuna-0.1.15-py3-none-any.whl

---
+++ packages/aws-fortuna/aws_fortuna-0.1.15-py3-none-any.whl/fortuna/typing.py
@@ -0,0 +1,24 @@
+import pathlib
+from typing import Dict, Iterable, Tuple, Union
+
+import jax.numpy as jnp
+import numpy as np
+from flax.core import FrozenDict
+from optax._src.base import GradientTransformation, PyTree
+
+Params = FrozenDict[str, FrozenDict[str, PyTree]]
+Mutable = FrozenDict[str, FrozenDict[str, PyTree]]
+CalibParams = FrozenDict[str, PyTree]
+CalibMutable = FrozenDict[str, PyTree]
+OptaxOptimizer = GradientTransformation
+Array = Union[jnp.ndarray, np.ndarray]
+Status = Dict[str, Array]
+Path = Union[str, pathlib.Path]
+InputData = Array
+Targets = Array
+Batch = Tuple[InputData, Targets]
+Outputs = jnp.ndarray
+Uncertainties = jnp.ndarray
+Predictions = jnp.ndarray
+AnyKey = Union[str, int]
+Shape = Iterable[int]
