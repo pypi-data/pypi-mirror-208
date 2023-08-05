dffc55527a13da9b9d8879e15cb616ddac60c054
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:01 +0000

    Add package justa/0.0.1.dev2/justa-0.0.1.dev2-py3-none-any.whl

---
+++ packages/justa/justa-0.0.1.dev2-py3-none-any.whl/justa/math_utils/__init__.py
@@ -0,0 +1,3 @@
+from justa.math_utils.domains import Domain, Universe, EmptySet, Reals, Intergers, Positive, Negative, Zero, NonNegative, Naturals
+from justa.math_utils.functions import Function, I, X, Sin, Cos, Tan, Sqrt, Sum
+from justa.math_utils.fields import Field
