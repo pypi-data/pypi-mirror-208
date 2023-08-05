f2a0ff95daf9fc927f62b22145eca4a4cf944eb5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:28 +0000

    Add package storms/0.1.0/storms-0.1.0-py2.py3-none-any.whl

---
+++ packages/storms/storms-0.1.0-py2.py3-none-any.whl/storms/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for storms."""
+
+__author__ = """Constantine Karos"""
+__email__ = "karosc@cdmsmith.com"
+__version__ = "0.1.0"
+
+from storms.precip import Raingage
+from storms.precip import Network
