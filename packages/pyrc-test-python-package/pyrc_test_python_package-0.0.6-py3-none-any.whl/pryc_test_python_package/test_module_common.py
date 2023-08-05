451e231c99bdf859de4f05d742c399c925d784b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:09 +0000

    Add package pyrc-test-python-package/0.0.6/pyrc_test_python_package-0.0.6-py3-none-any.whl

---
+++ packages/pyrc-test-python-package/pyrc_test_python_package-0.0.6-py3-none-any.whl/pryc_test_python_package/test_module_common.py
@@ -0,0 +1,6 @@
+def print_msg(msg: str):
+    print(msg)
+
+
+def test_module_common_stub():
+    pass
---
+++ packages/pyrc-test-python-package/pyrc_test_python_package-0.0.6-py3-none-any.whl/test_python_package/test_module.py
@@ -0,0 +1,10 @@
+from . import test_module_common as test_module_common
+
+
+def print_msg(msg: str):
+    test_module_common.print_msg(msg)
+
+
+def test_module_stub():
+    pass
+
