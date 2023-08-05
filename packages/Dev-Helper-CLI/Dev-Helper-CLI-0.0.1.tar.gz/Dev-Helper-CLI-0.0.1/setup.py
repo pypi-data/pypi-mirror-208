10f5b044d9b30d0591b909a0391e8b3916afd493
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:07:23 +0000

    Add package Dev-Helper-CLI/0.0.1/Dev-Helper-CLI-0.0.1.tar.gz

---
+++ packages/Dev-Helper-CLI/Dev-Helper-CLI-0.0.1.tar.gz/Dev-Helper-CLI-0.0.1/setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fhand:
+    long_description = fhand.read()
+
+REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
+
+setuptools.setup(
+    name="Dev-Helper-CLI",
+    version="0.0.1",
+    author="Luis Moreno",
+    author_email="luis.cfh.90@gmail.com",
+    description=("A CLI program to help devs be more productive."),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/luigicfh/Dev-Helper-2.0",
+    project_urls={
+        "Bug Tracker": "https://github.com/luigicfh/Dev-Helper-2.0/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=REQUIREMENTS,
+    packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "dh = dh.cli:parse",
+        ]
+    }
+)
