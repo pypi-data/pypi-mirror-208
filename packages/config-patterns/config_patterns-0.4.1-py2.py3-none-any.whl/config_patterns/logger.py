2714ae0b5007ae6c59a20246e143d2e8fc02491b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:09:00 +0000

    Add package config-patterns/0.4.1/config_patterns-0.4.1-py2.py3-none-any.whl

---
+++ packages/config-patterns/config_patterns-0.4.1-py2.py3-none-any.whl/config_patterns/logger.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+from .vendor.nest_logger import NestedLogger
+from .paths import PACKAGE_NAME
+
+
+logger = NestedLogger(
+    name=PACKAGE_NAME,
+    log_format="%(message)s",
+)
