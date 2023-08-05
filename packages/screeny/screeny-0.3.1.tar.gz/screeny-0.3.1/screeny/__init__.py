f5f648c9a7f00dddaef747fde436459727618068
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:10:24 +0000

    Add package screeny/0.3.1/screeny-0.3.1.tar.gz

---
+++ packages/screeny/screeny-0.3.1.tar.gz/screeny-0.3.1/screeny/__init__.py
@@ -0,0 +1,6 @@
+from PySide6.QtCore import QRect as Rect, QPoint as Point
+from .image import Image
+from .mouse import Mouse
+from .screeny import Screeny
+
+__all__ = ["Image", "Mouse", "Screeny", "Rect", "Point"]
