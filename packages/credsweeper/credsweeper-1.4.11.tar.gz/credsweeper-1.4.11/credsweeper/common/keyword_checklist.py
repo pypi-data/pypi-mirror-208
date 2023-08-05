3bb63afc57b3cf266936fa8d3d2a9a0643b05047
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:23 +0000

    Add package credsweeper/1.4.11/credsweeper-1.4.11.tar.gz

---
+++ packages/credsweeper/credsweeper-1.4.11.tar.gz/credsweeper-1.4.11/credsweeper/common/keyword_checklist.py
@@ -0,0 +1,36 @@
+import os
+from typing import List, Set
+
+from credsweeper.utils import Util
+
+
+class KeywordChecklist:
+    """KeywordsChecklist contains words 3 or more letters length"""
+    __keyword_list: List[str] = []
+
+    def __init__(self) -> None:
+        dir_path = os.path.dirname(os.path.realpath(__file__))
+        file_path = os.path.join(dir_path, "keyword_checklist.txt")
+        self.set_list(Util.read_file(file_path))
+
+    def get_list(self) -> List[str]:
+        """Get list with keywords.
+
+        Return:
+            List of strings
+
+        """
+        return self.__keyword_list
+
+    def set_list(self, keyword_list: List[str]) -> None:
+        """Remove old keywords and setup new one.
+
+        Args:
+            keyword_list: list of keywords to be added
+
+        """
+        keyword_set: Set[str] = set()
+        for i in keyword_list:
+            if 3 <= len(i):
+                keyword_set.add(i)
+        self.__keyword_list = list(keyword_set)
