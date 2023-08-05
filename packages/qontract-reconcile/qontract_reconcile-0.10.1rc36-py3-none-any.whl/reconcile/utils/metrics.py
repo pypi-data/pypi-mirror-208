33f9f6e98554f281fc02d4462fc20692416e9d74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:03 +0000

    Add package qontract-reconcile/0.10.1rc36/qontract_reconcile-0.10.1rc36-py3-none-any.whl

---
+++ packages/qontract-reconcile/qontract_reconcile-0.10.1rc36-py3-none-any.whl/reconcile/utils/metrics.py
@@ -0,0 +1,66 @@
+from prometheus_client import (
+    Counter,
+    Gauge,
+    Histogram,
+)
+
+run_time = Gauge(
+    name="qontract_reconcile_last_run_seconds",
+    documentation="Last run duration in seconds",
+    labelnames=["integration", "shards", "shard_id"],
+)
+
+run_status = Gauge(
+    name="qontract_reconcile_last_run_status",
+    documentation="Last run status",
+    labelnames=["integration", "shards", "shard_id"],
+)
+
+execution_counter = Counter(
+    name="qontract_reconcile_execution_counter",
+    documentation="Counts started integration executions",
+    labelnames=["integration", "shards", "shard_id"],
+)
+
+reconcile_time = Histogram(
+    name="qontract_reconcile_function_" "elapsed_seconds_since_bundle_commit",
+    documentation="Run time seconds for tracked " "functions",
+    labelnames=["name", "integration"],
+    buckets=(60.0, 150.0, 300.0, 600.0, 1200.0, 1800.0, 2400.0, 3000.0, float("inf")),
+)
+
+registry_reachouts = Counter(
+    name="qontract_reconcile_registry_get_manifest_total",
+    documentation="Number of GET requests on image registries",
+    labelnames=["integration", "shard", "shard_id", "registry"],
+)
+
+cache_hits = Counter(
+    name="qontract_reconcile_cache_hits_total",
+    documentation="Number of hits to this cache",
+    labelnames=["integration", "shards", "shard_id"],
+)
+
+cache_misses = Counter(
+    name="qontract_reconcile_cache_misses_total",
+    documentation="Number of misses on this cache",
+    labelnames=["integration", "shards", "shard_id"],
+)
+
+cache_size = Gauge(
+    name="qontract_reconcile_cache_cardinality",
+    documentation="Number of keys in the cache",
+    labelnames=["integration", "shards", "shard_id"],
+)
+
+copy_count = Counter(
+    name="qontract_reconcile_skopeo_copy_total",
+    documentation="Number of copy commands issued by Skopeo",
+    labelnames=["integration", "shard", "shard_id"],
+)
+
+gitlab_request = Counter(
+    name="qontract_reconcile_gitlab_request_total",
+    documentation="Number of calls made to Gitlab API",
+    labelnames=["integration"],
+)
