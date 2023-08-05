6deb3ec17651af5632bd5830d3d82d7b9491fe9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:18 +0000

    Add package prime-faster/0.0.9/prime-faster-0.0.9.tar.gz

---
+++ packages/prime-faster/prime-faster-0.0.9.tar.gz/prime-faster-0.0.9/setup.py
@@ -0,0 +1,32 @@
+# prime-faster - Faster Prime Number Generator
+# Author: Takahashi Akari <akaritakahashioss@gmail.com>
+# License: MIT License Copyright (c) 2022 Takahashi Akari <akaritakahashioss@gmail.com>
+# Version: 0.0.9
+# Date: 2022-08-08
+# Python: 3.10.6
+# Description: prime-faster - Faster Prime Number Generator
+
+import setuptools
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="prime-faster",
+    version="0.0.9",
+    author="Takahashi Akari",
+    author_email="akaritakahashioss@gmail.com",
+    description="prime-faster - Faster Prime Number Generator",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/takahashi-akari/prime-faster",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
