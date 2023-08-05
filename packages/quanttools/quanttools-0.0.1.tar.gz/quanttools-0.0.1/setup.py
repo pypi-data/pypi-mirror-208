abf6a0e20f96ea84316ad9f1c3f52f1c63cf2c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:51 +0000

    Add package quanttools/0.0.1/quanttools-0.0.1.tar.gz

---
+++ packages/quanttools/quanttools-0.0.1.tar.gz/quanttools-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='quanttools',
+    version='0.0.1',
+    description='Fundamental package for quantitative finance in Python',
+    long_description='QuantTools Fundamental package for Quantitative Finance in Python.',
+    url='https://www.quanttools.net',
+    author='Ali Moin',
+    author_email='ali@quanttools.net',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
