f8ab75c3912f62c70a2c57774686d16e3ec66134
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:17:23 +0000

    Add package gato-toolkit/0.2.0/gato-toolkit-0.2.0.tar.gz

---
+++ packages/gato-toolkit/gato-toolkit-0.2.0.tar.gz/gato-toolkit-0.2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+def get_long_description():
+    with open('README.md') as file:
+        return file.read()
+
+
+setup(
+    name="gato-toolkit",
+    version="0.2.0",
+    description="A toolkit for furthering research on AI alignment.",
+    url="https://github.com/FyZyX/gato-toolkit",
+    author="Lucas Lofaro <lucasmlofaro@gmail.com>",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    license='MIT',
+    long_description=get_long_description(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.10',
+    ],
+    install_requires=[
+        "openai>=0.27.6",
+        "pydantic>=1.10.7",
+    ],
+)
