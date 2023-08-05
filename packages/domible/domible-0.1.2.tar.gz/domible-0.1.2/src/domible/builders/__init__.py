e8188737550e10843b99a234399d01c0b61c0b61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:38 +0000

    Add package domible/0.1.2/domible-0.1.2.tar.gz

---
+++ packages/domible/domible-0.1.2.tar.gz/domible-0.1.2/src/domible/builders/__init__.py
@@ -0,0 +1,5 @@
+""" domible/src/domible/builders/__init__.py """
+
+from domible.builders.tableBuilder import RowInfo, TableInfo, createTableFromDicts
+
+# end of file
---
+++ packages/domible/domible-0.1.2.tar.gz/domible-0.1.2/src/domible/elements/__init__.py
@@ -0,0 +1,10 @@
+""" domible/src/domible/elements/__init__.py
+"""
+
+from domible.elements.roots import Html, Head, Body 
+from domible.elements.meta import Base, Meta, Title 
+from domible.elements.inline import Anchor, Div, Heading, Paragraph, Span
+from domible.elements.lists import DescriptionTerm, DescriptionDef, DListItem, ListItem, OrderedList, UnorderedList, MenuList, DescriptionList
+from domible.elements.tables import TableData, TableHeader, TableColumnHeader, TableRowHeader, TableRow, TableHead, TableBody, TableFoot, Caption, Table
+
+# end of file
