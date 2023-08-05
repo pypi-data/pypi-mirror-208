b697639a2a8ae45a4e432327c0dc3b5c25a31cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:20 +0000

    Add package hatch-showcase/0.5.0/hatch_showcase-0.5.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/hatch-showcase/hatch_showcase-0.5.0-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/hatch_showcase/cli/__init__.py
@@ -0,0 +1,22 @@
+# SPDX-FileCopyrightText: 2021-present Ofek Lev <oss@ofek.dev>
+#
+# SPDX-License-Identifier: MIT
+import click
+
+from hatch_showcase._version import version
+from hatch_showcase.fib import fibonacci
+
+# NOTE: The group/command decorators must come last to avoid the following issue at runtime:
+# https://github.com/pallets/click/issues/1199
+
+
+@click.version_option(version=version, prog_name='hatch-showcase')
+@click.group()
+def hatch_showcase():
+    pass
+
+
+@click.argument('n', type=int)
+@hatch_showcase.command()
+def fib(n: int):
+    click.echo(fibonacci(n))
