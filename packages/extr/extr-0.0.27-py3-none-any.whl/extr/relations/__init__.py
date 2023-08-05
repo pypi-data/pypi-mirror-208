b057b898142de5cdc609071811f9ad6bf1062186
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:13:03 +0000

    Add package extr/0.0.27/extr-0.0.27-py3-none-any.whl

---
+++ packages/extr/extr-0.0.27-py3-none-any.whl/extr/relations/__init__.py
@@ -0,0 +1,3 @@
+from .extractor import RelationExtractor
+from .annotator import RelationAnnotator, HtmlRelationAnnotator
+from .label_builder import RegExRelationLabelBuilder, RelationLabelBuilderConfig
