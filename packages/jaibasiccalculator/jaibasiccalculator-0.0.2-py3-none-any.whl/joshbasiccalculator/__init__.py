ffd93102920ce6cae76adf8398ce2f65b2a3d5c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:04:02 +0000

    Add package jaibasiccalculator/0.0.2/jaibasiccalculator-0.0.2-py3-none-any.whl

---
+++ packages/jaibasiccalculator/jaibasiccalculator-0.0.2-py3-none-any.whl/joshbasiccalculator/__init__.py
@@ -0,0 +1,14 @@
+import main
+from main import printfunc
+def add_numbers(num1, num2):
+    printfunc()
+    return num1 + num2
+
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    return num1 / num2
