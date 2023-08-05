5126c714fa160d3c5c5d43076de7859761de8f58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:18:39 +0000

    Add package django-choices-field/2.1.2/django_choices_field-2.1.2.tar.gz

---
+++ packages/django-choices-field/django_choices_field-2.1.2.tar.gz/django_choices_field-2.1.2/README.md
@@ -0,0 +1,72 @@
+# django-choices-field
+
+[![build status](https://img.shields.io/endpoint.svg?url=https%3A%2F%2Factions-badge.atrox.dev%2Fbellini666%2Fdjango-choices-field%2Fbadge%3Fref%3Dmaster&style=flat)](https://actions-badge.atrox.dev/bellini666/django-choices-field/goto?ref=master)
+[![coverage](https://img.shields.io/codecov/c/github/bellini666/django-choices-field.svg)](https://codecov.io/gh/bellini666/django-choices-field)
+[![PyPI version](https://img.shields.io/pypi/v/django-choices-field.svg)](https://pypi.org/project/django-choices-field/)
+![python version](https://img.shields.io/pypi/pyversions/django-choices-field.svg)
+![django version](https://img.shields.io/pypi/djversions/django-choices-field.svg)
+
+Django field that set/get django's new TextChoices/IntegerChoices enum.
+
+## Install
+
+```bash
+pip install django-choices-field
+```
+
+## Usage
+
+```python
+from django.db import models
+from django_choices_field import TextChoicesField, IntegerChoicesField
+
+
+class MyModel(models.Model):
+    class TextEnum(models.TextChoices):
+        FOO = "foo", "Foo Description"
+        BAR = "bar", "Bar Description"
+
+    class IntegerEnum(models.IntegerChoices):
+        FIRST = 1, "First Description"
+        SECOND = 2, "Second Description"
+
+    c_field = TextChoicesField(
+        choices_enum=TextEnum,
+        default=TextEnum.FOO,
+    )
+    i_field = IntegerChoicesField(
+        choices_enum=IntegerEnum,
+        default=IntegerEnum.FIRST,
+    )
+
+
+obj = MyModel()
+obj.c_field  # MyModel.TextEnum.FOO
+isinstance(obj.c_field, MyModel.TextEnum) # True
+obj.i_field  # MyModel.IntegerEnum.FIRST
+isinstance(obj.i_field, MyModel.IntegerEnum) # True
+```
+
+## License
+
+This project is licensed under MIT licence (see `LICENSE` for more info)
+
+## Contributing
+
+Make sure to have [poetry](https://python-poetry.org/) installed.
+
+Install dependencies with:
+
+```bash
+poetry install
+```
+
+Run the testsuite with:
+
+```bash
+poetry run pytest
+```
+
+Feel free to fork the project and send me pull requests with new features,
+corrections and translations. I'll gladly merge them and release new versions
+ASAP.
---
+++ packages/django-choices-field/django_choices_field-2.1.2.tar.gz/django_choices_field-2.1.2/django_choices_field/fields.py
@@ -0,0 +1,99 @@
+from typing import Optional, Type
+
+from django.core.exceptions import ValidationError
+from django.db import models
+
+
+class TextChoicesField(models.CharField):
+    description = "TextChoices"
+    default_error_messages = {
+        "invalid": "“%(value)s” must be a subclass of %(enum)s.",
+    }
+
+    def __init__(
+        self,
+        choices_enum: Type[models.TextChoices],
+        verbose_name: Optional[str] = None,
+        name: Optional[str] = None,
+        **kwargs,
+    ):
+        self.choices_enum = choices_enum
+        kwargs["choices"] = choices_enum.choices
+        kwargs.setdefault("max_length", max(len(c.value) for c in choices_enum))
+        super().__init__(verbose_name=verbose_name, name=name, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        kwargs["choices_enum"] = self.choices_enum
+        return name, path, args, kwargs
+
+    def to_python(self, value):
+        if value is None:
+            return None
+
+        try:
+            return self.choices_enum(value)
+        except ValueError as e:
+            raise ValidationError(
+                self.error_messages["invalid"],
+                code="invalid",
+                params={"value": value, "enum": self.choices_enum},
+            ) from e
+
+    def from_db_value(self, value, expression, connection):
+        return self.to_python(value)
+
+    def get_prep_value(self, value):
+        value = super().get_prep_value(value)
+        return self.to_python(value)
+
+
+class IntegerChoicesField(models.IntegerField):
+    description = "IntegerChoices"
+    default_error_messages = {
+        "invalid": "“%(value)s” must be a subclass of %(enum)s.",
+    }
+
+    def __init__(
+        self,
+        choices_enum: Type[models.IntegerChoices],
+        verbose_name: Optional[str] = None,
+        name: Optional[str] = None,
+        **kwargs,
+    ):
+        self.choices_enum = choices_enum
+        kwargs["choices"] = choices_enum.choices
+        super().__init__(verbose_name=verbose_name, name=name, **kwargs)
+
+    def deconstruct(self):
+        name, path, args, kwargs = super().deconstruct()
+        kwargs["choices_enum"] = self.choices_enum
+        return name, path, args, kwargs
+
+    def to_python(self, value):
+        if value is None:
+            return None
+
+        try:
+            return self.choices_enum(int(value) if isinstance(value, str) else value)
+        except ValueError as e:
+            raise ValidationError(
+                self.error_messages["invalid"],
+                code="invalid",
+                params={"value": value, "enum": self.choices_enum},
+            ) from e
+
+    def from_db_value(self, value, expression, connection):
+        return self.to_python(value)
+
+    def get_prep_value(self, value):
+        value = super().get_prep_value(value)
+        return self.to_python(value)
+
+    def formfield(self, **kwargs):  # pragma:nocover
+        return super().formfield(
+            **{
+                "coerce": self.to_python,
+                **kwargs,
+            },
+        )
