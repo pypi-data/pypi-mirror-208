8c265464af7b100432becb4e18af1900f2dff081
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:35 +0000

    Add package hccpy/0.1.8/hccpy-0.1.8.tar.gz

---
+++ packages/hccpy/hccpy-0.1.8.tar.gz/hccpy-0.1.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(packages=find_packages(),
+    name="hccpy",
+    version="0.1.8",
+    description="hccpy is a Python implementation of HCC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Yubin Park",
+    author_email="yubin.park@gmail.com",
+    url="https://github.com/yubin-park/hccpy",
+    license="Apache 2.0", 
+    install_requires = ["numpy"],
+    include_package_data=True,
+    package_dirc={"": "hccpy"},
+    package_data={"data": ["*.TXT", "*.csv", "*.json"]},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent"
+    ])
+
+
