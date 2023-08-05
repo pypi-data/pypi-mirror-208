4552258ec38acc8ae79caef70a0eee6a38c3f375
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:52 +0000

    Add package promptlayer/0.1.81/promptlayer-0.1.81.tar.gz

---
+++ packages/promptlayer/promptlayer-0.1.81.tar.gz/promptlayer-0.1.81/promptlayer/__init__.py
@@ -0,0 +1,15 @@
+from promptlayer.promptlayer import PromptLayerBase
+import promptlayer.langchain as langchain
+import promptlayer.prompts as prompts
+import promptlayer.track as track
+import openai
+import os
+
+api_key = os.environ.get("PROMPTLAYER_API_KEY")
+openai = PromptLayerBase(openai, function_name="openai")
+
+
+__all__ = [
+    "api_key",
+    "openai",
+]
