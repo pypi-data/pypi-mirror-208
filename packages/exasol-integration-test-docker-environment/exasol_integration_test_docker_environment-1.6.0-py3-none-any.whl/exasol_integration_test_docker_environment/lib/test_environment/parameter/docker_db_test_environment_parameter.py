4dc37e897574103a7cef2b11f0ef51d9f980f382
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:05:58 +0000

    Add package exasol-integration-test-docker-environment/1.6.0/exasol_integration_test_docker_environment-1.6.0-py3-none-any.whl

---
+++ packages/exasol-integration-test-docker-environment/exasol_integration_test_docker_environment-1.6.0-py3-none-any.whl/exasol_integration_test_docker_environment/lib/test_environment/parameter/docker_db_test_environment_parameter.py
@@ -0,0 +1,15 @@
+import luigi
+from luigi import Config
+
+
+class DockerDBTestEnvironmentParameter(Config):
+    docker_db_image_name = luigi.OptionalParameter(None)
+    docker_db_image_version = luigi.OptionalParameter(None)
+    reuse_database = luigi.BoolParameter(False, significant=False)
+    no_database_cleanup_after_success = luigi.BoolParameter(False, significant=False)
+    no_database_cleanup_after_failure = luigi.BoolParameter(False, significant=False)
+    database_port_forward = luigi.OptionalParameter(None, significant=False)
+    bucketfs_port_forward = luigi.OptionalParameter(None, significant=False)
+    mem_size = luigi.OptionalParameter("2 GiB", significant=False)
+    disk_size = luigi.OptionalParameter("2 GiB", significant=False)
+    nameservers = luigi.ListParameter([], significant=False)
---
+++ packages/exasol-integration-test-docker-environment/exasol_integration_test_docker_environment-1.6.0-py3-none-any.whl/exasol_integration_test_docker_environment/testing/exaslct_test_environment.py
@@ -0,0 +1,181 @@
+import functools
+import inspect
+import os
+import shutil
+import subprocess
+import tempfile
+from pathlib import Path
+import shlex
+from sys import stderr
+from typing import List
+
+from exasol_integration_test_docker_environment.lib.data.environment_info import EnvironmentInfo
+from exasol_integration_test_docker_environment.lib.docker import ContextDockerClient
+from exasol_integration_test_docker_environment.lib.docker.container.utils import remove_docker_container
+from exasol_integration_test_docker_environment.lib.docker.volumes.utils import remove_docker_volumes
+from exasol_integration_test_docker_environment.testing.docker_registry import default_docker_repository_name
+from exasol_integration_test_docker_environment.testing.exaslct_docker_test_environment import \
+    ExaslctDockerTestEnvironment
+from exasol_integration_test_docker_environment.testing.spawned_test_environments import SpawnedTestEnvironments
+from exasol_integration_test_docker_environment.testing.utils import find_free_ports, check_db_version_from_env
+
+
+def _cleanup(env_name: str):
+    remove_docker_container([f"test_container_{env_name}",
+                             f"db_container_{env_name}"])
+    remove_docker_volumes([f"db_container_{env_name}_volume"])
+
+
+class ExaslctTestEnvironment:
+
+    def __init__(self, test_object, executable="./exaslct", clean_images_at_close=True):
+        self.clean_images_at_close = clean_images_at_close
+        self.executable = executable
+        self.test_object = test_object
+        if not inspect.isclass(self.test_object):
+            self.test_class = self.test_object.__class__
+        else:
+            self.test_class = self.test_object
+        self.flavor_path = self.get_test_flavor()
+        self.name = self.test_class.__name__
+        self._docker_repository_name = default_docker_repository_name(self.name)
+        if "GOOGLE_CLOUD_BUILD" in os.environ:
+            # We need to put the output directories into the workdir,
+            # because only this is shared between the current container and
+            # host. Only path within this shared directory can be mounted
+            # to docker container started by exaslct
+            temp_dir_prefix_path = Path("./temp_outputs")
+            temp_dir_prefix_path.mkdir(exist_ok=True)
+            self.temp_dir = tempfile.mkdtemp(dir=temp_dir_prefix_path)
+        else:
+            self.temp_dir = tempfile.mkdtemp()
+        self._update_attributes()
+
+    def get_test_flavor(self):
+        source_file_of_test_object = inspect.getsourcefile(self.test_class)
+        flavor_path = Path(os.path.realpath(source_file_of_test_object)).parent.joinpath(
+            "resources/test-flavor")
+        return flavor_path
+
+    @property
+    def repository_name(self):
+        return self._docker_repository_name
+
+    @repository_name.setter
+    def repository_name(self, value):
+        self._docker_repository_name = value
+        self._update_attributes()
+
+    def _update_attributes(self):
+        self.flavor_path_argument = f"--flavor-path {self.get_test_flavor()}"
+        repository_name = self.repository_name
+        self.docker_repository_arguments = f"--source-docker-repository-name {repository_name} " \
+                                           f"--target-docker-repository-name {repository_name}"
+        self.clean_docker_repository_arguments = f"--docker-repository-name {repository_name}"
+        self.output_directory_arguments = f"--output-directory {self.temp_dir}"
+        self.task_dependencies_argument = " ".join([f"--task-dependencies-dot-file {self.name}.dot", ])
+
+    def clean_images(self):
+        self.run_command(f"{self.executable} clean-flavor-images", clean=True)
+
+    def run_command(self, command: str,
+                    use_output_directory: bool = True,
+                    use_flavor_path: bool = True,
+                    use_docker_repository: bool = True,
+                    track_task_dependencies: bool = False,
+                    clean: bool = False,
+                    capture_output: bool = False):
+        if use_output_directory:
+            command = f"{command} {self.output_directory_arguments}"
+        if track_task_dependencies:
+            command = f"{command} {self.task_dependencies_argument}"
+        if use_flavor_path:
+            command = f"{command} {self.flavor_path_argument}"
+        if use_docker_repository and not clean:
+            command = f"{command} {self.docker_repository_arguments}"
+        if use_docker_repository and clean:
+            command = f"{command} {self.clean_docker_repository_arguments}"
+        print(file=stderr)
+        print(f"command: {command}", file=stderr)
+        if capture_output:
+            completed_process = subprocess.run(shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        else:
+            completed_process = subprocess.run(shlex.split(command))
+        try:
+            completed_process.check_returncode()
+        except subprocess.CalledProcessError as e:
+            if capture_output:
+                print(e.stdout.decode("UTF-8"), file=stderr)
+            raise e
+        return completed_process
+
+    def close(self):
+        try:
+            if self.clean_images_at_close:
+                self.clean_images()
+        except Exception as e:
+            print(e, file=stderr)
+        try:
+            shutil.rmtree(self.temp_dir)
+        except Exception as e:
+            print(e, file=stderr)
+
+    def spawn_docker_test_environments(self, name: str, additional_parameter: List[str] = None) \
+            -> SpawnedTestEnvironments:
+        database_port, bucketfs_port = find_free_ports(2)
+        on_host_parameter = ExaslctDockerTestEnvironment(
+            name=self.name + "_" + name,
+            database_host="localhost",
+            db_username="sys",
+            db_password="exasol",
+            bucketfs_username="w",
+            bucketfs_password="write",
+            database_port=database_port,
+            bucketfs_port=bucketfs_port)
+        docker_db_version_parameter = ""
+        db_version_from_env = check_db_version_from_env()
+        if db_version_from_env is not None:
+            docker_db_version_parameter = f'--docker-db-image-version "{db_version_from_env}"'
+        if additional_parameter is None:
+            additional_parameter = []
+        arguments = " ".join([f"--environment-name {on_host_parameter.name}",
+                              f"--database-port-forward {on_host_parameter.database_port}",
+                              f"--bucketfs-port-forward {on_host_parameter.bucketfs_port}",
+                              docker_db_version_parameter] + additional_parameter)
+
+        command = f"{self.executable} spawn-test-environment {arguments}"
+        completed_process = self.run_command(command, use_flavor_path=False, use_docker_repository=False,
+                                             capture_output=True)
+        on_host_parameter.completed_process = completed_process
+        environment_info_json_path = Path(self.temp_dir,
+                                          f"cache/environments/{on_host_parameter.name}/environment_info.json")
+        if environment_info_json_path.exists():
+            with environment_info_json_path.open() as f:
+                environment_info = EnvironmentInfo.from_json(f.read())
+                on_host_parameter.environment_info = environment_info
+        on_host_parameter.clean_up = functools.partial(_cleanup, on_host_parameter.name)
+        if "GOOGLE_CLOUD_BUILD" in os.environ:
+            google_cloud_parameter = ExaslctDockerTestEnvironment(
+                name=on_host_parameter.name,
+                database_host="localhost",
+                db_username=on_host_parameter.db_username,
+                db_password=on_host_parameter.db_password,
+                bucketfs_username=on_host_parameter.bucketfs_username,
+                bucketfs_password=on_host_parameter.bucketfs_password,
+                database_port=8888,
+                bucketfs_port=6583,
+                environment_info=on_host_parameter.completed_process,
+                completed_process=on_host_parameter.completed_process
+            )
+
+            with ContextDockerClient() as docker_client:
+                db_container = docker_client.containers.get(f"db_container_{google_cloud_parameter.name}")
+                cloudbuild_network = docker_client.networks.get("cloudbuild")
+                cloudbuild_network.connect(db_container)
+                db_container.reload()
+                google_cloud_parameter.database_host = \
+                    db_container.attrs["NetworkSettings"]["Networks"][cloudbuild_network.name]["IPAddress"]
+                return SpawnedTestEnvironments(on_host_parameter, google_cloud_parameter)
+        else:
+            return SpawnedTestEnvironments(on_host_parameter, None)
+
