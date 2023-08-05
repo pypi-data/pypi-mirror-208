b06b30c7fe792269858ad6d0509b199b10b85ccd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:41 +0000

    Add package osnap-client/0.1.0/osnap_client-0.1.0-py3-none-any.whl

---
+++ packages/osnap-client/osnap_client-0.1.0-py3-none-any.whl/osnap_client/__init__.py
@@ -0,0 +1,12 @@
+from .core.api import OSNAP
+from .core.osnap import (
+    OSNAPAgent,
+    OSNAPApp,
+    OSNAPTool,
+    Scope,
+    OSNAPRegistry,
+    OSNAPRequest,
+    OSNAPResponse,
+    OSNAPError,
+)
+
