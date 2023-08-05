95f4ade5089b06bbe759cea3804982fa68516fc7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:06:30 +0000

    Add package itzma-lint/0.1.1/itzma_lint-0.1.1-py3-none-any.whl

---
+++ packages/itzma-lint/itzma_lint-0.1.1-py3-none-any.whl/flake8_itzma.py
@@ -0,0 +1,35 @@
+import ast
+import checks
+from helpers import Flake8ASTErrorInfo
+
+
+class MCodingASTVisitor(ast.NodeVisitor):
+    def __init__(self):
+        self.errors: list[Flake8ASTErrorInfo] = []
+
+    def visit_FunctionDef(self, node: ast.FunctionDef):  # find all function nodes
+        checks.LocalImportsNotAllowed.check(node, self.errors)
+        checks.UnconventionalFunctionNamesNotAllowed.check(node, self.errors)
+        checks.UnconventionalFunctionNamesNotAllowed.checkFirstWordIsVerb(
+            node, self.errors
+        )
+        checks.UnconventionalVariableNamesNotAllowed.check(node, self.errors)
+        self.generic_visit(node)
+
+    def visit_ClassDef(self, node: ast.ClassDef):
+        checks.UnconventionalClassNamesNotAllowed.check(node, self.errors)
+        self.generic_visit(node)
+
+
+class ItzmaASTPlugin:
+
+    name = "flake8_mcoding_ast"
+    version = "0.0.0"
+
+    def __init__(self, tree: ast.AST):
+        self._tree = tree
+
+    def run(self):
+        visitor = MCodingASTVisitor()
+        visitor.visit(self._tree)  # pass the root of the AST
+        yield from visitor.errors
