266c00692467400e1dd77d4f54ec7d4aca0c2165
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:08:53 +0000

    Add package django-healthpoint/0.5.0/django-healthpoint-0.5.0.tar.gz

---
+++ packages/django-healthpoint/django-healthpoint-0.5.0.tar.gz/django-healthpoint-0.5.0/healthpoint/__init__.py
@@ -0,0 +1,16 @@
+"""
+      ___           ___       __   __         ___
+|__| |__   /\  |     |  |__| |__) /  \ | |\ |  |
+|  | |___ /~~\ |___  |  |  | |    \__/ | | \|  |
+
+"""  # noqa
+VERSION = (0, 5, 0, "final", 0)
+
+__title__ = "django-healthpoint"
+__version_info__ = VERSION
+__version__ = ".".join(map(str, VERSION[:3])) + (
+    "-{}{}".format(VERSION[3], VERSION[4] or "") if VERSION[3] != "final" else ""
+)
+__author__ = "Raymond Penners"
+__license__ = "MIT"
+__copyright__ = "Copyright 2018-2023 Raymond Penners and contributors"
