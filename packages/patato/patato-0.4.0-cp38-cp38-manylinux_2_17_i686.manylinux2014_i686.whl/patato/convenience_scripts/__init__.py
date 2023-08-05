4baa79df694ef213ba91d2aa3c012ad082342103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:06:29 +0000

    Add package patato/0.4.0/patato-0.4.0-cp38-cp38-manylinux_2_17_i686.manylinux2014_i686.whl

---
+++ packages/patato/patato-0.4.0-cp38-cp38-manylinux_2_17_i686.manylinux2014_i686.whl/patato/convenience_scripts/__init__.py
@@ -0,0 +1,9 @@
+#  Copyright (c) Thomas Else 2023.
+#  License: MIT
+
+"""
+PATATO Command Line Interface
+=============================
+
+A series of convenience scripts for running the patato package in the command line.
+"""
