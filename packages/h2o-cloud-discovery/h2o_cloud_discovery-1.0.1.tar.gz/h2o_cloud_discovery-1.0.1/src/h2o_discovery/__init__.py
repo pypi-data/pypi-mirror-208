cc53f1ad5029708e06eb7fbf4325276d23404086
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:23:03 +0000

    Add package h2o-cloud-discovery/1.0.1/h2o_cloud_discovery-1.0.1.tar.gz

---
+++ packages/h2o-cloud-discovery/h2o_cloud_discovery-1.0.1.tar.gz/h2o_cloud_discovery-1.0.1/src/h2o_discovery/__init__.py
@@ -0,0 +1,47 @@
+from typing import Optional
+
+from h2o_discovery import model
+from h2o_discovery._internal import client
+from h2o_discovery._internal import load
+from h2o_discovery._internal import lookup
+from h2o_discovery._version import __version__  # noqa: F401
+
+Discovery = model.Discovery
+
+
+def discover(
+    environment: Optional[str] = None, discovery_address: Optional[str] = None
+) -> Discovery:
+    """Obtains and returns a Discovery object from the discovery service.
+
+    Both arguments are optional. If neither is provided, the environment variable
+    H2O_CLOUD_ENVIRONMENT is used. If that is not set, the environment variable
+    H2O_CLOUD_DISCOVERY is used. If that is not set, a LookupError is raised.
+
+    Args:
+        environment: The H2O Cloud environment URL to use (e.g. https://cloud.h2o.ai).
+        discovery_address: The address of the discovery service.
+
+    """
+    uri = lookup.determine_uri(environment, discovery_address)
+    cl = client.Client(uri)
+    return load.load_discovery(cl)
+
+
+async def discover_async(
+    environment: Optional[str] = None, discovery_address: Optional[str] = None
+) -> Discovery:
+    """Obtains and returns a Discovery object from the discovery service.
+
+    Both arguments are optional. If neither is provided, the environment variable
+    H2O_CLOUD_ENVIRONMENT is used. If that is not set, the environment variable
+    H2O_CLOUD_DISCOVERY is used. If that is not set, a LookupError is raised.
+
+    Args:
+        environment: The H2O Cloud environment URL to use (e.g. https://cloud.h2o.ai).
+        discovery_address: The address of the discovery service.
+
+    """
+    uri = lookup.determine_uri(environment, discovery_address)
+    cl = client.AsyncClient(uri)
+    return await load.load_discovery_async(cl)
