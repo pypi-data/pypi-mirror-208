7d7b4cc386a1e3a0b312ce59e9cb803877b33cd3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:52 +0000

    Add package checkify/0.4/checkify-0.4.tar.gz

---
+++ packages/checkify/checkify-0.4.tar.gz/checkify-0.4/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='checkify',
+    version='0.4',
+    packages=['checkify'],
+    install_requires=[
+        'openai'
+    ],
+    author="Tripathi Aditya Prakash",                     # Full name of the author
+    description="Python package that utilizes the GPT API to check for errors in code and explain the code.",
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/aditya0072001/checkify',
+        classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],                                      # Information to filter the project on PyPi website
+    python_requires='>=3.6',                # Minimum version requirement of the package
+    project_urls={
+        'Download Statistics': 'https://pepy.tech/project/checkify/month',
+    },
+)
