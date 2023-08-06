96d884e2e1bb09a839a6961f867790c6bb2c5e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:44 +0000

    Add package rhemati-firestore/1.0.0/rhemati-firestore-1.0.0.tar.gz

---
+++ packages/rhemati-firestore/rhemati-firestore-1.0.0.tar.gz/rhemati-firestore-1.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rhemati-firestore',
+    version='1.0.0',
+    description='CRUD Firestore python',
+    author='RHEMATI SpA',
+    author_email='contacto@rhemati.com',
+    url='https://rhemati.com/',
+    packages=find_packages(),
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
