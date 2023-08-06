e1e9cb9745b1a7df63bfb9546f13491b804979e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:55 +0000

    Add package allitnil/0.0.0/allitnil-0.0.0.tar.gz

---
+++ packages/allitnil/allitnil-0.0.0.tar.gz/allitnil-0.0.0/setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+# Third party modules
+from setuptools import setup
+
+setup(name='allitnil',
+      version='0.0.0',
+      description='place holder',
+      url='http://github.com/SK/allitnil',
+      license='MIT',
+      packages=['allitnil'],
+      zip_safe=False)
