7ac65371921dc76a0ae00e7e960fb5e2400bbe82
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:17 +0000

    Add package artistools/2023.5.16.3/artistools-2023.5.16.3.tar.gz

---
+++ packages/artistools/artistools-2023.5.16.3.tar.gz/artistools-2023.5.16.3/artistools/lightcurve/__main__.py
@@ -0,0 +1,9 @@
+import artistools.lightcurve.plotlightcurve
+
+
+def main() -> None:
+    artistools.lightcurve.plotlightcurve.main()
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/artistools/artistools-2023.5.16.3.tar.gz/artistools-2023.5.16.3/artistools/nltepops/__main__.py
@@ -0,0 +1,4 @@
+from .plotnltepops import main
+
+if __name__ == "__main__":
+    main()
