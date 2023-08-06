f3cb38503bfc5e080c4f6a59726b7b78fd2ccfdc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:24 +0000

    Add package ProtoMsg/1.0.3/ProtoMsg-1.0.3.tar.gz

---
+++ packages/ProtoMsg/ProtoMsg-1.0.3.tar.gz/ProtoMsg-1.0.3/ProtoMsg/__init__.py
@@ -0,0 +1,14 @@
+from __future__ import absolute_import
+
+name = "ProtoMsg"
+__title__ = 'scu yang proto msg'
+__author__ = 'loorr'
+__license__ = 'Apache License 2.0'
+__copyright__ = 'Copyright 2023 SCU MATH YANG and Contributors'
+
+from ProtoMsg import *
+
+__all__ = [
+    'ControlIn_pb2', 'ControlOut_pb2', 'SegmentCommon_pb2',
+    'SegmentOut_pb2', 'SegmentIn_pb2', 'KafkaMessage_pb2',
+]
