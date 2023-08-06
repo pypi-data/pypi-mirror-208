7b91554a01cef87f5af7137d932f52a3c228acde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:05 +0000

    Add package wwpdb.utils.emdb/1.3/wwpdb.utils.emdb-1.3.tar.gz

---
+++ packages/wwpdb.utils.emdb/wwpdb.utils.emdb-1.3.tar.gz/wwpdb.utils.emdb-1.3/wwpdb/utils/emdb/cif_emdb_translator/generatedsnamespaces.py
@@ -0,0 +1,9 @@
+# file: generatedsnamespaces.py
+
+GenerateDSNamespaceDefs = {
+    "entry_type": 'xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" '  
+                  'xsi:schemaLocation="https://github.com/emdb-empiar/emdb-schemas'
+                  '/blob/master/emdb_schemas/v3/v3_0_3_3/emdb.xsd" '
+                  'version="3.0.3.3"'
+}
+GenerateDSNamespaceTypePrefixes = {}
