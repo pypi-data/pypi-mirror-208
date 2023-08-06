00e40a598476e8d85b4add8b128ba3fda30ed3a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:10 +0000

    Add package pyNeuroML/1.0.7/pyNeuroML-1.0.7-py3-none-any.whl

---
+++ packages/pyNeuroML/pyNeuroML-1.0.7-py3-none-any.whl/pyneuroml/__init__.py
@@ -0,0 +1,23 @@
+import logging
+
+__version__ = "1.0.7"
+
+JNEUROML_VERSION = "0.12.2"
+
+# Define a logger for the package
+logging.basicConfig(
+    format="pyNeuroML >>> %(levelname)s - %(message)s",
+    level=logging.WARN,
+)
+
+
+def print_v(msg):
+    """Print message to console.
+
+    This is to be used for printing out messages during ordinary usage of the
+    tool. For status monitoring, fault investigation etc., use the logger.
+
+    :param msg: string to print
+    :type msg: str
+    """
+    print("pyNeuroML >>> " + msg)
---
+++ packages/pyNeuroML/pyNeuroML-1.0.7-py3-none-any.whl/pyneuroml/utils/__init__.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+"""
+The utils package contains various utility functions to aid users working with
+PyNeuroML
+
+Copyright 2023 NeuroML Contributors
+"""
+
+import typing
+import logging
+import re
+import neuroml
+
+
+logger = logging.getLogger(__name__)
+
+
+def extract_position_info(
+    nml_model: neuroml.NeuroMLDocument, verbose: bool = False
+) -> tuple:
+    """Extract position information from a NeuroML model
+
+    Returns a tuple of dictionaries:
+
+    - cell_id_vs_cell: dict(cell id, cell object)
+    - pop_id_vs_cell: dict(pop id, cell object)
+    - positions: dict(pop id, dict(cell id, position in x, y, z))
+    - pop_id_vs_color: dict(pop id, colour property)
+    - pop_id_vs_radii: dict(pop id, radius property)
+
+    :param nml_model: NeuroML2 model to extract position information from
+    :type nml_model: NeuroMLDocument
+    :param verbose: toggle function verbosity
+    :type verbose: bool
+    :returns: [cell id vs cell dict, pop id vs cell dict, positions dict, pop id vs colour dict, pop id vs radii dict]
+    :rtype: tuple of dicts
+    """
+
+    cell_id_vs_cell = {}
+    positions = {}
+    pop_id_vs_cell = {}
+    pop_id_vs_color = {}
+    pop_id_vs_radii = {}
+
+    cell_elements = []
+    cell_elements.extend(nml_model.cells)
+    cell_elements.extend(nml_model.cell2_ca_poolses)
+
+    for cell in cell_elements:
+        cell_id_vs_cell[cell.id] = cell
+
+    if len(nml_model.networks) > 0:
+        popElements = nml_model.networks[0].populations
+    else:
+        popElements = []
+        net = neuroml.Network(id="x")
+        nml_model.networks.append(net)
+        cell_str = ""
+        for cell in cell_elements:
+            pop = neuroml.Population(
+                id="dummy_population_%s" % cell.id, size=1, component=cell.id
+            )
+            net.populations.append(pop)
+            cell_str += cell.id + "__"
+        net.id = cell_str[:-2]
+
+        popElements = nml_model.networks[0].populations
+
+    for pop in popElements:
+        name = pop.id
+        celltype = pop.component
+        instances = pop.instances
+
+        if pop.component in cell_id_vs_cell.keys():
+            pop_id_vs_cell[pop.id] = cell_id_vs_cell[pop.component]
+        else:
+            pop_id_vs_cell[pop.id] = None
+
+        info = "Population: %s has %i positioned cells of type: %s" % (
+            name,
+            len(instances),
+            celltype,
+        )
+        if verbose:
+            print(info)
+
+        colour = "b"
+        substitute_radius = None
+
+        props = []
+        props.extend(pop.properties)
+        """ TODO
+        if pop.annotation:
+            props.extend(pop.annotation.properties)"""
+
+        for prop in props:
+            # print(prop)
+            if prop.tag == "color":
+                color = prop.value
+                color = (
+                    float(color.split(" ")[0]),
+                    float(color.split(" ")[1]),
+                    float(color.split(" ")[2]),
+                )
+
+                pop_id_vs_color[pop.id] = color
+                logger.debug(f"Colour determined to be: {color}")
+            if prop.tag == "radius":
+                substitute_radius = float(prop.value)
+                pop_id_vs_radii[pop.id] = substitute_radius
+
+        pop_positions = {}
+
+        if len(instances) > 0:
+            for instance in instances:
+                location = instance.location
+                id = int(instance.id)
+
+                x = float(location.x)
+                y = float(location.y)
+                z = float(location.z)
+                pop_positions[id] = (x, y, z)
+        else:
+            for id in range(pop.size):
+                pop_positions[id] = (0, 0, 0)
+
+        positions[name] = pop_positions
+
+    return cell_id_vs_cell, pop_id_vs_cell, positions, pop_id_vs_color, pop_id_vs_radii
+
+
+def convert_case(name):
+    """Converts from camelCase to under_score"""
+    s1 = re.sub("(.)([A-Z][a-z]+)", r"\1_\2", name)
+    return re.sub("([a-z0-9])([A-Z])", r"\1_\2", s1).lower()
