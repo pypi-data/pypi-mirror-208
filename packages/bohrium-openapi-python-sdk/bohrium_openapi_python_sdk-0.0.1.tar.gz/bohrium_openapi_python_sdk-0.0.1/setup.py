0498c4cebbf0d3673a05f97afaa037642b755f49
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:34 +0000

    Add package bohrium-openapi-python-sdk/0.0.1/bohrium_openapi_python_sdk-0.0.1.tar.gz

---
+++ packages/bohrium-openapi-python-sdk/bohrium_openapi_python_sdk-0.0.1.tar.gz/bohrium_openapi_python_sdk-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+import setuptools
+setup(
+    name="bohrium_openapi_python_sdk",
+    version="0.0.1",
+    author="dingzhaohan",
+    author_email="dingzh@dp.tech",
+    url="https://github.com/dingzhoahan",
+    description="bohrium openapi python sdk",
+    packages=setuptools.find_packages(),
+    install_requires=[],
+    python_requires='>=3.7'
+)
