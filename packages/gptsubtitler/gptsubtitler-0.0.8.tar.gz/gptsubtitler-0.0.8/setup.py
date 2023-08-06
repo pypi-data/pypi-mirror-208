6d47c36b05b7748b57ef61057a23e043c70ed374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:28 +0000

    Add package gptsubtitler/0.0.8/gptsubtitler-0.0.8.tar.gz

---
+++ packages/gptsubtitler/gptsubtitler-0.0.8.tar.gz/gptsubtitler-0.0.8/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+setup(
+    name="gptsubtitler",
+    version="0.0.8",
+    author="extremq",
+    author_email="extremqcontact@gmail.com",
+    description="Automatically subtitle any video spoken in any language to a language of your choice.",
+    install_requires=[
+        "transformers",
+        "openai-whisper",
+        "sentencepiece"
+    ],
+    packages=["gptsubtitler"],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    long_description=readme(),
+    long_description_content_type = "text/markdown"
+)
