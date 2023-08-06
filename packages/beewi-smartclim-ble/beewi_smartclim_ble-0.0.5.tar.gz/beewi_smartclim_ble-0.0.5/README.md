b23de288e5802bcb6bd83a7d9d4c8c295fb753ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:44 +0000

    Add package beewi-smartclim-ble/0.0.5/beewi_smartclim_ble-0.0.5.tar.gz

---
+++ packages/beewi-smartclim-ble/beewi_smartclim_ble-0.0.5.tar.gz/beewi_smartclim_ble-0.0.5/README.md
@@ -0,0 +1,2 @@
+# hassis_beewi_smartclim
+Home assistant plugin for the temperature and humidity sensor BeeWi SmartClim
---
+++ packages/beewi-smartclim-ble/beewi_smartclim_ble-0.0.5.tar.gz/beewi_smartclim_ble-0.0.5/src/smartclim_ble/__init__.py
@@ -0,0 +1,5 @@
+"""Module initialisation."""
+
+from .parser import BeeWiSmartClimAdvertisement
+
+__version__ = "0.0.5"
