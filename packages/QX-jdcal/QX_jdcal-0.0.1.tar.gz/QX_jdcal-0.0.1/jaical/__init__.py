c4c2dec60b889b910940d321b7d2dadea6fba211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:09 +0000

    Add package QX-jdcal/0.0.1/QX_jdcal-0.0.1.tar.gz

---
+++ packages/QX-jdcal/QX_jdcal-0.0.1.tar.gz/QX_jdcal-0.0.1/jaical/__init__.py
@@ -0,0 +1,18 @@
+import numpy as np
+def add_numbers(num1, num2):
+    return num1 + num2
+
+def subtract_numbers(num1, num2):
+    return num1 - num2
+
+def multiply_numbers(num1, num2):
+    return num1 * num2
+
+def divide_numbers(num1, num2):
+    return num1 / num2
+
+def deg_to_Rad(num1):
+    return np.deg2rad(num1)
+
+def power(num1,num2):
+    return np.power(num1,num2)
---
+++ packages/QX-jdcal/QX_jdcal-0.0.1.tar.gz/QX_jdcal-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='QX_jdcal',
+  version='0.0.1',
+  description='A very basic calculator',
+  url='',  
+  author='jaideep ',
+  author_email='jaideepkaushal2@gmail.com',
+  license='MIT', 
+  keywords='calculator', 
+  packages=find_packages(),
+  install_requires=['numpy'] 
+)
