eadb654f9c34a7d51607767fc59b58f2abcfcec3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:02 +0000

    Add package choclo/0.1.0/choclo-0.1.0-py3-none-any.whl

---
+++ packages/choclo/choclo-0.1.0-py3-none-any.whl/choclo/point/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2022 The Choclo Developers.
+# Distributed under the terms of the BSD 3-Clause License.
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# This code is part of the Fatiando a Terra project (https://www.fatiando.org)
+#
+"""
+Kernels and forward modelling functions for point sources
+"""
+from ._forward import (
+    gravity_e,
+    gravity_ee,
+    gravity_en,
+    gravity_eu,
+    gravity_n,
+    gravity_nn,
+    gravity_nu,
+    gravity_pot,
+    gravity_u,
+    gravity_uu,
+)
