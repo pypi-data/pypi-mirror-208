75039184031427e3900d651a84daf0378048d9a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:46 +0000

    Add package agri-sense/1.5/agri_sense-1.5.tar.gz

---
+++ packages/agri-sense/agri_sense-1.5.tar.gz/agri_sense-1.5/agri_sense/__init__.py
@@ -0,0 +1,4 @@
+import sys
+import os
+sys.path.append(os.path.realpath(__file__))
+from data.FarmData import FarmData
---
+++ packages/agri-sense/agri_sense-1.5.tar.gz/agri_sense-1.5/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='agri_sense',
+    version='1.5',
+    description='A package for agricultural sensing in India',
+    packages=['agri_sense'],
+    install_requires=[
+     # list your package dependencies here
+    'numpy',
+    'pandas',
+    'geopandas',
+    'matplotlib',
+    'descartes',
+    'rasterio',
+    'Pillow',
+    ]
+    )
