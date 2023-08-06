497a807d103d75aeecd98bfb513448ee6db44ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:31:21 +0000

    Add package geotribu/0.13.0/geotribu-0.13.0-py3-none-any.whl

---
+++ packages/geotribu/geotribu-0.13.0-py3-none-any.whl/geotribu_cli/cli.py
@@ -0,0 +1,169 @@
+#! python3  # noqa: E265
+
+"""Main CLI entrypoint."""
+
+# standard lib
+import argparse
+import logging
+
+from rich_argparse import RawDescriptionRichHelpFormatter
+
+# package
+from geotribu_cli.__about__ import (
+    __author__,
+    __cli_usage__,
+    __summary__,
+    __title__,
+    __title_clean__,
+    __uri_homepage__,
+    __version__,
+)
+from geotribu_cli.subcommands import (
+    parser_latest_content,
+    parser_search_content,
+    parser_search_image,
+    parser_upgrade,
+)
+
+RawDescriptionRichHelpFormatter.usage_markup = True
+
+
+# ############################################################################
+# ########## MAIN ################
+# ################################
+
+
+# this serves as a parent parser
+def add_common_arguments(parser_to_update: argparse.ArgumentParser):
+    """Apply common argument to an existing parser.
+
+    Args:
+        parser_to_update (argparse.ArgumentParser): _description_
+
+    Returns:
+        argparse.ArgumentParser: _description_
+    """
+    parser_to_update.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=1,
+        dest="verbosity",
+        help="Niveau de verbosité : None = WARNING, -v = INFO, -vv = DEBUG",
+    )
+    return parser_to_update
+
+
+def main(args: list[str] = None):
+    """Main CLI entrypoint.
+
+    Args:
+        args (List[str], optional): list of command-line arguments. Defaults to None.
+    """
+    # create the top-level parser
+    main_parser = argparse.ArgumentParser(
+        formatter_class=RawDescriptionRichHelpFormatter,
+        epilog=f"{__cli_usage__}\n\n"
+        f"Développé par {__author__}\n"
+        f"Documentation : {__uri_homepage__}",
+        description=f"{__title__} {__version__} - {__summary__}",
+    )
+
+    # -- ROOT ARGUMENTS --
+
+    # Optional verbosity counter (eg. -v, -vv, -vvv, etc.)
+    main_parser.add_argument(
+        "-v",
+        "--verbose",
+        action="count",
+        default=1,
+        dest="verbosity",
+        help="Niveau de verbosité : None = WARNING, -v = INFO, -vv = DEBUG",
+    )
+
+    main_parser.add_argument(
+        "--version",
+        action="version",
+        version=__version__,
+        help="Affiche la version du CLI",
+    )
+
+    # -- SUB-COMMANDS --
+    subparsers = main_parser.add_subparsers(title="Sous-commandes", dest="command")
+
+    # Latest Content
+    subcmd_latest_content = subparsers.add_parser(
+        "read-latest",
+        aliases=["latest", "rss"],
+        help="Consulter les derniers contenus du site",
+        formatter_class=main_parser.formatter_class,
+        prog="read-latest",
+    )
+    add_common_arguments(subcmd_latest_content)
+    parser_latest_content(subcmd_latest_content)
+
+    # Search Content
+    subcmd_search_content = subparsers.add_parser(
+        "search-content",
+        aliases=["contenus"],
+        help="Rechercher dans les contenus du site",
+        formatter_class=main_parser.formatter_class,
+        prog="search-content",
+    )
+    add_common_arguments(subcmd_search_content)
+    parser_search_content(subcmd_search_content)
+
+    # Search Image
+    subcmd_search_image = subparsers.add_parser(
+        "search-image",
+        aliases=["images"],
+        help="Rechercher dans les images du CDN",
+        formatter_class=main_parser.formatter_class,
+        prog="search-image",
+    )
+    add_common_arguments(subcmd_search_image)
+    parser_search_image(subcmd_search_image)
+
+    # Upgrader
+    subcmd_upgrade = subparsers.add_parser(
+        "upgrade",
+        aliases=["auto-update", "maj", "update"],
+        help="Mettre à jour Geotribu CLI.",
+        formatter_class=main_parser.formatter_class,
+        prog="upgrade",
+    )
+    add_common_arguments(subcmd_upgrade)
+    parser_upgrade(subcmd_upgrade)
+
+    # just get passed args
+    args = main_parser.parse_args(args)
+
+    # set log level depending on verbosity argument
+    if 0 < args.verbosity < 4:
+        args.verbosity = 40 - (10 * args.verbosity)
+    elif args.verbosity >= 4:
+        # debug is the limit
+        args.verbosity = 40 - (10 * 3)
+    else:
+        args.verbosity = 0
+
+    logging.basicConfig(
+        level=args.verbosity,
+        format="%(asctime)s||%(levelname)s||%(module)s||%(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    console = logging.StreamHandler()
+    console.setLevel(args.verbosity)
+
+    # add the handler to the root logger
+    logger = logging.getLogger(__title_clean__)
+    logger.debug(f"Log level set: {logging.getLevelName(args.verbosity)}")
+
+    # -- RUN LOGIC --
+    args.func(args)
+
+
+# -- Stand alone execution
+if __name__ == "__main__":
+    main()  # required by unittest
---
+++ packages/geotribu/geotribu-0.13.0-py3-none-any.whl/geotribu_cli/subcommands/search_image.py
@@ -0,0 +1,264 @@
+#! python3  # noqa: E265
+
+# ############################################################################
+# ########## IMPORTS #############
+# ################################
+
+# standard library
+import argparse
+import logging
+import sys
+from pathlib import Path
+
+# 3rd party
+import orjson
+from lunr.index import Index
+from rich import print
+from rich.table import Table
+
+# package
+from geotribu_cli.__about__ import __title__, __version__
+from geotribu_cli.constants import GeotribuDefaults
+from geotribu_cli.utils.file_downloader import download_remote_file_to_local
+from geotribu_cli.utils.formatters import convert_octets
+
+# ############################################################################
+# ########## GLOBALS #############
+# ################################
+
+logger = logging.getLogger(__name__)
+defaults_settings = GeotribuDefaults()
+
+# ############################################################################
+# ########## FUNCTIONS ###########
+# ################################
+
+
+def format_output_result(
+    result: list[dict], search_term: str = None, format_type: str = None, count: int = 5
+) -> str:
+    """Format result according to output option.
+
+    Args:
+        result (list[dict]): result to format
+        search_term (str, optional): term used for search. Defaults to None.
+        format_type (str, optional): format output option. Defaults to None.
+        count (int, optional): default number of results to display. Defaults to 5.
+
+    Returns:
+        str: formatted result ready to print
+    """
+
+    if format_type == "table":
+        table = Table(
+            title=f"Recherche d'images - {len(result)} résultats "
+            f"avec le terme : {search_term}",
+            show_lines=True,
+            highlight=True,
+            caption=f"{__title__} {__version__}",
+        )
+
+        # columns
+        table.add_column(header="Nom", justify="left", style="default")
+        table.add_column(header="Dimensions", justify="center", style="bright_black")
+        table.add_column(header="Score", style="magenta")
+        table.add_column(header="URL", justify="right", style="blue")
+
+        # iterate over results
+        for r in result[:count]:
+            table.add_row(
+                r.get("nom"),
+                r.get("dimensions"),
+                r.get("score"),
+                r.get("url"),
+            )
+
+        return table
+    else:
+        return result
+
+
+# ############################################################################
+# ########## CLI #################
+# ################################
+
+
+def parser_search_image(subparser: argparse.ArgumentParser) -> argparse.ArgumentParser:
+    """Set the argument parser for search-image subcommand.
+
+    Args:
+        subparser (argparse.ArgumentParser): parser to set up
+
+    Returns:
+        argparse.ArgumentParser: parser ready to use
+    """
+
+    subparser.add_argument(
+        "search_term",
+        help="Terme de recherche.",
+        type=str,
+        metavar="search-term",
+    )
+
+    subparser.add_argument(
+        "-r",
+        "--remote-index-file",
+        help="Emplacement du fichier distant.",
+        default=defaults_settings.cdn_search_index_full_url,
+        type=str,
+        dest="remote_index_file",
+    )
+
+    subparser.add_argument(
+        "-l",
+        "--local-index-file",
+        help="Emplacement du fichier local.",
+        default=Path().home() / ".geotribu/search/cdn_search_index.json",
+        type=Path,
+        dest="local_index_file",
+    )
+
+    subparser.add_argument(
+        "-f",
+        "--filter-type",
+        choices=["logo", "geoicone"],
+        default=None,
+        help="Filtrer sur un type d'images en particulier.",
+        dest="filter_type",
+    )
+
+    subparser.add_argument(
+        "-n",
+        "--results-number",
+        type=int,
+        default=5,
+        help="Nombre de résultats à retourner.",
+        dest="results_number",
+    )
+
+    subparser.add_argument(
+        "-x",
+        "--expiration-rotating-hours",
+        help="Nombre d'heures à partir duquel considérer le fichier local comme périmé.",
+        default=24,
+        type=int,
+        dest="expiration_rotating_hours",
+    )
+
+    subparser.add_argument(
+        "-o",
+        "--format-output",
+        choices=[
+            "table",
+        ],
+        default=None,
+        help="Format de sortie.",
+        dest="format_output",
+    )
+
+    subparser.set_defaults(func=run)
+
+    return subparser
+
+
+# ############################################################################
+# ########## MAIN ################
+# ################################
+
+
+def run(args: argparse.Namespace):
+    """Run the sub command logic.
+
+    Perform a search on images stored on the Geotribu pseudo-CDN \
+        (<https://cdn.geotribu.fr/>).
+
+    Args:
+        args (argparse.Namespace): arguments passed to the subcommand
+    """
+    logger.debug(f"Running {args.command} with {args}")
+
+    args.local_index_file.parent.mkdir(parents=True, exist_ok=True)
+
+    # get local search index
+    get_or_update_local_search_index = download_remote_file_to_local(
+        remote_url_to_download=args.remote_index_file,
+        local_file_path=args.local_index_file,
+        expiration_rotating_hours=args.expiration_rotating_hours,
+    )
+    if not isinstance(get_or_update_local_search_index, Path):
+        logger.error(
+            f"Le téléchargement du fichier distant {args.remote_index_file} "
+            f"ou la récupération du fichier local {args.local_index_file} a échoué."
+        )
+        if isinstance(get_or_update_local_search_index, Exception):
+            logger.error(get_or_update_local_search_index)
+        sys.exit()
+    logger.info(
+        f"Local index file: {args.local_index_file}, "
+        f"{convert_octets(args.local_index_file.stat().st_size)}"
+    )
+
+    # load the local index file
+    if not args.local_index_file.exists():
+        logger.error(f"{args.local_index_file.resolve()} does not exist")
+        sys.exit(f"{args.local_index_file.resolve()} does not exist")
+    # loads it
+    with args.local_index_file.open("rb") as fd:
+        serialized_idx = orjson.loads(fd.read())
+
+    # charge l'index sérialisé
+    idx = Index.load(serialized_idx.get("index"))
+    images_dict = serialized_idx.get("images")
+
+    # recherche
+    search_results: list[dict] = idx.search(f"*{args.search_term}*")
+
+    # résultats : enrichissement et filtre
+    final_results = []
+
+    for result in search_results:
+        # filter on image type
+        if args.filter_type == "logo" and not result.get("ref").startswith(
+            "logos-icones/"
+        ):
+            logger.debug(
+                f"Résultat ignoré par le filtre {args.filter_type}: {result.get('ref')}"
+            )
+            continue
+        elif args.filter_type == "geoicone" and not result.get("ref").startswith(
+            "internal/icons-rdp-news/"
+        ):
+            logger.debug(
+                f"Résultat ignoré par le filtre {args.filter_type}: {result.get('ref')}"
+            )
+            continue
+        else:
+            pass
+
+        mapped_img = images_dict.get(result.get("ref"))
+
+        # crée un résultat de sortie
+        out_result = {
+            "nom": result.get("ref").split("/")[-1],
+            "dimensions": f"{mapped_img[0]}x{mapped_img[1]}",
+            "score": f"{result.get('score'):.3}",
+            "url": f"{defaults_settings.cdn_base_url}"
+            f"{defaults_settings.cdn_base_path}/"
+            f"{result.get('ref')}",
+        }
+
+        final_results.append(out_result)
+
+    # formatage de la sortie
+    print(
+        format_output_result(
+            result=final_results,
+            format_type=args.format_output,
+            count=args.results_number,
+        )
+    )
+
+
+# -- Stand alone execution
+if __name__ == "__main__":
+    pass
