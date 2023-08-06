4a1740384e85e2dd1a9cfbab312d37f69c5049c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:47 +0000

    Add package rebuff/0.0.4/rebuff-0.0.4.tar.gz

---
+++ packages/rebuff/rebuff-0.0.4.tar.gz/rebuff-0.0.4/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="rebuff",
+    version="0.0.4",
+    packages=find_packages(),
+    install_requires=["pydantic>=1", "requests<3,>=2"],
+    extras_require={
+        "dev": [
+            "pytest",
+            "black>=23.0,<24",
+            "flake8>=6.0,<7",
+            "isort>=5.0,<6",
+            "mypy>=1.0,<2",
+            "langchain>=0.0.100",
+            "openai>=0.27.2"
+        ],
+    },
+    test_suite="tests",
+)
