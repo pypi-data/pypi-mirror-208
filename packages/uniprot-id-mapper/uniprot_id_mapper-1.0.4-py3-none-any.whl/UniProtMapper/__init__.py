449052ba05e9398c7b06e71c6ab3b90dbbe89741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:14:38 +0000

    Add package uniprot-id-mapper/1.0.4/uniprot_id_mapper-1.0.4-py3-none-any.whl

---
+++ packages/uniprot-id-mapper/uniprot_id_mapper-1.0.4-py3-none-any.whl/UniProtMapper/__init__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""A Python wrapper for the UniProt Mapping RESTful API."""
+
+from .field_retriever_api import UniProtRetriever  # noqa: F401
+from .id_mapping_api import UniProtIDMapper  # noqa: F401
+from .interface import abc_UniProtAPI  # noqa: F401
+from .swiss_parser import SwissProtParser  # noqa: F401
+
+__version__ = "1.0.4"
