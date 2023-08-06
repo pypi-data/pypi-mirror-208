539f6f340b50e45d387978ffe8b99dac0fc127d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:04 +0000

    Add package MosaBay/0.0.1/MosaBay-0.0.1-py3-none-any.whl

---
+++ packages/MosaBay/MosaBay-0.0.1-py3-none-any.whl/MosaBay/MosaBay.py
@@ -0,0 +1,22 @@
+import argparse
+
+from . import example
+from . import __VERSION__
+
+def main():
+    print("It begins")
+    parser = argparse.ArgumentParser(description="MosaBay")
+    parser.add_argument(
+        "-I",
+        "--inputs",
+        type=int,
+        default=1,
+        help="inputs (default: 1)",
+    )
+    args = parser.parse_args()
+    res=example.add_one(args.inputs)
+    print(res)
+    print("It ends")
+
+if __name__ == "__main__":
+    main()
