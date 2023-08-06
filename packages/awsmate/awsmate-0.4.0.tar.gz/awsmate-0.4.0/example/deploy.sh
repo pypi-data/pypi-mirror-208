8f3f2addec2c18dcc1a67575bf29b84abd608e17
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:18 +0000

    Add package awsmate/0.4.0/awsmate-0.4.0.tar.gz

---
+++ packages/awsmate/awsmate-0.4.0.tar.gz/awsmate-0.4.0/example/deploy.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+. ./check_terraform
+
+cd tf
+
+../${BUILD_DIR}/${TERRAFORM_EXECUTABLE} init
+../${BUILD_DIR}/${TERRAFORM_EXECUTABLE} apply -auto-approve -var-file="../variables.tfvars"
+
+cd ..
---
+++ packages/awsmate/awsmate-0.4.0.tar.gz/awsmate-0.4.0/example/variables.tfvars
@@ -0,0 +1,3 @@
+# AWS Lambda
+lambda_runtime = "python3.9"
+lambda_entry_point = "lambda_handler"
