549c29e308fd11dea34bb6c5c3f708f8450dc324
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:57:49 +0000

    Add package macrostrat-package-tools/1.0.0/macrostrat_package_tools-1.0.0.tar.gz

---
+++ packages/macrostrat-package-tools/macrostrat_package_tools-1.0.0.tar.gz/macrostrat_package_tools-1.0.0/macrostrat/package_tools/__init__.py
@@ -0,0 +1,7 @@
+from typer import Typer
+from .install import install_packages
+from .publish import publish_packages
+
+mono = Typer(no_args_is_help=True)
+mono.command(name="install")(install_packages)
+mono.command(name="publish")(publish_packages)
