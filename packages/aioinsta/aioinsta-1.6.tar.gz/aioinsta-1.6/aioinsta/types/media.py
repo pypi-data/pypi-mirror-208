963e7e2579b9cca1ac1c927fa6f8cafc55cf2d81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:19 +0000

    Add package aioinsta/1.6/aioinsta-1.6.tar.gz

---
+++ packages/aioinsta/aioinsta-1.6.tar.gz/aioinsta-1.6/aioinsta/types/media.py
@@ -0,0 +1,43 @@
+from datetime import datetime
+from enum import Enum
+
+from pydantic import BaseModel, HttpUrl
+
+from aioinsta.types.user import UserShort
+
+
+class MediaType(str, Enum):
+    Video = "video"
+    Photo = "photo"
+    Album = "album"
+
+
+class Video(BaseModel):
+    url: HttpUrl
+    preview: HttpUrl
+    view_count: int | None = 0
+    duration: float | None = 0.0
+
+
+class Photo(BaseModel):
+    url: HttpUrl
+
+
+class Resource(BaseModel):
+    pk: str
+    media_type: MediaType
+    photo: Photo | None = None
+    video: Video | None = None
+
+
+class Media(BaseModel):
+    pk: str
+    id: str
+    code: str
+    user: UserShort
+    caption: str | None
+    taken_at: datetime
+    media_type: MediaType
+    photo: Photo | None = None
+    video: Video | None = None
+    resource: list[Resource]
