4f65f9acec0739efec78043f83aaf84b5be4934a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:35 +0000

    Add package clinicadl/1.3.1/clinicadl-1.3.1.tar.gz

---
+++ packages/clinicadl/clinicadl-1.3.1.tar.gz/clinicadl-1.3.1/clinicadl/utils/network/__init__.py
@@ -0,0 +1,16 @@
+from .autoencoder.models import AE_Conv4_FC3, AE_Conv5_FC3
+from .cnn.models import (
+    Conv4_FC3,
+    Conv5_FC3,
+    ResNet3D,
+    SqueezeExcitationCNN,
+    Stride_Conv5_FC3,
+    resnet18,
+)
+from .cnn.random import RandomArchitecture
+from .vae.vanilla_vae import (
+    Vanilla3DdenseVAE,
+    Vanilla3DVAE,
+    VanillaDenseVAE,
+    VanillaSpatialVAE,
+)
