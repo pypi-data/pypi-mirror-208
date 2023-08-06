5e32c3d650c84d2cf6fe1da693de04970add4ede
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:05 +0000

    Add package pyirbrain/1.3/pyirbrain-1.3.tar.gz

---
+++ packages/pyirbrain/pyirbrain-1.3.tar.gz/pyirbrain-1.3/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "pyirbrain",
+    version = "1.3", 
+    description = "Library for IR-Brain Products",
+    author = "IR-Brain",
+    author_email = "ceo@ir-brain.com",
+    url = "http://www.ir-brain.com",
+    packages = ['pyirbrain', 
+        ],
+    install_requires = [
+        'pyserial>=3.4',
+        'pynput>=1.7.3',
+        ],
+)
