39b1e04633878f66c50bba5a9a2c58177b87df58
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:12 +0000

    Add package kwamehealthtoolboxpublic/1.3.1/kwamehealthtoolboxpublic-1.3.1.tar.gz

---
+++ packages/kwamehealthtoolboxpublic/kwamehealthtoolboxpublic-1.3.1.tar.gz/kwamehealthtoolboxpublic-1.3.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages, __version__
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="kwamehealthtoolboxpublic",
+    version="1.3.1",
+    author="Panashe Madzudzo",
+    author_email="admin@kwame.health",
+    description="Toolbox with custom functions",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Kwame-Health/khtoolbox",
+    project_urls = {
+        "Bug Tracker": "https://github.com/Kwame-Health/khtoolbox/issues"
+    },
+    packages=["kwamehealthtoolboxpublic"],
+    install_requires=["requests", "urllib3", "twilio", "pytz", "boto3", "convertapi"],
+)
