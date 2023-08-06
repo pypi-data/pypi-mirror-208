1960d4dfb8ba5ce4a1a0a05648ba5d1cd4a7c531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:49 +0000

    Add package mml-qae/1.0.0/mml_qae-1.0.0.tar.gz

---
+++ packages/mml-qae/mml_qae-1.0.0.tar.gz/mml_qae-1.0.0/setup.py
@@ -0,0 +1,10 @@
+import setuptools 
+setuptools.setup(
+      name='mml_qae',
+      version='1.0.0',
+      description='The packages for the machine learning(just easily learn!) are always hard to study, and difficult to use. Now you have the MyMachineLearning_Quicker_and_Easier! It can maybe help you to use machine learning.',
+      author='ETRO_secondleader',
+      author_email='ETRO_gfyx@163.com',
+      url='https://www.python.org',
+      packages=setuptools.find_packages(),
+     ) 
