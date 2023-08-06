dbc46430547340377b88968b48b9ba4f4620ddab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:29 +0000

    Add package pasec/1.2.3/pasec-1.2.3.tar.gz

---
+++ packages/pasec/pasec-1.2.3.tar.gz/pasec-1.2.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '1.2.3'
+DESCRIPTION = 'A password generator using passphrase'
+LONG_DESCRIPTION = 'A package that allows user to generate password from given passphrase .'
+
+# Setting up
+setup(
+    name="pasec",
+    readme="README.md",
+    version='1.2.3',
+    author="ssuroj",
+    keywords=['python', 'password', 'pasec'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
