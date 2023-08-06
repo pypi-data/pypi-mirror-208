55602fc3e95d882a7be474e1155a18e0bb3a294c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:24 +0000

    Add package twitter-api-py/0.6.2/twitter_api_py-0.6.2-py3-none-any.whl

---
+++ packages/twitter-api-py/twitter_api_py-0.6.2-py3-none-any.whl/twitter_api/types/v2_place/place.py
@@ -0,0 +1,29 @@
+from typing import Optional, Union
+
+from typing_extensions import Any
+
+from twitter_api.types.extra_permissive_model import ExtraPermissiveModel
+from twitter_api.types.v2_geo.geo import Geo
+from twitter_api.types.v2_place.place_country_code import PlaceCountryCode
+from twitter_api.types.v2_place.place_name import PlaceName
+
+from .place_full_name import PlaceFullName
+from .place_id import PlaceId
+
+
+class Place(ExtraPermissiveModel):
+    """
+    場所情報。
+
+    refer: https://developer.twitter.com/en/docs/twitter-api/data-dictionary/object-model/place
+    """
+
+    id: PlaceId
+    name: PlaceName
+    full_name: PlaceFullName
+    contained_within: Optional[list[Any]] = None
+    country: Optional[str] = None
+    # NOTE: 公式ではない国のコードを返す可能性があるため、 str も受け入れておく。
+    country_code: Optional[Union[PlaceCountryCode, str]] = None
+    geo: Optional[Geo] = None
+    place_type: Optional[str] = None
