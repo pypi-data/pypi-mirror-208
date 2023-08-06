76947912c4c4d0c6eab1d5a7edcb1889f83fff46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:57 +0000

    Add package yokkaichi/1.4.3/yokkaichi-1.4.3.tar.gz

---
+++ packages/yokkaichi/yokkaichi-1.4.3.tar.gz/yokkaichi-1.4.3/setup.py
@@ -0,0 +1,46 @@
+from yokkaichi import __version__ as yokkaichi_ver
+from setuptools import setup
+from pathlib import Path
+import sys
+
+# Load README from README.md
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+# Enforce Python version (3.7+)
+if sys.version_info[1] < 7:
+    sys.exit(
+        "Yokkaichi will NOT run on Python 3.6 and older. You can build the package yourself and remove the check, but don't report bugs that happen!"
+    )
+
+setup(
+    name="yokkaichi",
+    version=yokkaichi_ver,
+    description="Shodan-like server scanner for Minecraft (formely mcserverscanner)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Oreeeee",
+    license="MIT",
+    url="https://github.com/Oreeeee/yokkaichi",
+    install_requires=[
+        "rich",
+        "mcstatus",
+        "ip2location",
+        "python-masscan",
+        "requests",
+        "tomli >= 1.1.0",
+    ],
+    classifiers=[
+        "Environment :: Console",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: Implementation :: CPython",
+    ],
+    packages=["yokkaichi", "yokkaichi.constants", "yokkaichi.structs"],
+)
