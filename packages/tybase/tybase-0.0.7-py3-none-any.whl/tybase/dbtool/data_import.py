da90f37e3949e216f6729a732be1568d144b281f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:02 +0000

    Add package tybase/0.0.7/tybase-0.0.7-py3-none-any.whl

---
+++ packages/tybase/tybase-0.0.7-py3-none-any.whl/tybase/dbtool/data_import.py
@@ -0,0 +1,127 @@
+from pandas import read_csv, to_datetime
+
+
+class DataFrameProcessor:
+    """
+    DataFrameProcessor 类设计用于根据特定规则处理 CSV 文件中的数据。
+
+    属性:
+        config_df: 一个 pandas DataFrame 对象，用于存储配置数据。
+
+    使用方法:
+        processor = DataFrameProcessor("config.csv")
+        df = processor.process_csv("jihua_20230505-20230511_47190093_861922.csv", {"注册人数(转化时间)":"注册数"})
+    使用场景:
+        对CSV文件需要数据的时候,里面封装了一些处理逻辑
+    """
+
+    def __init__(self, config_file_path):
+        """使用配置数据初始化 DataFrameProcessor 类。"""
+        self.config_df = read_csv(config_file_path)
+
+    def _extract_app_name(self, account):
+        """
+        私有辅助方法，根据账户名称提取对应的应用名称。
+
+        参数:
+            account: 字符串类型，账户名称。
+
+        返回:
+            字符串类型，对应的应用名称。
+        """
+        return self.config_df[self.config_df.账户名称 == account].loc[0]["推广APP"]
+
+    @staticmethod
+    def _percentage_to_float(percentage):
+        """
+        私有辅助方法，将百分比字符串转化为浮点数。
+
+        参数:
+            percentage: 字符串类型，百分比字符串。
+
+        返回:
+            浮点数类型，百分比浮点数。
+        """
+        percentage = percentage.replace("%", "")
+        return round(float(percentage) / 100, 3)
+
+    @staticmethod
+    def _identify_operating_system(plan):
+        """
+        私有辅助方法，根据推广计划识别操作系统。
+
+        参数:
+            plan: 字符串类型，推广计划。
+
+        返回:
+            字符串类型，识别出的操作系统。
+        """
+        if "IOS" in plan:
+            return "IOS"
+        else:
+            return "安卓"
+
+    @staticmethod
+    def _rename_columns(df, column_mapping):
+        """
+        私有辅助方法，根据提供的映射关系重命名 DataFrame 的列。
+
+        参数:
+            df: pandas DataFrame 对象。
+            column_mapping: 字典类型，列名的映射关系。
+
+        返回:
+            pandas DataFrame 对象。
+        """
+        return df.rename(columns=column_mapping)
+
+
+class BaiduImport(DataFrameProcessor):
+    """
+    BaiduImport 类设计用于处理百度推广的 CSV 文件。
+
+    属性:
+        config_df: 一个 pandas DataFrame 对象，用于存储配置数据。
+        configh.csv的字段:
+        账户名称,推广APP,返点,代理商,密码
+        账户名称,推广的APP产品,返点,代理渠道,密码
+        (密码不一定需要,后期考虑到自动化分析的时候会用到)
+    使用方法:
+        processor = BaiduImport("config.csv")
+        df = processor.process_csv("file_path.csv", {"注册人数(转化时间)":"注册数"})
+    """
+
+    def __init__(self, config_file_path):
+        """使用配置数据初始化 BaiduImport 类。"""
+        super().__init__(config_file_path)
+
+    def process_plan_csv(self, file_path, column_mapping=None):
+        """
+        处理百度搜索计划导入,这里封装的一般流程,如果需要分析的话,需要把这里字符串的处理逻辑改一下
+         CSV 文件并返回一个 pandas DataFrame 对象。
+
+        参数:
+            file_path: 字符串类型，CSV 文件的路径。
+            column_mapping: 字典类型，列名的映射关系。
+
+        返回:
+            pandas DataFrame 对象。
+        """
+        df = read_csv(file_path, encoding='gbk', skiprows=5)
+        df["APP"] = df["账户"].apply(self._extract_app_name)
+        df["上方展现胜出率"] = df["上方展现胜出率"].str.replace("-", "0")
+        df["上方展现胜出率"] = df["上方展现胜出率"].apply(self._percentage_to_float)
+        df["点击率"] = df["点击率"].apply(self._percentage_to_float)
+        df['日期'] = to_datetime(df['日期'])
+        df["操作系统"] = df["推广计划"].apply(self._identify_operating_system)
+        df["推广计划ID"] = df["推广计划ID"].astype(str)
+
+        # 如果提供了列名映射关系，则重命名列名
+        if column_mapping is not None:
+            df = self._rename_columns(df, column_mapping)
+
+        return df
+
+
+if __name__ == '__main__':
+    pass
