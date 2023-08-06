b9009844e696344617f3916388c30f0fa758adde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:39:48 +0000

    Add package weiming-test/1.0.1/weiming-test-1.0.1.tar.gz

---
+++ packages/weiming-test/weiming-test-1.0.1.tar.gz/weiming-test-1.0.1/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="weiming-test",
+    version="1.0.1",
+    author="weiming.li",
+    author_email="silang1225@126.com",
+    description="This is a test",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://upload.pypi.org/legacy",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+)
