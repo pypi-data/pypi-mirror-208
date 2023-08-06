cbd73d0ab09741f61fb5d87ae99f49c0163d6bec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:59 +0000

    Add package neural-admixture/1.2.3/neural-admixture-1.2.3.tar.gz

---
+++ packages/neural-admixture/neural-admixture-1.2.3.tar.gz/neural-admixture-1.2.3/neural_admixture/entry.py
@@ -0,0 +1,20 @@
+import logging
+import sys
+from ._version import __version__
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+log = logging.getLogger(__name__)
+
+def main():
+    # nadm_version = version("neural-admixture")
+    log.info(f"Neural ADMIXTURE - Version {__version__}")
+    arg_list = tuple(sys.argv)
+    assert len(arg_list) > 1, 'Please provide either the argument "train" or "infer" to choose running mode.'
+    if sys.argv[1] == 'train':
+        from .src import train
+        sys.exit(train.main(arg_list[2:]))
+    if sys.argv[1] == 'infer':
+        from .src import inference
+        sys.exit(inference.main(arg_list[2:]))
+    log.error(f'Invalid argument {arg_list[1]}. Please run either "neural-admixture train" or "neural-admixture infer"')
+    sys.exit(1)
