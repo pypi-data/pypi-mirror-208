c98c0fad34a4f019934823f33c9b915e7f6e5be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:14:55 +0000

    Add package m1-api-wrapper/2.1.1/m1_api_wrapper-2.1.1.tar.gz

---
+++ packages/m1-api-wrapper/m1_api_wrapper-2.1.1.tar.gz/m1_api_wrapper-2.1.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='m1_api_wrapper',
+    version='2.1.1',
+    description='Molecule One API Wrapper',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author='Szymon Pilkowski',
+    author_email='szymon.pilkowski@molecule.one',
+    url='https://github.com/molecule-one/m1wrapper-python',
+    license=license,
+    packages=['m1wrapper'],
+    install_requires=[
+        'requests'
+    ] 
+)
+
