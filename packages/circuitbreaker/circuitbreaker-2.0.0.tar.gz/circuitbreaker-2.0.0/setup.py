50bd5ffc36387e76a2f0893023774c2aea28f750
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:39 +0000

    Add package circuitbreaker/2.0.0/circuitbreaker-2.0.0.tar.gz

---
+++ packages/circuitbreaker/circuitbreaker-2.0.0.tar.gz/circuitbreaker-2.0.0/setup.py
@@ -0,0 +1,36 @@
+"""
+Python Circuit Breaker
+"""
+from setuptools import setup
+
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+
+setup(
+    name='circuitbreaker',
+    version='2.0.0',
+    url='https://github.com/fabfuel/circuitbreaker',
+    download_url='https://github.com/fabfuel/circuitbreaker/archive/2.0.0.tar.gz',
+    license='BSD-3-Clause',
+    author='Fabian Fuelling',
+    author_email='pypi@fabfuel.de',
+    description='Python Circuit Breaker pattern implementation',
+    long_description=readme(),
+    py_modules=['circuitbreaker'],
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX',
+        'Operating System :: MacOS',
+        'Operating System :: Unix',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+    ],
+    options={'bdist_wheel': {'universal': True}}
+)
