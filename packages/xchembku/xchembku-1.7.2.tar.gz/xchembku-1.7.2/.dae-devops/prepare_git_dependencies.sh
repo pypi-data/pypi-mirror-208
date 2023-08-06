8b9061540be5aff2446656e45a63b758eae0615e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:07 +0000

    Add package xchembku/1.7.2/xchembku-1.7.2.tar.gz

---
+++ packages/xchembku/xchembku-1.7.2.tar.gz/xchembku-1.7.2/.dae-devops/prepare_git_dependencies.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# ********** Please don't edit this file!
+# ********** It has been generated automatically by dae_devops version 0.5.2.
+# ********** For repository_name xchembku
+
+me=${BASH_SOURCE}
+echo "${me}: installing 0 dependencies for repository_name xchembku"
+
+function __install {
+    echo ${me}: "$@"
+    "$@"
+}
+
+
+# dae_devops_fingerprint e0ae8f2ac0e475defc383c2fc1c4507a
