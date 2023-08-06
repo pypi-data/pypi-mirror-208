c2ff92466e7356b08c7ec6ecddb2c359b76f766f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:13 +0000

    Add package diskq/1.0.0/diskq-1.0.0.tar.gz

---
+++ packages/diskq/diskq-1.0.0.tar.gz/diskq-1.0.0/diskq/__init__.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+"""
+DiskQ is a Python package that provides persistent queues and stacks,
+allowing users to store and retrieve data even after the program has terminated.
+This is achieved by persistently storing the data on disk in pickle or dill format.
+DiskQ supports three different types of queues: a regular queue, a priority queue, and a LIFO queue.
+It's easy to use and can help with various scenarios where data needs to be stored in a persistent way.
+"""
+__version__ = "1.0.0"
+
+from .dqueue import PersistentQueue
+from .dqueue import PersistentPriorityQueue
+from .dqueue import PersistentLifoQueue
