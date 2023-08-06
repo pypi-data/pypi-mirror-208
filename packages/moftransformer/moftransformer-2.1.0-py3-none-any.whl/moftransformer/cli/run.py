edd01c11419a125ae1fbb86cee5f6f0e2f157981
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:48:34 +0000

    Add package moftransformer/2.1.0/moftransformer-2.1.0-py3-none-any.whl

---
+++ packages/moftransformer/moftransformer-2.1.0-py3-none-any.whl/moftransformer/cli/run.py
@@ -0,0 +1,25 @@
+# MOFTransformer version 2.0.0
+from pathlib import Path
+import os
+
+
+class CLICommand:
+    """
+    run moftransformer code
+
+    ex) moftransformer run downstream='example' num_gpus=1 max_epochs=10
+
+    """
+
+    @staticmethod
+    def add_arguments(parser):
+        parser.add_argument("args", nargs="*")
+
+    @staticmethod
+    def run(args):
+        from moftransformer import __root_dir__
+
+        run_path = Path(__root_dir__) / "run.py"
+        config = args.args
+        print(config)
+        os.system(f"python {run_path} {' '.join(config)}")
