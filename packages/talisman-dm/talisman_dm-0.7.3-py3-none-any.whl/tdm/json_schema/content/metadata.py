420f07a6dc83a21084bf4839cbbd64f66bca991d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:19 +0000

    Add package talisman-dm/0.7.3/talisman_dm-0.7.3-py3-none-any.whl

---
+++ packages/talisman-dm/talisman_dm-0.7.3-py3-none-any.whl/tdm/json_schema/content/metadata.py
@@ -0,0 +1,30 @@
+from typing import Dict, Optional
+
+from pydantic import BaseModel
+
+from tdm.abstract.datamodel import NodeType
+from tdm.datamodel import TreeDocumentContent
+
+
+class NodeMetadata(BaseModel):
+    node_type: NodeType
+    original_text: Optional[str]
+    text_translations: Dict[str, str] = {}
+    language: Optional[str] = None
+    hidden: bool = False
+
+    class Config:
+        extra = 'allow'  # any other extra fields will be kept
+
+    def parse(self) -> dict:
+        return self.dict()
+
+    @classmethod
+    def build(cls, node: TreeDocumentContent) -> 'NodeMetadata':
+        return cls.construct(
+            node_type=node.type,
+            original_text=node.original_node_text if node.original_node_text != node.node_text else None,
+            text_translations={lang: node.get_node_text_translation(lang) for lang in node.node_text_languages},
+            hidden=node.is_hidden,
+            language=node.language
+        )
