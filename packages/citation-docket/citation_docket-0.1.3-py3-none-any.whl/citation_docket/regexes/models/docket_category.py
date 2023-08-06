630d923dfd5803a856ec47a5e24cdb172f9926a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:21 +0000

    Add package citation-docket/0.1.3/citation_docket-0.1.3-py3-none-any.whl

---
+++ packages/citation-docket/citation_docket-0.1.3-py3-none-any.whl/citation_docket/regexes/models/docket_category.py
@@ -0,0 +1,47 @@
+from enum import Enum
+
+
+class DocketCategory(str, Enum):
+    """There are four common docket references involving Philippine Supreme Court
+    decisions.
+
+    Name | Value
+    :--|:--
+    `GR` | General Register
+    `AM` | Administrative Matter
+    `AC` | Administrative Case
+    `BM` | Bar Matter
+    `PET` | Presidential Electoral Tribunal
+    `OCA` | Office of the Court Administrator
+
+    Complication: These categories do not always represent decisions. For instance,
+    there are are `AM` and `BM` docket numbers that represent rules rather
+    than decisions.
+    """
+
+    GR = "General Register"
+    AM = "Administrative Matter"
+    AC = "Administrative Case"
+    BM = "Bar Matter"
+    PET = "Presidential Electoral Tribunal"
+    OCA = "Office of the Court Administrator"
+
+
+class ShortDocketCategory(str, Enum):
+    """For purposes of creating an enumeration for use in `sqlpyd` wherein
+    the value will be stored in the database.
+
+    Name | Value
+    :--|:--
+    `GR` | GR
+    `AM` | AM
+    `AC` | AC
+    `BM` | BM
+    """
+
+    GR = DocketCategory.GR.name
+    AM = DocketCategory.AM.name
+    AC = DocketCategory.AC.name
+    BM = DocketCategory.BM.name
+    PET = DocketCategory.PET.name
+    OCA = DocketCategory.OCA.name
