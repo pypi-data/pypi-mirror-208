95b82839be6d45527602a5ef36e92e329af2c3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:29 +0000

    Add package methodism/0.0.63/methodism-0.0.63.tar.gz

---
+++ packages/methodism/methodism-0.0.63.tar.gz/methodism-0.0.63/methodism/main.py
@@ -0,0 +1,75 @@
+#  Created by Xudoyberdi Egamberdiyev
+#
+#  Please contact before making any changes
+#
+#  Tashkent, Uzbekistan
+
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+from re import compile as re_compile
+
+from methodism.costumizing import CustomGenericAPIView
+from methodism.decors import method_and_params_checker
+from methodism.error_messages import MESSAGE
+from methodism.helper import custom_response, exception_data
+
+
+class METHODIZM(CustomGenericAPIView):
+    """
+                        Main Class | METHODIZM
+    file -> Asosiy funksiyalar joylashgan fileni ko'rsating
+    token_key -> Token Secret kalitini ko'rsating (default=Bearer)
+    auth_headers -> API headersda ushlab olinishi kerak bo'lgan kalitni ko'rsating (default=Authorization)
+    token_class -> Ro'yxatdan o'tganligini ko'rsatuvchi classni ko'rsating -> (default=Token)
+    not_auth_methods -> Ro'yxatdan o'tish talab qilinmaydigan funksiyalarni ko'rsating | list ko'rinishida
+
+    Bunga qo'shimcha boshqa Custom holardagi avtorizatsiyalardan ham foydalanishingiz mumkin
+
+    EXP:
+        authentication_classes = CustomBasicAuthentication,
+        permission_classes = IsAuthenticated,
+
+    DIQQAT !!!  BearerAuth yoki TokenAuthentication classlaridan foydalanish mumkin emas!!!
+    """
+
+    file = "__main__"
+    token_key = "Bearer"
+    auth_headers = 'Authorization'
+    token_class = Token
+    not_auth_methods = []  # def hello_world() => hello.world
+
+    @method_and_params_checker
+    def post(self, requests, *args, **kwargs):
+        method = requests.data.get("method")
+        params = requests.data.get("params")
+        headers = requests.headers
+        if method not in self.not_auth_methods:
+            authorization = headers.get(self.auth_headers, '')
+            pattern = re_compile(self.token_key + r" (.+)")
+
+            if not pattern.match(authorization):
+                return Response(custom_response(status=False, method=method, message=MESSAGE['NotAuthenticated']))
+            input_token = pattern.findall(authorization)[0]
+
+            # Authorize
+            token = self.token_class.objects.filter(key=input_token).first()
+            if not token:
+                return Response(custom_response(status=False, method=method, message=MESSAGE['AuthToken']))
+            requests.user = token.user
+        try:
+            funk = getattr(self.file, method.replace('.', '_').replace('-', '_'))
+        except AttributeError:
+            return Response(custom_response(False, method=method, message=MESSAGE['MethodDoesNotExist']))
+        except Exception as e:
+            return Response(custom_response(False, method=method, message=MESSAGE['UndefinedError'],
+                                            data=exception_data(e)))
+        res = map(funk, [requests], [params])
+        try:
+            response = Response(list(res)[0])
+            response.data.update({'method': method})
+        except Exception as e:
+            response = Response(custom_response(False, method=method, message=MESSAGE['UndefinedError'],
+                                                data=exception_data(e)))
+        return response
