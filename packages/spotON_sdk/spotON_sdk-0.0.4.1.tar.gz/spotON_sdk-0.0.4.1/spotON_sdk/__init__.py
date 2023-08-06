0483155e53bf6eed811681f709d94efdd32ea611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:52 +0000

    Add package spotON_sdk/0.0.4.1/spotON_sdk-0.0.4.1.tar.gz

---
+++ packages/spotON_sdk/spotON_sdk-0.0.4.1.tar.gz/spotON_sdk-0.0.4.1/spotON_sdk/__init__.py
@@ -0,0 +1,17 @@
+"""
+The spotON_sdk package provides tools for interacting with the spotON API.
+
+This package includes modules for authenticating with the API, sending requests,
+and processing responses. It also includes utility functions for working with
+the data returned by the API.
+"""
+
+from entsoe import Area
+
+from .settings import *
+from .data_helpers import *
+from . import spotON_controller
+
+
+__version__ = "0.0.4.1"
+__all__ = [name for name in dir() if not name.startswith('_')]
