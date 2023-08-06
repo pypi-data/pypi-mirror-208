73db5cbbcc9861274e6b5f643d64bec358a9312d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:41 +0000

    Add package rfmanalysis/0.1.6/rfmanalysis-0.1.6-py2.py3-none-any.whl

---
+++ packages/rfmanalysis/rfmanalysis-0.1.6-py2.py3-none-any.whl/rfmanalysis/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for rfmanalysis."""
+
+__author__ = """Emma Hovhannisyan"""
+__email__ = 'emmahovhannisyan02@gmail.com'
+__version__ = '0.1.6'
+
+
+from .rfmanalysis import RFMAnalysis, RFMVisualizer
