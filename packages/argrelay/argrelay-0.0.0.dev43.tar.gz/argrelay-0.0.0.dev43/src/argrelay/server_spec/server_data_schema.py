7ccfbe57a5f75f69d2a93d98cf42e883c7761741
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:32 +0000

    Add package argrelay/0.0.0.dev43/argrelay-0.0.0.dev43.tar.gz

---
+++ packages/argrelay/argrelay-0.0.0.dev43.tar.gz/argrelay-0.0.0.dev43/src/argrelay/server_spec/server_data_schema.py
@@ -0,0 +1,32 @@
+"""
+This package generates OpenAPI spec definitions for `relay_server` it publishes over HTTP.
+While `relay_client` is supposed to use these definitions, it does not.
+Instead, it uses `marshmallow` schemas these OpenAPI spec definitions are generated from.
+"""
+
+from apispec import APISpec
+from apispec.ext.marshmallow import MarshmallowPlugin
+
+from argrelay.schema_request.RequestContextSchema import request_context_desc
+from argrelay.schema_response.ArgValuesSchema import arg_values_desc
+from argrelay.server_spec.const_int import DEFAULT_OPEN_API_VERSION, UNUSED_TITLE, UNUSED_VERSION
+
+# This spec is only used to generate data schemas: Marshmallow Schemas -> JSON Schemas.
+# These data schemas are subsequently used in OAS specs for paths (which are defined manually).
+# All serialization/deserialization/validation is done using `marshmallow` schemas (instead of JSON Schemas below).
+server_op_data_schemas = APISpec(
+    title = UNUSED_TITLE,
+    version = UNUSED_VERSION,
+    openapi_version = DEFAULT_OPEN_API_VERSION,
+    plugins = [MarshmallowPlugin()],
+)
+
+# Generate data schemas: Marshmallow Schema -> JSON Schema:
+server_op_data_schemas.components.schema(
+    request_context_desc.ref_name,
+    schema = request_context_desc.dict_schema,
+)
+server_op_data_schemas.components.schema(
+    arg_values_desc.ref_name,
+    schema = arg_values_desc.dict_schema,
+)
