4053359902fa360ce85a9146cc3e53be7d7b5e1b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:46 +0000

    Add package checkify/0.3/checkify-0.3.tar.gz

---
+++ packages/checkify/checkify-0.3.tar.gz/checkify-0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='checkify',
+    version='0.3',
+    packages=['checkify'],
+    install_requires=[
+        'openai'
+    ],
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/aditya0072001/checkify',
+    project_urls={
+        'Download Statistics': 'https://pepy.tech/project/checkify/month',
+    },
+)
