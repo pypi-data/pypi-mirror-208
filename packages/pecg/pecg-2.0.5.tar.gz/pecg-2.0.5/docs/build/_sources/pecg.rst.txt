da617f68d7044d8d0f3a48f97a8637d0d96e0fb1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:01 +0000

    Add package pecg/2.0.5/pecg-2.0.5.tar.gz

---
+++ packages/pecg/pecg-2.0.5.tar.gz/pecg-2.0.5/docs/build/_sources/pecg.rst.txt
@@ -0,0 +1,34 @@
+pecg package
+============
+
+.. toctree::
+   :maxdepth: 4
+
+   pecg.ecg
+
+
+pecg.Preprocessing
+--------------------------
+
+.. automodule:: pecg.Preprocessing
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+pecg.Example
+--------------------------
+
+.. automodule:: pecg.Example
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
+
+
+Module contents
+---------------
+
+.. automodule:: pecg
+   :members:
+   :undoc-members:
+   :show-inheritance:
---
+++ packages/pecg/pecg-2.0.5.tar.gz/pecg-2.0.5/pecg/ecg/wavedet_exe/Wavdet.py
@@ -0,0 +1,35 @@
+import tempfile
+import platform
+import os, sys, stat
+import numpy as np
+import scipy.io as spio
+import pathlib
+
+
+def wavdet(signal, fs, peaks, matlab_pat):
+    my_path = str(pathlib.Path(__file__).parent.resolve())
+    tmpdirname = tempfile.TemporaryDirectory()
+    os.chdir(tmpdirname.name)
+
+    np.savetxt("peaks.txt", peaks)
+    np.savetxt("signal.txt", signal)
+    if platform.system() == "Linux":
+        wavedet_dir = my_path + "/run_peak_det_2023.sh"
+        for root, dirs, files in os.walk(my_path):
+            for d in dirs:
+                os.chmod(os.path.join(root, d), 0o777)
+            for f in files:
+                os.chmod(os.path.join(root, f), 0o777)
+
+        command = " ".join(
+            [wavedet_dir, matlab_pat, '"signal.txt" "peaks.txt"', str(fs)]
+        )
+        os.system(command)
+        fiducials_mat = spio.loadmat(tmpdirname.name + "/output.mat")
+    if platform.system() == "Windows":
+        wavedet_dir = my_path + "//peak_det_2023.exe"
+        command = " ".join([wavedet_dir, '"signal.txt" "peaks.txt" ', str(fs)])
+        os.system(command)
+        fiducials_mat = spio.loadmat(tmpdirname.name + "//output.mat")
+
+    return fiducials_mat, tmpdirname
