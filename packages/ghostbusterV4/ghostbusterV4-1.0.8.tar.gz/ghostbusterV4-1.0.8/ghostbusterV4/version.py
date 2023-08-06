3dba6426eb70215f9e87cf4981c315a4021c45fb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:21:10 +0000

    Add package ghostbusterV4/1.0.8/ghostbusterV4-1.0.8.tar.gz

---
+++ packages/ghostbusterV4/ghostbusterV4-1.0.8.tar.gz/ghostbusterV4-1.0.8/ghostbusterV4/version.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+This module contains project version information.
+
+.. currentmodule:: ghostbusterV4.version
+.. moduleauthor:: shubham_shah <sshah@assetnote.io>
+"""
+
+__version__ = "1.0.8"  #: the working version
+__release__ = "1.0.8"  #: the release version
