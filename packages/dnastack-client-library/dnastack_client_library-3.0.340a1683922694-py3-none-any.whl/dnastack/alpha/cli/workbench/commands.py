a66064adae84896e02a0f8d07ec822b8b2742fcc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:08:52 +0000

    Add package dnastack-client-library/3.0.340a1683922694/dnastack_client_library-3.0.340a1683922694-py3-none-any.whl

---
+++ packages/dnastack-client-library/dnastack_client_library-3.0.340a1683922694-py3-none-any.whl/dnastack/alpha/cli/workbench/commands.py
@@ -0,0 +1,15 @@
+import click
+
+from dnastack.alpha.cli.workbench.engines_commands import alpha_engines_command_group
+from dnastack.alpha.cli.workbench.runs_commands import alpha_runs_command_group
+from dnastack.alpha.cli.workbench.workflows_commands import alpha_workflows_command_group
+
+
+@click.group('workbench')
+def alpha_workbench_command_group():
+    """ Interact with Workbench """
+
+
+alpha_workbench_command_group.add_command(alpha_runs_command_group)
+alpha_workbench_command_group.add_command(alpha_workflows_command_group)
+alpha_workbench_command_group.add_command(alpha_engines_command_group)
---
+++ packages/dnastack-client-library/dnastack_client_library-3.0.340a1683922694-py3-none-any.whl/dnastack/constants.py
@@ -0,0 +1,5 @@
+import os
+
+__version__ = "3.0.340a1683922694"
+
+LOCAL_STORAGE_DIRECTORY = os.path.join(os.getenv('HOME'), '.dnastack')
