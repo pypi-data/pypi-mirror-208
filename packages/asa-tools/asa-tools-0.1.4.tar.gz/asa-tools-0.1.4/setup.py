d6aaf2ec6c947283c0ff12801242f475933958fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:06 +0000

    Add package asa-tools/0.1.4/asa-tools-0.1.4.tar.gz

---
+++ packages/asa-tools/asa-tools-0.1.4.tar.gz/asa-tools-0.1.4/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+setup(
+    name='asa-tools',
+    version='0.1.4',
+    py_modules=['turn2release'],
+    install_requires=[
+        'Click',
+        'openpyxl',
+        'requests',
+        'tqdm',
+        'parsel'
+    ],
+    entry_points='''
+        [console_scripts]
+        asa=turn2release:cli
+    ''',
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    description="asa's personal toolbox",
+    package_data={
+        '':['copyallmp4.py','out_dutys.py','spyderBili.py','conf.ini']
+    }
+)
