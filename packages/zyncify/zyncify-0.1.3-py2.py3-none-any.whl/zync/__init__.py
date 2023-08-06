84e98e9751b1e592576a310c5a23d4fe7eedfaf7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:11 +0000

    Add package zyncify/0.1.3/zyncify-0.1.3-py2.py3-none-any.whl

---
+++ packages/zyncify/zyncify-0.1.3-py2.py3-none-any.whl/zync/__init__.py
@@ -0,0 +1,13 @@
+from .logger import logger, bugger, wagger, egger, critter, fatal
+from .slugify import Slugger, slugger
+
+__all__ = [
+    "bugger",
+    "logger",
+    "wagger",
+    "egger",
+    "critter",
+    "fatal",
+    "Slugger",
+    "slugger",
+]
