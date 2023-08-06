16c1da905285a4f54b01062eaa2c31bf46506f11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:33 +0000

    Add package ebbe/1.13.0/ebbe-1.13.0.tar.gz

---
+++ packages/ebbe/ebbe-1.13.0.tar.gz/ebbe-1.13.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open("./README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="ebbe",
+    version="1.13.0",
+    description="Collection of typical helper functions for python.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/Yomguithereal/ebbe",
+    license="MIT",
+    author="Guillaume Plique",
+    author_email="kropotkinepiotr@gmail.com",
+    keywords="iter",
+    python_requires=">=3.5",
+    packages=find_packages(exclude=["test"]),
+    package_data={"docs": ["README.md"]},
+    install_requires=[],
+    zip_safe=True,
+)
