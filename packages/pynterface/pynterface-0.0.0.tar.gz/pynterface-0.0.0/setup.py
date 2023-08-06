76bc3c6274a22c781939ac793b8e41156691129d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:16 +0000

    Add package pynterface/0.0.0/pynterface-0.0.0.tar.gz

---
+++ packages/pynterface/pynterface-0.0.0.tar.gz/pynterface-0.0.0/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+import pynterface
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="pynterface",
+    version=pynterface.__version__,
+    author=pynterface.__author__,
+    author_email='singhvi.vivaan@gmail.com',
+    description='Terminal-Based Printing Tools!',
+    long_description=description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    packages=find_packages(),
+    python_requires='>=3.8',
+    include_package_data=True,
+)
