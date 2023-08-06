2f9b596aa2a78101c7b83ca340c5fe60cd573bde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:00 +0000

    Add package piccolo/0.111.1/piccolo-0.111.1.tar.gz

---
+++ packages/piccolo/piccolo-0.111.1.tar.gz/piccolo-0.111.1/piccolo/query/methods/table_exists.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+import typing as t
+
+from piccolo.custom_types import TableInstance
+from piccolo.query.base import Query
+from piccolo.querystring import QueryString
+
+
+class TableExists(Query[TableInstance, bool]):
+
+    __slots__: t.Tuple = ()
+
+    async def response_handler(self, response):
+        return bool(response[0]["exists"])
+
+    @property
+    def sqlite_querystrings(self) -> t.Sequence[QueryString]:
+        return [
+            QueryString(
+                "SELECT EXISTS(SELECT * FROM sqlite_master WHERE "
+                f"name = '{self.table._meta.tablename}') AS 'exists'"
+            )
+        ]
+
+    @property
+    def postgres_querystrings(self) -> t.Sequence[QueryString]:
+        return [
+            QueryString(
+                "SELECT EXISTS(SELECT * FROM information_schema.tables WHERE "
+                f"table_name = '{self.table._meta.tablename}')"
+            )
+        ]
+
+    @property
+    def cockroach_querystrings(self) -> t.Sequence[QueryString]:
+        return [
+            QueryString(
+                "SELECT EXISTS(SELECT * FROM information_schema.tables WHERE "
+                f"table_name = '{self.table._meta.tablename}')"
+            )
+        ]
