17bdcbb12d7c0eeaeff264149a92d8f856418ed7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:38 +0000

    Add package connectome/0.6.1/connectome-0.6.1.tar.gz

---
+++ packages/connectome/connectome-0.6.1.tar.gz/connectome-0.6.1/connectome/interface/__init__.py
@@ -0,0 +1,5 @@
+from .blocks import GroupBy
+from .decorators import *
+from .edges import *
+from .metaclasses import Mixin, Source, Transform
+from .nodes import Input, InverseInput, InverseOutput, Output, Parameter
