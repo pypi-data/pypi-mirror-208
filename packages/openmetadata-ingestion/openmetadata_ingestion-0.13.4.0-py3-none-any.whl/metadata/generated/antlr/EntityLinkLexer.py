0cfa2eee339e908c2310de220f674822876366d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:33 +0000

    Add package openmetadata-ingestion/0.13.4.0/openmetadata_ingestion-0.13.4.0-py3-none-any.whl

---
+++ packages/openmetadata-ingestion/openmetadata_ingestion-0.13.4.0-py3-none-any.whl/metadata/generated/antlr/EntityLinkLexer.py
@@ -0,0 +1,143 @@
+# Generated from /home/runner/work/OpenMetadata/OpenMetadata/openmetadata-spec/src/main/antlr4/org/openmetadata/schema/EntityLink.g4 by ANTLR 4.9.2
+from antlr4 import *
+from io import StringIO
+import sys
+if sys.version_info[1] > 5:
+    from typing import TextIO
+else:
+    from typing.io import TextIO
+
+
+
+def serializedATN():
+    with StringIO() as buf:
+        buf.write("\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\t")
+        buf.write("\u00e6\b\1\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7")
+        buf.write("\t\7\4\b\t\b\3\2\3\2\3\2\3\2\3\3\3\3\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3")
+        buf.write("\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4")
+        buf.write("\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\3\4\5\4\u00bb\n\4")
+        buf.write("\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3")
+        buf.write("\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5")
+        buf.write("\5\5\u00d8\n\5\3\6\3\6\3\6\3\7\6\7\u00de\n\7\r\7\16\7")
+        buf.write("\u00df\3\b\6\b\u00e3\n\b\r\b\16\b\u00e4\2\2\t\3\3\5\4")
+        buf.write("\7\5\t\6\13\7\r\b\17\t\3\2\4\3\2c|\n\2$$))..\60\60\62")
+        buf.write(";C\\aac|\2\u00fe\2\3\3\2\2\2\2\5\3\2\2\2\2\7\3\2\2\2\2")
+        buf.write("\t\3\2\2\2\2\13\3\2\2\2\2\r\3\2\2\2\2\17\3\2\2\2\3\21")
+        buf.write("\3\2\2\2\5\25\3\2\2\2\7\u00ba\3\2\2\2\t\u00d7\3\2\2\2")
+        buf.write("\13\u00d9\3\2\2\2\r\u00dd\3\2\2\2\17\u00e2\3\2\2\2\21")
+        buf.write("\22\7>\2\2\22\23\7%\2\2\23\24\7G\2\2\24\4\3\2\2\2\25\26")
+        buf.write("\7@\2\2\26\6\3\2\2\2\27\30\7v\2\2\30\31\7c\2\2\31\32\7")
+        buf.write("d\2\2\32\33\7n\2\2\33\u00bb\7g\2\2\34\35\7f\2\2\35\36")
+        buf.write("\7c\2\2\36\37\7v\2\2\37 \7c\2\2 !\7d\2\2!\"\7c\2\2\"#")
+        buf.write("\7u\2\2#\u00bb\7g\2\2$%\7f\2\2%&\7c\2\2&\'\7v\2\2\'(\7")
+        buf.write("c\2\2()\7d\2\2)*\7c\2\2*+\7u\2\2+,\7g\2\2,-\7U\2\2-.\7")
+        buf.write("e\2\2./\7j\2\2/\60\7g\2\2\60\61\7o\2\2\61\u00bb\7c\2\2")
+        buf.write("\62\63\7o\2\2\63\64\7g\2\2\64\65\7v\2\2\65\66\7t\2\2\66")
+        buf.write("\67\7k\2\2\678\7e\2\28\u00bb\7u\2\29:\7f\2\2:;\7c\2\2")
+        buf.write(";<\7u\2\2<=\7j\2\2=>\7d\2\2>?\7q\2\2?@\7c\2\2@A\7t\2\2")
+        buf.write("A\u00bb\7f\2\2BC\7r\2\2CD\7k\2\2DE\7r\2\2EF\7g\2\2FG\7")
+        buf.write("n\2\2GH\7k\2\2HI\7p\2\2I\u00bb\7g\2\2JK\7e\2\2KL\7j\2")
+        buf.write("\2LM\7c\2\2MN\7t\2\2N\u00bb\7v\2\2OP\7t\2\2PQ\7g\2\2Q")
+        buf.write("R\7r\2\2RS\7q\2\2ST\7t\2\2T\u00bb\7v\2\2UV\7v\2\2VW\7")
+        buf.write("q\2\2WX\7r\2\2XY\7k\2\2Y\u00bb\7e\2\2Z[\7o\2\2[\\\7n\2")
+        buf.write("\2\\]\7o\2\2]^\7q\2\2^_\7f\2\2_`\7g\2\2`\u00bb\7n\2\2")
+        buf.write("ab\7d\2\2bc\7q\2\2c\u00bb\7v\2\2de\7V\2\2ef\7J\2\2fg\7")
+        buf.write("T\2\2gh\7G\2\2hi\7C\2\2i\u00bb\7F\2\2jk\7n\2\2kl\7q\2")
+        buf.write("\2lm\7e\2\2mn\7c\2\2no\7v\2\2op\7k\2\2pq\7q\2\2q\u00bb")
+        buf.write("\7p\2\2rs\7i\2\2st\7n\2\2tu\7q\2\2uv\7u\2\2vw\7u\2\2w")
+        buf.write("x\7c\2\2xy\7t\2\2y\u00bb\7{\2\2z{\7i\2\2{|\7n\2\2|}\7")
+        buf.write("q\2\2}~\7u\2\2~\177\7u\2\2\177\u0080\7c\2\2\u0080\u0081")
+        buf.write("\7t\2\2\u0081\u0082\7{\2\2\u0082\u0083\7V\2\2\u0083\u0084")
+        buf.write("\7g\2\2\u0084\u0085\7t\2\2\u0085\u00bb\7o\2\2\u0086\u0087")
+        buf.write("\7v\2\2\u0087\u0088\7c\2\2\u0088\u00bb\7i\2\2\u0089\u008a")
+        buf.write("\7e\2\2\u008a\u008b\7n\2\2\u008b\u008c\7c\2\2\u008c\u008d")
+        buf.write("\7u\2\2\u008d\u008e\7u\2\2\u008e\u008f\7k\2\2\u008f\u0090")
+        buf.write("\7h\2\2\u0090\u0091\7k\2\2\u0091\u0092\7e\2\2\u0092\u0093")
+        buf.write("\7c\2\2\u0093\u0094\7v\2\2\u0094\u0095\7k\2\2\u0095\u0096")
+        buf.write("\7q\2\2\u0096\u00bb\7p\2\2\u0097\u0098\7v\2\2\u0098\u0099")
+        buf.write("\7{\2\2\u0099\u009a\7r\2\2\u009a\u00bb\7g\2\2\u009b\u009c")
+        buf.write("\7v\2\2\u009c\u009d\7g\2\2\u009d\u009e\7u\2\2\u009e\u009f")
+        buf.write("\7v\2\2\u009f\u00a0\7F\2\2\u00a0\u00a1\7g\2\2\u00a1\u00a2")
+        buf.write("\7h\2\2\u00a2\u00a3\7k\2\2\u00a3\u00a4\7p\2\2\u00a4\u00a5")
+        buf.write("\7k\2\2\u00a5\u00a6\7v\2\2\u00a6\u00a7\7k\2\2\u00a7\u00a8")
+        buf.write("\7q\2\2\u00a8\u00bb\7p\2\2\u00a9\u00aa\7v\2\2\u00aa\u00ab")
+        buf.write("\7g\2\2\u00ab\u00ac\7u\2\2\u00ac\u00ad\7v\2\2\u00ad\u00ae")
+        buf.write("\7U\2\2\u00ae\u00af\7w\2\2\u00af\u00b0\7k\2\2\u00b0\u00b1")
+        buf.write("\7v\2\2\u00b1\u00bb\7g\2\2\u00b2\u00b3\7v\2\2\u00b3\u00b4")
+        buf.write("\7g\2\2\u00b4\u00b5\7u\2\2\u00b5\u00b6\7v\2\2\u00b6\u00b7")
+        buf.write("\7E\2\2\u00b7\u00b8\7c\2\2\u00b8\u00b9\7u\2\2\u00b9\u00bb")
+        buf.write("\7g\2\2\u00ba\27\3\2\2\2\u00ba\34\3\2\2\2\u00ba$\3\2\2")
+        buf.write("\2\u00ba\62\3\2\2\2\u00ba9\3\2\2\2\u00baB\3\2\2\2\u00ba")
+        buf.write("J\3\2\2\2\u00baO\3\2\2\2\u00baU\3\2\2\2\u00baZ\3\2\2\2")
+        buf.write("\u00baa\3\2\2\2\u00bad\3\2\2\2\u00baj\3\2\2\2\u00bar\3")
+        buf.write("\2\2\2\u00baz\3\2\2\2\u00ba\u0086\3\2\2\2\u00ba\u0089")
+        buf.write("\3\2\2\2\u00ba\u0097\3\2\2\2\u00ba\u009b\3\2\2\2\u00ba")
+        buf.write("\u00a9\3\2\2\2\u00ba\u00b2\3\2\2\2\u00bb\b\3\2\2\2\u00bc")
+        buf.write("\u00bd\7e\2\2\u00bd\u00be\7q\2\2\u00be\u00bf\7n\2\2\u00bf")
+        buf.write("\u00c0\7w\2\2\u00c0\u00c1\7o\2\2\u00c1\u00c2\7p\2\2\u00c2")
+        buf.write("\u00d8\7u\2\2\u00c3\u00c4\7f\2\2\u00c4\u00c5\7g\2\2\u00c5")
+        buf.write("\u00c6\7u\2\2\u00c6\u00c7\7e\2\2\u00c7\u00c8\7t\2\2\u00c8")
+        buf.write("\u00c9\7k\2\2\u00c9\u00ca\7r\2\2\u00ca\u00cb\7v\2\2\u00cb")
+        buf.write("\u00cc\7k\2\2\u00cc\u00cd\7q\2\2\u00cd\u00d8\7p\2\2\u00ce")
+        buf.write("\u00cf\7v\2\2\u00cf\u00d0\7c\2\2\u00d0\u00d1\7i\2\2\u00d1")
+        buf.write("\u00d8\7u\2\2\u00d2\u00d3\7v\2\2\u00d3\u00d4\7c\2\2\u00d4")
+        buf.write("\u00d5\7u\2\2\u00d5\u00d6\7m\2\2\u00d6\u00d8\7u\2\2\u00d7")
+        buf.write("\u00bc\3\2\2\2\u00d7\u00c3\3\2\2\2\u00d7\u00ce\3\2\2\2")
+        buf.write("\u00d7\u00d2\3\2\2\2\u00d8\n\3\2\2\2\u00d9\u00da\7<\2")
+        buf.write("\2\u00da\u00db\7<\2\2\u00db\f\3\2\2\2\u00dc\u00de\t\2")
+        buf.write("\2\2\u00dd\u00dc\3\2\2\2\u00de\u00df\3\2\2\2\u00df\u00dd")
+        buf.write("\3\2\2\2\u00df\u00e0\3\2\2\2\u00e0\16\3\2\2\2\u00e1\u00e3")
+        buf.write("\t\3\2\2\u00e2\u00e1\3\2\2\2\u00e3\u00e4\3\2\2\2\u00e4")
+        buf.write("\u00e2\3\2\2\2\u00e4\u00e5\3\2\2\2\u00e5\20\3\2\2\2\7")
+        buf.write("\2\u00ba\u00d7\u00df\u00e4\2")
+        return buf.getvalue()
+
+
+class EntityLinkLexer(Lexer):
+
+    atn = ATNDeserializer().deserialize(serializedATN())
+
+    decisionsToDFA = [ DFA(ds, i) for i, ds in enumerate(atn.decisionToState) ]
+
+    T__0 = 1
+    T__1 = 2
+    ENTITY_TYPE = 3
+    ENTITY_FIELD = 4
+    RESERVED = 5
+    ENTITY_ATTRIBUTE = 6
+    ENTITY_FQN = 7
+
+    channelNames = [ u"DEFAULT_TOKEN_CHANNEL", u"HIDDEN" ]
+
+    modeNames = [ "DEFAULT_MODE" ]
+
+    literalNames = [ "<INVALID>",
+            "'<#E'", "'>'", "'::'" ]
+
+    symbolicNames = [ "<INVALID>",
+            "ENTITY_TYPE", "ENTITY_FIELD", "RESERVED", "ENTITY_ATTRIBUTE", 
+            "ENTITY_FQN" ]
+
+    ruleNames = [ "T__0", "T__1", "ENTITY_TYPE", "ENTITY_FIELD", "RESERVED", 
+                  "ENTITY_ATTRIBUTE", "ENTITY_FQN" ]
+
+    grammarFileName = "EntityLink.g4"
+
+    def __init__(self, input=None, output:TextIO = sys.stdout):
+        super().__init__(input, output)
+        self.checkVersion("4.9.2")
+        self._interp = LexerATNSimulator(self, self.atn, self.decisionsToDFA, PredictionContextCache())
+        self._actions = None
+        self._predicates = None
+
+
