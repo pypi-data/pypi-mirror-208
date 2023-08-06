e2d194515adfa3b2af01dfaf853b19821ea44777
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:11 +0000

    Add package crispr-shrinkage/0.1.66/crispr_shrinkage-0.1.66.tar.gz

---
+++ packages/crispr-shrinkage/crispr_shrinkage-0.1.66.tar.gz/crispr_shrinkage-0.1.66/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['crispr_shrinkage', 'crispr_shrinkage.framework']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['matplotlib>=3.6.3,<4.0.0', 'numpy>=1.24.1,<2.0.0', 'scipy>=1.10.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'crispr-shrinkage',
+    'version': '0.1.66',
+    'description': '',
+    'long_description': '',
+    'author': 'Basheer Becerra',
+    'author_email': 'bbecerra@fas.harvard.edu',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.12',
+}
+
+
+setup(**setup_kwargs)
