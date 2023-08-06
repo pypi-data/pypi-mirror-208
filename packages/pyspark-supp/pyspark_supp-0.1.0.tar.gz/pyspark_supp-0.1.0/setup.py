13c9fa315e2983facad0bec99bc7b63879e2b3b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:57 +0000

    Add package pyspark-supp/0.1.0/pyspark_supp-0.1.0.tar.gz

---
+++ packages/pyspark-supp/pyspark_supp-0.1.0.tar.gz/pyspark_supp-0.1.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+ 
+long_description = 'This library was made to help Data Engineer that uses Pyspark data frames. Using functions to compare, test and check data.'
+
+classifiers = [
+  'Development Status :: 1 - Development',
+  'Intended Audience :: Data Engineers',
+  'Operating System :: All',
+  'License :: OSI Approved :: MIT License',
+  'Programming Language :: Python :: 3'
+]
+ 
+setup(
+  name='pyspark_supp',
+  version='0.1.0',
+  description='Data Engineer Support PySpark Library',
+  long_description=long_description,
+  url='https://github.com/joaocaemerer/pyspark-supp',
+  author='Fernando Caemerer',
+  author_email='fernando.caemerer@gmail.com',
+  license='MIT',
+  keywords=['dataengineer', 'pyspark', 'dataframe'], 
+  packages=find_packages(),
+  install_requires=['pyspark']
+)
