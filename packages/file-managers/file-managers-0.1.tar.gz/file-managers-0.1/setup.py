2e957f0fb2bd866e46f75673ac3f58eccf772c3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:28 +0000

    Add package file-managers/0.1/file-managers-0.1.tar.gz

---
+++ packages/file-managers/file-managers-0.1.tar.gz/file-managers-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='file-managers',
+    version='0.1',
+    author='Md. Musfiqur Rahaman',
+    author_email='musfiqur.rahaman@northsouth.edu',
+    description='A Python Package to manage your files and folders easily in different ways.',
+    packages=['file_managers'],
+    install_requires=['twine', 'wheel']
+)
