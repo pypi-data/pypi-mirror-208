b981895eef5ea74600efbfa2c155bddf9b4f118a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:28 +0000

    Add package kolyaklimkLab3/0.6.0/kolyaklimkLab3-0.6.0.tar.gz

---
+++ packages/kolyaklimkLab3/kolyaklimkLab3-0.6.0.tar.gz/kolyaklimkLab3-0.6.0/setup.py
@@ -0,0 +1,31 @@
+# Always prefer setuptools over distutils
+from setuptools import setup, find_packages
+
+# To use a consistent encoding
+from codecs import open
+from os import path
+
+import kolyaklimkLab3
+
+setup(
+    name="kolyaklimkLab3",
+    version="0.6.0",
+    description=open('README.MD').read(),
+    author="kolyaklimk",
+    author_email="kolyaklimk@gmail.com",
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Operating System :: OS Independent"
+    ],
+    packages=["kolyaklimkLab3"],
+    include_package_data=True,
+    install_requires=["regex"]
+)
