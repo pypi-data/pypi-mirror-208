6b43fbd37eaae6b3530f253506c7d943dfaa8849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:18 +0000

    Add package references-parser/0.2.4/references_parser-0.2.4.tar.gz

---
+++ packages/references-parser/references_parser-0.2.4.tar.gz/references_parser-0.2.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='references_parser',
+    packages=setuptools.find_packages(),
+    version='0.2.4',
+    description="Tool for parsing bibtex in ssau's format",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Mukhin Artem',
+    author_email='artemmukhinssau@gmail.com',
+    url='https://github.com/Banayaki/ReferencesParser',
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ], install_requires=[
+        'bibtexparser~=1.2.0'
+    ]
+)
