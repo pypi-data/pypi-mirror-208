280f437b80645a71bc6589fd0505d9600219dc5b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:03 +0000

    Add package local-bert-python-backend/0.0.2/local-bert-python-backend-0.0.2.tar.gz

---
+++ packages/local-bert-python-backend/local-bert-python-backend-0.0.2.tar.gz/local-bert-python-backend-0.0.2/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+     name='local-bert-python-backend',  
+     version='0.0.2',
+     author="Circles",
+     author_email="info@bubbelz.life",
+     description="PyPI Package for Circles Local Bert Python",
+     long_description="This is a package for sharing common Logger function used in different repositories",
+     long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
