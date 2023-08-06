3ecbc8b4920d9a0a494de42f380d0e20784f700b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:41 +0000

    Add package dbt-athena-community/1.5.0/dbt-athena-community-1.5.0.tar.gz

---
+++ packages/dbt-athena-community/dbt-athena-community-1.5.0.tar.gz/dbt-athena-community-1.5.0/dbt/include/athena/macros/materializations/models/view/create_or_replace_view.sql
@@ -0,0 +1,48 @@
+{% macro create_or_replace_view(run_outside_transaction_hooks=True) %}
+  {%- set identifier = model['alias'] -%}
+
+  {%- set lf_tags = config.get('lf_tags', default=none) -%}
+  {%- set lf_tags_columns = config.get('lf_tags_columns', default=none) -%}
+  {%- set old_relation = adapter.get_relation(database=database, schema=schema, identifier=identifier) -%}
+  {%- set exists_as_view = (old_relation is not none and old_relation.is_view) -%}
+  {%- set target_relation = api.Relation.create(
+      identifier=identifier,
+      schema=schema,
+      database=database,
+      type='view',
+    ) -%}
+
+  {% if run_outside_transaction_hooks %}
+      -- no transactions on BigQuery
+      {{ run_hooks(pre_hooks, inside_transaction=False) }}
+  {% endif %}
+  -- `BEGIN` happens here on Snowflake
+  {{ run_hooks(pre_hooks, inside_transaction=True) }}
+  -- If there's a table with the same name and we weren't told to full refresh,
+  -- that's an error. If we were told to full refresh, drop it. This behavior differs
+  -- for Snowflake and BigQuery, so multiple dispatch is used.
+  {%- if old_relation is not none and old_relation.is_table -%}
+    {{ handle_existing_table(should_full_refresh(), old_relation) }}
+  {%- endif -%}
+
+  -- build model
+  {% call statement('main') -%}
+    {{ create_view_as(target_relation, sql) }}
+  {%- endcall %}
+
+  {% if lf_tags is not none or lf_tags_columns is not none %}
+    {{ adapter.add_lf_tags(target_relation.schema, identifier, lf_tags, lf_tags_columns) }}
+  {% endif %}
+
+  {{ run_hooks(post_hooks, inside_transaction=True) }}
+
+  {{ adapter.commit() }}
+
+  {% if run_outside_transaction_hooks %}
+      -- No transactions on BigQuery
+      {{ run_hooks(post_hooks, inside_transaction=False) }}
+  {% endif %}
+
+  {{ return({'relations': [target_relation]}) }}
+
+{% endmacro %}
