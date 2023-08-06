60215366b2e7298c80c3878937feffd124e28cf1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:03 +0000

    Add package prettyformatter/2.0.13/prettyformatter-2.0.13.tar.gz

---
+++ packages/prettyformatter/prettyformatter-2.0.13.tar.gz/prettyformatter-2.0.13/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(
+    description=
+        "Pretty formatter enables pretty formatting using hanging "
+        "indents, dataclasses, ellipses, and simple customizability "
+        "by registering formatters.",
+)
