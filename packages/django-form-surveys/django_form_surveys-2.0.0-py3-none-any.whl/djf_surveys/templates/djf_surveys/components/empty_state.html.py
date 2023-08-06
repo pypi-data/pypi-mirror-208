721f88224b1c3f561616227a91790fd6a44af1dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:26 +0000

    Add package django-form-surveys/2.0.0/django_form_surveys-2.0.0-py3-none-any.whl

---
+++ packages/django-form-surveys/django_form_surveys-2.0.0-py3-none-any.whl/djf_surveys/templates/djf_surveys/components/empty_state.html.py
@@ -0,0 +1,10 @@
+BBBB BBBB
+    XXXX XXXXXXXXXXXXXXX XXX XXXXXXXXXXX XXXXXX XXXXXXXXXXXXXXX XXXXXXXXXXXX
+        XXX XXXXXXXXXXXXXXX XXXXXXXXXXXXX
+             gettext(u"There's nothing here...") 
+        XXXXX
+
+        XX XXXXXXXXXXX XXXXXXX XXXXXXXXXXXXXXX
+             gettext(u'Created survey will appear here, try creating one!') 
+        XXXX
+    XXXXXX
