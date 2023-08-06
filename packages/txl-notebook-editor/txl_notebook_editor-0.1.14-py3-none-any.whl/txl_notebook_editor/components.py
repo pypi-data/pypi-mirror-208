e5e63e67d263bf27067b1018857e75f6a7d686e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:33 +0000

    Add package txl-notebook-editor/0.1.14/txl_notebook_editor-0.1.14-py3-none-any.whl

---
+++ packages/txl-notebook-editor/txl_notebook_editor-0.1.14-py3-none-any.whl/txl_notebook_editor/components.py
@@ -0,0 +1,70 @@
+from functools import partial
+
+from asphalt.core import Component, Context
+from textual.containers import Container
+
+from txl.base import CellFactory, Contents, Editor, Editors, FileOpenEvent, Kernels
+
+
+class NotebookEditorMeta(type(Editor), type(Container)):
+    pass
+
+
+class NotebookEditor(Editor, Container, metaclass=NotebookEditorMeta):
+    def __init__(
+        self,
+        contents: Contents,
+        kernels: Kernels,
+        cell_factory: CellFactory,
+    ) -> None:
+        super().__init__()
+        self.contents = contents
+        self.kernels = kernels
+        self.cell_factory = cell_factory
+        self.kernel = None
+
+    async def on_open(self, event: FileOpenEvent) -> None:
+        await self.open(event.path)
+
+    async def open(self, path: str) -> None:
+        self.ynb = await self.contents.get(path, type="notebook")
+        ipynb = self.ynb.source
+        self.language = (
+            ipynb.get("metadata", {}).get("kernelspec", {}).get("language", "")
+        )
+        kernel_name = ipynb.get("metadata", {}).get("kernelspec", {}).get("name")
+        if kernel_name:
+            self.kernel = self.kernels(kernel_name)
+        self.update()
+
+    def update(self):
+        for i_cell in range(self.ynb.cell_number):
+            cell = self.cell_factory(
+                self.ynb._ycells[i_cell], self.ynb.ydoc, self.language, self.kernel
+            )
+            self.mount(cell)
+
+
+class NotebookEditorComponent(Component):
+    def __init__(self, register: bool = True):
+        super().__init__()
+        self.register = register
+
+    async def start(
+        self,
+        ctx: Context,
+    ) -> None:
+        contents = await ctx.request_resource(Contents)
+        kernels = await ctx.request_resource(Kernels)
+        cell_factory = await ctx.request_resource(CellFactory)
+
+        notebook_editor_factory = partial(
+            NotebookEditor, contents, kernels, cell_factory
+        )
+
+        if self.register:
+            editors = await ctx.request_resource(Editors)
+            editors.register_editor_factory(notebook_editor_factory, [".ipynb"])
+        else:
+            notebook_editor = notebook_editor_factory()
+            ctx.add_resource(notebook_editor, types=Editor)
