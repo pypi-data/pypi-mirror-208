96d2f2610eb173ca7cd2445c424d7a7470ccea62
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:51 +0000

    Add package test-teste-21479813/1.0.0/test_teste_21479813-1.0.0-py3-none-any.whl

---
+++ packages/test-teste-21479813/test_teste_21479813-1.0.0-py3-none-any.whl/fundamentus_extractor/data/__init__.py
@@ -0,0 +1,8 @@
+from .extract.core import get_detail_data_list, get_result_shares
+from .tranforms import transform
+
+__all__ = [
+    transform,
+    get_detail_data_list,
+    get_result_shares
+]
