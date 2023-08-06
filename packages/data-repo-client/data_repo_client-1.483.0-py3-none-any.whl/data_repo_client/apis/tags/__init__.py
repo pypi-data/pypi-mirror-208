1377623a2a01c655928d03d403af780c8e943af8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:15 +0000

    Add package data-repo-client/1.483.0/data_repo_client-1.483.0-py3-none-any.whl

---
+++ packages/data-repo-client/data_repo_client-1.483.0-py3-none-any.whl/data_repo_client/apis/tags/__init__.py
@@ -0,0 +1,22 @@
+# do not import all endpoints into this module because that uses a lot of memory and stack frames
+# if you need the ability to import all endpoints from this module, import them with
+# from data_repo_client.apis.tag_to_api import tag_to_api
+
+import enum
+
+
+class TagValues(str, enum.Enum):
+    DATA_REPOSITORY_SERVICE = "DataRepositoryService"
+    CONFIGS = "configs"
+    REPOSITORY = "repository"
+    DATASETS = "datasets"
+    SEARCH = "search"
+    DUOS = "duos"
+    JOBS = "jobs"
+    JOURNAL = "journal"
+    PROFILES = "profiles"
+    RESOURCES = "resources"
+    REGISTER = "register"
+    SNAPSHOTS = "snapshots"
+    UPGRADE = "upgrade"
+    UNAUTHENTICATED = "unauthenticated"
