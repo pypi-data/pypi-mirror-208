7e182cbce1266e5cc4b9ad910af6e6f8adbf2315
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:48:14 +0000

    Add package cookie-composer/0.10.3/cookie_composer-0.10.3.tar.gz

---
+++ packages/cookie-composer/cookie_composer-0.10.3.tar.gz/cookie_composer-0.10.3/cookie_composer/__init__.py
@@ -0,0 +1,2 @@
+"""The Cookiecutter Composer."""
+__version__: str = "0.10.3"
---
+++ packages/cookie-composer/cookie_composer-0.10.3.tar.gz/cookie_composer-0.10.3/tests/test_cli.py
@@ -0,0 +1,29 @@
+import pytest
+from click import BadParameter
+from click.testing import CliRunner
+
+from cookie_composer import cli
+
+
+@pytest.fixture(scope="module")
+def runner():
+    return CliRunner()
+
+
+def test_validate_context_params():
+    """Make sure the context params are validated"""
+    with pytest.raises(BadParameter):
+        cli.validate_context_params(None, None, ("iaminvalid", "key=value"))
+
+    assert cli.validate_context_params(None, None, ("key1=value1", "key=value")) == {"key1": "value1", "key": "value"}
+
+
+def test_helps(runner):
+    result = runner.invoke(cli.create, "--help")
+    assert result.exit_code == 0
+    result = runner.invoke(cli.add, "--help")
+    assert result.exit_code == 0
+    result = runner.invoke(cli.update, "--help")
+    assert result.exit_code == 0
+    result = runner.invoke(cli.link, "--help")
+    assert result.exit_code == 0
