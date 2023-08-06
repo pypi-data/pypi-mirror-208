200436d797a9efe537471bbdb555d4d7e77af711
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:13:37 +0000

    Add package PyTraffic/1.0.5/PyTraffic-1.0.5.tar.gz

---
+++ packages/PyTraffic/PyTraffic-1.0.5.tar.gz/PyTraffic-1.0.5/PyTraffic/Density.py
@@ -0,0 +1,29 @@
+# PyTraffic - Density
+
+# Imports
+import cv2
+import os
+
+# Function 1 - Check Density
+def checkDensity(path):
+    if (os.path.exists(path)):
+        # Set Directory
+        os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+        carsXml = cv2.CascadeClassifier("models/cars.xml")
+
+        count = 0
+        frame = cv2.imread(path)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        cars = carsXml.detectMultiScale(gray, 1.1, 1)
+
+        for (x,y,w,h) in cars:
+            cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2) 
+            count+=1
+
+        cv2.waitKey(1)
+        cv2.destroyAllWindows()
+
+        return count
+    else:
+        raise Exception("The file path does not exist.")
