5219ccae8f22fa50433bf080f938172674a77aa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:53 +0000

    Add package openscm-runner/0.12.1/openscm_runner-0.12.1-py2.py3-none-any.whl

---
+++ packages/openscm-runner/openscm_runner-0.12.1-py2.py3-none-any.whl/openscm_runner/__init__.py
@@ -0,0 +1,18 @@
+"""
+OpenSCM-Runner, a thin wrapper to run simple climate models with a unified interface.
+
+See README and docs for more info.
+"""
+from .run import run  # noqa: F401
+
+try:
+    from importlib.metadata import version as _version
+except ImportError:
+    # no recourse if the fallback isn't there either...
+    from importlib_metadata import version as _version
+
+try:
+    __version__ = _version("openscm_runner")
+except Exception:  # pylint: disable=broad-except  # pragma: no cover
+    # Local copy, not installed with setuptools
+    __version__ = "unknown"
