95d89341f8a2284dfc976defd0e75d649722bbc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:52 +0000

    Add package styles/0.1.8/styles-0.1.8.tar.gz

---
+++ packages/styles/styles-0.1.8.tar.gz/styles-0.1.8/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup
+
+setup(
+    name='styles',
+    version='0.1.8',
+    description='A Style pack for python',
+    url='https://github.com/Xtarii/PythonStylePack',
+    author='Lord Alvin Hansen',
+    author_email='alvin.hansen@elev.ga.ntig.se',
+    license='BSD 2-clause',
+    packages=['styles'],
+
+    long_description="README.md",
+    long_description_content_type="text/markdown",
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
