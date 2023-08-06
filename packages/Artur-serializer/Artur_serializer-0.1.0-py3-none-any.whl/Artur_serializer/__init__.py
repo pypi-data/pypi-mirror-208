6162a3ccb3704f0502a9720711fb65576ed67a2d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:38:49 +0000

    Add package Artur-serializer/0.1.0/Artur_serializer-0.1.0-py3-none-any.whl

---
+++ packages/Artur-serializer/Artur_serializer-0.1.0-py3-none-any.whl/Artur_serializer/__init__.py
@@ -0,0 +1,6 @@
+from Artur_serializer.constants import BASE_TYPES, BASE_COLLECTIONS, SIMILAR_COLLECTIONS,\
+                      METHODS, CODE_PROPERTIES, CLASS_PROPERTIES, TYPESES
+
+from Artur_serializer.json_serializer import json_serializer
+from Artur_serializer.xml_serializer import xml_serializer
+from Artur_serializer.serializer_zavod import zavod
