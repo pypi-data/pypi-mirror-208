d92aca51aea8571f90d2d1ba6545e811b712770a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:25 +0000

    Add package pypcapkit/1.0.1.post1/pypcapkit-1.0.1.post1-py3-none-any.whl

---
+++ packages/pypcapkit/pypcapkit-1.0.1.post1-py3-none-any.whl/pcapkit/protocols/internet/mh.py
@@ -0,0 +1,258 @@
+# -*- coding: utf-8 -*-
+# pylint: disable=fixme
+"""mobility header
+
+:mod:`pcapkit.protocols.internet.mh` contains
+:class:`~pcapkit.protocols.internet.mh.MH` only,
+which implements extractor for Mobility Header
+(MH) [*]_, whose structure is described as below:
+
+======= ========= ================== ===============================
+Octets      Bits        Name                    Description
+======= ========= ================== ===============================
+  0           0   ``mh.next``                 Next Header
+  1           8   ``mh.length``               Header Length
+  2          16   ``mh.type``                 Mobility Header Type
+  3          24                               Reserved
+  4          32   ``mh.chksum``               Checksum
+  6          48   ``mh.data``                 Message Data
+======= ========= ================== ===============================
+
+.. [*] https://en.wikipedia.org/wiki/Mobile_IP#Changes_in_IPv6_for_Mobile_IPv6
+
+"""
+# TODO: Implements extractor for message data of all MH types.
+
+import math
+from typing import TYPE_CHECKING, overload
+
+from pcapkit.const.mh.packet import Packet as Enum_Packet
+from pcapkit.const.reg.transtype import TransType as Enum_TransType
+from pcapkit.protocols.data.internet.mh import MH as Data_MH
+from pcapkit.protocols.internet.internet import Internet
+from pcapkit.protocols.schema.internet.mh import MH as Schema_MH
+from pcapkit.utilities.exceptions import UnsupportedCall
+
+if TYPE_CHECKING:
+    from enum import IntEnum as StdlibEnum
+    from typing import IO, Any, NoReturn, Optional, Type
+
+    from aenum import IntEnum as AenumEnum
+    from typing_extensions import Literal
+
+    from pcapkit.corekit.protochain import ProtoChain
+    from pcapkit.protocols.protocol import Protocol
+    from pcapkit.protocols.schema.schema import Schema
+
+__all__ = ['MH']
+
+
+class MH(Internet[Data_MH, Schema_MH],
+         schema=Schema_MH, data=Data_MH):
+    """This class implements Mobility Header."""
+
+    ##########################################################################
+    # Properties.
+    ##########################################################################
+
+    @property
+    def name(self) -> 'Literal["Mobility Header"]':
+        """Name of current protocol."""
+        return 'Mobility Header'
+
+    @property
+    def length(self) -> 'int':
+        """Header length of current protocol."""
+        return self._info.length
+
+    @property
+    def payload(self) -> 'Protocol | NoReturn':
+        """Payload of current instance.
+
+        Raises:
+            UnsupportedCall: if the protocol is used as an IPv6 extension header
+
+        """
+        if self._extf:
+            raise UnsupportedCall(f"'{self.__class__.__name__}' object has no attribute 'payload'")
+        return super().payload
+
+    @property
+    def protocol(self) -> 'Optional[str] | NoReturn':
+        """Name of next layer protocol (if any).
+
+        Raises:
+            UnsupportedCall: if the protocol is used as an IPv6 extension header
+
+        """
+        if self._extf:
+            raise UnsupportedCall(f"'{self.__class__.__name__}' object has no attribute 'protocol'")
+        return super().protocol
+
+    @property
+    def protochain(self) -> 'ProtoChain | NoReturn':
+        """Protocol chain of current instance.
+
+        Raises:
+            UnsupportedCall: if the protocol is used as an IPv6 extension header
+
+        """
+        if self._extf:
+            raise UnsupportedCall(f"'{self.__class__.__name__}' object has no attribute 'protochain'")
+        return super().protochain
+
+    ##########################################################################
+    # Methods.
+    ##########################################################################
+
+    def read(self, length: 'Optional[int]' = None, *, version: 'Literal[4, 6]' = 4,  # pylint: disable=arguments-differ,unused-argument
+             extension: bool = False, **kwargs: 'Any') -> 'Data_MH':  # pylint: disable=unused-argument
+        """Read Mobility Header.
+
+        Structure of MH header [:rfc:`6275`]:
+
+        .. code-block:: text
+
+           +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+           | Payload Proto |  Header Len   |   MH Type     |   Reserved    |
+           +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+           |           Checksum            |                               |
+           +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+                               |
+           |                                                               |
+           .                                                               .
+           .                       Message Data                            .
+           .                                                               .
+           |                                                               |
+           +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+
+        Args:
+            length: Length of packet data.
+            version: IP protocol version.
+            extension: If the protocol is used as an IPv6 extension header.
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Parsed packet data.
+
+        """
+        if length is None:
+            length = len(self)
+        schema = self.__header__
+
+        mh = Data_MH(
+            next=schema.next,
+            length=(schema.length + 1) * 8,
+            type=schema.type,
+            chksum=schema.chksum,
+            data=schema.data,
+        )
+
+        if extension:
+            return mh
+        return self._decode_next_layer(mh, schema.next, length - mh.length)
+
+    def make(self,
+             next: 'Enum_TransType | StdlibEnum | AenumEnum | str | int' = Enum_TransType.UDP,
+             next_default: 'Optional[int]' = None,
+             next_namespace: 'Optional[dict[str, int] | dict[int, str] | Type[StdlibEnum] | Type[AenumEnum]]' = None,  # pylint: disable=line-too-long
+             next_reversed: 'bool' = False,
+             type: 'Enum_Packet | StdlibEnum | AenumEnum | str | int' = Enum_Packet.Binding_Refresh_Request,
+             type_default: 'Optional[int]' = None,
+             type_namespace: 'Optional[dict[str, int] | dict[int, str] | Type[StdlibEnum] | Type[AenumEnum]]' = None,  # pylint: disable=line-too-long
+             type_reversed: 'bool' = False,
+             chksum: 'bytes' = b'',
+             data: 'bytes' = b'\x00\x00',  # minimum length
+             payload: 'Protocol | Schema | bytes' = b'',
+             **kwargs: 'Any') -> 'Schema_MH':
+        """Make (construct) packet data.
+
+        Args:
+            **kwargs: Arbitrary keyword arguments.
+
+        Returns:
+            Constructed packet data.
+
+        """
+        next_val = self._make_index(next, next_default, namespace=next_namespace,  # type: ignore[call-overload]
+                                    reversed=next_reversed, pack=False)
+        type_val = self._make_index(type, type_default, namespace=type_namespace,  # type: ignore[call-overload]
+                                    reversed=type_reversed, pack=False)
+
+        return Schema_MH(
+            next=next_val,
+            length=math.ceil((len(data) + 6) / 8) - 1,
+            type=type_val,
+            chksum=chksum,
+            data=data,
+            payload=payload,
+        )
+
+    ##########################################################################
+    # Data models.
+    ##########################################################################
+
+    @overload
+    def __post_init__(self, file: 'IO[bytes] | bytes', length: 'Optional[int]' = ..., *,  # pylint: disable=arguments-differ
+                      extension: 'bool' = ..., **kwargs: 'Any') -> 'None': ...
+
+    @overload
+    def __post_init__(self, **kwargs: 'Any') -> 'None': ...  # pylint: disable=arguments-differ
+
+    def __post_init__(self, file: 'Optional[IO[bytes] | bytes]' = None, length: 'Optional[int]' = None, *,  # pylint: disable=arguments-differ
+                      extension: 'bool' = False, **kwargs: 'Any') -> 'None':
+        """Post initialisation hook.
+
+        Args:
+            file: Source packet stream.
+            length: Length of packet data.
+            extension: If the protocol is used as an IPv6 extension header.
+            **kwargs: Arbitrary keyword arguments.
+
+        See Also:
+            For construction argument, please refer to :meth:`make`.
+
+        """
+        #: bool: If the protocol is used as an IPv6 extension header.
+        self._extf = extension
+
+        # call super __post_init__
+        super().__post_init__(file, length, extension=extension, **kwargs)  # type: ignore[arg-type]
+
+    def __length_hint__(self) -> 'Literal[6]':
+        """Return an estimated length for the object."""
+        return 6
+
+    @classmethod
+    def __index__(cls) -> 'Enum_TransType':  # pylint: disable=invalid-index-returned
+        """Numeral registry index of the protocol.
+
+        Returns:
+            Numeral registry index of the protocol in `IANA`_.
+
+        .. _IANA: https://www.iana.org/assignments/protocol-numbers/protocol-numbers.xhtml
+
+        """
+        return Enum_TransType.Mobility_Header  # type: ignore[return-value]
+
+    ##########################################################################
+    # Utilities.
+    ##########################################################################
+
+    @classmethod
+    def _make_data(cls, data: 'Data_MH') -> 'dict[str, Any]':  # type: ignore[override]
+        """Create key-value pairs from ``data`` for protocol construction.
+
+        Args:
+            data: protocol data
+
+        Returns:
+            Key-value pairs for protocol construction.
+
+        """
+        return {
+            'next': data.next,
+            'type': data.type,
+            'chksum': data.chksum,
+            'data': data.data,
+            'payload': cls._make_payload(data),
+        }
