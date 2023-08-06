af3a9d1758917a538476ba7f360ea784d913750d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:30 +0000

    Add package async-adbc/1.0.0/async_adbc-1.0.0.tar.gz

---
+++ packages/async-adbc/async_adbc-1.0.0.tar.gz/async_adbc-1.0.0/async_adbc/adbclient.py
@@ -0,0 +1,45 @@
+import asyncio
+import functools
+from types import MethodType
+from typing import Any, AsyncGenerator, Coroutine
+from adbc.device import Device, Status
+from adbc.protocol import Connection, create_connection
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+
+from adbc.service.host import HostService
+
+
+DEFAULT_HOST = "127.0.0.1"
+DEFAULT_PORT = 5037
+
+
+class DeviceNotFoundError(Exception):
+    def __init__(self, serialno: str, *args: object) -> None:
+        super().__init__(f"{serialno} 不存在", *args)
+
+
+@dataclass_json
+@dataclass
+class DeviceStatusNotification:
+    serialno: str
+    status: Status
+
+
+@dataclass_json
+@dataclass
+class ForwardRule:
+    serialno: str
+    local: str
+    remote: str
+
+
+class ADBClient(HostService):
+    def __init__(self, host: str = DEFAULT_HOST, port: int = DEFAULT_PORT) -> None:
+        super().__init__()
+        self.host = host
+        self.port = port
+
+    async def create_connection(self) -> Connection:
+        conn = await create_connection(self.host, self.port)
+        return conn
