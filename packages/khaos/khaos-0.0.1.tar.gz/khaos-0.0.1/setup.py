a290e0bd0b8973a234d0dcb02df7d2dec33e195a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:52 +0000

    Add package khaos/0.0.1/khaos-0.0.1.tar.gz

---
+++ packages/khaos/khaos-0.0.1.tar.gz/khaos-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="khaos",
+    version="0.0.1",
+    author="khaos",
+    author_email="9239867+chaos-god@user.noreply.gitee.com",
+    description="khaos",
+    url="https://gitee.com/chaos-god/khaos.git",
+    packages=find_packages(),
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[
+        'pytest>=7.1.2',
+    ],
+    dependency_links=[
+    ],
+    extras_require={
+    }
+)
