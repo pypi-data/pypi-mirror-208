85aea04fd0d4afc7f3f60a1727fc7d7dd8fbace9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:40:46 +0000

    Add package ttaken/0.3.0/ttaken-0.3.0-py3-none-any.whl

---
+++ packages/ttaken/ttaken-0.3.0-py3-none-any.whl/ttaken.py
@@ -0,0 +1,20 @@
+#!/usr/local/bin/python3
+
+import logging
+import time
+
+"""
+This module prints the time taken by a function where it is decorated
+"""
+
+def ttaken(logger):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            end_time = time.time()
+            elapsed_time = end_time - start_time
+            logger.info(f"func.__name__ took {elapsed_time} seconds to execute!")
+            return result
+        return wrapper
+    return decorator
