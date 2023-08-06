4a3233259ae51e1a15125e58fd0a4e27157063b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:01 +0000

    Add package Public-API-Football4/0.1/Public_API_Football4-0.1.tar.gz

---
+++ packages/Public-API-Football4/Public_API_Football4-0.1.tar.gz/Public_API_Football4-0.1/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+setup(
+    name='Public_API_Football4',
+    version='0.1',
+    author='Dmytro Chebotarov',
+    author_email='chebodi@ukr.net',
+    packages=find_packages(),
+    license='MIT',
+    install_requires=[
+        'requests',
+    ],
+)
+
+# packages=find_packages(exclude="Library"),
+# packages=['Library'],
