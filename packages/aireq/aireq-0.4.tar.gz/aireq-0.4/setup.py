b46682ae460137401f9ce4c85ba49d8b900b473c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:37 +0000

    Add package aireq/0.4/aireq-0.4.tar.gz

---
+++ packages/aireq/aireq-0.4.tar.gz/aireq-0.4/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="aireq",
+      version="0.4",
+      description="It contains all ipppr codes",
+      author="Shashank",
+      packages=['ai','ippr','mwa'],
+      install_requires=[])
