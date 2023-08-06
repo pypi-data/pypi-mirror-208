256e3ade1559f0f566610d57dfb9b630dd07c560
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:44:27 +0000

    Add package browserist/1.4.6/browserist-1.4.6-py3-none-any.whl

---
+++ packages/browserist/browserist-1.4.6-py3-none-any.whl/browserist/model/browser/base/driver.py
@@ -0,0 +1,82 @@
+from abc import ABC, abstractmethod
+
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+from selenium.webdriver.edge.options import Options as EdgeOptions
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.ie.options import Options as IEOptions
+from selenium.webdriver.safari.options import Options as SafariOptions
+
+from .... import helper
+from .settings import BrowserSettings
+from .type import BrowserType
+
+
+class BrowserDriver(ABC):
+    """Abstract class that contains the Selenium web driver based on browser type and configuration."""
+
+    __slots__ = ["settings", "chrome_options", "edge_options", "firefox_options",
+                 "ie_options", "safari_options", "webdriver"]
+
+    def __init__(self, settings: BrowserSettings) -> None:
+        """Initiates basic properties of the Selenium web driver."""
+
+        self.settings = settings
+        helper.directory.create_if_not_exists(self.settings._screenshot_dir)
+
+        match(self.settings.type):
+            case BrowserType.CHROME | BrowserType.OPERA:
+                self.chrome_options: ChromeOptions = ChromeOptions()
+            case BrowserType.EDGE:
+                self.edge_options: EdgeOptions = EdgeOptions()
+            case BrowserType.FIREFOX:
+                self.firefox_options: FirefoxOptions = FirefoxOptions()
+            case BrowserType.INTERNET_EXPLORER:
+                self.ie_options: IEOptions = IEOptions()
+            case BrowserType.SAFARI:
+                self.safari_options: SafariOptions = SafariOptions()
+
+        self.ensure_browser_type()
+        self.set_options_and_profile()
+        self.webdriver: object = self.set_webdriver()
+
+    @abstractmethod
+    def ensure_browser_type(self) -> None:
+        """Method to ensure the correct browser type if a specific browser instance is created directly from a subclass (e.g. FirefoxBrowserDriver) without the optional settings as argument, simply as Chrome is default browser."""
+
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def set_webdriver(self) -> object:
+        """Method to set web driver based on the settings."""
+
+        raise NotImplementedError  # pragma: no cover
+
+    def set_options_and_profile(self) -> None:
+        """Internal task initializer that runs the configuration methods to disable images, enable headless, etc."""
+
+        self.disable_images()
+        self.enable_headless()
+        self.set_page_load_strategy()
+
+    @abstractmethod
+    def disable_images(self) -> None:
+        """Method to configure web driver to disable download of images for faster browsing."""
+
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def enable_headless(self) -> None:
+        """Method to enable headless version of web driver (i.e. don't open browser window) for faster browsing."""
+
+        raise NotImplementedError  # pragma: no cover
+
+    @abstractmethod
+    def set_page_load_strategy(self) -> None:
+        """Method to set the page load strategy to define whether the web driver should wait until all assets are downloaded (slower) or not (faster)."""
+
+        raise NotImplementedError  # pragma: no cover
+
+    def get_webdriver(self) -> object:
+        """Returns the Selenium web driver."""
+
+        return self.webdriver
---
+++ packages/browserist/browserist-1.4.6-py3-none-any.whl/browserist/model/browser/firefox.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+
+from ... import factory
+from .base.driver import BrowserDriver
+from .base.type import BrowserType
+
+
+class FirefoxBrowserDriver(BrowserDriver):
+    def ensure_browser_type(self) -> None:
+        self.settings.type = BrowserType.FIREFOX
+
+    def set_webdriver(self) -> object:
+        if self.settings._path_to_executable is None:
+            return webdriver.Firefox(
+                options=self.firefox_options)
+        else:
+            return webdriver.Firefox(
+                executable_path=self.settings._path_to_executable,
+                options=self.firefox_options)
+
+    def disable_images(self) -> None:
+        if self.settings.disable_images:
+            self.firefox_options.set_preference("permissions.default.image", 2)
+            self.firefox_options.set_preference("dom.ipc.plugins.enabled.libflashplayer.so", "false")
+
+    def enable_headless(self) -> None:
+        if self.settings.headless:
+            self.firefox_options.add_argument("--headless")  # type: ignore
+
+    def set_page_load_strategy(self) -> None:
+        self.firefox_options = factory.set.page_load_strategy(self, self.firefox_options)  # type: ignore
