26c0a4f6bbc1f4125d35272fb437962d55f46c66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:12:08 +0000

    Add package glaceon/1.2.0/glaceon-1.2.0.tar.gz

---
+++ packages/glaceon/glaceon-1.2.0.tar.gz/glaceon-1.2.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+	name="glaceon",
+	version = "1.2.0",
+	author = "zappy",
+	description = "A Simple And Clean Logging Library That Can Be Used For Any Type Of Application.",
+	packages=["glaceon"]
+)
