608a0c2236c312208462695f77752eb0ccf160f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:23 +0000

    Add package worktoy/0.28.8/worktoy-0.28.8.tar.gz

---
+++ packages/worktoy/worktoy-0.28.8.tar.gz/worktoy-0.28.8/src/worktoy/__init__.py
@@ -0,0 +1,13 @@
+"""Documentation: WorkToy
+Collection of General Utilities"""
+#  MIT License
+#  Copyright (c) 2023 Asger Jon Vistisen
+from __future__ import annotations
+
+from ._maybe import maybe
+from ._maybeType import maybeType
+from ._searchKeys import searchKeys
+from ._maybeTypes import maybeTypes
+from ._stringlist import stringList
+from ._anywayuwantit import AnywayUWantIt
+from ._callmemaybe import CallMeMaybe
