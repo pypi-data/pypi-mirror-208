e1e1480cb820e4e9cba2cbd1986f571c1a24a839
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:54 +0000

    Add package siliconcompiler/0.11.2/siliconcompiler-0.11.2-cp39-cp39-macosx_11_0_arm64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.11.2-cp39-cp39-macosx_11_0_arm64.whl/siliconcompiler/tools/verilator/lint.py
@@ -0,0 +1,24 @@
+
+from siliconcompiler.tools.verilator.verilator import setup as setup_tool
+
+
+def setup(chip):
+    '''
+    Lints Verilog source. Takes in a single pickled Verilog file from
+    ``inputs/<design>.v`` and produces no outputs. Results of linting can be
+    programatically queried using errors/warnings metrics.
+    '''
+
+    # Generic tool setup.
+    setup_tool(chip)
+
+    tool = 'verilator'
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    task = chip._get_task(step, index)
+    design = chip.top()
+
+    chip.add('tool', tool, 'task', task, 'option', ['--lint-only', '--debug'],
+             step=step, index=index)
+    chip.set('tool', tool, 'task', task, 'input', f'{design}.v',
+             step=step, index=index)
