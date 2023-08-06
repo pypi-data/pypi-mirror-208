ba4db85e8eecd23393e981865dc60890ad14b3cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:51 +0000

    Add package siliconcompiler/0.11.2/siliconcompiler-0.11.2-cp38-cp38-win_amd64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.11.2-cp38-cp38-win_amd64.whl/siliconcompiler/tools/verilator/compile.py
@@ -0,0 +1,48 @@
+import subprocess
+
+from siliconcompiler.tools.verilator.verilator import setup as setup_tool
+
+
+def setup(chip):
+    '''
+    Compiles Verilog and C/C++ sources into an executable.  Takes in a single
+    pickled Verilog file from ``inputs/<design>.v`` and a set of C/C++ sources
+    from :keypath:`input, c`. Outputs an executable in
+    ``outputs/<design>.vexe``.
+
+    This step supports using the :keypath:`option, trace` parameter to enable
+    Verilator's ``--trace`` flag.
+    '''
+
+    # Generic tool setup.
+    setup_tool(chip)
+
+    tool = 'verilator'
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    task = chip._get_task(step, index)
+    design = chip.top()
+
+    chip.add('tool', tool, 'task', task, 'option', ['--cc', '--exe'],
+             step=step, index=index)
+    chip.set('tool', tool, 'task', task, 'input', f'{design}.v',
+             step=step, index=index)
+    chip.add('tool', tool, 'task', task, 'option', f'-o ../outputs/{design}.vexe',
+             step=step, index=index)
+
+
+def post_process(chip):
+    ''' Tool specific function to run after step execution
+    '''
+
+    design = chip.top()
+
+    # Run make to compile Verilated design into executable.
+    # If we upgrade our minimum supported Verilog, we can remove this and
+    # use the --build flag instead.
+    proc = subprocess.run(['make', '-C', 'obj_dir', '-f', f'V{design}.mk'])
+    if proc.returncode > 0:
+        chip.error(
+            f'Make returned error code {proc.returncode} when compiling '
+            'Verilated design', fatal=True
+        )
