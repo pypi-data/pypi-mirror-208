a69d2bd8fe81ae733c7e0743be6ee626cc1757cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:19 +0000

    Add package pyvrp/0.2.1/pyvrp-0.2.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyvrp/pyvrp-0.2.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyvrp/cpp/Individual.h
@@ -0,0 +1,129 @@
+#ifndef INDIVIDUAL_H
+#define INDIVIDUAL_H
+
+#include "ProblemData.h"
+#include "XorShift128.h"
+
+#include <string>
+#include <vector>
+
+class Individual
+{
+    friend struct std::hash<Individual>;  // friend struct to enable hashing
+
+    using Client = int;
+    using Route = std::vector<Client>;
+    using Routes = std::vector<Route>;
+
+    size_t numRoutes_ = 0;   // Number of routes
+    size_t distance_ = 0;    // Total distance
+    size_t excessLoad_ = 0;  // Total excess load over all routes
+    size_t timeWarp_ = 0;    // Total time warp over all routes
+
+    Routes routes_;  // Routes - only the first numRoutes_ are non-empty
+    std::vector<std::pair<Client, Client>> neighbours;  // pairs of [pred, succ]
+
+    // Determines the [pred, succ] pairs for each client.
+    void makeNeighbours();
+
+    // Evaluates this solution's characteristics.
+    void evaluate(ProblemData const &data);
+
+public:
+    /**
+     * Returns the number of non-empty routes in this individual's solution.
+     * Such non-empty routes are guaranteed to be in the lower indices of the
+     * routes returned by ``getRoutes``.
+     */
+    [[nodiscard]] size_t numRoutes() const;
+
+    /**
+     * Returns this individual's routing decisions.
+     */
+    [[nodiscard]] Routes const &getRoutes() const;
+
+    /**
+     * Returns a vector of [pred, succ] clients for each client (index) in this
+     * individual's routes. Includes the depot at index 0.
+     */
+    [[nodiscard]] std::vector<std::pair<Client, Client>> const &
+    getNeighbours() const;
+
+    /**
+     * @return True when this solution is feasible; false otherwise.
+     */
+    [[nodiscard]] bool isFeasible() const;
+
+    /**
+     * @return True if the solution violates load constraints.
+     */
+    [[nodiscard]] bool hasExcessLoad() const;
+
+    /**
+     * @return True if the solution violates time window constraints.
+     */
+    [[nodiscard]] bool hasTimeWarp() const;
+
+    /**
+     * @return Total distance over all routes.
+     */
+    [[nodiscard]] size_t distance() const;
+
+    /**
+     * @return Total excess load over all routes.
+     */
+    [[nodiscard]] size_t excessLoad() const;
+
+    /**
+     * @return Total time warp over all routes.
+     */
+    [[nodiscard]] size_t timeWarp() const;
+
+    bool operator==(Individual const &other) const;
+
+    Individual &operator=(Individual const &other) = delete;  // is immutable
+    Individual &operator=(Individual &&other) = delete;       // is immutable
+
+    Individual(Individual const &other) = default;
+    Individual(Individual &&other) = default;
+
+    /**
+     * Constructs a random individual using the given random number generator.
+     *
+     * @param data           Data instance describing the problem that's being
+     *                       solved.
+     * @param rng            Random number generator.
+     */
+    Individual(ProblemData const &data, XorShift128 &rng);
+
+    /**
+     * Constructs an individual having the given routes as its solution.
+     *
+     * @param data           Data instance describing the problem that's being
+     *                       solved.
+     * @param routes         Solution's route list.
+     */
+    Individual(ProblemData const &data, Routes routes);
+};
+
+// Outputs an individual into a given ostream in VRPLIB format
+std::ostream &operator<<(std::ostream &out, Individual const &indiv);
+
+namespace std
+{
+template <> struct hash<Individual>
+{
+    std::size_t operator()(Individual const &individual) const
+    {
+        size_t res = 17;
+        res = res * 31 + std::hash<size_t>()(individual.numRoutes_);
+        res = res * 31 + std::hash<size_t>()(individual.distance_);
+        res = res * 31 + std::hash<size_t>()(individual.excessLoad_);
+        res = res * 31 + std::hash<size_t>()(individual.timeWarp_);
+
+        return res;
+    }
+};
+}  // namespace std
+
+#endif
---
+++ packages/pyvrp/pyvrp-0.2.1-cp310-cp310-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyvrp/cpp/Individual_bindings.cpp
@@ -0,0 +1,60 @@
+#include "Individual.h"
+
+#include <pybind11/operators.h>
+#include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
+
+#include <sstream>
+
+namespace py = pybind11;
+
+PYBIND11_MODULE(_Individual, m)
+{
+    py::class_<Individual>(m, "Individual")
+        .def(py::init<ProblemData const &, std::vector<std::vector<int>>>(),
+             py::arg("data"),
+             py::arg("routes"))
+        .def_property_readonly_static(
+            "make_random",                // this is a bit of a workaround for
+            [](py::object)                // classmethods, because pybind does
+            {                             // not yet support those natively.
+                return py::cpp_function(  // See issue 1693 in the pybind repo.
+                    [](ProblemData const &data, XorShift128 &rng) {
+                        return Individual(data, rng);
+                    },
+                    py::arg("data"),
+                    py::arg("rng"));
+            })
+        .def("num_routes", &Individual::numRoutes)
+        .def("get_routes",
+             &Individual::getRoutes,
+             py::return_value_policy::reference_internal)
+        .def("get_neighbours",
+             &Individual::getNeighbours,
+             py::return_value_policy::reference_internal)
+        .def("is_feasible", &Individual::isFeasible)
+        .def("has_excess_load", &Individual::hasExcessLoad)
+        .def("has_time_warp", &Individual::hasTimeWarp)
+        .def("distance", &Individual::distance)
+        .def("excess_load", &Individual::excessLoad)
+        .def("time_warp", &Individual::timeWarp)
+        .def(
+            "__copy__",
+            [](Individual const &individual) { return Individual(individual); })
+        .def(
+            "__deepcopy__",
+            [](Individual const &individual, py::dict) {
+                return Individual(individual);
+            },
+            py::arg("memo"))
+        .def("__hash__",
+             [](Individual const &individual) {
+                 return std::hash<Individual>()(individual);
+             })
+        .def(pybind11::self == pybind11::self)  // this is __eq__
+        .def("__str__", [](Individual const &individual) {
+            std::stringstream stream;
+            stream << individual;
+            return stream.str();
+        });
+}
