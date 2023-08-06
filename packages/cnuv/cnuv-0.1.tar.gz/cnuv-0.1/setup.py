9cb452874674480e138e69ca495e5f3c756eb896
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:41 +0000

    Add package cnuv/0.1/cnuv-0.1.tar.gz

---
+++ packages/cnuv/cnuv-0.1.tar.gz/cnuv-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="cnuv",
+version="0.1",
+description="This is the package with all the cn codes",
+author="Yuvraj",
+packages=['pkg'],
+install_requires = [])
