f4cbfdea53426348e0bc8368637f18b4713b40a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:44 +0000

    Add package piicli/0.0.1/piicli-0.0.1-py3-none-any.whl

---
+++ packages/piicli/piicli-0.0.1-py3-none-any.whl/piicli/main.py
@@ -0,0 +1,15 @@
+
+def get_val():
+    return "Working fine"
+
+
+def main():
+    val = get_val()
+    print(val)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
