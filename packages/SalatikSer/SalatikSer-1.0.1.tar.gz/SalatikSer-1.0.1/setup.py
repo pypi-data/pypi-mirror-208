acd7be8402657d6ef18a9f6a5e288db8d586ade4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:11:33 +0000

    Add package SalatikSer/1.0.1/SalatikSer-1.0.1.tar.gz

---
+++ packages/SalatikSer/SalatikSer-1.0.1.tar.gz/SalatikSer-1.0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="SalatikSer",
+    version="1.0.1",
+    description="JSON and XLM serializer",
+    author="Daniil Litvinets",
+    author_email="salatik.dan@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+)
