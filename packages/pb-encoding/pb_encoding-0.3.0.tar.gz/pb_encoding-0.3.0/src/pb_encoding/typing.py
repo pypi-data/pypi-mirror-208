d5db51094474d21b7acf870c03914dc18b20c88f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:16:15 +0000

    Add package pb-encoding/0.3.0/pb_encoding-0.3.0.tar.gz

---
+++ packages/pb-encoding/pb_encoding-0.3.0.tar.gz/pb_encoding-0.3.0/src/pb_encoding/typing.py
@@ -0,0 +1,16 @@
+from typing import Literal
+
+SupportedType = Literal[
+    "int32",
+    "int64",
+    "uint32",
+    "uint64",
+    "sint32",
+    "sint64",
+    "fixed32",
+    "fixed64",
+    "sfixed32",
+    "sfixed64",
+    "string",
+    "bytes",
+]
