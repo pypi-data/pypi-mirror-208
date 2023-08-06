1bb99a001b53da81fe51a37fc776ed92c4a90774
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:46 +0000

    Add package MyConnect-SQL/1.0.4/MyConnect%20-%20SQL-1.0.4.tar.gz

---
+++ packages/MyConnect-SQL/MyConnect%20-%20SQL-1.0.4.tar.gz/MyConnect - SQL-1.0.4/MyConnect/moduleTest.py
@@ -0,0 +1,24 @@
+import AuthLogin
+
+if AuthLogin.username == "":
+    # Title
+
+    AuthLogin.login.setTitle("AuthLogin - Login")
+
+    # Setup
+
+    AuthLogin.login.form.form('Login', '23', '#57a1f8', '300', '70')
+    AuthLogin.login.form.userInput(widht=25)
+    AuthLogin.login.form.passwordInput(widht=25)
+    AuthLogin.login.form.buttonLogin()
+
+    # Config login
+            
+
+    AuthLogin.login.MySQLConfig('localhost', 'root', '', 'user', 'user')
+
+    # Loop
+
+    AuthLogin.login.loop()
+else:
+    print("Welcome "+AuthLogin.username)
