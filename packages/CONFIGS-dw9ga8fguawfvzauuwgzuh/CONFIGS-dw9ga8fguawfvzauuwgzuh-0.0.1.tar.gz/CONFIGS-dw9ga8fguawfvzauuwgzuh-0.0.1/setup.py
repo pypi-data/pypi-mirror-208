98eda4719f6d69c718e3b024797a50ccd274d6b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:06 +0000

    Add package CONFIGS-dw9ga8fguawfvzauuwgzuh/0.0.1/CONFIGS-dw9ga8fguawfvzauuwgzuh-0.0.1.tar.gz

---
+++ packages/CONFIGS-dw9ga8fguawfvzauuwgzuh/CONFIGS-dw9ga8fguawfvzauuwgzuh-0.0.1.tar.gz/CONFIGS-dw9ga8fguawfvzauuwgzuh-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+VERSION = '0.0.1'
+DESCRIPTION = 'A python package to make configs easier!'
+LONG_DESCRIPTION = 'A package that allows to read and write to configs.'
+
+setup(
+    name="CONFIGS-dw9ga8fguawfvzauuwgzuh",
+    version=VERSION,
+    author="tntgamer685347",
+    author_email="gamer@fampl.de",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
