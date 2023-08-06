9f53ea4683c1e9d3dd8e17d21e0c79964c80a3be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:09 +0000

    Add package qwert1234/1.0.2/qwert1234-1.0.2.tar.gz

---
+++ packages/qwert1234/qwert1234-1.0.2.tar.gz/qwert1234-1.0.2/qwert1234/back.py
@@ -0,0 +1,2 @@
+def main()：
+    print("test")
---
+++ packages/qwert1234/qwert1234-1.0.2.tar.gz/qwert1234-1.0.2/qwert1234/setup.py
@@ -0,0 +1,14 @@
+from setuptools import find_packages, setup
+import os
+setup(
+    name="test",
+    version="",
+    description="",
+    author="",
+    author_email="",
+    platforms=["any"],
+    license="",
+    url="",
+    packages=find_packages(),
+    long_description_content_type="text/x-rst",
+    long_description="test")
