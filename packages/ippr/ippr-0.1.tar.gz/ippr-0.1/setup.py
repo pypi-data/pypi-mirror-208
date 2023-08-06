5777f762b2554281452b0fc8abde3f7771c75d55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:36 +0000

    Add package ippr/0.1/ippr-0.1.tar.gz

---
+++ packages/ippr/ippr-0.1.tar.gz/ippr-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="ippr",
+      version="0.1",
+      description="It contains all ipppr codes",
+      author="Shashank",
+      packages=['ai','ippr'],
+      install_requires=[])
