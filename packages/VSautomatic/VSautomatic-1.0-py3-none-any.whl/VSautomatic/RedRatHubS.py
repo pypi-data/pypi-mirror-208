3581babcc3c0bbf55ec44a1331c94b1bb63b6113
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:12:42 +0000

    Add package VSautomatic/1.0/VSautomatic-1.0-py3-none-any.whl

---
+++ packages/VSautomatic/VSautomatic-1.0-py3-none-any.whl/VSautomatic/RedRatHubS.py
@@ -0,0 +1,45 @@
+
+import subprocess
+from time import sleep
+import os
+from VSautomatic.TestCfgParse import CfgParse
+
+
+log_file_path = os.path.realpath(os.path.dirname(os.path.dirname(__file__)) + '//VSautomatic')
+# AutoTestAppINI = log_file_path + '/AutoTestApp.INI'
+# restCfg = CfgParse(AutoTestAppINI)
+# RedRatdate = restCfg.GetCfgInfo("RedRat","RedRat_dataset")
+
+
+
+class RedRatHubCmdSetUp():
+
+    def __init__(self,RedRatdate):
+        self.RedRatdate = RedRatdate
+
+    def run(self):
+        try:
+            # print(log_file_path + '\RedRatHub-V4.27\RedRatHubCmd '+ self.RedRatdate + '.xml')
+            subprocess.Popen(log_file_path + '\RedRatHub-V4.27\RedRatHubCmd  ' + log_file_path + '\\RedRatHub-V4.27\\' + self.RedRatdate + '.xml')
+            print("RedRatHubCmd server boot up")
+
+        except Exception as e:
+            print("Error: 无法启动进程")
+            print(repr(e))
+
+
+    def kill(self):
+        try:
+            subprocess.Popen('taskkill /F /im RedRatHubCmd.exe')
+            print("kill RedRatHubCmd server")
+
+        except Exception as e:
+            print("Error: 无法启动进程")
+            print(repr(e))
+
+
+if __name__ == '__main__':
+    RedRat = RedRatHubCmdSetUp()
+    RedRat.run()
+    sleep(60)
+    RedRat.kill()
