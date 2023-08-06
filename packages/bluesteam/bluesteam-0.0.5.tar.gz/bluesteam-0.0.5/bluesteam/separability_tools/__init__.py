f518b00522b8de5b4cf7ca27ff520780e5305b25
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:34 +0000

    Add package bluesteam/0.0.5/bluesteam-0.0.5.tar.gz

---
+++ packages/bluesteam/bluesteam-0.0.5.tar.gz/bluesteam-0.0.5/bluesteam/separability_tools/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Bluesteam
+# Copyright (C) 2022-, Sarang Bhagwat <sarangb2@illinois.edu>
+# 
+# This module is under the MIT open-source license. See 
+# github.com/BluestemBiosciences/bluesteam/LICENSE.txt
+# for license details.
+"""
+"""
+__version__ = '0.0.1'
+__author__ = 'Sarang S. Bhagwat'
+
+# %% Initialize AutoSynthesis
+
+from . import estimate_separability, utils
+
+
+__all__ = (
+    'estimate_separability', 'utils',
+)
