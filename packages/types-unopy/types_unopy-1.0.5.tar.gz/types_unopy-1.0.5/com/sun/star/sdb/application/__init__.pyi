9c61e066f0eb9c6c9bb16b63cd8f4274d4ca52cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:36 +0000

    Add package types-unopy/1.0.5/types_unopy-1.0.5.tar.gz

---
+++ packages/types-unopy/types_unopy-1.0.5.tar.gz/types_unopy-1.0.5/com/sun/star/sdb/application/__init__.pyi
@@ -0,0 +1,24 @@
+# coding: utf-8
+#
+# Copyright 2023 :Barry-Thomas-Paul: Moss
+#
+# Licensed under the Apache License, Version 2.0 (the "License")
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http: // www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+from ..._pyi.sdb.application.copy_table_row_event import CopyTableRowEvent as CopyTableRowEvent
+from ..._pyi.sdb.application.copy_table_wizard import CopyTableWizard as CopyTableWizard
+from ..._pyi.sdb.application.default_view_controller import DefaultViewController as DefaultViewController
+from ..._pyi.sdb.application.named_database_object import NamedDatabaseObject as NamedDatabaseObject
+from ..._pyi.sdb.application.x_copy_table_listener import XCopyTableListener as XCopyTableListener
+from ..._pyi.sdb.application.x_copy_table_wizard import XCopyTableWizard as XCopyTableWizard
+from ..._pyi.sdb.application.x_database_document_ui import XDatabaseDocumentUI as XDatabaseDocumentUI
+from ..._pyi.sdb.application.x_table_ui_provider import XTableUIProvider as XTableUIProvider
