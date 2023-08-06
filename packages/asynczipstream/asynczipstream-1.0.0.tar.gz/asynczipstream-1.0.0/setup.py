e30ba6883c48a43a8349a60d0b0548cd4d76b4cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:09:42 +0000

    Add package asynczipstream/1.0.0/asynczipstream-1.0.0.tar.gz

---
+++ packages/asynczipstream/asynczipstream-1.0.0.tar.gz/asynczipstream-1.0.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='asynczipstream',
+    version='1.0.0',
+    description='Asynchronous Zipfile generator that takes input files as well as streams',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='lososkin',
+    author_email='yalososka@gmail.com',
+    url='https://github.com/lososkin/python-zipstream-aio',
+    packages=find_packages(exclude=['tests']),
+    keywords='async zip streaming',
+    install_requires="aiofiles>22.1.0",
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Topic :: System :: Archiving :: Compression",
+    ],
+)
