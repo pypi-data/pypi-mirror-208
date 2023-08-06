2e58ad82218fdb417cf14729080b91210cd01a1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:12:34 +0000

    Add package Geode-Conversion/5.0.5/Geode_Conversion-5.0.5-cp310-cp310-win_amd64.whl

---
+++ packages/Geode-Conversion/Geode_Conversion-5.0.5-cp310-cp310-win_amd64.whl/geode_conversion/model.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2019 - 2023 Geode-solutions. All rights reserved.
+#
+
+import opengeode
+import geode_common
+
+from .bin.geode_conversion_py_model import *
+ConversionModelLibrary.initialize()
