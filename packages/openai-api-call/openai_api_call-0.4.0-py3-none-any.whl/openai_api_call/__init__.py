fc3dc4b55a9517f0a51bd2e8d860d346ac4e225a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:50:37 +0000

    Add package openai-api-call/0.4.0/openai_api_call-0.4.0-py3-none-any.whl

---
+++ packages/openai-api-call/openai_api_call-0.4.0-py3-none-any.whl/openai_api_call/__init__.py
@@ -0,0 +1,35 @@
+"""Top-level package for Openai API call."""
+
+__author__ = """Rex Wang"""
+__email__ = '1073853456@qq.com'
+__version__ = '0.4.0'
+
+import os
+from .chattool import Chat, Resp, chat_completion, usage_status
+from .proxy import proxy_on, proxy_off, proxy_status
+
+# read API key from the environment variable
+if os.environ.get('OPENAI_API_KEY') is not None:
+    api_key = os.environ.get('OPENAI_API_KEY')
+    # skip checking the validity of the API key
+    # if not api_key.startswith("sk-"):
+    #     print("Warning: The default environment variable `OPENAI_API_KEY` is not a valid API key.")
+else:
+    api_key = None
+
+def show_apikey():
+    if api_key is not None:
+        print(f"API key:\t{api_key}")
+    else:
+        print("API key is not set!")
+
+def default_prompt(msg:str):
+    """Default prompt message for the API call
+
+    Args:
+        msg (str): prompt message
+
+    Returns:
+        List[Dict]: default prompt message
+    """
+    return [{"role": "user", "content": msg},]
