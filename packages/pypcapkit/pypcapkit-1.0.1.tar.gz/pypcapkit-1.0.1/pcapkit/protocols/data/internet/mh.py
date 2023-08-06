5db56a1802d65895319027d51f1a0a9d008129e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:14 +0000

    Add package pypcapkit/1.0.1/pypcapkit-1.0.1.tar.gz

---
+++ packages/pypcapkit/pypcapkit-1.0.1.tar.gz/pypcapkit-1.0.1/pcapkit/protocols/data/internet/mh.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""data model for MH protocol"""
+
+from typing import TYPE_CHECKING
+
+from pcapkit.corekit.infoclass import info_final
+from pcapkit.protocols.data.data import Data
+
+if TYPE_CHECKING:
+    from pcapkit.const.mh.packet import Packet
+    from pcapkit.const.reg.transtype import TransType
+
+__all__ = ['MH']
+
+
+@info_final
+class MH(Data):
+    """Data model for MH protocol."""
+
+    #: Next header.
+    next: 'TransType'
+    #: Header length.
+    length: 'int'
+    #: Mobility header type.
+    type: 'Packet'
+    #: Checksum.
+    chksum: 'bytes'
+    #: Message data.
+    data: 'bytes'
+
+    if TYPE_CHECKING:
+        def __init__(self, next: 'TransType', length: 'int', type: 'Packet', chksum: 'bytes', data: 'bytes') -> 'None': ...  # pylint: disable=unused-argument,super-init-not-called,multiple-statements,redefined-builtin,line-too-long
