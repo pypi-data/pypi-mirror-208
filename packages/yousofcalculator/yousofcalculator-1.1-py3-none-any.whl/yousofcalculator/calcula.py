ae950e4de8d0029ca37680a42f389d87281051a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:40 +0000

    Add package yousofcalculator/1.1/yousofcalculator-1.1-py3-none-any.whl

---
+++ packages/yousofcalculator/yousofcalculator-1.1-py3-none-any.whl/yousofcalculator/calcula.py
@@ -0,0 +1,44 @@
+class calc:
+    """
+    This is calc module have add,sub,mult,div function
+    """
+    def add(self,num1,num2):
+        """
+        add 2 number.
+        Parameters:
+        add(num1=int,num2=int) 
+        Returns:
+        int:the sum of num1 and num2  
+        """
+        return num1+num2
+
+
+    def sub(self,num1,num2):
+        """
+        sub 2 number.
+        Parameters:
+        sub(num1=int,num2=int) 
+        Returns:
+        int:the sub of num1 and num2
+        """ 
+        return num1-num2
+
+    def mult(self,num1,num2):
+        """
+        multiple 2 number.
+        Parameters:
+        mul(num1=int,num2=int) 
+        Returns:
+        int:the multiple of num1 and num2
+        """ 
+        return num1*num2
+    def div(self,num1,num2):
+        """
+        division 2 number.
+        Parameters:
+        div(num1=int,num2=int) 
+        Returns:
+        int:the division of num1 and num2
+        """ 
+        return num1/num2
+       
