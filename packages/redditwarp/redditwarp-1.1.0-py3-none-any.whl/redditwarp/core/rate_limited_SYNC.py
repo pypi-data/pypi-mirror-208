0eb65c480f446711de6572c67ce162a765bf4e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:15 +0000

    Add package redditwarp/1.1.0/redditwarp-1.1.0-py3-none-any.whl

---
+++ packages/redditwarp/redditwarp-1.1.0-py3-none-any.whl/redditwarp/core/rate_limited_SYNC.py
@@ -0,0 +1,64 @@
+
+from __future__ import annotations
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from ..http.handler_SYNC import Handler
+    from ..http.send_params import SendParams
+    from ..http.exchange import Exchange
+
+import time
+
+from ..http.delegating_handler_SYNC import DelegatingHandler
+from ..util.token_bucket import TokenBucket
+
+class RateLimited(DelegatingHandler):
+    def __init__(self, handler: Handler) -> None:
+        super().__init__(handler)
+        self.remaining: int = 0
+        ("")
+        self.reset: int = 0
+        ("")
+        self.used: int = 0
+        ("")
+        self._delta: float = 0.
+        self._timestamp: float = time.monotonic()
+        self._burst_control = TokenBucket(5, 1/2)
+
+    def _send(self, p: SendParams) -> Exchange:
+        h = self._burst_control.hard_consume(1)
+        s = 0.
+        if self.remaining <= 1:
+            s = self.reset
+        else:
+            w = self.reset / self.remaining
+            if not h or w >= 2:
+                # Burst this request, but only if the API didn't
+                # want us to wait for more than two seconds.
+                # Use 2 because at worst the user is allocated a 2/s (600/300)
+                # rate limit when a client credentials grant is used.
+                s = w
+
+        time.sleep(s)
+
+        xchg = super()._send(p)
+
+        now = time.monotonic()
+        self._delta = now - self._timestamp
+        self._timestamp = now
+
+        headers = xchg.response.headers
+        if 'x-ratelimit-reset' in headers:
+            self.remaining = int(float(headers['x-ratelimit-remaining']))
+            self.reset = int(headers['x-ratelimit-reset'])
+            self.used = int(headers['x-ratelimit-used'])
+        else:
+            if self.reset > 0:
+                self.remaining -= 1
+                self.reset = max(0, self.reset - int(self._delta))
+                self.used += 1
+            else:
+                self.remaining = 300
+                self.reset = 600
+                self.used = 0
+
+        return xchg
---
+++ packages/redditwarp/redditwarp-1.1.0-py3-none-any.whl/redditwarp/core/recorded_SYNC.py
@@ -0,0 +1,111 @@
+
+from __future__ import annotations
+from typing import TYPE_CHECKING, Optional, MutableSequence
+if TYPE_CHECKING:
+    from ..http.handler_SYNC import Handler
+    from ..http.send_params import SendParams
+    from ..http.exchange import Exchange
+    from ..http.requisition import Requisition
+    from ..http.request import Request
+    from ..http.response import Response
+
+from collections import deque
+
+from ..http.delegating_handler_SYNC import DelegatingHandler
+
+class Recorded(DelegatingHandler):
+    def __init__(self, handler: Handler) -> None:
+        super().__init__(handler)
+        self.last_requisition: Optional[Requisition] = None
+        ("")
+        self.last_exchange: Optional[Exchange] = None
+        ("")
+        self.last_transmit: Optional[tuple[Requisition, Optional[Exchange]]] = None
+        ("")
+        self.last_transfer: Optional[tuple[Requisition, Exchange]] = None
+        ("")
+        self.last_requisition_queue: MutableSequence[Requisition] = deque(maxlen=16)
+        ("")
+        self.last_exchange_queue: MutableSequence[Exchange] = deque(maxlen=16)
+        ("")
+        self.last_transmit_queue: MutableSequence[tuple[Requisition, Optional[Exchange]]] = deque(maxlen=16)
+        ("")
+        self.last_transfer_queue: MutableSequence[tuple[Requisition, Exchange]] = deque(maxlen=16)
+        ("")
+
+    def clear(self) -> None:
+        self.last_requisition = None
+        self.last_exchange = None
+        self.last_transmit = None
+        self.last_transfer = None
+        self.last_requisition_queue.clear()
+        self.last_exchange_queue.clear()
+        self.last_transmit_queue.clear()
+        self.last_transfer_queue.clear()
+
+    def _send(self, p: SendParams) -> Exchange:
+        exchange = None
+        try:
+            exchange = super()._send(p)
+        finally:
+            self.last_requisition = p.requisition
+            self.last_transmit = (p.requisition, exchange)
+            self.last_requisition_queue.append(self.last_requisition)
+            self.last_transmit_queue.append(self.last_transmit)
+            if exchange is None:
+                self.last_exchange = None
+                self.last_transfer = None
+            else:
+                self.last_exchange = exchange
+                self.last_transfer = (p.requisition, exchange)
+                self.last_exchange_queue.append(self.last_exchange)
+                self.last_transfer_queue.append(self.last_transfer)
+        if exchange is None:
+            raise Exception
+        return exchange
+
+
+
+class Last:
+    @property
+    def requisition(self) -> Optional[Requisition]:
+        return self.recorder.last_requisition
+    @property
+    def exchange(self) -> Optional[Exchange]:
+        return self.recorder.last_exchange
+    @property
+    def request(self) -> Optional[Request]:
+        xchg = self.recorder.last_exchange
+        if xchg is None:
+            return None
+        return xchg.request
+    @property
+    def response(self) -> Optional[Response]:
+        xchg = self.recorder.last_exchange
+        if xchg is None:
+            return None
+        return xchg.response
+    @property
+    def transmit(self) -> Optional[tuple[Requisition, Optional[Exchange]]]:
+        return self.recorder.last_transmit
+    @property
+    def transfer(self) -> Optional[tuple[Requisition, Exchange]]:
+        return self.recorder.last_transfer
+    @property
+    def requisition_queue(self) -> MutableSequence[Requisition]:
+        return self.recorder.last_requisition_queue
+    @property
+    def exchange_queue(self) -> MutableSequence[Exchange]:
+        return self.recorder.last_exchange_queue
+    @property
+    def transmit_queue(self) -> MutableSequence[tuple[Requisition, Optional[Exchange]]]:
+        return self.recorder.last_transmit_queue
+    @property
+    def transfer_queue(self) -> MutableSequence[tuple[Requisition, Exchange]]:
+        return self.recorder.last_transfer_queue
+
+    def clear(self) -> None:
+        self.recorder.clear()
+
+    def __init__(self, recorder: Recorded) -> None:
+        self.recorder: Recorded = recorder
