bd2221a322d99ec5ee4e13a0652c00d323333597
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:14 +0000

    Add package e2xgrader/0.1.0.dev3/e2xgrader-0.1.0.dev3-py3-none-any.whl

---
+++ packages/e2xgrader/e2xgrader-0.1.0.dev3-py3-none-any.whl/e2xgrader/__init__.py
@@ -0,0 +1,38 @@
+"""
+A system for creating assignments.
+"""
+
+import glob
+import os
+from os.path import join as pjoin
+
+
+def _jupyter_nbextension_paths():
+    root = os.path.dirname(__file__)
+    base_path = pjoin(root, "static", "nbextensions")
+
+    paths = []
+
+    for section in ["notebook", "tree"]:
+        for notebook_extension in glob.glob(pjoin(base_path, section, "*")):
+            name = f"{os.path.split(notebook_extension)[-1]}_{section}"
+            paths.append(
+                dict(
+                    section=section,
+                    src=notebook_extension,
+                    dest=name,
+                    require=pjoin(name, "main"),
+                )
+            )
+
+    return paths
+
+
+def _jupyter_server_extension_paths():
+    paths = [
+        dict(module="e2xgrader.server_extensions.assignment_list"),
+        dict(module="e2xgrader.server_extensions.grader"),
+        dict(module="e2xgrader.server_extensions.validate_assignment"),
+    ]
+
+    return paths
---
+++ packages/e2xgrader/e2xgrader-0.1.0.dev3-py3-none-any.whl/e2xgrader/utils/__init__.py
@@ -0,0 +1,4 @@
+from .notebookvariableextractor import NotebookVariableExtractor
+from .utils import format_url, get_nbgrader_config, urljoin
+
+__all__ = ["NotebookVariableExtractor", "urljoin", "format_url", "get_nbgrader_config"]
