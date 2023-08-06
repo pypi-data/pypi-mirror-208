4a7b5faf249177fd26bb892f4a1936d1f2f30d2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:31:15 +0000

    Add package ufusion/1.0.4/ufusion-1.0.4.tar.gz

---
+++ packages/ufusion/ufusion-1.0.4.tar.gz/ufusion-1.0.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ufusion',
+    version='1.0.4',
+    description='',
+    author='f09l',
+    author_email='fostn.contact@gmail.com',
+    packages=find_packages(),
+    install_requires=['requests','colorama','blessed'],
+    entry_points={
+        'console_scripts': [
+            'fusion=Fusion:main',
+        ],
+    },
+)
