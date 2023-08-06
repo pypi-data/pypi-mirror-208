c3a464290b1e38249e471082a4b285f4f55a0cd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:14 +0000

    Add package streamlit-wordcloud-v2/0.3.0/streamlit_wordcloud_v2-0.3.0-py3-none-any.whl

---
+++ packages/streamlit-wordcloud-v2/streamlit_wordcloud_v2-0.3.0-py3-none-any.whl/streamlit_wordcloud/frontend/build/precache-manifest.ef1bf4962af1cd28593ff85f4ab184b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d8bef76783b7bc2f7b996d6e8839572d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "ec319f950cfed392f911",
+    "url": "./static/css/2.98f1a7d1.chunk.css"
+  },
+  {
+    "revision": "ec319f950cfed392f911",
+    "url": "./static/js/2.eaec345a.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.eaec345a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d157f8e565847442770",
+    "url": "./static/js/main.cb0b4d91.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
