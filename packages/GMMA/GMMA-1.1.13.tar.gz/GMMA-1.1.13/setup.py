187cfc5eedc7c29c7797777e55c505379c967610
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:10 +0000

    Add package GMMA/1.1.13/GMMA-1.1.13.tar.gz

---
+++ packages/GMMA/GMMA-1.1.13.tar.gz/GMMA-1.1.13/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="GMMA",
+    version="1.1.13",
+    long_description="*GaMMA*: *Ga*ussian *M*ixture *M*odel *A*ssociation",
+    long_description_content_type="text/markdown",
+    packages=["gamma"],
+    install_requires=[
+        "scikit-learn",
+        "scipy",
+        "numpy",
+        "pyproj",
+        "tqdm"
+    ],
+)
