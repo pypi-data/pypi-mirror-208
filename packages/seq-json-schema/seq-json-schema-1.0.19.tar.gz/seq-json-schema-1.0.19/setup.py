144969ab6339518eb446837992ac00e6360fab37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:52 +0000

    Add package seq-json-schema/1.0.19/seq-json-schema-1.0.19.tar.gz

---
+++ packages/seq-json-schema/seq-json-schema-1.0.19.tar.gz/seq-json-schema-1.0.19/setup.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from setuptools import setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / 'README.md').read_text()
+
+setup(
+  author='camargo',
+  author_email='Christopher.A.Camargo@jpl.nasa.gov',
+  description='Standardized JSON Schema for authoring multi-mission sequences',
+  include_package_data=True,
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  name='seq-json-schema',
+  packages=['seq-json-schema'],
+  url='https://github.com/NASA-AMMOS/seq-json-schema',
+  version='1.0.19'
+)
