08d3fe7adc5e8a1d71c3412c7e2670f3b73b6644
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:09 +0000

    Add package fancify-text/0.2.4/fancify-text-0.2.4.tar.gz

---
+++ packages/fancify-text/fancify-text-0.2.4.tar.gz/fancify-text-0.2.4/setup.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+
+# Packages
+from setuptools import setup
+
+setup(
+    name="fancify-text",
+    version="0.2.4",
+    author="Secret-chest",
+    url="https://github.com/Secret-chest/fancify-text",
+    description=(
+        "Convert text to one of 27 fancy unicode representations"
+    ),
+    long_description=open("README.md", encoding="utf8").read(),
+    long_description_content_type="text/markdown",
+    packages=["fancify_text"],
+    install_requires=[],
+    scripts=[
+        "fancify-circled", "fancify-curly", "fancify-wiry", "fancify-bolditalicserif", "fancify-italicserif",
+        "fancify-boxed", "fancify-squared", "fancify-wide", "fancify-parenthesized", "fancify-cursive",
+        "fancify-bold", "fancify-reversed", "fancify-upsidedown",
+        "fancify-blue", "fancify-boldfraktur", "fancify-bolditalic", "fancify-italic",
+        "fancify-smallcaps", "fancify-currency", "fancify-monospaced", "fancify-boldserif",
+        "fancify-sansserif", "fancify-cool", "fancify-doublestruck", "fancify-magic", "fancify-heavycircled",
+        "fancify-fraktur"
+    ],
+)
