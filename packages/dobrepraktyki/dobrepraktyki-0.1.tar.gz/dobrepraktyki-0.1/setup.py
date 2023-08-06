39b56cf462a4441d35a64e499405a8c7e6432d21
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:38 +0000

    Add package dobrepraktyki/0.1/dobrepraktyki-0.1.tar.gz

---
+++ packages/dobrepraktyki/dobrepraktyki-0.1.tar.gz/dobrepraktyki-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='dobrepraktyki',
+      version='0.1',
+      author='Karol Polit',
+      author_email='256438@student.pwr.edu.pl',
+      packages=find_packages(),
+      install_requires=["requests"]
+      )
