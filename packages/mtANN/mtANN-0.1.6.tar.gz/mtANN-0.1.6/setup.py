bda41aef7c1f3938c210efafcbf87445dcf3047e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:43:05 +0000

    Add package mtANN/0.1.6/mtANN-0.1.6.tar.gz

---
+++ packages/mtANN/mtANN-0.1.6.tar.gz/mtANN-0.1.6/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="mtANN", 
+    version="0.1.6",
+    # py_modules=['mtANN','mtANN.model', 'mtANN.utils'],
+    author="Yi-Xuan Xiong",
+    author_email="xyxuana@mails.ccnu.edu.cn",
+    description="Ensemble Multiple References for Single-cell RNA Seuquencing Data Annotation and Unseen Cells Identification",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+    install_requires=['pandas', 'numpy',
+    'scanpy','scipy','scikit-learn',
+    'torch','giniclust3','rpy2'],
+)
