775f03d1cf6598151faaf02a05123d8c2e5734e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:13 +0000

    Add package permhash/0.1.1/permhash-0.1.1-py3-none-any.whl

---
+++ packages/permhash/permhash-0.1.1-py3-none-any.whl/permhash/scripts/cli_permhash_apk_manifest.py
@@ -0,0 +1,39 @@
+"""
+Copyright 2023 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    https://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import argparse
+from permhash.functions import permhash_apk_manifest
+
+
+def main():
+    """
+    Intended to help handle argparsing
+    and CLI function calling
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-p",
+        "--path",
+        required=True,
+        action="store",
+        help="Full path to file to calculate permhash from.",
+    )
+    args = parser.parse_args()
+    print(permhash_apk_manifest(args.path))
+
+
+if __name__ == " __main__":
+    main()
