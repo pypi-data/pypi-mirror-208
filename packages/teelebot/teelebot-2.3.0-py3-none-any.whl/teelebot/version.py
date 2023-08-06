d3de5de33a2738cba9e45d0591556321295d238d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:02 +0000

    Add package teelebot/2.3.0/teelebot-2.3.0-py3-none-any.whl

---
+++ packages/teelebot/teelebot-2.3.0-py3-none-any.whl/teelebot/version.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+"""
+@description: A bot framework based on the Telegram Bot API
+@creation date: 2019-11-15
+@last modification: 2023-05-14
+@author: Pluto (github:plutobell)
+@version: 2.3.0
+"""
+
+__version__ = "2.3.0"
+
+__author__ = "Pluto"
+__email__ = "hi@ojoll.com"
+__blog__ = "https://ojoll.com"
+__github__ = "https://github.com/plutobell/teelebot"
+__description__ = "teelebot is a robot framework based on Telegram Bot API, with plug-in system, easy to extend."
+
+
