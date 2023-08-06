e795a201a64834838322000838e277e0a8bcb6a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:27 +0000

    Add package kof-parser/0.0.17/kof_parser-0.0.17.tar.gz

---
+++ packages/kof-parser/kof_parser-0.0.17.tar.gz/kof_parser-0.0.17/src/kof_parser/model.py
@@ -0,0 +1,13 @@
+from enum import Enum
+from typing import Optional
+
+from pydantic import BaseModel, Field
+
+
+class Location(BaseModel):
+    name: str = ""
+    methods: list[str] = []
+    point_easting: Optional[float]
+    point_northing: Optional[float]
+    point_z: Optional[float] = Field(None, ge=-10000, le=10000)
+    srid: Optional[int]
