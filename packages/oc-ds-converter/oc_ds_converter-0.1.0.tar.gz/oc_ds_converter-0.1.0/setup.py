f9d23928e30d033691a15122f6046a660a9d94b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:05 +0000

    Add package oc-ds-converter/0.1.0/oc_ds_converter-0.1.0.tar.gz

---
+++ packages/oc-ds-converter/oc_ds_converter-0.1.0.tar.gz/oc_ds_converter-0.1.0/setup.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['oc_ds_converter',
+ 'oc_ds_converter.crossref',
+ 'oc_ds_converter.datacite',
+ 'oc_ds_converter.datasource',
+ 'oc_ds_converter.jalc',
+ 'oc_ds_converter.lib',
+ 'oc_ds_converter.medra',
+ 'oc_ds_converter.preprocessing',
+ 'oc_ds_converter.pubmed',
+ 'oc_ds_converter.run']
+
+package_data = \
+{'': ['*'], 'oc_ds_converter.pubmed': ['support_files/*']}
+
+install_requires = \
+['PyYAML>=6.0,<7.0',
+ 'beautifulsoup4>=4.12.1,<5.0.0',
+ 'fakeredis>=2.12.1,<3.0.0',
+ 'ndjson>=0.3.1,<0.4.0',
+ 'oc-idmanager>=0.2.6,<0.3.0',
+ 'redis>=4.5.5,<5.0.0',
+ 'requests>=2.28.2,<3.0.0',
+ 'tqdm>=4.65.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'oc-ds-converter',
+    'version': '0.1.0',
+    'description': 'A library for converting metadata provided by various data sources, e.g. Crossref, DataCite, JaLC, and mEDRA, into the format used by OpenCitations Meta.',
+    'long_description': '[<img src="https://img.shields.io/badge/powered%20by-OpenCitations-%239931FC?labelColor=2D22DE" />](http://opencitations.net)\n[![Run tests](https://github.com/opencitations/ra_processor/actions/workflows/run_tests.yml/badge.svg)](https://github.com/opencitations/oc_meta/actions/workflows/run_tests.yml)\n![Coverage](https://raw.githubusercontent.com/opencitations/ra_processor/main/test/coverage/coverage.svg)\n<!-- ![PyPI](https://img.shields.io/pypi/pyversions/oc_meta) -->\n![GitHub code size in bytes](https://img.shields.io/github/languages/code-size/opencitations/ra_processor)\n\n\n\n# OpenCitations Data Sources Converter\n\nThis repository contains scripts useful for converting scholarly bibliographic metadata from various data sources into the format accepted by OpenCitations Meta. The data sources currently supported are Crossref, DataCite, PubMed, mEDRA a JaLC.\n',
+    'author': 'arcangelo7',
+    'author_email': 'arcangelomas@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
