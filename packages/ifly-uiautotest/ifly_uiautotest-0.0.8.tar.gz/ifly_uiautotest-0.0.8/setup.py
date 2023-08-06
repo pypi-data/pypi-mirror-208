f4b2efbf7735a60774da0e8281aebeb7f70ba369
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:27 +0000

    Add package ifly-uiautotest/0.0.8/ifly_uiautotest-0.0.8.tar.gz

---
+++ packages/ifly-uiautotest/ifly_uiautotest-0.0.8.tar.gz/ifly_uiautotest-0.0.8/setup.py
@@ -0,0 +1,49 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ifly_uiautotest",
+    version="0.0.8",
+    author="",
+    author_email="",
+    description="A small example package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+        entry_points={
+        'console_scripts': [
+            'autotest = ifly_autotest.ifly_autotest:main',
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+     install_requires=[
+            'airtest==1.2.6',
+            'pyppeteer==0.2.6',
+            'artifactory==0.1.17',
+            'pixelmatch',
+            'paramiko==2.11.0',
+            'pandas==1.1.5',
+            'allure-combine==1.0.6',
+            'allure-pytest==2.9.45',
+            'allure-python-commons==2.9.45',
+            'openpyxl==3.0.10',
+            'pytest-ordering==0.6',
+            'pytest-repeat==0.9.1',
+            'numpy==1.19.5',
+            'opencv-contrib-python',
+            'Levenshtein',
+            'onnxruntime',
+            'pyclipper',
+            'requests',
+            'Shapely==1.7.1',
+            'tqdm',
+            'addict'           
+    ],  
+)
