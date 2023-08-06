afe7ad75d39e892bd9bfc017b389e3e188992952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:39 +0000

    Add package data-loader-xyz/0.0.4/data_loader_xyz-0.0.4.tar.gz

---
+++ packages/data-loader-xyz/data_loader_xyz-0.0.4.tar.gz/data_loader_xyz-0.0.4/configs/base_config.py
@@ -0,0 +1,8 @@
+import os
+
+# L'emplacment du projet
+project_empl = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+config_empl = project_empl + "/configs/config.ini"
+
+yaml_config_empl = project_empl + "/configs/data_loader_xyz_config.yaml"
