dd9c4fc14e047de7af3c247c67f313a2df02efab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:29 +0000

    Add package bpy-addon-build/0.1.7/bpy_addon_build-0.1.7.tar.gz

---
+++ packages/bpy-addon-build/bpy_addon_build-0.1.7.tar.gz/bpy_addon_build-0.1.7/bpy_addon_build/actions.py
@@ -0,0 +1,33 @@
+import subprocess
+import shlex
+from typing import List
+from pathlib import Path
+import shutil
+import re
+from rich.console import Console
+
+# Execute an action
+def execute_action(action: str, inter_dir: Path, console: Console):
+    extracted_str = re.search("\((.+?)\)", action)
+    if not extracted_str:
+        console.print(
+            f"Invalid action: {action}, perhaps you forgot parenthesis?",
+            style="[bold red]"
+        )
+        quit()
+
+    if "create_file" in action:
+        file_path = inter_dir / Path(extracted_str.group(1).replace('"', ""))
+        with open(file_path, "w") as f:
+            f.write("")
+
+    if "copy_file" in action:
+        files: List[str] = extracted_str.group(1).split("->")
+        src: Path = Path(files[0].replace('"', "")).expanduser()
+        dst: Path = inter_dir / Path(files[1].replace('"', ""))
+        shutil.copy2(src, dst)
+
+    if "execute_sh" in action:
+        command = shlex.split(extracted_str.group(1))
+        output = subprocess.run(command, shell=True, cwd=inter_dir, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        console.print(output.stdout.decode("UTF-8"))
