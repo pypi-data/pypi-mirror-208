6f03d4f3279db75cccd0e3f3de039290c01d9062
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:37:33 +0000

    Add package aiuv/0.9/aiuv-0.9.tar.gz

---
+++ packages/aiuv/aiuv-0.9.tar.gz/aiuv-0.9/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+setup(name="aiuv",
+version="0.9",
+description="This is the package with all the ai codes",
+author="Yuvraj",
+packages=['pkg'],
+install_requires = [])
