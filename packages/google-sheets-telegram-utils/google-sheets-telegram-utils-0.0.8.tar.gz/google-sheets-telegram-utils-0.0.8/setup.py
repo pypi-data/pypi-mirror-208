a9a65ce8495a19edd9cca35a457404dd5aff13ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:51 +0000

    Add package google-sheets-telegram-utils/0.0.8/google-sheets-telegram-utils-0.0.8.tar.gz

---
+++ packages/google-sheets-telegram-utils/google-sheets-telegram-utils-0.0.8.tar.gz/google-sheets-telegram-utils-0.0.8/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='google-sheets-telegram-utils',
+    version='0.0.8',
+    description='A package with utils to work with google spreadsheet and telegram bots',
+    url='https://github.com/alexVarkalov/google_sheets_telegram_utils',
+    author='Alexander Varkalov',
+    author_email='alex.varkalov@gmail.com',
+    license='BSD 2-clause',
+    packages=find_packages(),
+    install_requires=[
+        'python-telegram-bot==20.3',
+        'python-dotenv==1.0.0',
+        'google-api-python-client==2.86.0',
+        'google-auth-httplib2==0.1.0',
+        'google-auth-oauthlib==1.0.0',
+        'gspread==5.9.0',
+    ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Other Environment',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
