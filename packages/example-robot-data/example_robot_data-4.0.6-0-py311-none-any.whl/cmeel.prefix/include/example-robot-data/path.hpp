d07193ae9f09a3729dd656f3ee07bd2bae32aa52
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:16:06 +0000

    Add package example-robot-data/4.0.6/example_robot_data-4.0.6-0-py311-none-any.whl

---
+++ packages/example-robot-data/example_robot_data-4.0.6-0-py311-none-any.whl/cmeel.prefix/include/example-robot-data/path.hpp
@@ -0,0 +1,6 @@
+#ifndef EXAMPLE_ROBOT_DATA_PATH_HPP
+#define EXAMPLE_ROBOT_DATA_PATH_HPP
+
+#define EXAMPLE_ROBOT_DATA_MODEL_DIR "/tmp/cmeel-0gqpm95h/whl/cmeel.prefix/share/example-robot-data/robots"
+
+#endif
