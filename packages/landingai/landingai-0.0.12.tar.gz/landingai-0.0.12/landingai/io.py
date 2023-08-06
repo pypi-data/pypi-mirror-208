556e71f2108d3c0624ed250dea497013f55529cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:04 +0000

    Add package landingai/0.0.12/landingai-0.0.12.tar.gz

---
+++ packages/landingai/landingai-0.0.12.tar.gz/landingai-0.0.12/landingai/io.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def read_image(source: str) -> np.ndarray:
+    pass
+
+
+def sample_images_from_video(
+    video_file: str, output_path: str, sample_rate: float = None
+) -> list[str]:
+    pass
