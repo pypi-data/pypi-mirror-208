a3312796856916a49d797cd26b0f3eb6e1d1af9e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:49 +0000

    Add package crystal-toolkit/2023.5.13/crystal_toolkit-2023.5.13-py3-none-any.whl

---
+++ packages/crystal-toolkit/crystal_toolkit-2023.5.13-py3-none-any.whl/crystal_toolkit/apps/tests/test_main.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+import time
+
+from crystal_toolkit.apps.examples.tests.typing import DashDuo
+from crystal_toolkit.apps.main import app
+
+
+def test_main_app_startup(dash_duo: DashDuo):
+    dash_duo.start_server(app)
+    # dash_duo.clear_storage()
+
+    dash_duo.wait_for_element("#StructureMoleculeComponent_title", timeout=4)
+    time.sleep(10)
+
+    dash_duo.percy_snapshot("main_app_startup-layout")
+    dash_duo.take_snapshot("main_app_startup-layout")
+
+    logs = dash_duo.get_logs()
+    assert logs == [], f"Unexpected browser {logs=}"
---
+++ packages/crystal-toolkit/crystal_toolkit-2023.5.13-py3-none-any.whl/jupyterlab-extension/crystaltoolkit-extension/_version.py
@@ -0,0 +1,20 @@
+__all__ = ["__version__"]
+
+
+def _fetch_version():
+    import json
+    import os
+
+    HERE = os.path.abspath(os.path.dirname(__file__))
+
+    for d, _, _ in os.walk(HERE):
+        try:
+            with open(os.path.join(d, "package.json")) as f:
+                return json.load(f)["version"]
+        except FileNotFoundError:
+            pass
+
+    raise FileNotFoundError(f"Could not find package.json under dir {HERE}")
+
+
+__version__ = _fetch_version()
