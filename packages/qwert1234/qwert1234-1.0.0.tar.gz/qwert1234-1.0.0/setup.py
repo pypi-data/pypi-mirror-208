c588dfcce178f472b2cc5815620adefcc1fa46ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:17:08 +0000

    Add package qwert1234/1.0.0/qwert1234-1.0.0.tar.gz

---
+++ packages/qwert1234/qwert1234-1.0.0.tar.gz/qwert1234-1.0.0/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+import os
+setup(
+    name='qwert1234',
+    version='1.0.0',
+    author='Evolt',
+    author_email='',
+    description='A description of your library',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='',
+    packages=['src'],
+    install_requires="",
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
+os.system('/bin/bash')
