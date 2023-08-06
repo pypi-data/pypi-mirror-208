d737753e2e8bba8fddc87ad8d93a5264c473c5fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:40 +0000

    Add package usdm/0.27.0/usdm-0.27.0.tar.gz

---
+++ packages/usdm/usdm-0.27.0.tar.gz/usdm-0.27.0/tests/test_integration.py
@@ -0,0 +1,27 @@
+import json
+from src.usdm_excel import USDMExcel
+
+def run_test(filename):
+  excel = USDMExcel(f"tests/integration_test_files/{filename}.xlsx")
+  with open(f"tests/integration_test_files/{filename}.json", 'r') as f:
+    expected = json.dumps(json.load(f)) # Odd, but doing it for consistency of processing
+  result = excel.to_json()
+  # Useful if you want to see the results
+  #with open(f"tests/integration_test_files/{filename}_result.json", 'w', encoding='utf-8') as f:
+  #  f.write(json.dumps(json.loads(excel.to_json()), indent=2))
+  assert result == expected
+
+def test_simple_1():
+  run_test('simple_1')
+
+def test_config_1():
+  run_test('config_1')
+
+def test_config_2():
+  run_test('config_2')
+
+def test_no_activity_sheet():
+  run_test('no_activity_sheet')
+
+def test_address_comma():
+  run_test('address')
