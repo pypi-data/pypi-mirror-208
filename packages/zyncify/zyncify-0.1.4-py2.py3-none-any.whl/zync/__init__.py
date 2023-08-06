f9085b4bdd89486667db299366b7809c86cd0bd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:03 +0000

    Add package zyncify/0.1.4/zyncify-0.1.4-py2.py3-none-any.whl

---
+++ packages/zyncify/zyncify-0.1.4-py2.py3-none-any.whl/zync/__init__.py
@@ -0,0 +1,9 @@
+from .logger import logger, bugger
+from .slugify import Slugger, slugger
+
+__all__ = [
+    "bugger",
+    "logger",
+    "Slugger",
+    "slugger",
+]
---
+++ packages/zyncify/zyncify-0.1.4-py2.py3-none-any.whl/zync/logger.py
@@ -0,0 +1,97 @@
+import logging
+import inspect
+import os
+
+
+W = "\033[39m"
+B = "\033[94m"
+G = "\033[92m"
+Y = "\033[33m"
+R = "\033[91m"
+M = "\033[35m"
+C = "\033[36m"
+X = "\033[0m"
+
+
+class BuggerFormat(logging.Formatter):
+    def format(self, record):
+        record.levelname = "bugger"
+        levelname = record.levelname.upper()
+        record.levelname = levelname
+        return super().format(record)
+
+
+class LoggerFormat(logging.Formatter):
+    def format(self, record):
+        record.levelname = "logger"
+        levelname = record.levelname.upper()
+        record.levelname = levelname
+        return super().format(record)
+
+
+class Bugger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        file_handler = logging.FileHandler(".zync.log")
+        formatter = BuggerFormat(
+            f"{G}[{X}"
+            f"{G}%(levelname)s{X}"
+            f"{R}:{X}"
+            f"{C}%(line)s{X}"
+            f"{G}@{X}"
+            f"{M}%(location)s{X}"
+            f"{G}%(asctime)s{X}"
+            f"{G}]{X}"
+            f"{G}> {X}"
+            f"%(message)s{X}",
+            datefmt="%H:%M:%S",
+        )
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+    def __call__(self, log, line, location):
+        self.logger.debug(log, extra={"line": line, "location": location})
+
+
+class Logger:
+    def __init__(self, name):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.INFO)
+        file_handler = logging.FileHandler(".zync.log")
+        formatter = LoggerFormat(
+            f"{Y}[{X}"
+            f"{Y}%(levelname)s{X}"
+            f"{R}:{X}"
+            f"{C}%(line)s{X}"
+            f"{Y}@{X}"
+            f"{M}%(location)s{X}"
+            f"{Y}%(asctime)s{X}"
+            f"{Y}]{X}"
+            f"{Y}> {X}"
+            f"%(message)s{X}",
+            datefmt="%H:%M:%S",
+        )
+        file_handler.setFormatter(formatter)
+        self.logger.addHandler(file_handler)
+
+    def __call__(self, log, line, location):
+        self.logger.info(log, extra={"line": line, "location": location})
+
+
+bugger_base = Bugger("bugger")
+logger_base = Logger("logger")
+
+
+def bugger(log):
+    frame = inspect.currentframe().f_back
+    line = inspect.getframeinfo(frame).positions.lineno
+    location = os.path.basename(inspect.getframeinfo(frame).filename)
+    return bugger_base(log, line, location)
+
+
+def logger(log):
+    frame = inspect.currentframe().f_back
+    line = inspect.getframeinfo(frame).positions.lineno
+    location = os.path.basename(inspect.getframeinfo(frame).filename)
+    return logger_base(log, line, location)
