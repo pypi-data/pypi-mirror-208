e6b85bf6197fa1ddd84dca9c6b16e10469fddf8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:16:40 +0000

    Add package eispac/0.94.4.dev0/eispac-0.94.4.dev0.tar.gz

---
+++ packages/eispac/eispac-0.94.4.dev0.tar.gz/eispac-0.94.4.dev0/eispac/__init__.py
@@ -0,0 +1,10 @@
+# __version__ = '0.94.1'
+
+from .version import version as __version__
+from .version import full_version as __full_version__
+from . import download
+from . import templates
+from .core import *
+from .extern import *
+from .util import *
+from . import data
