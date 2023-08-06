1b1e506de3c543220644e54b3e980c224e680935
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:52 +0000

    Add package PatryksAutoAI/0.4.3/PatryksAutoAI-0.4.3.tar.gz

---
+++ packages/PatryksAutoAI/PatryksAutoAI-0.4.3.tar.gz/PatryksAutoAI-0.4.3/KaggleAutoAI/__init__.py
@@ -0,0 +1,11 @@
+from .model_data import *
+from .classification import *
+from .regression import *
+from .automated_ai import *
+
+__all__ = [
+    'model_data'
+    'classification',
+    'regression_automated',
+    "automated_ai"
+]
