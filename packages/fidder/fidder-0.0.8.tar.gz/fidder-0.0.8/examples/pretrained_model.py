4abfd81cab1578a8afc84a4a2bf091fe14345daf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:41 +0000

    Add package fidder/0.0.8/fidder-0.0.8.tar.gz

---
+++ packages/fidder/fidder-0.0.8.tar.gz/fidder-0.0.8/examples/pretrained_model.py
@@ -0,0 +1,27 @@
+from pathlib import Path
+
+from fidder.predict.cli import predict_fiducial_mask
+from fidder.erase.cli import erase_masked_region
+
+image = Path('TS_01_0deg_bin8.mrc')
+mask = Path(image.stem + '_mask.mrc')
+probabilities = Path(image.stem + '_probabilities.mrc')
+checkpoint = Path(
+    '../training/lightning_logs/version_3123283/checkpoints/epoch=24-step=600.ckpt'
+)
+erased = Path(image.stem + '_erased.mrc')
+
+predict_fiducial_mask(
+    input_image=image,
+    pixel_spacing=None,
+    probability_threshold=0.5,
+    output_mask=mask,
+    output_probabilities=probabilities,
+    model_checkpoint_file=checkpoint
+)
+
+erase_masked_region(
+    input_image=image,
+    input_mask=mask,
+    output=erased,
+)
