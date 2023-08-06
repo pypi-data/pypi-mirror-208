63dd962d7c052799ef096d56d813ba41ce8225aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:09 +0000

    Add package polars-lts-cpu/0.17.14/polars_lts_cpu-0.17.14.tar.gz

---
+++ packages/polars-lts-cpu/polars_lts_cpu-0.17.14.tar.gz/polars_lts_cpu-0.17.14/local_dependencies/polars-core/src/chunked_array/ops/reverse.rs
@@ -0,0 +1,52 @@
+use crate::prelude::*;
+use crate::series::IsSorted;
+use crate::utils::{CustomIterTools, NoNull};
+
+impl<T> ChunkReverse<T> for ChunkedArray<T>
+where
+    T: PolarsNumericType,
+{
+    fn reverse(&self) -> ChunkedArray<T> {
+        let mut out = if let Ok(slice) = self.cont_slice() {
+            let ca: NoNull<ChunkedArray<T>> = slice.iter().rev().copied().collect_trusted();
+            ca.into_inner()
+        } else {
+            self.into_iter().rev().collect_trusted()
+        };
+        out.rename(self.name());
+
+        match self.is_sorted_flag() {
+            IsSorted::Ascending => out.set_sorted_flag(IsSorted::Descending),
+            IsSorted::Descending => out.set_sorted_flag(IsSorted::Ascending),
+            _ => {}
+        }
+
+        out
+    }
+}
+
+macro_rules! impl_reverse {
+    ($arrow_type:ident, $ca_type:ident) => {
+        impl ChunkReverse<$arrow_type> for $ca_type {
+            fn reverse(&self) -> Self {
+                let mut ca: Self = self.into_iter().rev().collect_trusted();
+                ca.rename(self.name());
+                ca
+            }
+        }
+    };
+}
+
+impl_reverse!(BooleanType, BooleanChunked);
+impl_reverse!(Utf8Type, Utf8Chunked);
+impl_reverse!(BinaryType, BinaryChunked);
+impl_reverse!(ListType, ListChunked);
+
+#[cfg(feature = "object")]
+impl<T: PolarsObject> ChunkReverse<ObjectType<T>> for ObjectChunked<T> {
+    fn reverse(&self) -> Self {
+        // Safety
+        // we we know we don't get out of bounds
+        unsafe { self.take_unchecked((0..self.len()).rev().into()) }
+    }
+}
