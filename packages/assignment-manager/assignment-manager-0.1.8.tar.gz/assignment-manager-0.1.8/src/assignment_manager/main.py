3a8246c1f8065a602729f376639ee11635288d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:23 +0000

    Add package assignment-manager/0.1.8/assignment-manager-0.1.8.tar.gz

---
+++ packages/assignment-manager/assignment-manager-0.1.8.tar.gz/assignment-manager-0.1.8/src/assignment_manager/main.py
@@ -0,0 +1,60 @@
+import typer
+
+from assignment_manager import assignments
+
+
+app = typer.Typer()
+backup_app = typer.Typer()
+app.add_typer(backup_app, name="backup")
+
+
+@app.command()
+def add():
+    assignments.add_assignment()
+
+
+@app.command()
+def show(one: bool = False, sort: bool = False):
+    if one:
+        assignments.show_specific_assignment()
+    else:
+        assignments.show_all_assignments(sort)
+
+
+@app.command()
+def update():
+    assignments.update_assignment()
+
+
+@app.command()
+def rename():
+    assignments.rename_assignment()
+
+
+@app.command()
+def remove():
+    assignments.remove_assignment()
+
+
+@backup_app.command("copy")
+def copy_backup():
+    """
+    Copy the data file. This will OVERWRITE the BACKUP file.
+    """
+    assignments.copy_backup()
+
+
+@backup_app.command("paste")
+def paste_backup():
+    """
+    Paste the backup file. This will OVERWRITE the DATA file.
+    """
+    assignments.paste_backup()
+
+
+def main():
+    app()
+
+
+if __name__ == "__main__":
+    main()
