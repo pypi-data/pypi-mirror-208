27d0fa9695f1a5900b7c466b9b4009ef3902728b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:11 +0000

    Add package simple-pygame/0.0.5/simple_pygame-0.0.5.tar.gz

---
+++ packages/simple-pygame/simple_pygame-0.0.5.tar.gz/simple_pygame-0.0.5/src/simple_pygame/constants.py
@@ -0,0 +1,15 @@
+"""
+A module that holds all the constants of the package.
+"""
+MusicIsLoading = "MusicIsLoading"
+MusicEnded = "MusicEnded"
+
+SInt8 = "SInt8"
+SInt16 = "SInt16"
+SInt32 = "SInt32"
+UInt8 = "UInt8"
+
+MixerModule = "MixerModule"
+MusicClass = "MusicClass"
+
+__version__ = "0.0.5"
