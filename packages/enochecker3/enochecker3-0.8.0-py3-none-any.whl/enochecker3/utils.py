23526e40308a0de3dab1982c6781fa32b3b34c14
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:51 +0000

    Add package enochecker3/0.8.0/enochecker3-0.8.0-py3-none-any.whl

---
+++ packages/enochecker3/enochecker3-0.8.0-py3-none-any.whl/enochecker3/utils.py
@@ -0,0 +1,32 @@
+import hashlib
+import re
+from typing import Any, Optional, Union
+
+from .types import MumbleException
+
+
+def assert_in(o1: Any, o2: Any, message: Optional[str] = None) -> None:
+    if not o2 or o1 not in o2:
+        raise MumbleException(
+            message or "Checker assertion failed",
+        )
+
+
+def assert_equals(o1: Any, o2: Any, message: Optional[str] = None) -> None:
+    if o1 != o2:
+        raise MumbleException(message or "Checker assertion failed")
+
+
+class FlagSearcher:
+    def __init__(self, flag_regex: str, flag_hash: str):
+        self._flag_re: re.Pattern = re.compile(flag_regex.encode())
+        self.flag_hash: str = flag_hash
+
+    def search_flag(self, haystack: Union[str, bytes]) -> Optional[bytes]:
+        if type(haystack) == str:
+            haystack = haystack.encode()
+        for flag in self._flag_re.findall(haystack):
+            hash_ = hashlib.sha256(flag).hexdigest()
+            if hash_ == self.flag_hash:
+                return flag
+        return None
