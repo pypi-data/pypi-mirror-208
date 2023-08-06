f703edcec64fbdb43ec44013bb96c4eae5ce83ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:13 +0000

    Add package facebook-page-info-scraper/1.0.5/facebook-page-info-scraper-1.0.5.tar.gz

---
+++ packages/facebook-page-info-scraper/facebook-page-info-scraper-1.0.5.tar.gz/facebook-page-info-scraper-1.0.5/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open('README.rst', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='facebook-page-info-scraper',
+    version='1.0.5',
+    author='Wael hkiri',
+    author_email='wael.hkiri.dev@gmail.com',
+    description='A Python package capable of crawling Facebook page information',
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    packages=find_packages(),
+    install_requires=[
+    'geopy==2.2.0',
+    'retry~=0.9.2',
+    'selenium~=4.8.2',
+    'googletrans~=3.1.0a0'
+],
+)
