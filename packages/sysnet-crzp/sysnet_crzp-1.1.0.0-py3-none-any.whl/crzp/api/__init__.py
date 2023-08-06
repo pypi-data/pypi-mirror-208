73bc85ef94fa8c29c93de2ddbbd52f4ff633a854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:11 +0000

    Add package sysnet-crzp/1.1.0.0/sysnet_crzp-1.1.0.0-py3-none-any.whl

---
+++ packages/sysnet-crzp/sysnet_crzp-1.1.0.0-py3-none-any.whl/crzp/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from crzp.api.developers_api import DevelopersApi
+from crzp.api.ippc_api import IppcApi
+from crzp.api.public_api import PublicApi
