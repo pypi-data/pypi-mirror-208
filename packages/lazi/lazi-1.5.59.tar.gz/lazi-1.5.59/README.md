ba56278d943900153a4b3a7d009cb61bfe12e81c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:25:52 +0000

    Add package lazi/1.5.59/lazi-1.5.59.tar.gz

---
+++ packages/lazi/lazi-1.5.59.tar.gz/lazi-1.5.59/README.md
@@ -0,0 +1,74 @@
+# Lazi: Lazy Imports Everywhere
+
+An easy way to implement and track lazy imports globally.
+
+No external dependencies.
+
+**Requres Python 3.11.**
+
+## Usage:
+
+```shell
+poetry add lazi
+```
+
+```python
+"""Automatic lazy loading example.
+
+Install django to run, or change the imports.
+"""
+import lazi.auto as lazi         # Install import tracking.
+import django.test               # Import stuff.
+# print(lazi.used_count())         # Count loaded modules. <Not yet re-implemented>
+TestCase = django.test.TestCase  # Trigger lazy loading.
+# print(lazi.used_count())         # More modules were lazy loaded. <TBD>
+```
+
+```shell
+python example.py
+```
+
+```python
+0
+262
+```
+
+```python
+"""Manual lazy loading example.
+"""
+import lazi.core as lazi                # Import Lazi.
+django = lazi.lazy("django")            # Import stuff.
+django_test = lazi.lazy("django.test")  # Import more stuff.
+# print(lazi.used_count())              # Count loaded modules. <TBD>
+TestCase = django_test.TestCase         # Trigger lazy loading.
+# print(lazi.used_count())              # Module was lazy loaded. <TBD>
+```
+
+```python
+0
+2
+```
+
+## Configuration
+
+The `lazi.conf` namespace package contains configuration modules
+that get autoloaded (in import order) by `lazi.conf.conf`.
+It is fully decoupled from the rest of the codebase.
+
+As a result, it's possible configure Lazi by creating `lazi.conf`
+modules in your project (within the `lazi.conf` namespace package),
+and use conf modules provided by other packages.
+
+Configuration is not yet controllable via environment variables,
+but this is planned for the future. Update: Supported for DEBUG_TRACING.
+
+It's also possible to manually change the configuration at runtime,
+with the caveat that some variables may have already been used by
+`lazi.core`. To avoid this, configure Lazi before importing it:
+
+```python
+from lazi.conf import conf
+conf.DEBUG_TRACING = 1
+import lazi.auto
+# ...
+```
---
+++ packages/lazi/lazi-1.5.59.tar.gz/lazi-1.5.59/lazi/core/finder.py
@@ -0,0 +1,101 @@
+from __future__ import annotations
+
+import sys
+from types import ModuleType
+from importlib.util import find_spec, module_from_spec
+from importlib.abc import MetaPathFinder
+
+from lazi.util import classproperty, debug
+
+from .spec import Spec
+from .loader import Loader
+from .module import Module
+
+__all__ = "Finder", "__finder__"
+
+
+class Finder(MetaPathFinder):
+    Spec: type[Spec] = Spec
+    Loader: type[Loader] = Loader
+    Module: type[Module] = Module
+
+    __refs__: int = 0
+    __specs__: dict[str, Spec]
+
+    __stack__: list[Finder] = []
+
+    finders = classproperty(lambda cls: (_ for _ in sys.meta_path if isinstance(_, cls)))
+
+    def __init__(self):
+        self.__specs__ = {}
+
+    def __enter__(self) -> Finder:
+        if self not in sys.meta_path:
+            assert self.__refs__ == 0, self.__refs__
+            assert None is debug.trace(
+                f"+ {self.__class__.__name__}[{id(self)}] <refs:{self.__refs__}> "
+                f"<inst:{len([_ for _ in sys.meta_path if isinstance(_, type(self))])}>"
+            )
+            sys.meta_path.insert(0, self)
+
+        self.__refs__ += 1
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback) -> bool:
+        assert None is debug.trace(f"- {self.__class__.__name__}[{id(self)}] <refs:{self.__refs__}>")
+
+        self.__refs__ = max(self.__refs__ - 1, 0)
+
+        if self.__refs__ == 0 and self in sys.meta_path:
+            sys.meta_path.remove(self)
+            self.invalidate_caches()
+            return True
+
+        return False
+
+    @classmethod
+    def install(cls) -> Finder:
+        return __finder__.__enter__()
+
+    @classmethod
+    def uninstall(cls) -> bool:
+        return __finder__.__exit__(None, None, None)
+
+    def lazy(self, name: str, path: list[str] | None = None, target: ModuleType | None = None) -> ModuleType:
+        if (spec := self.find_spec(name, path, target)) is not None:
+            module = module_from_spec(spec)
+            spec.loader.exec_module(module)
+            return module
+
+    def find_spec(self, name: str, path: list[str] | None = None, target: ModuleType | None = None) -> Spec | None:
+        if self in self.__stack__:
+            return None
+
+        assert None is debug.traced(
+            1,
+            f"<find> {name} <p:{len(path) if path else path!r}> <t:{target!r}> <f:{id(self)}>"
+        )
+
+        if (spec := self.__specs__.get(name)) is not None:
+            assert spec.finder is self, (spec.finder, self)
+            return spec
+
+        self.__stack__.append(self)
+
+        try:
+            if (spec := find_spec(name, path)) is not None:
+                spec = self.__specs__[name] = self.Spec(self, spec, path, target)
+                assert None is debug.traced(3, f"<foun> {spec.name} <L:{spec.loader_state}>")
+                return spec
+
+        finally:
+            pop = self.__stack__.pop()
+            assert pop is self, (pop, self)
+
+    def invalidate_caches(self) -> None:
+        while self.__specs__ and (spec := self.__specs__.popitem()[1]):
+            if spec.loader_state == spec.loader.State.LOAD:
+                spec.loader.unload_module(spec)
+
+
+__finder__: Finder = Finder()
