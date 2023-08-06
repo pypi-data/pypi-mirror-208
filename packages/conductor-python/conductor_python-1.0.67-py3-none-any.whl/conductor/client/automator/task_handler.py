2f5689f9dc27dd7f3da150682b39a3a7e175a9d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:00:01 +0000

    Add package conductor-python/1.0.67/conductor_python-1.0.67-py3-none-any.whl

---
+++ packages/conductor-python/conductor_python-1.0.67-py3-none-any.whl/conductor/client/automator/task_handler.py
@@ -0,0 +1,130 @@
+from conductor.client.automator.task_runner import TaskRunner
+from conductor.client.configuration.configuration import Configuration
+from conductor.client.configuration.settings.metrics_settings import MetricsSettings
+from conductor.client.telemetry.metrics_collector import MetricsCollector
+from conductor.client.worker.worker_interface import WorkerInterface
+from multiprocessing import Process, freeze_support
+from typing import List
+import logging
+
+logger = logging.getLogger(
+    Configuration.get_logging_formatted_name(
+        __name__
+    )
+)
+
+
+class TaskHandler:
+    def __init__(
+            self,
+            workers: List[WorkerInterface],
+            configuration: Configuration = None,
+            metrics_settings: MetricsSettings = None
+    ):
+        if not isinstance(workers, list):
+            workers = [workers]
+        self.__create_task_runner_processes(
+            workers, configuration, metrics_settings
+        )
+        self.__create_metrics_provider_process(
+            metrics_settings
+        )
+        logger.info('Created all processes')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.stop_processes()
+
+    def stop_processes(self) -> None:
+        self.__stop_task_runner_processes()
+        self.__stop_metrics_provider_process()
+        logger.debug('stopped processes')
+
+    def start_processes(self) -> None:
+        logger.info('Starting worker processes...')
+        freeze_support()
+        self.__start_task_runner_processes()
+        self.__start_metrics_provider_process()
+        logger.info('Started all processes')
+
+    def join_processes(self) -> None:
+        self.__join_task_runner_processes()
+        self.__join_metrics_provider_process()
+        logger.info('Joined all processes')
+
+    def __create_metrics_provider_process(self, metrics_settings: MetricsSettings) -> None:
+        if metrics_settings == None:
+            self.metrics_provider_process = None
+            return
+        self.metrics_provider_process = Process(
+            target=MetricsCollector.provide_metrics,
+            args=(metrics_settings,)
+        )
+        logger.info('Created MetricsProvider process')
+
+    def __create_task_runner_processes(
+        self,
+        workers: List[WorkerInterface],
+        configuration: Configuration,
+        metrics_settings: MetricsSettings
+    ) -> None:
+        self.task_runner_processes = []
+        for worker in workers:
+            self.__create_task_runner_process(
+                worker, configuration, metrics_settings
+            )
+        logger.info('Created TaskRunner processes')
+
+    def __create_task_runner_process(
+        self,
+        worker: WorkerInterface,
+        configuration: Configuration,
+        metrics_settings: MetricsSettings
+    ) -> None:
+        task_runner = TaskRunner(worker, configuration, metrics_settings)
+        process = Process(
+            target=task_runner.run
+        )
+        self.task_runner_processes.append(process)
+
+    def __start_metrics_provider_process(self):
+        if self.metrics_provider_process == None:
+            return
+        self.metrics_provider_process.start()
+        logger.info('Started MetricsProvider process')
+
+    def __start_task_runner_processes(self):
+        for task_runner_process in self.task_runner_processes:
+            task_runner_process.start()
+        logger.info('Started TaskRunner processes')
+
+    def __join_metrics_provider_process(self):
+        if self.metrics_provider_process == None:
+            return
+        self.metrics_provider_process.join()
+        logger.info('Joined MetricsProvider processes')
+
+    def __join_task_runner_processes(self):
+        for task_runner_process in self.task_runner_processes:
+            task_runner_process.join()
+        logger.info('Joined TaskRunner processes')
+
+    def __stop_metrics_provider_process(self):
+        self.__stop_process(self.metrics_provider_process)
+
+    def __stop_task_runner_processes(self):
+        for task_runner_process in self.task_runner_processes:
+            self.__stop_process(task_runner_process)
+
+    def __stop_process(self, process: Process):
+        if process == None:
+            return
+        try:
+            process.kill()
+            logger.debug(f'Killed process: {process}')
+        except Exception as e:
+            logger.debug(f'Failed to kill process: {process}, reason: {e}')
+            process.terminate()
+            logger.debug('Terminated process: {process}')
