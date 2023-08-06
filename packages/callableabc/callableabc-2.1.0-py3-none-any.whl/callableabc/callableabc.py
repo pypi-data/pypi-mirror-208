eef32f06cc2d9b6e5d5b5ae461561852c771e91d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:46 +0000

    Add package callableabc/2.1.0/callableabc-2.1.0-py3-none-any.whl

---
+++ packages/callableabc/callableabc-2.1.0-py3-none-any.whl/callableabc/callableabc.py
@@ -0,0 +1,41 @@
+from abc import ABCMeta
+
+
+def make_callable_abc_meta(class_call_name: str, /) -> type:
+    class CallableABCMeta(ABCMeta):
+        @classmethod
+        def __prepare__(
+            cls, name: str, bases: tuple[type, ...], /, **kwds: object
+        ) -> dict[str, object]:
+            namespace = dict(super().__prepare__(name, bases, **kwds))  # type: ignore
+            # ^ pyright bug
+            namespace[class_call_name] = None
+            return namespace
+
+        def __init__(
+            self, name: str, bases: tuple[type, ...], namespace: dict[str, object]
+        ) -> None:
+            if class_call_name not in namespace:
+                namespace[class_call_name] = None
+            super().__init__(name, bases, namespace)
+
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            class_call = getattr(self, class_call_name, None)
+            if class_call is not None and callable(class_call):
+                return getattr(self, class_call_name)(*args, **kwargs)
+            return super().__call__(*args, **kwargs)
+
+    CallableABCMeta.__qualname__ = "CallableABCMeta"
+    return CallableABCMeta
+
+
+def make_callable_abc(class_call_name: str = "class_call", /) -> type:
+    callable_abc = make_callable_abc_meta(class_call_name)(
+        "CallableABC", (), {class_call_name: staticmethod(make_callable_abc)}
+    )
+    callable_abc.__qualname__ = "CallableABC"
+    callable_abc.__module__ = "callableabc.callableabc"
+    return callable_abc
+
+
+CallableABC: type = make_callable_abc()
