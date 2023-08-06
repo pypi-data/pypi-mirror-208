3e54a96272f5631726d0867d27c2743ccc297da8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:00 +0000

    Add package pypreql-nlp/0.0.6/pypreql_nlp-0.0.6-py3-none-any.whl

---
+++ packages/pypreql-nlp/pypreql_nlp-0.0.6-py3-none-any.whl/preql_nlp/prompts/__init__.py
@@ -0,0 +1,2 @@
+from .prompt_executor import run_prompt, SelectionPromptCase, SemanticExtractionPromptCase, SemanticToTokensPromptCase
+__all__ = ["run_prompt", "SelectionPromptCase", "SemanticExtractionPromptCase", "SemanticToTokensPromptCase"]
