39b5ade42fcccc8577a8634df0b0ef494aa44f5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:39 +0000

    Add package pynuwa/0.0.1/pynuwa-0.0.1.tar.gz

---
+++ packages/pynuwa/pynuwa-0.0.1.tar.gz/pynuwa-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pynuwa",
+    version="0.0.1",
+    author="pynuwa",
+    author_email="9239867+chaos-god@user.noreply.gitee.com",
+    description="pynuwa",
+    url="https://gitee.com/chaos-god/pynuwa.git",
+    packages=find_packages(),
+    install_requires=[],
+    setup_requires=[],
+    tests_require=[
+        'pytest>=7.1.2',
+    ],
+    dependency_links=[
+    ],
+    extras_require={
+    }
+)
