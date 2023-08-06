cd8fc219538dcc1325600b35da60c5d3a73e60d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:53 +0000

    Add package spotPython/0.0.69/spotPython-0.0.69-py3-none-any.whl

---
+++ packages/spotPython/spotPython-0.0.69-py3-none-any.whl/spotPython/utils/init.py
@@ -0,0 +1,31 @@
+def fun_control_init():
+    """Initialize fun_control dictionary.
+    Args:
+        None
+        Returns:
+            fun_control (dict): A dictionary containing the information about the core model and the hyperparameters.
+        Example:
+            >>> fun_control_init()
+            {'data': None,
+                'train': None,
+                'test': None,
+                'n_samples': None,
+                'target_column': None,
+                'shuffle': None,
+                'k_folds': None,
+                'device': None}
+    """
+    fun_control = {
+        "data": None,
+        "train": None,
+        "test": None,
+        "n_samples": None,
+        "target_column": None,
+        "shuffle": None,
+        "eval": None,
+        "k_folds": None,
+        "device": None,
+        "metric_params": {},
+        "show_batch_interval": 1_000_000,
+    }
+    return fun_control
