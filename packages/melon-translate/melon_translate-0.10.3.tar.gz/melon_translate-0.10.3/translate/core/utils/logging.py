df0dadd4409d591eac325e15c5752a3a0f6ce1ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:56 +0000

    Add package melon-translate/0.10.3/melon_translate-0.10.3.tar.gz

---
+++ packages/melon-translate/melon_translate-0.10.3.tar.gz/melon_translate-0.10.3/translate/core/utils/logging.py
@@ -0,0 +1,13 @@
+import logging
+import sys
+
+log = logging.getLogger("translate_server")
+log.setLevel(logging.DEBUG)
+
+handler = logging.StreamHandler(sys.stdout)
+handler.setLevel(logging.DEBUG)
+
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+handler.setFormatter(formatter)
+
+log.addHandler(handler)
