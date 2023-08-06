b9cf818bf688fcf6e3c43a66cc2f218cf2ec6395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:21 +0000

    Add package covidillness/0.0.8/covidillness-0.0.8.tar.gz

---
+++ packages/covidillness/covidillness-0.0.8.tar.gz/covidillness-0.0.8/setup.py
@@ -0,0 +1,32 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="covidillness",
+    version="0.0.8",
+    author="Izuru Inose",
+    author_email="i.inose0304@gmail.com",
+    description="The graph of the number of ICU patients and cases due to Covid-19",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/i-inose/covidillness",
+    project_urls={
+        "Bug Tracker": "https://github.com/i-inose/covidillness",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    py_modules=['covidillness'],
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.7",
+    entry_points = {
+        'console_scripts': [
+            'covidillness = covidillness:main'
+        ]
+    },
+)
