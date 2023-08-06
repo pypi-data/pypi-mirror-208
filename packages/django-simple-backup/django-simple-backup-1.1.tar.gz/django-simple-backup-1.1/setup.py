92067ecc967a6427dbb57acd78aa22428b5115a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:55 +0000

    Add package django-simple-backup/1.1/django-simple-backup-1.1.tar.gz

---
+++ packages/django-simple-backup/django-simple-backup-1.1.tar.gz/django-simple-backup-1.1/setup.py
@@ -0,0 +1,17 @@
+import ez_setup
+ez_setup.use_setuptools()
+from distutils.core import setup
+from setuptools import find_packages
+
+setup(
+    name='django-simple-backup',
+    version     = '1.1',
+    author        = 'Evgeny Fadeev',
+    author_email = 'evgeny.fadeev@gmail.com',
+    url            = '',
+    description    = 'A simple backup command for Django',
+    packages=find_packages(),
+    python_requires='<3',
+    include_package_data=True,
+)
+
