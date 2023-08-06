49cfc6649c7aadc8e6474c5c2ad4486b6f89bc2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:21 +0000

    Add package harborapi/0.19.0/harborapi-0.19.0-py3-none-any.whl

---
+++ packages/harborapi/harborapi-0.19.0-py3-none-any.whl/harborapi/__init__.py
@@ -0,0 +1,26 @@
+# Disable the loguru logger unless otherwise specified
+from loguru import logger
+
+logger.disable("harborapi")
+
+from . import _types, auth, client, client_sync, exceptions, utils, version
+from .__about__ import __version__ as __version__
+from .client import HarborAsyncClient
+from .client_sync import HarborClient
+
+# Import after everything else to avoid circular imports
+from . import ext  # isort: skip
+
+
+__all__ = [
+    "HarborAsyncClient",
+    "HarborClient",
+    "auth",
+    "client",
+    "client_sync",
+    "exceptions",
+    "ext",
+    "_types",
+    "utils",
+    "version",
+]
