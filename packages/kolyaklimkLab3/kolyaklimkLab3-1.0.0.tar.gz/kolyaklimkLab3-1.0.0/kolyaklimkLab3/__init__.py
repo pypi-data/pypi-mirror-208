121a87cffe3302cc22be4bd6a865b00944987604
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:55 +0000

    Add package kolyaklimkLab3/1.0.0/kolyaklimkLab3-1.0.0.tar.gz

---
+++ packages/kolyaklimkLab3/kolyaklimkLab3-1.0.0.tar.gz/kolyaklimkLab3-1.0.0/kolyaklimkLab3/__init__.py
@@ -0,0 +1,5 @@
+from kolyaklimkLab3.MyParser.Parser import Parser
+from kolyaklimkLab3.MySerializer.Factory import Factory
+from kolyaklimkLab3.MySerializer.Serializer import Serializer
+from kolyaklimkLab3.MyParser.XmlParser.Xml import Xml
+from kolyaklimkLab3.MyParser.JsonParser.Json import Json
