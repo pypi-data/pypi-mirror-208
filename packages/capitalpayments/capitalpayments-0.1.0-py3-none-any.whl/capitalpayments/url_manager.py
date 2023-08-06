65cf381b3e8fde1b868d4739392223b627ab767b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:39 +0000

    Add package capitalpayments/0.1.0/capitalpayments-0.1.0-py3-none-any.whl

---
+++ packages/capitalpayments/capitalpayments-0.1.0-py3-none-any.whl/capitalpayments/url_manager.py
@@ -0,0 +1,20 @@
+url_manager = {
+    'get_environment': 'https://www.capitalpayments.me/app/api/getEnvironment',
+    'get_account': 'https://www.capitalpayments.me/app/api/getAccount',
+    'login': 'https://www.capitalpayments.me/app/api/login',
+
+    # invoices
+    'create_invoice': 'https://www.capitalpayments.me/app/api/createInvoice',
+    'get_invoice_status': 'https://www.capitalpayments.me/app/api/getInvoiceStatus',
+    'cancel_invoice': 'https://www.capitalpayments.me/app/api/cancelInvoice',
+    
+    # payouts
+    'create_payout': 'https://www.capitalpayments.me/app/api/createPayout',
+    'get_payout_status': 'https://www.capitalpayments.me/app/api/getPayoutStatus',
+    'cancel_payout': 'https://www.capitalpayments.me/app/api/cancelPayout',
+
+    # wallet
+    'get_balance': 'https://www.capitalpayments.me/app/api/getBalance',
+    'get_main_wallet': 'https://www.capitalpayments.me/app/api/getMainWallet',
+    'get_wallets': 'https://www.capitalpayments.me/app/api/getWallets'
+}
