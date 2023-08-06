5c3958cac80f210bdc43cb69d06b66b91dc012f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:40 +0000

    Add package mybassm/0.1.0/mybassm-0.1.0-py2.py3-none-any.whl

---
+++ packages/mybassm/mybassm-0.1.0-py2.py3-none-any.whl/mybassm/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for mybassm."""
+
+__author__ = """Anahit Zakaryan"""
+__email__ = 'anukzak@gmail.com'
+__version__ = '0.1.0'
+
+
+from mybassm.mybassm import (diffusion, bass_f, bass_F, predict_bass_model)
