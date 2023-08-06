85719a526cf25a29779260882d261d440a422304
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:31 +0000

    Add package jpterm/0.1.14/jpterm-0.1.14.tar.gz

---
+++ packages/jpterm/jpterm-0.1.14.tar.gz/jpterm-0.1.14/plugins/local_kernels/txl_local_kernels/components.py
@@ -0,0 +1,25 @@
+import y_py as Y
+from asphalt.core import Component, Context
+
+from txl.base import Kernels
+
+from .driver import KernelDriver
+
+
+class LocalKernels(Kernels):
+
+    comm_handlers = []
+
+    def __init__(self, kernel_name: str | None = None):
+        self.kernel = KernelDriver(kernel_name, comm_handlers=self.comm_handlers)
+
+    async def execute(self, ydoc: Y.YDoc, ycell: Y.YMap):
+        await self.kernel.execute(ydoc, ycell)
+
+
+class LocalKernelsComponent(Component):
+    async def start(
+        self,
+        ctx: Context,
+    ) -> None:
+        ctx.add_resource(LocalKernels, types=Kernels)
