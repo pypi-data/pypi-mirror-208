008faec93bcb77ae907ecc794be5fdb44a4d2844
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:38 +0000

    Add package rcp-colors/0.112/rcp-colors-0.112.tar.gz

---
+++ packages/rcp-colors/rcp-colors-0.112.tar.gz/rcp-colors-0.112/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rcp-colors',
+    version='0.112',
+    packages=find_packages(),
+    description='A Rich Color Picker app',
+    long_description='Terminal based rich color picker app',
+    long_description_content_type='text/markdown',
+    url='https://github.com/PlusPlusMan/rcp',
+    author='PlusPlusMan',
+    author_email='contact@plusplusman.com',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
+    keywords='color picker, terminal app rgb hsl hex colors',
+    python_requires='>=3.6, <4',
+    install_requires=[
+        'textual',
+        'appdirs',
+    ],
+    entry_points={
+        'console_scripts': [
+            'rcp=rcp_colors.rcp:main',
+        ],
+    },
+    project_urls={
+        'Source': 'https://github.com/PlusPlusMan/rcp/',
+    },
+)
