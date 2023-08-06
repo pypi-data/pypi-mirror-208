011cd016d1f146a2ab686114fceb1b2e485bf205
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:02 +0000

    Add package nonebot-plugin-record/1.0.4/nonebot_plugin_record-1.0.4-py3-none-any.whl

---
+++ packages/nonebot-plugin-record/nonebot_plugin_record-1.0.4-py3-none-any.whl/nonebot_plugin_record/config.py
@@ -0,0 +1,17 @@
+from nonebot import get_driver
+
+from typing import Optional
+from pydantic import (
+    BaseModel,
+    Extra
+)
+
+
+class Config(BaseModel, extra=Extra.ignore):
+    nonebot_plugin_gocqhttp: Optional[bool] = False
+    asr_api_provider: str = None
+    asr_api_key: str = None
+    asr_secret_key: str = None
+
+
+plugin_config = Config.parse_obj(get_driver().config.dict())
