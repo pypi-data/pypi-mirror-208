1e9f8e19a09527dfa3a6ce052d6595ccd385f738
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:55 +0000

    Add package picsellia-tf2/0.11.10/picsellia_tf2-0.11.10.tar.gz

---
+++ packages/picsellia-tf2/picsellia_tf2-0.11.10.tar.gz/picsellia_tf2-0.11.10/README.md
@@ -0,0 +1,4 @@
+# Picsellia TF2
+
+This package is a wrapper of Tensorflow Object Detection
+It is mostly used for training models around Tensorflow environment
---
+++ packages/picsellia-tf2/picsellia_tf2-0.11.10.tar.gz/picsellia_tf2-0.11.10/picsellia_tf2/__init__.py
@@ -0,0 +1,13 @@
+import os
+import sys
+
+from setuptools import find_packages
+
+import picsellia_tf2
+
+for p in find_packages(where=picsellia_tf2.__path__[0]):
+    sys.path.append(os.path.join(picsellia_tf2.__path__[0], p.replace(".", "/")))
+
+sys.path.append(picsellia_tf2.__path__[0])
+sys.path.append(os.path.join(picsellia_tf2.__path__[0], "object_detection"))
+sys.path.append(os.path.join(picsellia_tf2.__path__[0], "slim"))
