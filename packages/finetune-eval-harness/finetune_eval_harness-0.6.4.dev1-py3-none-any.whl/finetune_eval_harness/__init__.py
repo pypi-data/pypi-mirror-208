bc11d619ce9e2dac366f01ee0d2965be10e75fba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:37 +0000

    Add package finetune-eval-harness/0.6.4.dev1/finetune_eval_harness-0.6.4.dev1-py3-none-any.whl

---
+++ packages/finetune-eval-harness/finetune_eval_harness-0.6.4.dev1-py3-none-any.whl/finetune_eval_harness/__init__.py
@@ -0,0 +1,45 @@
+
+
+## please make sure that the vesion numbers are same here and setup.py
+
+#__version__ = "0.6.0.dev0"
+
+'''
+from .hf_scripts import (
+    data_trainining_args,
+    hgf_fine_tune_class,
+    hgf_fine_tune_ner,
+    hgf_fine_tune_qa,
+    initial_arguments,
+    model_args,
+    trainer_qa,
+    utility_functions,
+    utils_qa,
+)
+
+from .tasks import (
+    classification,
+    german_europarl,
+    german_ner,
+    germeval2017,
+    germeval2018,
+    gnad10,
+    ner,
+    qa,
+    german_quad,
+)
+'''
+import sys 
+sys.path.append('../')
+#from . import process_args
+#from . import main
+from src.finetune_eval_harness.tasks.task_registry import get_all_tasks, get_all_task_types, get_dataset_information
+#from .process_args import process_arguments
+#import src.finetune_eval.process_args as process_args
+
+
+#import src.finetune_eval.main as main
+import src.finetune_eval_harness.tasks
+import src.finetune_eval_harness.hf_scripts
+import src.finetune_eval_harness.tasks as tasks
+import src.finetune_eval_harness.hf_scripts as hf_scripts
