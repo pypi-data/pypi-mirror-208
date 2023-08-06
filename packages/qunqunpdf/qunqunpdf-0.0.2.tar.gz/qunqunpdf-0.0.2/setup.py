a0eff29c6b4cdecc9a66363807195ee9d5cea164
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:55 +0000

    Add package qunqunpdf/0.0.2/qunqunpdf-0.0.2.tar.gz

---
+++ packages/qunqunpdf/qunqunpdf-0.0.2.tar.gz/qunqunpdf-0.0.2/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="qunqunpdf",
+    version='0.0.2',
+    description="say hello",
+    py_modules=["pdf2image", "pdf2text"],
+    package_dir={'': 'qunqunpdf'},
+)
