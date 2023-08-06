bdf015e13a21f0f928d6eeb2a9cfe2f999eedfde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:42 +0000

    Add package dustpylib/0.4.0/dustpylib-0.4.0-py3-none-any.whl

---
+++ packages/dustpylib/dustpylib-0.4.0-py3-none-any.whl/dustpylib/__init__.py
@@ -0,0 +1,17 @@
+"""
+``dustpylib`` is a package with auxiliary tools and extensions for the dust evolution software ``DustPy``.
+"""
+
+from dustpylib import grid
+from dustpylib import planetesimals
+from dustpylib import radtrans
+from dustpylib import substructures
+
+__version__ = "0.4.0"
+
+__all__ = [
+    "grid",
+    "planetesimals",
+    "radtrans",
+    "substructures",
+]
---
+++ packages/dustpylib/dustpylib-0.4.0-py3-none-any.whl/dustpylib/radtrans/__init__.py
@@ -0,0 +1,9 @@
+"""
+This package contains interfaces to radiative transfer codes from ``DustPy`` models.
+"""
+
+from dustpylib.radtrans import radmc3d
+
+__all__ = [
+    "radmc3d",
+]
