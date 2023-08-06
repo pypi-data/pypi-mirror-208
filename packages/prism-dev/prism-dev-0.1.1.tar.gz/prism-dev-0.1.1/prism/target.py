8f10629bd55dbce5ab5e18862e38e10769b19977
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:13:40 +0000

    Add package prism-dev/0.1.1/prism-dev-0.1.1.tar.gz

---
+++ packages/prism-dev/prism-dev-0.1.1.tar.gz/prism-dev-0.1.1/prism/target.py
@@ -0,0 +1,65 @@
+"""
+Target class definition.
+
+Table of Contents
+- Imports
+- Class definitions
+- Target decorators
+"""
+
+###########
+# Imports #
+###########
+
+# Prism imports
+import prism.exceptions
+import prism.infra.hooks
+import prism.infra.task_manager
+
+
+#####################
+# Class definitions #
+#####################
+
+class PrismTarget:
+
+    def __init__(self, obj, loc, hooks):
+        self.obj = obj
+        self.loc = loc
+        self.hooks = hooks
+
+    def save(self):
+        raise prism.exceptions.RuntimeException(message="`save` method not implemented")
+
+
+class PySparkParquet(PrismTarget):
+
+    def save(self, **kwargs):
+        self.obj.write.parquet(self.loc, **kwargs)
+
+
+class PandasCsv(PrismTarget):
+
+    def save(self, **kwargs):
+        self.obj.to_csv(self.loc, **kwargs)
+
+
+class NumpyTxt(PrismTarget):
+
+    def save(self, **kwargs):
+        import numpy as np
+        np.savetxt(self.loc, self.obj, **kwargs)
+
+
+class Txt(PrismTarget):
+
+    def save(self, **kwargs):
+        with open(self.loc, "w") as f:
+            f.write(self.obj, **kwargs)
+        f.close()
+
+
+class MatplotlibPNG(PrismTarget):
+
+    def save(self, **kwargs):
+        self.obj.savefig(self.loc, **kwargs)
