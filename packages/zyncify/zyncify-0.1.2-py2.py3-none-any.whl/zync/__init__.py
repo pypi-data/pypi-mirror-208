ad3427668b45965b4948dcf233e9cd9ccbd0d039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:18 +0000

    Add package zyncify/0.1.2/zyncify-0.1.2-py2.py3-none-any.whl

---
+++ packages/zyncify/zyncify-0.1.2-py2.py3-none-any.whl/zync/__init__.py
@@ -0,0 +1,7 @@
+from .logging import logger, bugger
+from .slugify import Slugger, slugger
+
+__all__ = ["logger", "bugger", "Slugger", "slugger"]
+
+__version__ = "0.1.0"
+__author__ = "TJ Bredemeyer"
