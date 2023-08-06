edae22af310d588765b86dda962392dfd9d3e35d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:08 +0000

    Add package midoTrim/1.2.10/midoTrim-1.2.10-py3-none-any.whl

---
+++ packages/midoTrim/midoTrim-1.2.10-py3-none-any.whl/midoTrim/version.py
@@ -0,0 +1,18 @@
+import packaging.version
+
+try:
+    # Python 3.8+
+    import importlib.metadata as importlib_metadata
+except ImportError:
+    # <Python 3.7 and lower
+    import importlib_metadata
+
+__version__ = "0.0.0.dev0"
+
+try:
+    __version__ = importlib_metadata.version("mido")
+except importlib_metadata.PackageNotFoundError:
+    # package is not installed
+    pass
+
+version_info = packaging.version.Version(__version__)
