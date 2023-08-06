95aceb48f2b7e40372108be6cca5efec39b18c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:37 +0000

    Add package collecting-faces/1.0.4/collecting_faces-1.0.4.tar.gz

---
+++ packages/collecting-faces/collecting_faces-1.0.4.tar.gz/collecting_faces-1.0.4/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="collecting_faces", # Replace with your own username
+    version="1.0.4",
+    author="Seokhee-Jin",
+    author_email="seokhee749@gmail.com",
+    description="Collecting square faces from images for training stylegan2-ada",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Seokhee-Jin/collecting_faces",
+    packages=setuptools.find_packages(),
+    package_data={'collecting_faces.preprocess': ['shape_predictor_68_face_landmarks.dat']},
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
