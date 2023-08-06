6dab3e3cce0a9e2bb1b7bed4ce7cd3dfd9aaf4a7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:16:59 +0000

    Add package ecowitt2mqtt/2023.5.0/ecowitt2mqtt-2023.5.0-py3-none-any.whl

---
+++ packages/ecowitt2mqtt/ecowitt2mqtt-2023.5.0-py3-none-any.whl/ecowitt2mqtt/helpers/publisher/factory.py
@@ -0,0 +1,24 @@
+"""Define a publisher factory."""
+from __future__ import annotations
+
+from asyncio_mqtt import Client
+
+from ecowitt2mqtt.config import Config
+from ecowitt2mqtt.helpers.publisher import MqttPublisher
+from ecowitt2mqtt.helpers.publisher.hass import HomeAssistantDiscoveryPublisher
+from ecowitt2mqtt.helpers.publisher.topic import TopicPublisher
+
+
+def get_publisher(config: Config, client: Client) -> MqttPublisher:
+    """Get an MQTT publisher.
+
+    Args:
+        config: A Config object.
+        client: An MQTT Client object.
+
+    Returns:
+        An MqttPublisher object.
+    """
+    if config.hass_discovery:
+        return HomeAssistantDiscoveryPublisher(config, client)
+    return TopicPublisher(config, client)
