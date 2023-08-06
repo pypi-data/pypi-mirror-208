4fb05dafabade95d11087262bb836262928579f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:15:40 +0000

    Add package dubinka-custom-serializer/0.0.2/dubinka-custom-serializer-0.0.2.tar.gz

---
+++ packages/dubinka-custom-serializer/dubinka-custom-serializer-0.0.2.tar.gz/dubinka-custom-serializer-0.0.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='dubinka-custom-serializer',
+    version='0.0.2',
+    packages=['custom_serializer'],
+    entry_points={
+        "console_scripts": [
+            "custom-serialize = custom_serializer.custom_serializer:main"
+        ]
+    },
+    url='',
+    license='',
+    author='misha-rab-ymniy',
+    author_email='',
+    description=''
+)
