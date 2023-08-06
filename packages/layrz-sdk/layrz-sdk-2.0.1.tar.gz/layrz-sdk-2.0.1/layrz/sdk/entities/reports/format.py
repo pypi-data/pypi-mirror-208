c7aed69ac0c12189a427e9026fa5a791d9a1a0f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:08:18 +0000

    Add package layrz-sdk/2.0.1/layrz-sdk-2.0.1.tar.gz

---
+++ packages/layrz-sdk/layrz-sdk-2.0.1.tar.gz/layrz-sdk-2.0.1/layrz/sdk/entities/reports/format.py
@@ -0,0 +1,21 @@
+""" Report formats """
+from enum import Enum
+
+class ReportFormat(Enum):
+  """
+  Report format definition.
+  """
+  MICROSOFT_EXCEL = 'MICROSOFT_EXCEL'
+
+  @property
+  def __readable(self):
+    """ Readable """
+    return f'ReportFormat.{self.value}'
+
+  def __str__(self):
+    """ Readable property """
+    return self.__readable
+
+  def __repr__(self):
+    """ Readable property """
+    return self.__readable
