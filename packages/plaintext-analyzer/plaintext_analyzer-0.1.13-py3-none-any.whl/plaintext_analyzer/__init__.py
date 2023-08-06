4896a6d634c1e70f71b2c2135cd09aa91ac7e53e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:07:09 +0000

    Add package plaintext-analyzer/0.1.13/plaintext_analyzer-0.1.13-py3-none-any.whl

---
+++ packages/plaintext-analyzer/plaintext_analyzer-0.1.13-py3-none-any.whl/plaintext_analyzer/__init__.py
@@ -0,0 +1,39 @@
+"""
+.. topic:: Analyze vocabularies
+
+  .. code:: python
+
+    from plaintext_analyzer import PlaintextReader
+    from plaintext_analyzer import VocabAnalyzer
+
+    sf = PlaintextReader(source, stype, lang)
+    sens = sf.sentences
+    print(sens)
+
+    analyzer = VocabAnalyzer(lang)
+    exs = analyzer.overview_vocabs(sens, google)
+    print(exs)
+
+.. topic:: Analyze phrases
+
+  .. code:: python
+ 
+    from plaintext_analyzer import PlaintextReader
+    from plaintext_analyzer import PhraseAnalyzer
+
+    sf = PlaintextReader(source, stype, lang)
+    sens = sf.sentences
+    print(sens)
+
+    analyzer = PhraseAnalyzer(lang)
+    exs = analyzer.overview_phrases(sens, google)
+    print(exs)
+"""
+
+__version__ = '0.1.12'
+
+from .file_reader import PlaintextReader
+from .file_writer import CSVWriter, write_to_json
+from .vocab_analyzer import VocabAnalyzer
+from .phrase_analyzer import PhraseAnalyzer
+from .structure_kg_analyzer import StructureKGAnalyzer
---
+++ packages/plaintext-analyzer/plaintext_analyzer-0.1.13-py3-none-any.whl/plaintext_analyzer/entry.py
@@ -0,0 +1,106 @@
+from plaintext_analyzer import PlaintextReader
+from plaintext_analyzer import VocabAnalyzer, PhraseAnalyzer, StructureKGAnalyzer
+from plaintext_analyzer import CSVWriter, write_to_json
+
+import click
+import json
+
+#pta_file2phrase = "plaintext_analyzer.entry:file2phrase"
+
+#pta_text2vocab = "plaintext_analyzer.entry:text2vocab"
+#pta_text2phrase = "plaintext_analyzer.entry:text2phrase"
+
+
+@click.command()
+@click.option("--source", help="Specify the filename or plaintext itself", prompt="source")
+@click.option("--stype", help="RAW or FILE", prompt="source type[TEXT|FILE]")
+@click.option("--lang", help="Specify the language", default="en", prompt="language")
+@click.option("--dstname", required=False, help="Specify the output csv name", default=None)
+@click.option("--external", required=False, help="Whether extra help needed", type=bool, default=False)
+def parser_vocab(source, stype, lang, dstname, external):
+
+  phase = {"step": 1, "msg": "Start sentenizing"}
+  print(json.dumps(phase), flush=True)
+
+  sf = PlaintextReader(source, stype, lang)
+  sens = sf.sentences
+
+  phase = {"step": 2, "msg": "Finish sentenizing"}
+  print(json.dumps(phase), flush=True)
+
+  analyzer = VocabAnalyzer(lang)
+  exs = analyzer.overview_vocabs(sens, external)
+  simple_exs = [{k:v for k,v in e.items() if k in ['word', 'dict_pos', 'meaning']} for e in exs]
+  shown = simple_exs[:15]
+
+  phase = {"step": 3, "msg": "Finish vocabs dictionary lookup", "vocabs": shown}
+  print(json.dumps(phase), flush=True)
+
+  if dstname:
+    csv_writer = CSVWriter(dstname)
+    csv_writer.write(simple_exs)
+    csv_writer = CSVWriter(dstname+'.forpptx')
+    csv_writer.write(exs)
+
+    phase = {"step": 4, "msg": "Finish csv saving"} 
+    print(json.dumps(phase), flush=True)
+
+
+@click.command()
+@click.option("--source", help="Specify the filename or plaintext itself", prompt="source")
+@click.option("--stype", help="RAW or FILE", prompt="source type[TEXT|FILE]")
+@click.option("--lang", help="Specify the language", default="en", prompt="language")
+@click.option("--dstname", required=False, help="Specify the output csv name", default=None)
+def parser_phrase(source, stype, lang, dstname):
+
+  phase = {"step": 1, "msg": "Start sentenizing"}
+  print(json.dumps(phase), flush=True)
+
+  sf = PlaintextReader(source, stype, lang)
+  sens = sf.sentences
+
+  phase = {"step": 2, "msg": "Finish sentenizing"}
+  print(json.dumps(phase), flush=True)
+
+  analyzer = PhraseAnalyzer(lang)
+  exs = analyzer.overview_phrases(sens)
+
+  phase = {"step": 3, "msg": "Finish phrase dictionary lookup", "phrases": exs[:2]}
+  print(json.dumps(phase), flush=True)
+
+  if dstname:
+    csv_writer = CSVWriter(dstname+'.forpptx')
+    csv_writer.write(exs)
+
+    phase = {"step": 4, "msg": "Finish csv saving"} 
+    print(json.dumps(phase), flush=True)
+
+@click.command()
+@click.option("--source", help="Specify the filename or plaintext itself", prompt="source")
+@click.option("--stype", help="RAW or FILE", prompt="source type[TEXT|FILE]")
+@click.option("--lang", help="Specify the language", default="en", prompt="language")
+@click.option("--dstname", required=False, help="Specify the output json name", default=None)
+def parser_structure_kg(source, stype, lang, dstname):
+
+  phase = {"step": 1, "msg": "Start sentenizing"}
+  print(json.dumps(phase), flush=True)
+
+  sf = PlaintextReader(source, stype, lang)
+  sens = sf.sentences
+
+  phase = {"step": 2, "msg": "Finish sentenizing"}
+  print(json.dumps(phase), flush=True)
+
+  analyzer = StructureKGAnalyzer(lang)
+  exs = analyzer.overview_structure_kg(sens)
+
+  phase = {"step": 3, "msg": "Finish dictionary lookup", "structure_kg": exs[:2]}
+  print(json.dumps(phase), flush=True)
+
+  if dstname:
+    write_to_json(exs, dstname)
+
+    phase = {"step": 4, "msg": "Finish json saving"} 
+    print(json.dumps(phase), flush=True)
+
+
