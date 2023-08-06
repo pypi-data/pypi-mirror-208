67d419fa578765f7383dd18b04d232afe8557d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:30:38 +0000

    Add package pyphpfunc/1.1/pyphpfunc-1.1.tar.gz

---
+++ packages/pyphpfunc/pyphpfunc-1.1.tar.gz/pyphpfunc-1.1/pyphpfunc/other.py
@@ -0,0 +1,2 @@
+def echoto(v):
+	print(v)
---
+++ packages/pyphpfunc/pyphpfunc-1.1.tar.gz/pyphpfunc-1.1/setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyphpfunc",
+    version="1.1",
+    author="lgnove",
+    author_email="lgynove@163.com",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="",
+    packages=setuptools.find_packages(),
+
+    classifiers=[
+    ],
+    install_requires=[
+    ],
+    python_requires=">=3",
+)
