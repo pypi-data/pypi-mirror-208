e9d510678da53935ffd9dc4e8bdd28e2c0807f3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:42:24 +0000

    Add package codewithgpu/0.2.8/codewithgpu-0.2.8-py3-none-any.whl

---
+++ packages/codewithgpu/codewithgpu-0.2.8-py3-none-any.whl/codewithgpu/version.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+version = '0.2.8'
+git_version = '796021fbce012d8b1e3a9175d1db01a0ed76340e'
