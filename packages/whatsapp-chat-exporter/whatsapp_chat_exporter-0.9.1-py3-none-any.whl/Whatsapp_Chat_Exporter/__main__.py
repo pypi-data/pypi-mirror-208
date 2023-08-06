0509e02053610c063262770d0fa03c0fcb3b206a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:43 +0000

    Add package whatsapp-chat-exporter/0.9.1/whatsapp_chat_exporter-0.9.1-py3-none-any.whl

---
+++ packages/whatsapp-chat-exporter/whatsapp_chat_exporter-0.9.1-py3-none-any.whl/Whatsapp_Chat_Exporter/__main__.py
@@ -0,0 +1,287 @@
+try:
+    from .__init__ import __version__
+except ImportError:
+    from Whatsapp_Chat_Exporter.__init__ import __version__
+from Whatsapp_Chat_Exporter import extract, extract_iphone
+from Whatsapp_Chat_Exporter import extract_iphone_media
+from Whatsapp_Chat_Exporter.data_model import ChatStore
+from Whatsapp_Chat_Exporter.utility import Crypt, check_update
+from argparse import ArgumentParser
+import os
+import sqlite3
+import shutil
+import json
+import string
+from sys import exit
+
+
+def main():
+    parser = ArgumentParser(
+        description = 'A customizable Android and iPhone WhatsApp database parser that '
+                      'will give you the history of your WhatsApp conversations inHTML '
+                      'and JSON. Android Backup Crypt12, Crypt14 and Crypt15 supported.',
+        epilog = f'WhatsApp Chat Exporter: {__version__} Licensed with MIT'
+    )
+    parser.add_argument(
+        '-a',
+        '--android',
+        dest='android',
+        default=False,
+        action='store_true',
+        help="Define the target as Android")
+    parser.add_argument(
+        '-i',
+        '--iphone',
+        '--ios',
+        dest='iphone',
+        default=False,
+        action='store_true',
+        help="Define the target as iPhone/iPad")
+    parser.add_argument(
+        "-w",
+        "--wa",
+        dest="wa",
+        default=None,
+        help="Path to contact database (default: wa.db/ContactsV2.sqlite)")
+    parser.add_argument(
+        "-m",
+        "--media",
+        dest="media",
+        default=None,
+        help="Path to WhatsApp media folder (default: WhatsApp)")
+    parser.add_argument(
+        "-b",
+        "--backup",
+        dest="backup",
+        default=None,
+        help="Path to Android (must be used together "
+             "with -k)/iPhone WhatsApp backup")
+    parser.add_argument(
+        "-o",
+        "--output",
+        dest="output",
+        default="result",
+        help="Output to specific directory (default: result)")
+    parser.add_argument(
+        '-j',
+        '--json',
+        dest='json',
+        nargs='?',
+        default=None,
+        type=str,
+        const="result.json",
+        help="Save the result to a single JSON file (default if present: result.json)")
+    parser.add_argument(
+        '-d',
+        '--db',
+        dest='db',
+        default=None,
+        help="Path to database file (default: msgstore.db/"
+             "7c7fba66680ef796b916b067077cc246adacf01d)")
+    parser.add_argument(
+        '-k',
+        '--key',
+        dest='key',
+        default=None,
+        help="Path to key file"
+    )
+    parser.add_argument(
+        "-t",
+        "--template",
+        dest="template",
+        default=None,
+        help="Path to custom HTML template"
+    )
+    parser.add_argument(
+        "-e",
+        "--embedded",
+        dest="embedded",
+        default=False,
+        action='store_true',
+        help="Embed media into HTML file (not yet implemented)"
+    )
+    parser.add_argument(
+        "-s",
+        "--showkey",
+        dest="showkey",
+        default=False,
+        action='store_true',
+        help="Show the HEX key used to decrypt the database"
+    )
+    parser.add_argument(
+        "-c",
+        "--move-media",
+        dest="move_media",
+        default=False,
+        action='store_true',
+        help="Move the media directory to output directory if the flag is set, otherwise copy it"
+    )
+    parser.add_argument(
+        "--offline",
+        dest="offline",
+        default=None,
+        help="Relative path to offline static files"
+    )
+    parser.add_argument(
+        "--size",
+        "--output-size",
+        dest="size",
+        default=None,
+        help="Maximum size of a single output file in bytes, 0 for auto (not yet implemented)"
+    )
+    parser.add_argument(
+        "--no-html",
+        dest="no_html",
+        default=False,
+        action='store_true',
+        help="Do not output html files"
+    )
+    parser.add_argument(
+        "--check-update",
+        dest="check_update",
+        default=False,
+        action='store_true',
+        help="Check for updates"
+    )
+    args = parser.parse_args()
+
+    # Check for updates
+    if args.check_update:
+        exit(check_update())
+
+    # Sanity checks
+    if args.android and args.iphone:
+        print("You must define only one device type.")
+        exit(1)
+    if not args.android and not args.iphone:
+        print("You must define the device type.")
+        exit(1)
+    if args.no_html and not args.json:
+        print("You must either specify a JSON output file or enable HTML output.")
+        exit(1)
+
+    data = {}
+
+    if args.android:
+        contacts = extract.contacts
+        messages = extract.messages
+        media = extract.media
+        vcard = extract.vcard
+        create_html = extract.create_html
+        if args.db is None:
+            msg_db = "msgstore.db"
+        else:
+            msg_db = args.db
+        if args.key is not None:
+            if args.backup is None:
+                print("You must specify the backup file with -b")
+                exit(1)
+            print("Decryption key specified, decrypting WhatsApp backup...")
+            if "crypt12" in args.backup:
+                crypt = Crypt.CRYPT12
+            elif "crypt14" in args.backup:
+                crypt = Crypt.CRYPT14
+            elif "crypt15" in args.backup:
+                crypt = Crypt.CRYPT15
+            if os.path.isfile(args.key):
+                key = open(args.key, "rb")
+            elif all(char in string.hexdigits for char in args.key):
+                key = bytes.fromhex(args.key)
+            db = open(args.backup, "rb").read()
+            error = extract.decrypt_backup(db, key, msg_db, crypt, args.showkey)
+            if error != 0:
+                if error == 1:
+                    print("Dependencies of decrypt_backup and/or extract_encrypted_key"
+                          " are not present. For details, see README.md.")
+                    exit(3)
+                elif error == 2:
+                    print("Failed when decompressing the decrypted backup. "
+                          "Possibly incorrect offsets used in decryption.")
+                    exit(4)
+                else:
+                    print("Unknown error occurred.", error)
+                    exit(5)
+        if args.wa is None:
+            contact_db = "wa.db"
+        else:
+            contact_db = args.wa
+        if args.media is None:
+            args.media = "WhatsApp"
+
+        if os.path.isfile(contact_db):
+            with sqlite3.connect(contact_db) as db:
+                db.row_factory = sqlite3.Row
+                contacts(db, data)
+
+    elif args.iphone:
+        import sys
+        if "--iphone" in sys.argv:
+            print("WARNING: The --iphone flag is deprecated and will be removed in the future. Use --ios instead.")
+        messages = extract_iphone.messages
+        media = extract_iphone.media
+        vcard = extract_iphone.vcard
+        create_html = extract_iphone.create_html
+        if args.backup is not None:
+            extract_iphone_media.extract_media(args.backup)
+        if args.db is None:
+            msg_db = "7c7fba66680ef796b916b067077cc246adacf01d"
+        else:
+            msg_db = args.db
+        if args.wa is None:
+            contact_db = "ContactsV2.sqlite"
+        else:
+            contact_db = args.wa
+        if args.media is None:
+            args.media = "Message"
+
+    if os.path.isfile(msg_db):
+        with sqlite3.connect(msg_db) as db:
+            db.row_factory = sqlite3.Row
+            messages(db, data)
+            media(db, data, args.media)
+            vcard(db, data)
+        if not args.no_html:
+            create_html(
+                data,
+                args.output,
+                args.template,
+                args.embedded,
+                args.offline,
+                args.size
+            )
+    else:
+        print(
+            "The message database does not exist. You may specify the path "
+            "to database file with option -d or check your provided path."
+        )
+        exit(2)
+
+    if os.path.isdir(args.media):
+        if os.path.isdir(f"{args.output}/{args.media}"):
+            print("Media directory already exists in output directory. Skipping...")
+        else:
+            if not args.move_media:
+                print("Copying media directory...")
+                shutil.copytree(args.media, f"{args.output}/WhatsApp")
+            else:
+                try:
+                    shutil.move(args.media, f"{args.output}/")
+                except PermissionError:
+                    print("Cannot remove original WhatsApp directory. "
+                        "Perhaps the directory is opened?")
+
+    if args.json:
+        if isinstance(data[next(iter(data))], ChatStore):
+            data = {jik: chat.to_json() for jik, chat in data.items()}
+        with open(args.json, "w") as f:
+            data = json.dumps(data)
+            print(f"\nWriting JSON file...({int(len(data)/1024/1024)}MB)")
+            f.write(data)
+    else:
+        print()
+
+    print("Everything is done!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ packages/whatsapp-chat-exporter/whatsapp_chat_exporter-0.9.1-py3-none-any.whl/Whatsapp_Chat_Exporter/extract_iphone.py
@@ -0,0 +1,332 @@
+#!/usr/bin/python3
+
+import sqlite3
+import json
+import jinja2
+import os
+import shutil
+from pathlib import Path
+from datetime import datetime
+from mimetypes import MimeTypes
+from Whatsapp_Chat_Exporter.utility import sanitize_except, determine_day, APPLE_TIME
+
+
+def messages(db, data):
+    c = db.cursor()
+    # Get contacts
+    c.execute("""SELECT count() FROM ZWACHATSESSION""")
+    total_row_number = c.fetchone()[0]
+    print(f"Gathering contacts...({total_row_number})")
+
+    c.execute("""SELECT ZCONTACTJID, ZPARTNERNAME FROM ZWACHATSESSION; """)
+    row = c.fetchone()
+    while row is not None:
+        data[row[0]] = {"name": row[1], "messages": {}}
+        row = c.fetchone()
+
+    # Get message history
+    c.execute("""SELECT count() FROM ZWAMESSAGE""")
+    total_row_number = c.fetchone()[0]
+    print(f"Gathering messages...(0/{total_row_number})", end="\r")
+
+    c.execute("""SELECT COALESCE(ZFROMJID, ZTOJID),
+                        ZWAMESSAGE.Z_PK,
+                        ZISFROMME,
+                        ZMESSAGEDATE,
+                        ZTEXT,
+                        ZMESSAGETYPE,
+                        ZWAGROUPMEMBER.ZMEMBERJID
+                 FROM main.ZWAMESSAGE
+                    LEFT JOIN main.ZWAGROUPMEMBER
+                        ON main.ZWAMESSAGE.ZGROUPMEMBER = main.ZWAGROUPMEMBER.Z_PK;""")
+    i = 0
+    content = c.fetchone()
+    while content is not None:
+        if content[0] not in data:
+            data[content[0]] = {"name": None, "messages": {}}
+        ts = APPLE_TIME + content[3]
+        data[content[0]]["messages"][content[1]] = {
+            "from_me": bool(content[2]),
+            "timestamp": ts,
+            "time": datetime.fromtimestamp(ts).strftime("%H:%M"),
+            "media": False,
+            "reply": None,
+            "caption": None,
+            "meta": False,
+            "data": None
+        }
+        if "-" in content[0] and content[2] == 0:
+            name = None
+            if content[6] is not None:
+                if content[6] in data:
+                    name = data[content[6]]["name"]
+                if "@" in content[6]:
+                    fallback = content[6].split('@')[0]
+                else:
+                    fallback = None
+            else:
+                fallback = None
+            data[content[0]]["messages"][content[1]]["sender"] = name or fallback
+        else:
+            data[content[0]]["messages"][content[1]]["sender"] = None
+        if content[5] == 6:
+            # Metadata
+            if "-" in content[0]:
+                # Group
+                if content[4] is not None:
+                    # Chnaged name
+                    try:
+                        int(content[4])
+                    except ValueError:
+                        msg = f"The group name changed to {content[4]}"
+                        data[content[0]]["messages"][content[1]]["data"] = msg
+                        data[content[0]]["messages"][content[1]]["meta"] = True
+                    else:
+                        del data[content[0]]["messages"][content[1]]
+                else:
+                    data[content[0]]["messages"][content[1]]["data"] = None
+            else:
+                data[content[0]]["messages"][content[1]]["data"] = None
+        else:
+            # real message
+            if content[2] == 1:
+                if content[5] == 14:
+                    msg = "Message deleted"
+                    data[content[0]]["messages"][content[1]]["meta"] = True
+                else:
+                    msg = content[4]
+                    if msg is not None:
+                        if "\r\n" in msg:
+                            msg = msg.replace("\r\n", "<br>")
+                        if "\n" in msg:
+                            msg = msg.replace("\n", "<br>")
+            else:
+                if content[5] == 14:
+                    msg = "Message deleted"
+                    data[content[0]]["messages"][content[1]]["meta"] = True
+                else:
+                    msg = content[4]
+                    if msg is not None:
+                        if "\r\n" in msg:
+                            msg = msg.replace("\r\n", "<br>")
+                        if "\n" in msg:
+                            msg = msg.replace("\n", "<br>")
+            data[content[0]]["messages"][content[1]]["data"] = msg
+        i += 1
+        if i % 1000 == 0:
+            print(f"Gathering messages...({i}/{total_row_number})", end="\r")
+        content = c.fetchone()
+    print(
+        f"Gathering messages...({total_row_number}/{total_row_number})", end="\r")
+
+
+def media(db, data, media_folder):
+    c = db.cursor()
+    # Get media
+    c.execute("""SELECT count() FROM ZWAMEDIAITEM""")
+    total_row_number = c.fetchone()[0]
+    print(f"\nGathering media...(0/{total_row_number})", end="\r")
+    i = 0
+    c.execute("""SELECT COALESCE(ZWAMESSAGE.ZFROMJID, ZWAMESSAGE.ZTOJID) as _id,
+                        ZMESSAGE,
+                        ZMEDIALOCALPATH,
+                        ZMEDIAURL,
+                        ZVCARDSTRING,
+                        ZMEDIAKEY,
+                        ZTITLE
+                 FROM ZWAMEDIAITEM
+                    INNER JOIN ZWAMESSAGE
+                        ON ZWAMEDIAITEM.ZMESSAGE = ZWAMESSAGE.Z_PK
+                 WHERE ZMEDIALOCALPATH IS NOT NULL
+                 ORDER BY _id ASC""")
+    content = c.fetchone()
+    mime = MimeTypes()
+    while content is not None:
+        file_path = f"{media_folder}/{content[2]}"
+        data[content[0]]["messages"][content[1]]["media"] = True
+
+        if os.path.isfile(file_path):
+            data[content[0]]["messages"][content[1]]["data"] = file_path
+            if content[4] is None:
+                guess = mime.guess_type(file_path)[0]
+                if guess is not None:
+                    data[content[0]]["messages"][content[1]]["mime"] = guess
+                else:
+                    data[content[0]]["messages"][content[1]]["mime"] = "data/data"
+            else:
+                data[content[0]]["messages"][content[1]]["mime"] = content[4]
+        else:
+            # if "https://mmg" in content[4]:
+            # try:
+            # r = requests.get(content[3])
+            # if r.status_code != 200:
+            # raise RuntimeError()
+            # except:
+            # data[content[0]]["messages"][content[1]]["data"] = "{The media is missing}"
+            # data[content[0]]["messages"][content[1]]["mime"] = "media"
+            # else:
+            data[content[0]]["messages"][content[1]]["data"] = "The media is missing"
+            data[content[0]]["messages"][content[1]]["mime"] = "media"
+            data[content[0]]["messages"][content[1]]["meta"] = True
+        if content[6] is not None:
+            data[content[0]]["messages"][content[1]]["caption"] = content[6]
+        i += 1
+        if i % 100 == 0:
+            print(f"Gathering media...({i}/{total_row_number})", end="\r")
+        content = c.fetchone()
+    print(
+        f"Gathering media...({total_row_number}/{total_row_number})", end="\r")
+
+
+def vcard(db, data):
+    c = db.cursor()
+    c.execute("""SELECT DISTINCT ZWAVCARDMENTION.ZMEDIAITEM,
+                        ZWAMEDIAITEM.ZMESSAGE,
+                        COALESCE(ZWAMESSAGE.ZFROMJID,
+                        ZWAMESSAGE.ZTOJID) as _id,
+                        ZVCARDNAME,
+                        ZVCARDSTRING
+                 FROM ZWAVCARDMENTION
+                    INNER JOIN ZWAMEDIAITEM
+                        ON ZWAVCARDMENTION.ZMEDIAITEM = ZWAMEDIAITEM.Z_PK
+                    INNER JOIN ZWAMESSAGE
+                        ON ZWAMEDIAITEM.ZMESSAGE = ZWAMESSAGE.Z_PK""")
+    rows = c.fetchall()
+    total_row_number = len(rows)
+    print(f"\nGathering vCards...(0/{total_row_number})", end="\r")
+    base = "Message/vCards"
+    if not os.path.isdir(base):
+        Path(base).mkdir(parents=True, exist_ok=True)
+    for index, row in enumerate(rows):
+        file_name = "".join(x for x in row[3] if x.isalnum())
+        file_path = f"{base}/{file_name[:200]}.vcf"
+        if not os.path.isfile(file_path):
+            with open(file_path, "w", encoding="utf-8") as f:
+                f.write(row[4])
+        data[row[2]]["messages"][row[1]]["data"] = row[3] + \
+            "The vCard file cannot be displayed here, " \
+            f"however it should be located at {file_path}"
+        data[row[2]]["messages"][row[1]]["mime"] = "text/x-vcard"
+        data[row[2]]["messages"][row[1]]["media"] = True
+        data[row[2]]["messages"][row[1]]["meta"] = True
+        print(f"Gathering vCards...({index + 1}/{total_row_number})", end="\r")
+
+
+def create_html(data, output_folder, template=None, embedded=False, offline_static=False, maximum_size=None):
+    if template is None:
+        template_dir = os.path.dirname(__file__)
+        template_file = "whatsapp.html"
+    else:
+        template_dir = os.path.dirname(template)
+        template_file = os.path.basename(template)
+    templateLoader = jinja2.FileSystemLoader(searchpath=template_dir)
+    templateEnv = jinja2.Environment(loader=templateLoader)
+    templateEnv.globals.update(determine_day=determine_day)
+    templateEnv.filters['sanitize_except'] = sanitize_except
+    template = templateEnv.get_template(template_file)
+
+    total_row_number = len(data)
+    print(f"\nCreating HTML...(0/{total_row_number})", end="\r")
+
+    if not os.path.isdir(output_folder):
+        os.mkdir(output_folder)
+
+    w3css = "https://www.w3schools.com/w3css/4/w3.css"
+    if offline_static:
+        import urllib.request
+        static_folder = os.path.join(output_folder, offline_static)
+        if not os.path.isdir(static_folder):
+            os.mkdir(static_folder)
+        w3css_path = os.path.join(static_folder, "w3.css")
+        if not os.path.isfile(w3css_path):
+            with urllib.request.urlopen(w3css) as resp:
+                with open(w3css_path, "wb") as f: f.write(resp.read())
+        w3css = os.path.join(offline_static, "w3.css")
+
+    for current, contact in enumerate(data):
+        if len(data[contact]["messages"]) == 0:
+            continue
+        phone_number = contact.split('@')[0]
+        if "-" in contact:
+            file_name = ""
+        else:
+            file_name = phone_number
+
+        if data[contact]["name"] is not None:
+            if file_name != "":
+                file_name += "-"
+            file_name += data[contact]["name"].replace("/", "-")
+            name = data[contact]["name"]
+        else:
+            name = phone_number
+
+        safe_file_name = ''
+        safe_file_name = "".join(x for x in file_name if x.isalnum() or x in "- ")
+        with open(f"{output_folder}/{safe_file_name}.html", "w", encoding="utf-8") as f:
+            f.write(
+                template.render(
+                    name=name,
+                    msgs=data[contact]["messages"].values(),
+                    my_avatar=None,
+                    their_avatar=f"WhatsApp/Avatars/{contact}.j",
+                    w3css=w3css
+                )
+            )
+        if current % 10 == 0:
+            print(f"Creating HTML...({current}/{total_row_number})", end="\r")
+
+    print(f"Creating HTML...({total_row_number}/{total_row_number})", end="\r")
+
+
+if __name__ == "__main__":
+    from optparse import OptionParser
+    parser = OptionParser()
+    parser.add_option(
+        "-w",
+        "--wa",
+        dest="wa",
+        default="wa.db",
+        help="Path to contact database")
+    parser.add_option(
+        "-m",
+        "--media",
+        dest="media",
+        default="Message",
+        help="Path to WhatsApp media folder"
+    )
+    # parser.add_option(
+    #     "-t",
+    #     "--template",
+    #     dest="html",
+    #     default="wa.db",
+    #     help="Path to HTML template")
+    (options, args) = parser.parse_args()
+    msg_db = "7c7fba66680ef796b916b067077cc246adacf01d"
+    output_folder = "temp"
+    contact_db = options.wa
+    media_folder = options.media
+
+    if len(args) == 1:
+        msg_db = args[0]
+    elif len(args) == 2:
+        msg_db = args[0]
+        output_folder = args[1]
+
+    data = {}
+
+    if os.path.isfile(msg_db):
+        with sqlite3.connect(msg_db) as db:
+            messages(db, data)
+            media(db, data, media_folder)
+            vcard(db, data)
+        create_html(data, output_folder)
+
+    if not os.path.isdir(f"{output_folder}/WhatsApp"):
+        shutil.move(media_folder, f"{output_folder}/")
+
+    with open("result.json", "w") as f:
+        data = json.dumps(data)
+        print(f"\nWriting JSON file...({int(len(data)/1024/1024)}MB)")
+        f.write(data)
+
+    print("Everything is done!")
