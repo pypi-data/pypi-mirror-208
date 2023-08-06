874b1840ef082f3af4f35d5e68f9b344ed6f86b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:09:36 +0000

    Add package wyoming-piper/0.0.3/wyoming_piper-0.0.3.tar.gz

---
+++ packages/wyoming-piper/wyoming_piper-0.0.3.tar.gz/wyoming_piper-0.0.3/wyoming_piper/handler.py
@@ -0,0 +1,110 @@
+"""Event handler for clients of the server."""
+import argparse
+import asyncio
+import logging
+import math
+import os
+import wave
+
+from wyoming.audio import AudioChunk, AudioStart, AudioStop
+from wyoming.event import Event
+from wyoming.info import Describe, Info
+from wyoming.server import AsyncEventHandler
+from wyoming.tts import Synthesize
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class PiperEventHandler(AsyncEventHandler):
+    def __init__(
+        self,
+        wyoming_info: Info,
+        cli_args: argparse.Namespace,
+        piper_proc: asyncio.subprocess.Process,
+        piper_proc_lock: asyncio.Lock,
+        *args,
+        **kwargs,
+    ) -> None:
+        super().__init__(*args, **kwargs)
+
+        self.cli_args = cli_args
+        self.wyoming_info_event = wyoming_info.event()
+        self.piper_proc = piper_proc
+        self.piper_proc_lock = piper_proc_lock
+
+    async def handle_event(self, event: Event) -> bool:
+        if Describe.is_type(event.type):
+            await self.write_event(self.wyoming_info_event)
+            _LOGGER.debug("Sent info")
+            return True
+
+        if not Synthesize.is_type(event.type):
+            _LOGGER.warning("Unexpected event: %s", event)
+            return True
+
+        synthesize = Synthesize.from_event(event)
+        raw_text = synthesize.text
+
+        # Join multiple lines
+        text = " ".join(raw_text.strip().splitlines())
+
+        if self.cli_args.auto_punctuation and text:
+            # Add automatic punctuation (important for some voices)
+            has_punctuation = False
+            for punc_char in self.cli_args.auto_punctuation:
+                if text[-1] == punc_char:
+                    has_punctuation = True
+                    break
+
+            if not has_punctuation:
+                text = text + self.cli_args.auto_punctuation[0]
+
+        async with self.piper_proc_lock:
+            _LOGGER.debug("synthesize: raw_text=%s, text='%s'", raw_text, text)
+
+            # Text in, file path out
+            self.piper_proc.stdin.write((text + "\n").encode())
+            await self.piper_proc.stdin.drain()
+
+            output_path = (await self.piper_proc.stdout.readline()).decode().strip()
+            _LOGGER.debug(output_path)
+
+            wav_file: wave.Wave_read = wave.open(output_path, "rb")
+            with wav_file:
+                rate = wav_file.getframerate()
+                width = wav_file.getsampwidth()
+                channels = wav_file.getnchannels()
+
+                await self.write_event(
+                    AudioStart(
+                        rate=rate,
+                        width=width,
+                        channels=channels,
+                    ).event(),
+                )
+
+                # Audio
+                audio_bytes = wav_file.readframes(wav_file.getnframes())
+                bytes_per_sample = width * channels
+                bytes_per_chunk = bytes_per_sample * self.cli_args.samples_per_chunk
+                num_chunks = int(math.ceil(len(audio_bytes) / bytes_per_chunk))
+
+                # Split into chunks
+                for i in range(num_chunks):
+                    offset = i * bytes_per_chunk
+                    chunk = audio_bytes[offset : offset + bytes_per_chunk]
+                    await self.write_event(
+                        AudioChunk(
+                            audio=chunk,
+                            rate=rate,
+                            width=width,
+                            channels=channels,
+                        ).event(),
+                    )
+
+            await self.write_event(AudioStop().event())
+            _LOGGER.debug("Completed request")
+
+            os.unlink(output_path)
+
+        return True
