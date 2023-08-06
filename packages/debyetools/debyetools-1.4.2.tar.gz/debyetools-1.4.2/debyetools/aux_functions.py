57db025dc67c68c9e55be34f7a5d36df15a9ccd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:06 +0000

    Add package debyetools/1.4.2/debyetools-1.4.2.tar.gz

---
+++ packages/debyetools/debyetools-1.4.2.tar.gz/debyetools-1.4.2/debyetools/aux_functions.py
@@ -0,0 +1,187 @@
+import itertools as it
+import re
+import numpy as np
+from typing import Tuple
+
+class logging(object):
+    def __init__(self, *files):
+        self.files = files
+    def write(self, obj):
+        for f in self.files:
+            f.write(obj)
+            f.flush() # If you want the output to be visible immediately
+    def flush(self) :
+        for f in self.files:
+            f.flush()
+def c_types(atom_types: str) -> Tuple[list, list]:
+    """
+    returns all the pair types combinations.
+
+    :param str atom_types: the types of each atom in the primitive cell in the same order as the basis vectors.
+    :return: pair types and list wuth individual types.
+    :rtype: Tuple[list, list]
+    """
+    types_all = re.findall('[A-Z][^A-Z]*', atom_types)
+    ptypes=list(set([s for s in types_all]))
+    ptypes.sort()
+    combs_types = list(it.combinations_with_replacement(ptypes, r=2))
+    combs_types = [A[0]+'-'+A[1] for A in combs_types]
+    return combs_types, types_all
+
+def generate_cells_coordinates(size, primitive_cell,center):
+    """ generate the cell coordinates for which we are going
+    to calculate the neighbor list.
+
+    :param np.ndarray size: Number of times we are replicating the primitive cell
+    :param np.ndarray primitive_cell: The primitive cell
+    :param np.ndarray center: The position in space where the system of reference is
+    :return: cell coordinates.
+    :rtype: np.ndarray
+
+    """
+
+    cell_coords = np.array(list((it.product(np.arange(size[0]),
+                                            np.arange(size[1]),
+                                            np.arange(size[2])))))
+
+    cell_coords_centered = cell_coords + center
+    cell_coords_centered = np.dot(cell_coords_centered, primitive_cell)
+
+    return cell_coords_centered
+
+def gen_Ts(Ti: float, Tf: float, nTs: int) -> np.ndarray:
+    """
+    Function to generate a range of temperatures.
+
+    :param float Ti: Initial temperature. (Try not to use the value 0. Use 0.1 instead.)
+    :param float Tf: Final temperature.
+    :param int nTs: Number of values. This does not include room temperature, which is included anyways.
+
+    :retun np.ndarray: Values of temperatures between Ti and Tf, inclusive, plus room temperature.
+    """
+    minF_step = (Tf - Ti)/(nTs - 1.)
+    Ts = np.arange(Ti, Tf+minF_step, minF_step)
+    Ts = np.r_[Ts, [298.15]]
+    Ts.sort()
+    return Ts
+def gen_Ps(Ti,Tf,nTs):
+    """
+    Function to generate a range of temperatures.
+
+    :param float Ti: Initial temperature. (Try not to use the value 0. Use 0.1 instead.)
+    :param float Tf: Final temperature.
+    :param int nTs: Number of values. This does not include room temperature, which is included anyways.
+
+    :retun list_of_floats: Values of temperatures between Ti and Tf, inclusive, plus room temperature.
+    """
+    if nTs <= 1: return np.array([Ti])
+    minF_step = (Tf - Ti)/(nTs - 1.)
+    Ts = np.arange(Ti, Tf+1, minF_step)
+
+    return Ts
+
+def load_doscar(filename_sufix, list_filetags = ['-0.10', '-0.09','-0.08','-0.07','-0.06','-0.05','-0.04','-0.03',
+                                                 '-0.02','-0.01','-0.00','0.01','0.02','0.03','0.04','0.05','0.06',
+                                                 '0.07','0.08','0.09','0.10']):
+    list_filetags = [str(li) for li in list_filetags]
+    E = []
+    N = []
+    Ef = []
+    nat = 0
+    for dosfile in list_filetags:
+        countline = 0
+        EN = []
+
+        filename = filename_sufix+dosfile
+        with open(filename) as infile:
+                for line in infile:
+                    if countline == 0:
+                        nat = float(line.split()[0])
+                    if countline == 5:
+                        Ef.append(float(line.split()[3]))
+                    if countline > 5:
+                        EN.append(line.split()[0:2])
+
+                    countline +=1
+        ENAl = np.array(EN)
+        # print(dosfile, EN)
+        E.append([float(s) for s in list(ENAl[:,0])])
+        N.append([float(s)/nat for s in list(ENAl[:,1])])
+
+    return E,N,Ef
+
+def load_V_E(energy_dir_summary,energy_dir_contcar, units='eV/atom'):
+    with open(energy_dir_contcar,'r') as f_poscar:
+        f_poscar_lines = f_poscar.readlines()
+        cell_poscar = f_poscar_lines[2:5]
+
+        cell_lst = [c.split() for c in cell_poscar]
+        diag_cell = [float(c[i]) for i, c in enumerate(cell_lst)]
+
+        try:
+            nat = sum([int(li) for li in np.fromstring(f_poscar_lines[5], dtype=int, sep=' ')])
+            1/nat
+        except:
+            nat = sum([int(li) for li in np.fromstring(f_poscar_lines[6], dtype=int, sep=' ')])
+            1/nat
+    with open(energy_dir_summary) as f_summary:
+        f_summary_lines = f_summary.readlines()
+        f_summary_lines = list(dict.fromkeys(f_summary_lines))
+        ds = []
+        E = []
+        for l in f_summary_lines:
+            l_lst = l.split(' ')
+            ds.append(float(l_lst[0]))
+            E.append(float(l_lst[3])/nat)
+
+    V = []
+    for di in ds:
+        V.append(np.product(np.array(diag_cell)*(1+di))/nat)
+
+    if units=='J/mol':
+        uconvE=(0.160218e-18*6.02214e23)
+        uconvV=(1e-30*6.02e23)
+    elif units=='eV/atom':
+        uconvE,uconvV = 1,1
+    return np.array(V).T*uconvV, np.array(E).T*uconvE
+
+def load_EM(filename_outcar_eps):
+    EM = []
+
+    with open(filename_outcar_eps) as f:
+        lines = f.readlines()
+        for i, line in enumerate(lines):
+            if line.startswith('  SYMMETRIZED ELASTIC MODULI (kBar)'):
+                j = i + 3
+                data = lines[j:j+6]
+                break
+
+    for line in data:
+        EM += [[float(x) for x in line.split()[1:]]]
+    EM = np.array(EM)
+
+    return EM
+
+def load_cell(filename_contcar):
+    with open(filename_contcar) as f:
+        poscar_lines=f.readlines()
+    mult = float(poscar_lines[1])
+    cell = np.array([np.fromstring(line_i, dtype=float,sep=' ') for line_i in poscar_lines[2:5]])
+    cell = cell*mult
+
+    ix_nats=6
+    re.findall('[A-Z][^A-Z]*', 'ABC')
+    ats_types = re.findall('[A-Z][^A-Z]*', poscar_lines[ix_nats-1].replace('  ','').replace(' ','').replace('\n',''))
+    ats_types = [ai+'x' for ai in ats_types]
+    nats = np.fromstring(poscar_lines[ix_nats], dtype=int,sep=' ')
+
+    formula_lst=[]
+    for at_i, na_i in zip(ats_types,nats):
+        formula_lst.append(at_i*na_i)
+    formula=''.join(formula_lst)
+    tots_nats=sum(nats)
+
+    basis = np.array([np.fromstring(line_i, dtype=float,sep=' ') for line_i in poscar_lines[8:8+tots_nats]])
+    # basis = np.dot(basis,cell)
+
+    return formula, cell, basis
---
+++ packages/debyetools/debyetools-1.4.2.tar.gz/debyetools-1.4.2/debyetools/electronic.py
@@ -0,0 +1,105 @@
+import numpy as np
+from scipy.optimize import leastsq
+
+NAv  = 0.6022140857e24
+kB   = 0.138064852e-22
+
+class Electronic:
+    """
+    Implementation of the electronic contribution to the free energy.
+
+    :param float params: N(Ef)(V) function parameters.
+    """
+    def __init__(self,*params):
+        self.pel = params
+        self.r=1
+        for n,q in enumerate(params):
+            setattr(self,'q'+str(n),q)
+
+    def NfV(self,V):
+        """
+        N(Ef)(V)
+
+        :param float V: Volume.
+        """
+        return self.q0*V**0 + self.q1*V**1 + self.q2*V**2 + self.q3*V**3
+    def dNfVdV_T(self,V):
+        return 3*V**2*self.q3+2*V*self.q2+self.q1
+    def d2NfVdV2_T(self,V):
+        return 6*V*self.q3+2*self.q2
+    def d3NfVdV3_T(self,V):
+        return 6*self.q3
+    def d4NfVdV4_T(self,V):
+        return 0
+
+    def E(self,T,V):
+        return (1/6)*np.pi**2*NAv*self.r*kB**2*T**2*self.NfV(V)/(0.160218e-18)
+    def S(self,T,V):
+        return (2/6)*np.pi**2*NAv*self.r*kB**2*T*self.NfV(V)/(0.160218e-18)
+    def F(self,T,V):
+        """
+        Electronic contribution to the free energy.
+
+        :param float T: Temperature.
+        :param float V: Volume.
+        """
+        return - (1/6)*np.pi**2*NAv*self.r*kB**2*T**2*self.NfV(V)/(0.160218e-18)
+    def dFdV_T(self, T, V):
+        return - (1/6)*np.pi**2*NAv*self.r*kB**2*T**2*self.dNfVdV_T(V)/(0.160218e-18)
+    def dFdT_V(self,T,V):
+        return - 2*T*np.pi**2*NAv*self.r*kB**2*self.NfV(V)*(1/6)/(0.160218e-18)
+    def d2FdT2_V(self,T,V):
+        return - 2*np.pi**2*NAv*self.r*kB**2*self.NfV(V)*(1/6)/(0.160218e-18)
+    def d2FdV2_T(self,T,V):
+        return - (1/6)*np.pi**2*NAv*self.r*kB**2*T**2*self.d2NfVdV2_T(V)/(0.160218e-18)
+    def d3FdV3_T(self,T,V):
+        return - (1/6)*np.pi**2*NAv*self.r*kB**2*T**2*self.d3NfVdV3_T(V)/(0.160218e-18)
+    def d4FdV4_T(self,T,V):
+        return - (1/6)*np.pi**2*NAv*self.r*kB**2*T**2*self.d4NfVdV4_T(V)/(0.160218e-18)
+
+    def d2FdVdT(self,T,V):
+        return -2*np.pi**2*NAv*self.r*kB**2*T*self.dNfVdV_T(V)*(1/6)/(0.160218e-18)
+    def d3FdV2dT(self,T,V):
+        return -2*np.pi**2*NAv*self.r*kB**2*T*self.d2NfVdV2_T(V)*(1/6)/(0.160218e-18)
+    def d3FdVdT2(self,T,V):
+        return -2*np.pi**2*NAv*self.r*kB**2*self.dNfVdV_T(V)*(1/6)/(0.160218e-18)
+
+def fit_electronic(Vs, p_el,E,N,Ef, ixss=8, ixse=-1):
+    """
+    Fitting procedure for the N(Ef)(V) function.
+
+    :param list_of_floats Vs: Volumes.
+    :param list_of_floats p_el: Initial parameters.
+    :param list_of_lists_of_floats E: Matrix with energies at each level for each volume.
+    :param list_of_lists_of_floats N: Matrix with densities of state at each level for each volume.
+    :param list_of_floats Ef: Fermi levels as function of temperature.
+
+    :retun list_of_floats: optimized parameters.
+    """
+
+    V = np.array(Vs)
+    ix_V0=4
+    EfV0 = float(Ef[ix_V0])
+    ixs=[i for i,x in enumerate(E[ix_V0]) if x>=Ef[ix_V0]]
+    E1 = float(E[ix_V0][ixs[0]-1])
+    E2 = float(E[ix_V0][ixs[0]])
+    N1 = float(N[ix_V0][ixs[0]-1])
+    N2 = float(N[ix_V0][ixs[0]])
+    NfV0 = (EfV0 - E1)*(N2 - N1)/(E2 - E1) + N1
+    NfV = np.array([NfV0*np.sqrt(ef/EfV0) for ef in Ef][ixss:ixse])
+    P2 = leastsq(NfV2m, p_el, args=(V[ixss:ixse], NfV),maxfev=1000)
+
+    P2 = P2[0]
+
+    NfVcalc = [NfV_poly_fun(Vi, P2[0], P2[1], P2[2], P2[3]) for Vi in V[ixss:ixse]]
+    # print('#V NfVdata NfVcalc')
+    # for Vi, Ndatai, Ncalci in zip(V[ixss:ixse], NfV, NfVcalc):
+    #     print('%.5e %.5e %.5e' % (Vi, Ndatai, Ncalci))
+    return P2
+
+def NfV_poly_fun(V, _A, _B, _C, _D):
+    return _A + _B*V + _C*V**2 + _D*V**3
+
+def NfV2m(P, Vdata, NfVdata):
+    NfVcalc = [NfV_poly_fun(Vi, P[0], P[1], P[2], P[3]) for Vi in Vdata]
+    return NfVcalc-NfVdata
