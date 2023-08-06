951361a7b128455d3428d39f76968a19765dee4a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:49 +0000

    Add package nlp-models/1.1.1/nlp_models-1.1.1-py3-none-any.whl

---
+++ packages/nlp-models/nlp_models-1.1.1-py3-none-any.whl/multi_task_model/metrics.py
@@ -0,0 +1,43 @@
+from typing import Optional, Literal
+from torchmetrics.classification import (
+    MultilabelHammingDistance,
+    BinaryHammingDistance,
+    MultilabelAccuracy,
+    BinaryAccuracy
+)
+
+
+def hamming_distance(
+    outputs, 
+    targets, 
+    multi_label: bool = False, 
+    num_labels: Optional[int] = None, 
+    average:Literal['micro', 'macro', 'weighted', 'none']='macro'
+    ):
+    if multi_label:
+        if not num_labels:
+            raise ValueError('num_labels is required for multi_label is True')
+        else:
+            hamming_distance = MultilabelHammingDistance(num_labels, average=average)
+    else:
+        hamming_distance = BinaryHammingDistance()
+    return hamming_distance(outputs, targets)
+
+
+def accuracy(
+    outputs, 
+    targets, 
+    multi_label: bool = False, 
+    num_labels: Optional[int] = None,
+    device = 'cpu',
+    average: Literal['micro', 'macro', 'weighted', 'none'] = 'macro'
+    ):
+    if multi_label:
+        if not num_labels:
+            raise ValueError('num_labels is required for multi_label is True')
+        else:
+            accuracy = MultilabelAccuracy(num_labels, average=average)
+    else:
+        accuracy = BinaryAccuracy()
+    accuracy.to(device)
+    return accuracy(outputs, targets)
