aaee94ae72b3e1fbc60eb0f956175f21349e63d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:46:33 +0000

    Add package mmtools/0.0.37/mmtools-0.0.37-py3-none-any.whl

---
+++ packages/mmtools/mmtools-0.0.37-py3-none-any.whl/mmtools/arguments.py
@@ -0,0 +1,193 @@
+""" mmblocks """
+
+import logging
+import logging.handlers
+import os
+import socket
+import sys
+from logging import error, info
+from pathlib import Path
+from typing import Any, Optional, cast
+
+import caep
+import passpy
+import requests
+from pydantic import BaseModel, Field, SecretStr, root_validator
+from urllib3.exceptions import InsecureRequestWarning
+
+CONFIG_ID = "mmtools"
+CONFIG_NAME = "config"
+
+
+class ArgumentError(Exception):
+    pass
+
+
+class NotFound(Exception):
+    """Item not Found"""
+
+    def __init__(self, *args: Any) -> None:
+        Exception.__init__(self, *args)
+
+
+def fatal(message: str, exit_code: int = 1) -> None:
+    """send message to error log and exit"""
+    error(message)
+    sys.exit(exit_code)
+
+
+def passpy_store(gpgbinary: Optional[str] = None) -> passpy.store.Store:
+    """passpy store"""
+    if not gpgbinary:
+        gpgbinary = whereis(["gpg2", "gpg"])
+        if not gpgbinary:
+            raise NotFound("gpg not found")
+
+    return passpy.store.Store(gpg_bin=gpgbinary)
+
+
+def whereis(filenames: list[str]) -> Optional[str]:
+    "Locate file"
+    if isinstance(filenames, type(str)):
+        filenames = [filenames]
+
+    for filename in filenames:
+        for path in os.environ["PATH"].split(":"):
+            if os.path.isfile(f"{path}/{filename}"):
+                return os.path.realpath(f"{path}/{filename}")
+    return None
+
+
+def gettpassentry(key: str) -> Any:
+    """Get pass entry from passpy"""
+    entry = passpy_store().get_key(key)
+
+    if not entry:
+        fatal(f"pass entry {key} not found")
+    else:
+        return entry.split("\n")[0]
+
+
+def setup_logging(
+    loglevel: str = "debug",
+    logfile: Optional[str] = None,
+    prefix: str = "mmtools",
+    maxBytes: int = 10000000,  # 10 MB
+    backupCount: int = 5,
+) -> None:
+    """Setup loglevel and optional log to file"""
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError("Invalid log level: %s" % loglevel)
+
+    datefmt = "%Y-%m-%d %H:%M:%S"
+    formatter = "[%(asctime)s] app=" + prefix + " level=%(levelname)s msg=%(message)s"
+
+    if logfile:
+        logdir = os.path.dirname(logfile)
+
+        if not os.path.isdir(logdir):
+            os.makedirs(logdir)
+
+        # Support strftime in log file names
+        handlers = [
+            logging.handlers.RotatingFileHandler(
+                logfile, maxBytes=maxBytes, backupCount=backupCount
+            )
+        ]
+
+        logging.basicConfig(
+            level=numeric_level, handlers=handlers, format=formatter, datefmt=datefmt
+        )
+    else:
+        logging.basicConfig(
+            level=numeric_level, stream=sys.stdout, format=formatter, datefmt=datefmt
+        )
+
+
+class Config(BaseModel):
+    server: str = Field(description="Mattermost Server")
+    user: str = Field(description="Mattermost User")
+    port: int = Field(443, description="Mattermost port")
+    ignore: Optional[str] = Field(
+        description="Regular expression of channels to ignore"
+    )
+    no_verify: bool = Field(False, description="SSL verify")
+    logfile: Optional[str] = Field(description="Log to file")
+    loglevel: str = Field("info", description="Log level (default=INFO)")
+    password: Optional[SecretStr] = Field(description="Mattermost password")
+    chat_prefix: str = Field(
+        "🗨️ ",
+        description="Prefix to show on statusbar and notification messages",
+    )
+    team: Optional[str] = Field(description="Mattermost team (optional)")
+    password_pass_entry: Optional[str] = Field(
+        description="pass entry to insert into password"
+    )
+
+    @root_validator
+    def check_arguments(cls, values: dict[str, Any]) -> dict[str, Any]:
+        password = values.get("password")
+        password_pass_entry = values.get("password_pass_entry")
+        if not (password or password_pass_entry):
+            raise ArgumentError("Specify --password or --password-pass-entry")
+
+        return values
+
+
+def handle_args(model: type[caep.schema.BaseModelType], section: str) -> Config:
+    """Verify default arguments"""
+
+    hostname = socket.gethostname()
+    hostname_short = socket.gethostname().split(".")[0]
+
+    host_config_name = f"{CONFIG_NAME}-{hostname}"
+    host_short_config_name = f"{CONFIG_NAME}-{hostname_short}"
+
+    if (Path(caep.get_config_dir(CONFIG_ID)) / host_config_name).is_file():
+        config_name = host_config_name
+
+    elif (Path(caep.get_config_dir(CONFIG_ID)) / host_short_config_name).is_file():
+        config_name = host_short_config_name
+    else:
+        config_name = CONFIG_NAME
+
+    try:
+        args: Config = cast(
+            Config,
+            caep.load(
+                model,
+                section,
+                CONFIG_ID,
+                config_name,
+                section,
+            ),
+        )
+    except ArgumentError as e:
+        fatal(str(e))
+
+    setup_logging(args.loglevel, args.logfile)
+
+    info(f"args: {args}")
+    info(f"config: {CONFIG_ID}/{config_name}")
+
+    args.chat_prefix = args.chat_prefix.strip()
+
+    if not args.server:
+        fatal("--server not specified")
+
+    if not args.user:
+        fatal("--user not specified")
+
+    if args.no_verify:
+        requests.packages.urllib3.disable_warnings(  # type: ignore
+            category=InsecureRequestWarning
+        )
+
+    if args.password_pass_entry:
+        args.password = SecretStr(gettpassentry(args.password_pass_entry))
+
+    if not args.password:
+        fatal("Must specify either --password or --password-pass-entry")
+
+    return args
---
+++ packages/mmtools/mmtools-0.0.37-py3-none-any.whl/mmtools/config.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+""" Handle config"""
+
+import os
+import sys
+from typing import cast
+
+import caep
+from pkg_resources import resource_string
+from pydantic import Field
+
+from mmtools import arguments
+
+
+class Config(arguments.Config):
+    show: bool = Field(False, description="Print default config")
+    init: bool = Field(
+        False,
+        description="Copy default config to {}/{}".format(
+            caep.get_config_dir(arguments.CONFIG_ID), arguments.CONFIG_NAME
+        ),
+    )
+
+
+def default_ini() -> str:
+    """Get content of default ini file"""
+    return resource_string("mmtools", f"etc/{arguments.CONFIG_NAME}").decode("utf-8")
+
+
+def save_config(filename: str) -> None:
+    """Save config to specified filename"""
+    if os.path.isfile(filename):
+        sys.stderr.write(f"Config already exists: {filename}\n")
+        sys.exit(1)
+
+    try:
+        with open(filename, "w") as f:
+            f.write(default_ini())
+    except PermissionError as err:
+        sys.stderr.write(f"{err}\n")
+        sys.exit(2)
+
+    print(f"Config copied to {filename}")
+
+
+def main() -> None:
+    "main function"
+    args: Config = cast(Config, arguments.handle_args(Config, "config"))
+
+    if args.show:
+        print(default_ini())
+    elif args.init:
+        config_dir = caep.get_config_dir(arguments.CONFIG_ID, create=True)
+        save_config(os.path.join(config_dir, arguments.CONFIG_NAME))
+
+    else:
+        arguments.fatal("You must specify --show or --init")
+
+
+if __name__ == "__main__":
+    main()
