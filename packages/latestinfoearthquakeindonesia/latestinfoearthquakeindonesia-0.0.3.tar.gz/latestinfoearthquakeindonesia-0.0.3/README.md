577450d53b7250aeccd18dc0d4dd1fbbc3f4c0c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:40:22 +0000

    Add package latestinfoearthquakeindonesia/0.0.3/latestinfoearthquakeindonesia-0.0.3.tar.gz

---
+++ packages/latestinfoearthquakeindonesia/latestinfoearthquakeindonesia-0.0.3.tar.gz/latestinfoearthquakeindonesia-0.0.3/README.md
@@ -0,0 +1,7 @@
+# Latest Indonesia Earthquake
+This package will get the latest earthquake from BMKG | Meteorological, Climatological, and Geophysical Agency.
+
+## How does it work ?
+This package will scrape from [BMKG](https://www.bmkg.go.id) to get latest earthquake information in Indonesia
+
+This package will use Beautifulsoup4 to produce output in the form of JSON which is ready to use in web or mobile application
---
+++ packages/latestinfoearthquakeindonesia/latestinfoearthquakeindonesia-0.0.3.tar.gz/latestinfoearthquakeindonesia-0.0.3/setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="latestinfoearthquakeindonesia",
+    version="0.0.3",
+    author="Hendra Go",
+    author_email="hendrago91@gmail.com",
+    description="This package will get the latest earthquake from BMKG | Meteorological, Climatological, and "
+                "Geophysical Agency.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/TOSS-ID/latest-indonesia-earthquake",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Development Status :: 5 - Production/Stable"
+    ],
+    python_requires='>=3.6',
+)
