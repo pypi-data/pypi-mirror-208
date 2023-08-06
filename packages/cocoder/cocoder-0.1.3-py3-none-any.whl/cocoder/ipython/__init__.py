73fc062421de22ea1b266354e3da9dc02c189627
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:11:14 +0000

    Add package cocoder/0.1.3/cocoder-0.1.3-py3-none-any.whl

---
+++ packages/cocoder/cocoder-0.1.3-py3-none-any.whl/cocoder/ipython/__init__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2023 parkminwoo
+from cocoder.ipython.core import ExceptIpyCocoder
+from cocoder.chatbase.bard_receiver import receive_bard_advice
+from cocoder.chatbase.openai_receiver import receive_openai_advice
+from cocoder.chatbase.openai_receiver import get_resp_openai_advice
+
+try:
+    get_ipython().set_custom_exc((Exception,), ExceptIpyCocoder)
+except:
+    pass
+
+__version__ = "0.1.3"
+__author__ = "daniel park <parkminwoo1991@gmail.com>"
+__all__ = ["ExceptIpyCocoder", "receive_bard_advice", "receive_openai_advice", "get_resp_openai_advice"]
