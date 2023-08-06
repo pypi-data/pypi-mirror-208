50d5ed76ce1a4c70f8eb979af4c019f91eadd720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:51:02 +0000

    Add package PyRvcTestA/1.7.0/PyRvcTestA-1.7.0.tar.gz

---
+++ packages/PyRvcTestA/PyRvcTestA-1.7.0.tar.gz/PyRvcTestA-1.7.0/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name         ='PyRvcTestA',
+    version      ='1.7.0',
+    py_modules   =[''],
+    author       ='wzq',
+    author_email ='',
+    description  ='test',
+    data_files=[
+        './PyRVCTest170/PyRvcTestA.pyd',
+        ]
+    )
