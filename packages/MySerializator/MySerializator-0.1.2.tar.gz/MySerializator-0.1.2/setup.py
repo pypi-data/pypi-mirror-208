96073b7cd7f6600574316b3ee0d6cd61f407cbcf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:11:26 +0000

    Add package MySerializator/0.1.2/MySerializator-0.1.2.tar.gz

---
+++ packages/MySerializator/MySerializator-0.1.2.tar.gz/MySerializator-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name="MySerializator",
+    version="0.1.2",
+    description="My Library for serialization",
+    author="Ilya Danilenko",
+    author_email="ilyadanilenko2003@mail.ru",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent"
+    ],
+    packages=["serializers"],
+    include_package_data=True,
+)
