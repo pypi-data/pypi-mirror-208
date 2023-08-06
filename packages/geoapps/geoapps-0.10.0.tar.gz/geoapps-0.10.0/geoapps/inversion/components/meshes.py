d15fc73cc7013ef1bb4edf8535461e00ba93d2dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:30:46 +0000

    Add package geoapps/0.10.0/geoapps-0.10.0.tar.gz

---
+++ packages/geoapps/geoapps-0.10.0.tar.gz/geoapps-0.10.0/geoapps/inversion/components/meshes.py
@@ -0,0 +1,102 @@
+#  Copyright (c) 2023 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
+import numpy as np
+from geoh5py.objects import DrapeModel, Octree
+
+from geoapps.octree_creation.params import OctreeParams
+from geoapps.shared_utils.utils import drape_2_tensor, octree_2_treemesh
+
+if TYPE_CHECKING:
+    from geoh5py.workspace import Workspace
+
+    from . import InversionData, InversionTopography
+
+from discretize import TensorMesh, TreeMesh
+
+
+class InversionMesh:
+    """
+    Retrieve octree mesh data from workspace and convert to Treemesh.
+
+    Attributes
+    ----------
+
+    n_cells:
+        Number of cells in the mesh.
+    rotation :
+        Rotation of original octree mesh.
+    permutation:
+        Permutation vector to restore cell centers or model values to
+        origin octree mesh order.
+
+    """
+
+    def __init__(
+        self,
+        workspace: Workspace,
+        params: OctreeParams,
+        inversion_data: InversionData,
+        inversion_topography: InversionTopography,
+    ) -> None:
+        """
+        :param workspace: Workspace object containing mesh data.
+        :param params: Params object containing mesh parameters.
+        :param window: Center and size defining window for data, topography, etc.
+
+        """
+        self.workspace = workspace
+        self.params = params
+        self.inversion_data = inversion_data
+        self.inversion_topography = inversion_topography
+        self.mesh: TreeMesh | TensorMesh = None
+        self.n_cells: int = None
+        self.rotation: dict[str, float] = None
+        self.permutation: np.ndarray = None
+        self.entity: Octree = None
+        self._initialize()
+
+    def _initialize(self) -> None:
+        """
+        Collects mesh data stored in geoh5 workspace into TreeMesh object.
+
+        Handles conversion from geoh5's native octree mesh type to TreeMesh
+        type required for SimPEG inversion and stores data needed to restore
+        original the octree mesh type.
+        """
+
+        if self.params.mesh is None:
+            raise ValueError("Must pass pre-constructed mesh.")
+        else:
+            self.entity = self.params.mesh.copy(
+                parent=self.params.out_group, copy_children=False
+            )
+
+        if getattr(self.entity, "rotation", None) and self.inversion_data.has_tensor:
+            msg = "Cannot use tensor components with rotated mesh."
+            raise NotImplementedError(msg)
+
+        self.uid = self.entity.uid
+        self.n_cells = self.entity.n_cells
+
+        if isinstance(self.entity, Octree):
+            if self.entity.rotation:
+                origin = self.entity.origin.tolist()
+                angle = self.entity.rotation[0]
+                self.rotation = {"origin": origin, "angle": angle}
+
+            self.mesh = octree_2_treemesh(self.entity)
+            self.permutation = getattr(self.mesh, "_ubc_order")
+
+        if isinstance(self.entity, DrapeModel) and self.mesh is None:
+            self.mesh, self.permutation = drape_2_tensor(
+                self.entity, return_sorting=True
+            )
---
+++ packages/geoapps/geoapps-0.10.0.tar.gz/geoapps-0.10.0/geoapps/octree_creation/driver.py
@@ -0,0 +1,101 @@
+#  Copyright (c) 2023 Mira Geoscience Ltd.
+#
+#  This file is part of geoapps.
+#
+#  geoapps is distributed under the terms and conditions of the MIT License
+#  (see LICENSE file at the root of this source code package).
+
+
+from __future__ import annotations
+
+import sys
+from os import path
+
+from discretize.utils import mesh_builder_xyz, refine_tree_xyz
+from geoh5py.objects import ObjectBase, Octree
+from geoh5py.shared.utils import fetch_active_workspace
+from geoh5py.ui_json import monitored_directory_copy
+
+from geoapps.driver_base.driver import BaseDriver
+from geoapps.driver_base.utils import treemesh_2_octree
+from geoapps.octree_creation.constants import validations
+from geoapps.octree_creation.params import OctreeParams
+
+
+class OctreeDriver(BaseDriver):
+    _params_class = OctreeParams
+    _validations = validations
+
+    def __init__(self, params: OctreeParams):
+        super().__init__(params)
+        self.params: OctreeParams = params
+
+    def run(self) -> Octree:
+        """
+        Create an octree mesh from input values
+        """
+        with fetch_active_workspace(self.params.geoh5, mode="r+"):
+            octree = self.octree_from_params(self.params)
+
+            if self.params.monitoring_directory is not None and path.exists(
+                self.params.monitoring_directory
+            ):
+                monitored_directory_copy(self.params.monitoring_directory, octree)
+
+        return octree
+
+    @staticmethod
+    def octree_from_params(params: OctreeParams):
+        print("Setting the mesh extent")
+        entity = params.objects
+
+        p_d = [
+            [
+                params.horizontal_padding,
+                params.horizontal_padding,
+            ],
+            [
+                params.horizontal_padding,
+                params.horizontal_padding,
+            ],
+            [params.vertical_padding, params.vertical_padding],
+        ]
+
+        treemesh = mesh_builder_xyz(
+            entity.vertices,
+            [
+                params.u_cell_size,
+                params.v_cell_size,
+                params.w_cell_size,
+            ],
+            padding_distance=p_d,
+            mesh_type="tree",
+            depth_core=params.depth_core,
+        )
+
+        for label, value in params.free_parameter_dict.items():
+            if not isinstance(getattr(params, value["object"]), ObjectBase):
+                continue
+
+            print(f"Applying {label} on: {getattr(params, value['object']).name}")
+
+            treemesh = refine_tree_xyz(
+                treemesh,
+                getattr(params, value["object"]).vertices,
+                method=getattr(params, value["type"]),
+                octree_levels=getattr(params, value["levels"]),
+                max_distance=getattr(params, value["distance"]),
+                finalize=False,
+            )
+
+        print("Finalizing . . .")
+        treemesh.finalize()
+
+        octree = treemesh_2_octree(params.geoh5, treemesh, name=params.ga_group_name)
+
+        return octree
+
+
+if __name__ == "__main__":
+    file = sys.argv[1]
+    OctreeDriver.start(file)
