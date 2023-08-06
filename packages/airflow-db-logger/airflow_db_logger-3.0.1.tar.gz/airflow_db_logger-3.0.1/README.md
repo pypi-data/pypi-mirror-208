9f56c3674354f8867899c17fed88c81188779579
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:15:27 +0000

    Add package airflow-db-logger/3.0.1/airflow_db_logger-3.0.1.tar.gz

---
+++ packages/airflow-db-logger/airflow_db_logger-3.0.1.tar.gz/airflow_db_logger-3.0.1/README.md
@@ -0,0 +1,137 @@
+# AirflowDBLogger (python package)
+
+##### Remember: If you like it \* it, so other people would also use it.
+
+An airflow logger that stores its results in a database given an SQLAlchemy connection.
+
+Supports:
+1. Airflow <= 1.x.x (for AirflowDBLogger <= 2.0.3)
+2. Airflow >= 2.x.x (for AirflowDBLogger >= 3.0.0)
+3. Write and read logs to db.
+4. Logs cleanup operator
+
+
+### Deprecation WARNING
+
+Versions before 2.0.5 do not support the current logger config and classes. Class structure has changed
+and configuration parameters have changed.
+
+# Install
+
+To install using pip @ https://pypi.org/project/airflow-db-logger/,
+
+```shell
+pip install airflow_db_logger
+```
+
+To install from master branch,
+
+```shell
+pip install git+https://github.com/LamaAni/AirflowDBLogger.git@master
+```
+
+To install from a release (tag)
+
+```shell
+pip install git+https://github.com/LamaAni/AirflowDBLogger.git@[tag]
+```
+
+# TL;DR
+
+Add to airflow.cfg,
+
+```ini
+[core or logging(airflow 2)]
+logging_config_class = airflow_db_logger.LOGGING_CONFIG
+
+[db_logger]
+# Defaults loaded from airflow config
+
+logging_level = INFO
+processor_log_level = 
+
+# Log flags
+show_reverse_order = false 
+show_log_splash = true
+
+# Write control
+write_dag_processing_to_db = false
+# Auto defaults to true in DebugExecutor
+write_to_shell = false 
+# Internal default
+log_splash =
+
+# SQL Alchemy config
+sql_alchemy_schema = 
+sql_alchemy_connection = 
+sql_alchemy_connection_args = 
+sql_alchemy_create_indexes = 
+sql_alchemy_pool_enabled = true
+sql_alchemy_pool_size = 5
+sql_alchemy_max_overflow = 1
+sql_alchemy_pool_recycle = 1800
+sql_alchemy_pool_pre_ping = true
+sql_alchemy_engine_encoding = utf-8
+
+# Logging handlers
+# Add airflow default handlers (will write to files)
+add_task_default_log_handler = false
+add_processor_default_log_handler = false
+# Override the default console handler
+override_default_console_handler = true
+
+# Override the default formatters
+console_formatter = airflow_coloured
+task_formatter = airflow
+processor_formatter = aiflow
+
+# Internal logger (logging status)
+# Default: %%(blue)s[%%(asctime)s]%%(purple)s[airflow_db_logger]%%(log_color)s[%%(levelname)s]%%(reset)s %%(message)s
+colored_log_format=
+# Default: [%%(asctime)s][airflow_db_logger][%%(levelname)s] %%(message)s
+log_format=
+```
+
+Or use the airflow builtin envs,
+
+```shell
+export AIRFLOW__DB_LOGGER_[config value name]="my_value"
+```
+
+# Maintenance
+
+You can use the built in airflow operator to do cleanup. for example cleanup of logs older than 30 days,
+
+```python
+
+from airflow_db_logger.operators import AirflowDBLoggerCleanupOperator
+
+...
+
+with dag:
+    AirflowDBLoggerCleanupOperator(
+        task_id="db_log_cleanup",
+        up_to=days_ago(30),
+        since=None,
+        include_operations_log=True,
+        include_task_logs=True,
+    )
+```
+
+This operator will delete old logs in the database, and reduce the DB size.
+
+# Contributions
+
+Are welcome, please post issues or PR's if needed.
+
+## Implementations still missing
+
+Add an issue (or better submit PR) if you need these.
+
+1. Examples (other than TL;DR)
+
+# License
+
+Copyright ©
+`Zav Shotan` and other [contributors](https://github.com/LamaAni/AirflowDBLogger/graphs/contributors).
+It is free software, released under the MIT licence, and may be redistributed under the terms specified in [LICENSE](docs/LICENSE).
---
+++ packages/airflow-db-logger/airflow_db_logger-3.0.1.tar.gz/airflow_db_logger-3.0.1/airflow_db_logger/config.py
@@ -0,0 +1,119 @@
+import logging
+from typing import Union, List, Type
+from enum import Enum
+from airflow.configuration import conf, AirflowConfigException, log
+from airflow.version import version as AIRFLOW_VERSION
+
+
+AIRFLOW_CONFIG_SECTION_NAME = "db_logger"
+AIRFLOW_VERSION_PARTS = AIRFLOW_VERSION.split(".")
+AIRFLOW_VERSION_PARTS = [int(v) for v in AIRFLOW_VERSION_PARTS]
+AIRFLOW_MAJOR_VERSION = AIRFLOW_VERSION_PARTS[0]
+
+DB_LOGGER_DEFAULT_LOG_SPLASH = "[Airflow DBLogger] logs loaded from database\n"
+
+
+def conf_get_no_warnings_no_errors(*args, **kwargs):
+    old_level = log.level
+    log.level = logging.ERROR
+    try:
+        val = conf.get(*args, **kwargs)
+    except AirflowConfigException:
+        val = None
+    log.level = old_level
+    return val
+
+
+def __add_core(*collections):
+    if AIRFLOW_MAJOR_VERSION < 1:
+        collections = ["core", *collections]
+    return collections
+
+
+def get(
+    key: str,
+    default=None,
+    otype: Type = None,
+    allow_empty: bool = False,
+    collection: Union[str, List[str]] = None,
+):
+    collection = collection or AIRFLOW_CONFIG_SECTION_NAME
+    if isinstance(collection, str):
+        collection = [collection]
+    assert all(isinstance(v, str) for v in collection), ValueError("Collection must be a string or a list of strings")
+    collection = [v for v in collection if len(v.strip()) > 0]
+    assert len(collection) > 0, ValueError("Collection must be a non empty string or list of non empty strings")
+
+    otype = otype or str if default is None else default.__class__
+    for col in collection:
+        val = conf_get_no_warnings_no_errors(col, key)
+        if val is not None:
+            break
+
+    if issubclass(otype, Enum):
+        allow_empty = False
+
+    value_is_empty = val is None or (isinstance(val, str) and len(val.strip()) == 0)
+
+    if not allow_empty and value_is_empty:
+        assert default is not None, AirflowConfigException(
+            f"Airflow configuration {collection}.{key} not found, and no default value"
+        )
+        return default
+
+    if val is None:
+        return None
+    if otype == bool:
+        return val.lower() == "true"
+    elif issubclass(otype, Enum):
+        val = val.strip()
+        return otype(val.strip())
+    else:
+        return otype(val)
+
+
+# Loading airflow parameters
+LOG_LEVEL = get(collection=__add_core("logging"), key="logging_level").upper()
+AIRFLOW_EXECUTOR = get(collection="core", key="executor")
+IS_RUNNING_DEBUG_EXECUTOR = AIRFLOW_EXECUTOR == "DebugExecutor"
+IS_USING_COLORED_CONSOLE = get(collection=__add_core("logging"), key="colored_console_log", default=False)
+
+# Loading sql parameters
+SQL_ALCHEMY_CONN = get(collection=__add_core("database"), key="sql_alchemy_conn", allow_empty=False)
+SQL_ALCHEMY_SCHEMA = get(collection=__add_core("database"), key="sql_alchemy_schema", allow_empty=True)
+
+DB_LOGGER_LOG_LEVEL = get(key="logging_level", default=LOG_LEVEL).upper()
+DB_LOGGER_PROCESSOR_LOG_LEVEL = get("processor_log_level", default=LOG_LEVEL, allow_empty=True, otype=str)
+DB_LOGGER_SHOW_REVERSE_ORDER = get("show_reverse", False)
+DB_LOGGER_SHOW_LOG_SPLASH = get("show_log_splash", True)
+DB_LOGGER_LOG_SPLASH = get("log_splash", DB_LOGGER_DEFAULT_LOG_SPLASH)
+
+DB_LOGGER_SQL_ALCHEMY_SCHEMA = get("sql_alchemy_schema", SQL_ALCHEMY_SCHEMA)
+DB_LOGGER_SQL_ALCHEMY_CONNECTION = get("sql_alchemy_conn", SQL_ALCHEMY_CONN)
+DB_LOGGER_SQL_ALCHEMY_CONNECTION_ARGS = get("sql_alchemy_conn_args", None, allow_empty=True)
+DB_LOGGER_SQL_ALCHEMY_CREATE_INDEXES = get("create_index", True)
+DB_LOGGER_SQL_ALCHEMY_POOL_ENABLED = get("sql_alchemy_pool_enabled", False)
+DB_LOGGER_SQL_ALCHEMY_POOL_SIZE = get("sql_alchemy_pool_size", 5)
+DB_LOGGER_SQL_ALCHEMY_MAX_OVERFLOW = get("sql_alchemy_max_overflow", 1)
+DB_LOGGER_SQL_ALCHEMY_POOL_RECYCLE = get("sql_alchemy_pool_recycle", 1800)
+DB_LOGGER_SQL_ALCHEMY_POOL_PRE_PING = get("sql_alchemy_pool_pre_ping", True)
+DB_LOGGER_SQL_ALCHEMY_ENGINE_ENCODING = get("sql_engine_encoding", None, allow_empty=True)
+
+DB_LOGGER_ADD_TASK_DEFAULT_LOG_HANDLER = get("add_task_default_log_handler", default=True)
+DB_LOGGER_ADD_PROCESSOR_DEFAULT_LOG_HANDLER = get("add_processor_default_log_handler", default=False)
+DB_LOGGER_WRITE_DAG_PROCESSING_TO_DB = get("write_dag_processing_to_db", False)
+DB_LOGGER_WRITE_TO_SHELL = get("write_to_shell", default=IS_RUNNING_DEBUG_EXECUTOR)
+DB_LOGGER_OVERRIDE_DEFAULT_CONSOLE_HANDLER = get("override_default_console_handler", default=True)
+
+DB_LOGGER_CONSOLE_FORMATTER = "airflow_coloured" if IS_USING_COLORED_CONSOLE else "airflow"
+DB_LOGGER_TASK_FORMATTER = "airflow"
+DB_LOGGER_PROCESSOR_FORMATTER = "airflow"
+
+DB_LOGGER_COLORED_LOG_FORMAT = get(
+    "colored_log_format",
+    "%(blue)s[%(asctime)s]%(purple)s[airflow_db_logger]%(log_color)s[%(levelname)s]%(reset)s %(message)s",
+)
+DB_LOGGER_LOG_FORMAT = get(
+    "log_format",
+    "[%(asctime)s][airflow_db_logger][%(levelname)s] %(message)s",
+)
