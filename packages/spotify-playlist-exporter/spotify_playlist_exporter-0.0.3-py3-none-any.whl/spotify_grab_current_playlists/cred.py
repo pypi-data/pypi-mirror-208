b28fe2c6a238531de4eebcf4a7c41f354a00c5cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:23 +0000

    Add package spotify-playlist-exporter/0.0.3/spotify_playlist_exporter-0.0.3-py3-none-any.whl

---
+++ packages/spotify-playlist-exporter/spotify_playlist_exporter-0.0.3-py3-none-any.whl/spotify_grab_current_playlists/cred.py
@@ -0,0 +1,4 @@
+username 	= "your_spotify_username"
+client_id 	= "your_API_client_ID"
+client_secret 	= "your_API_client_secret"
+redirect_uri 	= "http://localhost:9000" # this can be any URL, but must be registered in your app settings
---
+++ packages/spotify-playlist-exporter/spotify_playlist_exporter-0.0.3-py3-none-any.whl/spotify_grab_current_playlists/cred_my.py
@@ -0,0 +1,4 @@
+username = "phanta_stick"
+client_id = "d4cbf68c29034fbf931ef56c28dde292"
+client_secret = "d4dd4e7045484a99b9e725bdad466633"
+redirect_uri = "http://localhost:9000" # this can be any URL, but must be registered in your app settings
