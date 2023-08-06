1159c33b3b8dd2fa95105f88fe8b92cb783574bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:18:49 +0000

    Add package rcmpy/1.5.0/rcmpy-1.5.0-py3-none-any.whl

---
+++ packages/rcmpy/rcmpy-1.5.0-py3-none-any.whl/rcmpy/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.2
+# hash=01f75a43f918c56d5ba1f831c029be03
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A configuration-file management system."
+PKG_NAME = "rcmpy"
+VERSION = "1.5.0"
