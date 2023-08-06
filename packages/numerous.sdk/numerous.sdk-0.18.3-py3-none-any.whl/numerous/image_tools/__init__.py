fabe14cceab88cd2530743284cf7ae9d2bd12c8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:18:09 +0000

    Add package numerous.sdk/0.18.3/numerous.sdk-0.18.3-py3-none-any.whl

---
+++ packages/numerous.sdk/numerous.sdk-0.18.3-py3-none-any.whl/numerous/image_tools/__init__.py
@@ -0,0 +1,6 @@
+from .example_models import (  # TODO: fix missing/previously overridden import of .example_models.multiply_by_two
+    model,
+)
+from .examples import multiply_by_two
+
+__all__ = ("model", "multiply_by_two")
