b9828462d69d5cf5f12c66f39ca513401e1e82b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:06 +0000

    Add package pynamer/1.2.1/pynamer-1.2.1.tar.gz

---
+++ packages/pynamer/pynamer-1.2.1.tar.gz/pynamer-1.2.1/tests/test_run_command TBD.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# Core Library modules
+from pathlib import Path
+
+# Third party modules
+import pytest
+
+# First party modules
+from pynamer import pynamer
+
+BASE_DIR = Path(__file__).parents[0]
+
+
+def test_run_command():
+    pass
