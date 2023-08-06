fab0d2f3fc95089b72885e007011746f58be3ded
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:52 +0000

    Add package aioqui/1.0.0/aioqui-1.0.0-py3-none-any.whl

---
+++ packages/aioqui/aioqui-1.0.0-py3-none-any.whl/aioqui/misc/__init__.py
@@ -0,0 +1,6 @@
+from . import types, utils
+
+from .ctq import ConditionalThreadQueue, ConditionalThread
+from .parser import StyleSheetParser
+from .utils import find, serializable
+from .types import Icon, Size
---
+++ packages/aioqui/aioqui-1.0.0-py3-none-any.whl/aioqui/widgets/scroll_area.py
@@ -0,0 +1,39 @@
+from PySide6.QtWidgets import QScrollArea, QWidget
+from PySide6.QtCore import QObject
+from typing import Sequence, Iterable
+
+from .frame import Frame
+from .layout import Layout
+from ..objects import ContextObj
+from ..enums import Orientation, ScrollPolicy
+
+
+class ScrollArea(ContextObj, QScrollArea):
+    def __init__(self, parent: QWidget, name: str, visible: bool = True, stylesheet: str = None):
+        QScrollArea.__init__(self, parent)
+        ContextObj.__init__(self, parent, name, visible)
+        if stylesheet:
+            self.setStyleSheet(stylesheet)
+
+    async def init(
+            self, *,
+            orientation: Orientation.Orientation,
+            hpolicy: ScrollPolicy.ScrollPolicy = ScrollPolicy.WhenNeeded,
+            vpolicy: ScrollPolicy.ScrollPolicy = ScrollPolicy.WhenNeeded,
+
+            margins: tuple[int, ...] = (0, 0, 0, 0), spacing: int = 0, alignment: Layout.Alignment = None,
+            items: Sequence[QObject] = ()
+    ) -> 'ScrollArea':
+        self.setHorizontalScrollBarPolicy(hpolicy)
+        self.setVerticalScrollBarPolicy(vpolicy)
+        self.setWidgetResizable(True)
+        frame = Frame(self, f'{self.objectName()}Widget')
+        self.setWidget(await frame.init(
+            layout=await Layout.oriented(orientation, frame, f'{frame.objectName()}Layout').init(
+                margins=margins, spacing=spacing, alignment=alignment, items=items
+            )
+        ))
+        return self
+
+    def clear(self, exceptions: Iterable[QObject] = ()):
+        self.widget().layout().clear(exceptions)
