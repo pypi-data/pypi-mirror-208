5651e2c784ef09dcc3a7d618405c3f624db72d8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:29 +0000

    Add package e2enetworks/0.0.8/e2enetworks-0.0.8.tar.gz

---
+++ packages/e2enetworks/e2enetworks-0.0.8.tar.gz/e2enetworks-0.0.8/e2enetworks/cloud/aiplatform/EndPoint/__init__.py
@@ -0,0 +1,15 @@
+class EndPoint:
+    def __init__(self, model=None):
+        self.model = model
+
+    def create(self, sku_id=None, storage_url=None, replica=None,):
+        pass
+
+    def get(self, endpoint_id):
+        pass
+
+    def list(self):
+        pass
+
+    def delete(self, endpoint_id):
+        pass
