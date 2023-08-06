09f00a94e18c7b1f11017120a655430e3330c9f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:18:15 +0000

    Add package coolpandas/0.3.0/coolpandas-0.3.0.tar.gz

---
+++ packages/coolpandas/coolpandas-0.3.0.tar.gz/coolpandas-0.3.0/coolpandas/plot/__init__.py
@@ -0,0 +1,6 @@
+"""Plot module for coolpandas."""
+from .barplot import barplot
+from .distplot import distplot
+from .geoplot import geoplot
+from .lineplot import lineplot
+from .mapplot import mapplot
