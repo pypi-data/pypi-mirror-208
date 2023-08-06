181658aee8287020908b328f0fabbd806070a371
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:47 +0000

    Add package openimis-be-claim/1.5.1/openimis-be-claim-1.5.1.tar.gz

---
+++ packages/openimis-be-claim/openimis-be-claim-1.5.1.tar.gz/openimis-be-claim-1.5.1/claim/schema.py
@@ -0,0 +1,220 @@
+from core.models import Officer
+from location.models import HealthFacility
+from .services import check_unique_claim_code
+import django
+from core.schema import signal_mutation_module_validate
+from django.db.models import OuterRef, Subquery, Avg, Q
+import graphene_django_optimizer as gql_optimizer
+from core.schema import OrderedDjangoFilterConnectionField, OfficerGQLType
+from core import filter_validity
+from django.db.models.functions import Cast
+
+from .models import ClaimMutation
+from django.utils.translation import gettext as _
+from graphene_django.filter import DjangoFilterConnectionField
+import ast
+
+# We do need all queries and mutations in the namespace here.
+from .gql_queries import *  # lgtm [py/polluting-import]
+from .gql_mutations import *  # lgtm [py/polluting-import]
+
+
+class Query(graphene.ObjectType):
+    claims = OrderedDjangoFilterConnectionField(
+        ClaimGQLType,
+        diagnosisVariance=graphene.Int(),
+        code_is_not=graphene.String(),
+        orderBy=graphene.List(of_type=graphene.String),
+        items=graphene.List(of_type=graphene.String),
+        services=graphene.List(of_type=graphene.String),
+        json_ext=graphene.JSONString(),
+    )
+
+    claim = graphene.Field(
+        ClaimGQLType, id=graphene.Int(), uuid=graphene.UUID()
+    )
+
+    claim_attachments = DjangoFilterConnectionField(
+        ClaimAttachmentGQLType
+    )
+    claim_admins = DjangoFilterConnectionField(
+        ClaimAdminGQLType,
+        search=graphene.String(),
+        region_uuid=graphene.String(),
+        district_uuid=graphene.String()
+    )
+    claim_officers = DjangoFilterConnectionField(
+        OfficerGQLType, search=graphene.String()
+    )
+
+    validate_claim_code = graphene.Field(
+        graphene.Boolean,
+        claim_code=graphene.String(required=True),
+        description="Checks that the specified claim code is unique."
+    )
+
+    def resolve_validate_claim_code(self, info, **kwargs):
+        if not info.context.user.has_perms(ClaimConfig.gql_query_claims_perms):
+            raise PermissionDenied(_("unauthorized"))
+        errors = check_unique_claim_code(code=kwargs['claim_code'])
+        return False if errors else True
+
+    def resolve_claim(self, info, id=None, uuid=None, **kwargs):
+        if (
+            not info.context.user.has_perms(ClaimConfig.gql_query_claims_perms)
+            and settings.ROW_SECURITY
+        ):
+            raise PermissionDenied(_("unauthorized"))
+
+        if id is not None:
+            return Claim.objects.get(id=id)
+        if uuid is not None:
+            return Claim.objects.get(uuid=uuid)
+
+    def resolve_claims(self, info, **kwargs):
+        if (
+            not info.context.user.has_perms(ClaimConfig.gql_query_claims_perms)
+            and settings.ROW_SECURITY
+        ):
+            raise PermissionDenied(_("unauthorized"))
+        query = Claim.objects
+        code_is_not = kwargs.get("code_is_not", None)
+        if code_is_not:
+            query = query.exclude(code=code_is_not)
+        variance = kwargs.get("diagnosisVariance", None)
+
+        items = kwargs.get("items", None)
+        services = kwargs.get("services", None)
+
+        if items:
+            query = query.filter(items__item__code__in=items)
+
+        if services:
+            query = query.filter(services__service__code__in=services)
+
+        json_ext = kwargs.get("json_ext", None)
+
+        if json_ext:
+            query = query.filter(json_ext__jsoncontains=json_ext)
+
+        if variance:
+            from core import datetime, datetimedelta
+
+            last_year = datetime.date.today() + datetimedelta(years=-1)
+            diag_avg = (
+                Claim.objects.filter(*filter_validity(**kwargs))
+                .filter(date_claimed__gt=last_year)
+                .values("icd__code")
+                .filter(icd__code=OuterRef("icd__code"))
+                .annotate(diag_avg=Avg("approved"))
+                .values("diag_avg")
+            )
+            variance_filter = Q(claimed__gt=(
+                1 + variance / 100) * Subquery(diag_avg))
+            if not ClaimConfig.gql_query_claim_diagnosis_variance_only_on_existing:
+                diags = (
+                    Claim.objects.filter(*filter_validity(**kwargs))
+                    .filter(date_claimed__gt=last_year)
+                    .values("icd__code")
+                    .distinct()
+                )
+                variance_filter = variance_filter | ~Q(icd__code__in=diags)
+            query = query.filter(variance_filter)
+
+        from location.models import Location
+        user_districts = UserDistrict.get_user_districts(info.context.user._u)
+        query = query.filter(
+            Q(health_facility__location__in=Location.objects.filter(uuid__in=user_districts.values_list('location__uuid', flat=True))) | Q(
+                health_facility__location__in=Location.objects.filter(uuid__in=user_districts.values_list('location__parent__uuid', flat=True))))
+
+        return gql_optimizer.query(query.all(), info)
+
+    def resolve_claim_attachments(self, info, **kwargs):
+        if not info.context.user.has_perms(ClaimConfig.gql_query_claims_perms):
+            raise PermissionDenied(_("unauthorized"))
+
+    def resolve_claim_admins(
+            self,
+            info,
+            search=None,
+            **kwargs
+    ):
+        if not info.context.user.has_perms(
+                ClaimConfig.gql_query_claim_admins_perms
+        ):
+            raise PermissionDenied(_("unauthorized"))
+
+        hf_filters = [*filter_validity(**kwargs)]
+        district_uuid = kwargs.get('district_uuid', None)
+        region_uuid = kwargs.get('region_uuid', None)
+        if district_uuid is not None:
+            hf_filters += [Q(location__uuid=district_uuid)]
+        if region_uuid is not None:
+            hf_filters += [Q(location__parent__uuid=region_uuid)]
+        if settings.ROW_SECURITY:
+            dist = UserDistrict.get_user_districts(info.context.user._u)
+            hf_filters += [Q(location__id__in=[l.location_id for l in dist])]
+        user_health_facility = HealthFacility.objects.filter(*hf_filters)
+
+        filters = [*filter_validity(**kwargs)]
+        if user_health_facility:
+            filters += [Q(health_facility__in=user_health_facility)]
+
+        if search:
+            filters += filters[Q(code__icontains=search) |
+                               Q(last_name__icontains=search) |
+                               Q(other_names__icontains=search)]
+
+        return ClaimAdmin.objects.filter(*filters)
+
+    def resolve_claim_officers(self, info, search=None, **kwargs):
+        if not info.context.user.has_perms(ClaimConfig.gql_query_claim_officers_perms):
+            raise PermissionDenied(_("unauthorized"))
+
+        qs = Officer.objects
+
+        if search is not None:
+            qs = qs.filter(
+                Q(code__icontains=search)
+                | Q(last_name__icontains=search)
+                | Q(other_names__icontains=search)
+            )
+        return qs
+
+
+class Mutation(graphene.ObjectType):
+    create_claim = CreateClaimMutation.Field()
+    update_claim = UpdateClaimMutation.Field()
+    create_claim_attachment = CreateAttachmentMutation.Field()
+    update_claim_attachment = UpdateAttachmentMutation.Field()
+    delete_claim_attachment = DeleteAttachmentMutation.Field()
+    submit_claims = SubmitClaimsMutation.Field()
+    select_claims_for_feedback = SelectClaimsForFeedbackMutation.Field()
+    deliver_claim_feedback = DeliverClaimFeedbackMutation.Field()
+    bypass_claims_feedback = BypassClaimsFeedbackMutation.Field()
+    skip_claims_feedback = SkipClaimsFeedbackMutation.Field()
+    select_claims_for_review = SelectClaimsForReviewMutation.Field()
+    save_claim_review = SaveClaimReviewMutation.Field()
+    deliver_claims_review = DeliverClaimsReviewMutation.Field()
+    bypass_claims_review = BypassClaimsReviewMutation.Field()
+    skip_claims_review = SkipClaimsReviewMutation.Field()
+    process_claims = ProcessClaimsMutation.Field()
+    delete_claims = DeleteClaimsMutation.Field()
+
+
+def on_claim_mutation(sender, **kwargs):
+    uuids = kwargs["data"].get("uuids", [])
+    if not uuids:
+        uuid = kwargs["data"].get("claim_uuid", None)
+        uuids = [uuid] if uuid else []
+    if not uuids:
+        return []
+    impacted_claims = Claim.objects.filter(uuid__in=uuids).all()
+    for claim in impacted_claims:
+        ClaimMutation.objects.create(
+            claim=claim, mutation_id=kwargs["mutation_log_id"])
+    return []
+
+
+def bind_signals():
+    signal_mutation_module_validate["claim"].connect(on_claim_mutation)
---
+++ packages/openimis-be-claim/openimis-be-claim-1.5.1.tar.gz/openimis-be-claim-1.5.1/claim/test_services.py
@@ -0,0 +1,214 @@
+from django.test import TestCase
+from unittest import mock
+
+import datetime
+from .services import *
+import core
+
+
+class ClaimSubmitServiceTestCase(TestCase):
+
+    def test_minimal_item_claim_submit_xml(self):
+        items = [
+            ClaimItemSubmit(code='aa', quantity=2),
+        ]
+        item = "<Item><ItemCode>aa</ItemCode><ItemQuantity>2</ItemQuantity></Item>"
+
+        claim = ClaimSubmit(
+            date=core.datetime.date(2020, 1, 9),
+            code="code_ABVC",
+            icd_code="ICD_CODE_WWQ",
+            total=334,
+            start_date=core.datetime.date(2020, 1, 13),
+            claim_admin_code='ADM_CODE_ADKJ',
+            insuree_chf_id='CHFID_UUZIS',
+            health_facility_code="HFCode_JQL",
+            item_submits=items,
+        )
+        details = "<Details>"
+        details = details + "<ClaimDate>09/01/2020</ClaimDate>"
+        details = details + "<HFCode>HFCode_JQL</HFCode>"
+        details = details + "<ClaimAdmin>ADM_CODE_ADKJ</ClaimAdmin>"
+        details = details + "<ClaimCode>code_ABVC</ClaimCode>"
+        details = details + "<CHFID>CHFID_UUZIS</CHFID>"
+        details = details + "<StartDate>13/01/2020</StartDate>"
+        details = details + "<ICDCode>ICD_CODE_WWQ</ICDCode>"
+        details = details + "<Total>334</Total>"
+        details = details + "</Details>"
+        expected = "<Claim>%s<Items>%s</Items></Claim>" % (details, item)
+        self.assertEquals(expected, claim.to_xml())
+
+    def test_minimal_service_claim_submit_xml(self):
+        services = [
+            ClaimServiceSubmit(code='aa', quantity=2),
+        ]
+        service = "<Service><ServiceCode>aa</ServiceCode><ServiceQuantity>2</ServiceQuantity></Service>"
+
+        claim = ClaimSubmit(
+            date=core.datetime.date(2020, 1, 9),
+            code="code_ABVC",
+            icd_code="ICD_CODE_WWQ",
+            total=334,
+            start_date=core.datetime.date(2020, 1, 13),
+            claim_admin_code='ADM_CODE_ADKJ',
+            insuree_chf_id='CHFID_UUZIS',
+            health_facility_code="HFCode_JQL",
+            service_submits=services,
+        )
+        details = "<Details>"
+        details = details + "<ClaimDate>09/01/2020</ClaimDate>"
+        details = details + "<HFCode>HFCode_JQL</HFCode>"
+        details = details + "<ClaimAdmin>ADM_CODE_ADKJ</ClaimAdmin>"
+        details = details + "<ClaimCode>code_ABVC</ClaimCode>"
+        details = details + "<CHFID>CHFID_UUZIS</CHFID>"
+        details = details + "<StartDate>13/01/2020</StartDate>"
+        details = details + "<ICDCode>ICD_CODE_WWQ</ICDCode>"
+        details = details + "<Total>334</Total>"
+        details = details + "</Details>"
+        expected = "<Claim>%s<Services>%s</Services></Claim>" % (
+            details, service)
+        self.assertEquals(expected, claim.to_xml())
+
+    def test_extended_claim_submit_xml(self):
+        items = [
+            ClaimItemSubmit(code='aa', quantity=2),
+            ClaimItemSubmit(code='bb', quantity=1, price=12.3),
+        ]
+        item_a = "<Item><ItemCode>aa</ItemCode><ItemQuantity>2</ItemQuantity></Item>"
+        item_b = "<Item><ItemCode>bb</ItemCode><ItemPrice>12.3</ItemPrice><ItemQuantity>1</ItemQuantity></Item>"
+
+        services = [
+            ClaimServiceSubmit(code='aa-serv', quantity=2),
+            ClaimServiceSubmit(code='a<a\'-serv', quantity=1, price=35),
+        ]
+        service_a = "<Service><ServiceCode>aa-serv</ServiceCode><ServiceQuantity>2</ServiceQuantity></Service>"
+        service_b = "<Service><ServiceCode>a&lt;a\'-serv</ServiceCode><ServicePrice>35</ServicePrice><ServiceQuantity>1</ServiceQuantity></Service>"
+
+        claim = ClaimSubmit(
+            date=core.datetime.date(2020, 1, 9),
+            code="code_ABVC",
+            icd_code="ICD_CODE_WWQ",
+            total=334,
+            start_date=core.datetime.date(2020, 1, 13),
+            claim_admin_code='ADM_CODE_ADKJ',
+            insuree_chf_id='CHFID_UUZIS',
+            health_facility_code="HFCode_JQL",
+            item_submits=items,
+            service_submits=services
+        )
+        details = "<Details>"
+        details = details + "<ClaimDate>09/01/2020</ClaimDate>"
+        details = details + "<HFCode>HFCode_JQL</HFCode>"
+        details = details + "<ClaimAdmin>ADM_CODE_ADKJ</ClaimAdmin>"
+        details = details + "<ClaimCode>code_ABVC</ClaimCode>"
+        details = details + "<CHFID>CHFID_UUZIS</CHFID>"
+        details = details + "<StartDate>13/01/2020</StartDate>"
+        details = details + "<ICDCode>ICD_CODE_WWQ</ICDCode>"
+        details = details + "<Total>334</Total>"
+        details = details + "</Details>"
+        expected = "<Claim>%s" % details
+        expected = expected + "<Items>%s%s</Items>" % (item_a, item_b)
+        expected = expected + \
+                   "<Services>%s%s</Services>" % (service_a, service_b)
+        expected = expected + "</Claim>"
+        self.assertEquals(expected, claim.to_xml())
+
+    @mock.patch('django.db.connections')
+    def test_claim_submit_error(self, mock_connections):
+        with mock.patch("claim.services.ClaimSubmitService.hf_scope_check") as mock_security:
+            mock_security.return_value = None
+            query_result = [2]
+            mock_connections.__getitem__.return_value.cursor.return_value \
+                .__enter__.return_value.fetchone.return_value = query_result
+            mock_user = mock.Mock(is_anonymous=False)
+            mock_user.has_perm = mock.MagicMock(return_value=True)
+            claim = ClaimSubmit(
+                date=core.datetime.date(2020, 1, 9),
+                code="code_ABVC",
+                icd_code="ICD_CODE_WWQ",
+                total=334,
+                start_date=core.datetime.date(2020, 1, 13),
+                claim_admin_code='ADM_CODE_ADKJ',
+                insuree_chf_id='CHFID_UUZIS',
+                health_facility_code="HFCode_JQL"
+            )
+            service = ClaimSubmitService(user=mock_user)
+            with self.assertRaises(ClaimSubmitError) as cm:
+                service.submit(claim)
+            self.assertNotEqual(cm.exception.code, 0)
+
+    def test_claim_submit_allgood_xml(self):
+        with mock.patch("django.db.backends.utils.CursorWrapper") as mock_cursor:
+            # required for all modules tests
+            mock_cursor.return_value.description = None
+            # required for claim module tests
+            mock_cursor.return_value.__enter__.return_value.description = None
+            with mock.patch("claim.services.ClaimSubmitService.hf_scope_check") as mock_security:
+                mock_security.return_value = None
+                mock_user = mock.Mock(is_anonymous=False)
+                mock_user.has_perm = mock.MagicMock(return_value=True)
+                claim = ClaimSubmit(
+                    date=core.datetime.date(2020, 1, 9),
+                    code="code_ABVC",
+                    icd_code="ICD_CODE_WWQ",
+                    total=334,
+                    start_date=core.datetime.date(2020, 1, 13),
+                    claim_admin_code='ADM_CODE_ADKJ',
+                    insuree_chf_id='CHFID_UUZIS',
+                    health_facility_code="HFCode_JQL"
+                )
+                service = ClaimSubmitService(user=mock_user)
+                service.submit(claim)  # doesn't raise an error
+
+    @mock.patch("claim.services.ClaimSubmitService._validate_user_hf")
+    @mock.patch("claim.services.ClaimCreateService._validate_user_hf")
+    def test_claim_enter_and_submit(self, check_hf_submit, check_hf_enter):
+        check_hf_submit.return_value, check_hf_enter.return_value = True, True
+        mock_user = mock.Mock(is_anonymous=False)
+        mock_user.has_perm = mock.MagicMock(return_value=True)
+        mock_user.id_for_audit = -1
+
+        claim = self._get_test_dict()
+        service = ClaimSubmitService(user=mock_user)
+        submitted_claim = service.enter_and_submit(claim, False)
+        expected_claimed = 2 * 7 * 11  # 2 provisions, both qty = 7, price asked == 11
+
+        self.assertEqual(submitted_claim.status, Claim.STATUS_CHECKED)
+        self.assertEqual(submitted_claim.approved, expected_claimed)
+        self.assertEqual(submitted_claim.claimed, expected_claimed)
+        self.assertEqual(submitted_claim.health_facility.id, 18)
+        self.assertEqual(len(submitted_claim.items.all()), 1)
+        self.assertEqual(len(submitted_claim.services.all()), 1)
+        self.assertEqual(submitted_claim.audit_user_id, -1)
+        self.assertTrue(submitted_claim.id is not None)
+
+    @mock.patch("claim.services.ClaimSubmitService._validate_user_hf")
+    @mock.patch("claim.services.ClaimCreateService._validate_user_hf")
+    def test_claim_enter_duplicate_exception(self, check_hf_submit, check_hf_enter):
+        check_hf_submit.return_value, check_hf_enter.return_value = True, True
+        mock_user = mock.Mock(is_anonymous=False)
+        mock_user.has_perm = mock.MagicMock(return_value=True)
+        mock_user.id_for_audit = -1
+
+        claim = self._get_test_dict()
+        service = ClaimSubmitService(user=mock_user)
+
+        service.enter_and_submit(claim, False)
+
+        with self.assertRaises(ValidationError):
+            service.enter_and_submit(claim, False)
+
+    def _get_test_dict(self):
+        return {
+            "health_facility_id": 18, "icd_id": 116, "date_from": datetime.datetime(2019, 6, 1), "code": "CLCODE1",
+            "date_claimed": datetime.datetime(2019, 6, 1), "date_to": datetime.datetime(2019, 6, 1),
+            "audit_user_id": 1, "insuree_id": 2, "status": 2, "validity_from": datetime.datetime(2019, 6, 1),
+            "items": [{
+                "qty_provided": 7, "price_asked": 11, "item_id": 23, "status": 1, "availability": True,
+                "validity_from": "2019-06-01", "validity_to": None, "audit_user_id": -1
+            }],
+            "services": [{
+                "qty_provided": 7, "price_asked": 11, "service_id": 23,  # Skin graft, no cat
+                "status": 1, "validity_from": "2019-06-01", "validity_to": None, "audit_user_id": -1
+            }]
+        }
