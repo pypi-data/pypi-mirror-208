bd6cb7aae1f637e02add801d6683f6f58d25b890
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:56 +0000

    Add package CactusSerializer/3.0.0/CactusSerializer-3.0.0.tar.gz

---
+++ packages/CactusSerializer/CactusSerializer-3.0.0.tar.gz/CactusSerializer-3.0.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CactusSerializer",
+    version="3.0.0",
+    description="module for serialization/deserialization(JSON, XML)",
+    url="https://github.com/Antilevskaya-Ksenia-153501/Python-labs/tree/lab3",
+    author="Ksenia Antilevskaya",
+    author_email="antikevkun@gmail.com",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent"
+    ],
+    packages=["Serializer"],
+    include_package_data=True
+)
