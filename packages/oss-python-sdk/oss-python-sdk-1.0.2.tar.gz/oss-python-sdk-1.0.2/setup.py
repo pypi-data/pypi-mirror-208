02019de49bc78278e431e2bf023945fe67cdb624
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:10:49 +0000

    Add package oss-python-sdk/1.0.2/oss-python-sdk-1.0.2.tar.gz

---
+++ packages/oss-python-sdk/oss-python-sdk-1.0.2.tar.gz/oss-python-sdk-1.0.2/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+with open('README.md', 'rb') as f:
+    readme = f.read().decode('utf-8')
+
+setup(
+    name='oss-python-sdk',
+    version='1.0.2',
+    description='OSS Python SDK',
+    author='wangyingbin',
+    author_email='869063218@qq.com',
+    long_description=readme,
+    packages=['oss'],
+    install_requires=['requests!=2.9.0', 'crcmod>=1.7'],
+    include_package_data=True,
+    url='https://github.com/ybwang0211/oss-python-sdk'
+)
