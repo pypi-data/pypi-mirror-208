8938a85ab86a86a5d7e80cd408a116cc4a31e9eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:25 +0000

    Add package sampling-constraints/0.0.12/sampling_constraints-0.0.12-py3-none-any.whl

---
+++ packages/sampling-constraints/sampling_constraints-0.0.12-py3-none-any.whl/scs/constraint/json.py
@@ -0,0 +1,17 @@
+from . import SyntaxConstraint
+
+from ..incremental_parse.json import JSONParser
+
+
+def valid_json(
+    allow_outer_list: bool = True,
+    allow_empty: bool = True,
+    allow_empty_children: bool = True
+) -> SyntaxConstraint:
+    return SyntaxConstraint(
+        JSONParser(
+            allow_outer_list=allow_outer_list,
+            allow_empty=allow_empty,
+            allow_empty_children=allow_empty_children,
+        )
+    )
