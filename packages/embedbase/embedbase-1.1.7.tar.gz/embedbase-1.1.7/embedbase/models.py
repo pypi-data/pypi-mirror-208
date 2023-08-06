f38cd35d137f30773439b66e3eb9d54a1c0767c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:12:41 +0000

    Add package embedbase/1.1.7/embedbase-1.1.7.tar.gz

---
+++ packages/embedbase/embedbase-1.1.7.tar.gz/embedbase-1.1.7/embedbase/models.py
@@ -0,0 +1,34 @@
+from typing import List, Optional, Union
+from pydantic import BaseModel
+
+# TODO: response models once stable
+
+class Document(BaseModel):
+    # data can be
+    # - a string - for example  "This is a document"
+    # TODO: currently only string is supported (later could be images, audio, multi/cross-modal)
+    # etc.
+    data: str
+    metadata: Optional[dict]
+
+
+class AddRequest(BaseModel):
+    documents: List[Document]
+    store_data: bool = True
+
+class UpdateDocument(BaseModel):
+    id: str
+    data: Optional[str] = None
+    metadata: Optional[dict] = None
+
+class UpdateRequest(BaseModel):
+    documents: List[UpdateDocument]
+
+class DeleteRequest(BaseModel):
+    ids: List[str]
+
+
+class SearchRequest(BaseModel):
+    query: str
+    top_k: int = 6
+    where: Optional[Union[dict, List[dict]]] = None
