f2e26e5998e25f0f20ac01e01f81bce9a6f4f5d7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:41 +0000

    Add package python-sonarqube-api/1.3.7/python_sonarqube_api-1.3.7-py3-none-any.whl

---
+++ packages/python-sonarqube-api/python_sonarqube_api-1.3.7-py3-none-any.whl/sonarqube/__init__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @Author: Jialiang Shi
+__version__ = "1.3.7"
+
+from sonarqube.community import SonarQubeClient
+from sonarqube.cloud import SonarCloudClient
+from sonarqube.enterprise import SonarEnterpriseClient
