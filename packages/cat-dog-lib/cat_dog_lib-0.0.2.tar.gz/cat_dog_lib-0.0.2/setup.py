b517dc322b050bba60700401bf39abee46b8c0e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:53 +0000

    Add package cat-dog-lib/0.0.2/cat_dog_lib-0.0.2.tar.gz

---
+++ packages/cat-dog-lib/cat_dog_lib-0.0.2.tar.gz/cat_dog_lib-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import find_packages, setup
+
+
+setup(
+    name="cat_dog_lib",
+    version="0.0.2",
+    description="Mô tả ngắn",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description='Mô tả dài',
+    long_description_content_type="text/markdown",
+    url="https://github.com/anthule123/first_package_An",
+    author="anthule123",
+    author_email="lethithuan_t63@hus.edu.vn",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    extras_require={
+        "dev": ["pytest>=7.0",
+                "twine>=3.4.2",]
+        
+    },
+    python_requires=">=3.10",
+)
