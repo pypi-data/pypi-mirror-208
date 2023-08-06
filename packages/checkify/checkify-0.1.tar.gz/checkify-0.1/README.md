d8bfe80a919f5cdfb7b8ebb6250ad6416ba56ff3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:32 +0000

    Add package checkify/0.1/checkify-0.1.tar.gz

---
+++ packages/checkify/checkify-0.1.tar.gz/checkify-0.1/README.md
@@ -0,0 +1,2 @@
+# checkify
+ Python package that utilizes the GPT API to check for errors in code and explain the code
---
+++ packages/checkify/checkify-0.1.tar.gz/checkify-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='checkify',
+    version='0.1',
+    packages=['checkify'],
+    install_requires=[
+        'openai'
+    ],
+)
