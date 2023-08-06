24033b83752b5416f3589a4aa6ba79138d2d570f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:56:09 +0000

    Add package geomulticorr/0.1.5/geomulticorr-0.1.5.tar.gz

---
+++ packages/geomulticorr/geomulticorr-0.1.5.tar.gz/geomulticorr-0.1.5/src/geomulticorr/__init__.py
@@ -0,0 +1,13 @@
+version = '0.1.5'
+
+# User
+try:
+    from geomulticorr.session import Open
+    
+# Developer
+except ModuleNotFoundError:
+   from src.geomulticorr.session import Open
+
+print(f'''-------------
+geomulticorr {version}
+-------------''')
