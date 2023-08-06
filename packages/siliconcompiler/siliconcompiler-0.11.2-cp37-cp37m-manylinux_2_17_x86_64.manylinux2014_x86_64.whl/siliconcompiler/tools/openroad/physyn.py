5f3a572039a49c8dc15f11e4fa7364e4d65222e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:27:44 +0000

    Add package siliconcompiler/0.11.2/siliconcompiler-0.11.2-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/siliconcompiler/siliconcompiler-0.11.2-cp37-cp37m-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/siliconcompiler/tools/openroad/physyn.py
@@ -0,0 +1,29 @@
+
+from siliconcompiler.tools.openroad.openroad import setup as setup_tool
+from siliconcompiler.tools.openroad.openroad import build_pex_corners
+from siliconcompiler.tools.openroad.openroad import post_process as or_post_process
+
+
+def setup(chip):
+    '''
+    Not implemented yet
+    '''
+
+    # Generic tool setup.
+    setup_tool(chip)
+
+    tool = 'openroad'
+    design = chip.top()
+    step = chip.get('arg', 'step')
+    index = chip.get('arg', 'index')
+    task = chip._get_task(step, index)
+
+    chip.add('tool', tool, 'task', task, 'input', design + '.def', step=step, index=index)
+
+
+def pre_process(chip):
+    build_pex_corners(chip)
+
+
+def post_process(chip):
+    or_post_process(chip)
