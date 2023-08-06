f0aad07fc689b7982b0c153f5cca8b81aca399af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:17:50 +0000

    Add package wwsimulator/1.0.0/wwsimulator-1.0.0.tar.gz

---
+++ packages/wwsimulator/wwsimulator-1.0.0.tar.gz/wwsimulator-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="wwsimulator",
+    version="1.0.0",
+    packages=["C:/Users/HP/Desktop/WWSimulator"],
+    install_requires=[
+        # Liste der Abhängigkeiten deiner Bibliothek
+    ],
+    # weitere Optionen hier
+    
+)
