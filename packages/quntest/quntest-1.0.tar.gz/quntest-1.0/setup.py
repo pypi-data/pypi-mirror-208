80e3f4673c2f051bb727483e301924478a418ba3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:00 +0000

    Add package quntest/1.0/quntest-1.0.tar.gz

---
+++ packages/quntest/quntest-1.0.tar.gz/quntest-1.0/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="quntest",
+    version=1.0,
+    long_description="come back to this in a second",
+    py_modules=["pdf2image", "pdf2text"],
+    package_dir={'': 'src'},
+)
