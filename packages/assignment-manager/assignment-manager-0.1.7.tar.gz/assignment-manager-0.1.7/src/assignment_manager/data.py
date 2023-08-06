dc4b72bca502a55a7fbc31a2bb653f2d6e16ea3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:47:04 +0000

    Add package assignment-manager/0.1.7/assignment-manager-0.1.7.tar.gz

---
+++ packages/assignment-manager/assignment-manager-0.1.7.tar.gz/assignment-manager-0.1.7/src/assignment_manager/data.py
@@ -0,0 +1,93 @@
+import os
+import shutil
+from pathlib import Path
+import json
+from enum import IntEnum
+
+
+class Progress(IntEnum):
+    NOT_STARTED = 0
+    JUST_STARTED = 1
+    IN_PROGRESS = 2
+    ALMOST_DONE = 3
+    DONE = 4
+
+
+def is_file(path):
+    _, extension = os.path.splitext(path)
+    return extension != ""
+
+
+def file_empty(path):
+    return os.stat(path).st_size == 0
+
+
+def data_file_empty():
+    return file_empty(get_data_file_path())
+
+
+def backup_file_empty():
+    return file_empty(get_backup_data_file_path())
+
+
+def create_path(path):
+    if is_file(path):
+        if not os.path.exists(os.path.dirname(path)):
+            os.makedirs(os.path.dirname(path))
+        Path(path).touch()
+    else:
+        os.makedirs(path)
+
+
+def join(*args):
+    path = os.path.join(*args)
+    if not os.path.exists(path):
+        create_path(path)
+    return path
+
+
+def get_config_dir():
+    return join(os.path.expanduser("~"), ".config", "assignment-manager")
+
+
+def get_data_dir():
+    return join(get_config_dir(), "data")
+
+
+def get_backup_dir():
+    return join(get_data_dir(), "backup")
+
+
+def get_data_file_path():
+    return join(get_data_dir(), "data.json")
+
+
+def get_backup_data_file_path():
+    return join(get_backup_dir(), "data.json")
+
+
+def load_data():
+    if data_file_empty():
+        raise EOFError(
+            (
+                "The data file is empty. "
+                "You should first add assignments before accessing them."
+            ),
+            get_data_file_path(),
+        )
+    with open(get_data_file_path(), "r") as f:
+        data = json.load(f)
+    return data
+
+
+def write_data(data):
+    with open(get_data_file_path(), "w") as f:
+        json.dump(data, f, indent=4)
+
+
+def copy_backup():
+    shutil.copyfile(get_data_file_path(), get_backup_data_file_path())
+
+
+def paste_backup():
+    shutil.copyfile(get_backup_data_file_path(), get_data_file_path())
---
+++ packages/assignment-manager/assignment-manager-0.1.7.tar.gz/assignment-manager-0.1.7/src/assignment_manager/io.py
@@ -0,0 +1,100 @@
+from rich import print
+from rich.table import Table as RichTable
+import inquirer
+
+
+def print_table(table_headers, table_rows):
+    if len(table_headers) != len(table_rows[0]):
+        raise ValueError(
+            "The length of the given lists must match",
+            table_headers,
+            table_rows,
+        )
+
+    table = RichTable(*table_headers)
+    for row in table_rows:
+        table.add_row(*row)
+    print(table)
+
+
+def print_msg(msg):
+    print(msg)
+
+
+def confirmation_prompt(msg=""):
+    if msg != "":
+        print(msg)
+
+    if binary_prompt("Are you sure you want to continue?"):
+        return True
+    print("[bold]Aborting...[/bold]")
+    return False
+
+
+def add_assignment_response():
+    questions = [
+        inquirer.Text("name", message="Name of course"),
+        inquirer.Text("start_date", message="First published date"),
+        inquirer.Text("end_date", message="When is the deadline"),
+        inquirer.Text(
+            "torus", message="How many days until the next assignment (torus)"
+        ),
+        inquirer.Text("amount", message="Number of total assignments"),
+    ]
+    answers = inquirer.prompt(questions)
+    return (
+        answers["name"],
+        answers["start_date"],
+        answers["end_date"],
+        int(answers["torus"]),
+        int(answers["amount"]),
+    )
+
+
+def binary_prompt(msg):
+    question = [
+        inquirer.List(
+            "answer",
+            message=msg,
+            choices=["Yes", "No"],
+        )
+    ]
+    if inquirer.prompt(question)["answer"] == "Yes":
+        return True
+    return False
+
+
+def list_prompt(choices, msg):
+    question = [
+        inquirer.List(
+            "answer",
+            message=msg,
+            choices=choices,
+        ),
+    ]
+    return inquirer.prompt(question)["answer"]
+
+
+def update_assignment_response(cycle_choices, progress_choices):
+    questions = [
+        inquirer.List(
+            "cycle",
+            message="Which cycle do you want to update?",
+            choices=cycle_choices,
+        ),
+        inquirer.List(
+            "progress", message="New progress status", choices=progress_choices
+        ),
+    ]
+    answers = inquirer.prompt(questions)
+    cycle = cycle_choices.index(answers["cycle"])
+    progress = progress_choices.index(answers["progress"])
+
+    return (cycle, progress)
+
+
+def rename_assignment_response():
+    question = [
+        inquirer.Text("name", message="What's the new name of the course?")
+    ]
+    return inquirer.prompt(question)["name"]
