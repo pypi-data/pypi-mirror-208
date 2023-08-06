ac113a8144480e712d3c76cbc57849b3c82e493f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:29:03 +0000

    Add package pynamer/1.2.0/pynamer-1.2.0.tar.gz

---
+++ packages/pynamer/pynamer-1.2.0.tar.gz/pynamer-1.2.0/tests/test_main TBC.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# Core Library modules
+from pathlib import Path
+
+# First party modules
+from pynamer import pynamer
+
+BASE_DIR = Path(__file__).parents[0]
+
+
+def test_main(monkeypatch):
+    pass
---
+++ packages/pynamer/pynamer-1.2.0.tar.gz/pynamer-1.2.0/tests/test_pypi_search_index.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# Core Library modules
+from pathlib import Path
+
+# First party modules
+from pynamer import pynamer
+
+BASE_DIR = Path(__file__).parents[0]
+
+
+def test_pypi_search_index(monkeypatch):
+    monkeypatch.setattr(pynamer, "project_path", BASE_DIR / "resources")
+    assert pynamer._pypi_search_index("pynball") is True
+    assert pynamer._pypi_search_index("zeedonk") is False
