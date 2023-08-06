7112b14a3418d8706bde210d9d771396c278e72d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:11:54 +0000

    Add package lifx-cli/0.68.0/lifx_cli-0.68.0-py3-none-any.whl

---
+++ packages/lifx-cli/lifx_cli-0.68.0-py3-none-any.whl/src/lifx/scenes.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+class Scenes:
+
+    def __init__(self):
+        from src.lifx.auth import Auth
+
+        self.auth = Auth()
+        self.auth_headers = self.auth.auth()
+
+    def get(self):
+        """Print a list of all scenes on this account."""
+        import json
+        from requests import get
+        from tabulate import tabulate
+
+        response = get('https://api.lifx.com/v1/scenes', headers=self.auth_headers)
+        response = json.loads(response.content)
+        
+        scenes = []
+        
+        for key in range(0, len(response)):
+            scenes += [[response[key]["name"], response[key]["uuid"]]]
+        
+        scenes.sort()
+        print(tabulate(scenes, headers=["Name", "ID"]))
+
+    def activate(self, scene_id):
+        """Activates the specified scene. Requires scene UUID."""
+        from requests import put
+
+        put(f'https://api.lifx.com/v1/scenes/scene_id:{scene_id}/activate', headers=self.auth_headers)
