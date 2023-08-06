521fe6374db71d49c2f54c6e3a71f73b159b0418
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:51 +0000

    Add package serial-deserial-lib/0.1/serial_deserial_lib-0.1.tar.gz

---
+++ packages/serial-deserial-lib/serial_deserial_lib-0.1.tar.gz/serial_deserial_lib-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='serial_deserial_lib',
+      version='0.1',
+      description='Serialisation and deserialoisation class',
+      packages=['serial_deserial_lib'],
+      author_email='zhagorov2004@gmail.com',
+      zip_safe=False
+)
