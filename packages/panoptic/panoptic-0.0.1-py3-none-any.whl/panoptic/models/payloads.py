5cf5cd1e1591423db3252be09e695570a0ae50b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:34 +0000

    Add package panoptic/0.0.1/panoptic-0.0.1-py3-none-any.whl

---
+++ packages/panoptic/panoptic-0.0.1-py3-none-any.whl/panoptic/models/payloads.py
@@ -0,0 +1,44 @@
+from typing import Optional, Any
+from fastapi_camelcase import CamelModel
+
+from .models import PropertyType, JSON
+
+
+class ImagePayload(CamelModel):
+    file_path: str
+
+
+class PropertyPayload(CamelModel):
+    name: str
+    type: PropertyType
+
+
+class UpdatePropertyPayload(CamelModel):
+    id: int
+    name: Optional[str]
+    type: Optional[PropertyType]
+
+
+class AddImagePropertyPayload(CamelModel):
+    property_id: int
+    sha1: str
+    value: Any
+
+
+class DeleteImagePropertyPayload(CamelModel):
+    property_id: int
+    sha1: str
+
+
+class AddTagPayload(CamelModel):
+    property_id: int
+    value: str
+    parent_id: Optional[int] = None
+    color: Optional[str]
+
+
+class UpdateTagPayload(CamelModel):
+    id: int
+    value: Optional[str]
+    parent_id: Optional[int]
+    color: Optional[str]
