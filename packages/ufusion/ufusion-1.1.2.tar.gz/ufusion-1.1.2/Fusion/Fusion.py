7acad1aafa75f3f486e43c9f011c74c3cf35b364
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:06 +0000

    Add package ufusion/1.1.2/ufusion-1.1.2.tar.gz

---
+++ packages/ufusion/ufusion-1.1.2.tar.gz/ufusion-1.1.2/Fusion/Fusion.py
@@ -0,0 +1,32 @@
+from .ui.menu import Menu
+from .download.downloader import Downloader
+from .Packages.packager import Packager
+import json
+class Fusion:
+    def __init__(self):
+        version = self.get_version()
+        logo = f"""
+       ______           _                
+      / ____/_  _______(_)___  ____      
+     / /_  / / / / ___/ / __ \/ __ \     
+    / __/ / /_/ (__  ) / /_/ / / / /     
+   /_/    \__,_/____/_/\____/_/ /_/      
+        powered by f09l
+        version {version}                               
+        """                        
+            
+        main_menu = Menu([
+            ["Downloader", lambda: Downloader(main_menu=main_menu)],
+            ["Packages", lambda: Packager(main_menu=main_menu)],
+            ["Exit", lambda:exit()]
+        ],logo=logo)
+
+        # Start the main menu
+        main_menu.start()
+    def get_version(self):
+        try:
+            with open("version.json") as f:
+                    return json.load(f)["version"]
+        except:
+             return ""
+Fusion()
