8006c2b6d04be7398bf031ca5999ebd22e481117
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:41:29 +0000

    Add package pydivsufsort/0.0.9/pydivsufsort-0.0.9-cp310-cp310-win32.whl

---
+++ packages/pydivsufsort/pydivsufsort-0.0.9-cp310-cp310-win32.whl/pydivsufsort/__init__.py
@@ -0,0 +1,24 @@
+from .divsufsort import divsufsort, bw_transform, inverse_bw_transform, sa_search
+from .stringalg import (
+    kasai,
+    lcp_segtree,
+    lcp_query,
+    levenshtein,
+    most_frequent_substrings,
+)
+from .wonderstring import WonderString, common_substrings
+
+
+__all__ = [
+    "divsufsort",
+    "bw_transform",
+    "inverse_bw_transform",
+    "sa_search",
+    "kasai",
+    "lcp_segtree",
+    "lcp_query",
+    "levenshtein",
+    "most_frequent_substrings",
+    "WonderString",
+    "common_substrings",
+]
