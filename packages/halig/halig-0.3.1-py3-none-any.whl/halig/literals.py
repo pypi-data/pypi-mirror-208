e5c2a5426bec620f86bdd9cc79b3c3e805f66e0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:46 +0000

    Add package halig/0.3.1/halig-0.3.1-py3-none-any.whl

---
+++ packages/halig/halig-0.3.1-py3-none-any.whl/halig/literals.py
@@ -0,0 +1,27 @@
+# COMMANDS
+COMMANDS_NOTEBOOKS_HELP = "List all notebooks and notes, tree-style"
+COMMANDS_EDIT_HELP = "Edit or add a note into a notebook"
+COMMANDS_SHOW_HELP = "Show a note's contents"
+COMMANDS_VERSION = "Show halig's version"
+
+# OPTIONS
+OPTION_CONFIG_HELP = "Configuration file. Must be YAML and schema compatible"
+OPTION_LEVEL_HELP = (
+    "Tree max recursion level; negative numbers indicate a value of infinity"
+)
+
+# ARGUMENTS
+ARGUMENT_EDIT_NOTE_HELP = """A valid, settings-relative path.
+Be aware that valid can also mean implicit notes, that is, pointing to a
+current-day note just by its notebook name. For example, if today is
+2023-04-04 and you have a notebook containing a 2023-04-04.age note,
+simply pointing to the notebook's name, e.g. `halig edit notebook` will
+edit the 2023-04-04.age note. Also keep in mind that the note may or may
+not exist and it'll be created accordingly; the only requirement is that
+the notebook folder structure is correct and exists"""
+ARGUMENT_SHOW_NOTE_HELP = """A valid, settings-relative path.
+Be aware that valid can also mean implicit notes, that is, pointing to a
+current-day note just by its notebook name. For example, if today is
+2023-04-04 and you have a notebook containing a 2023-04-04.age note,
+simply pointing to the notebook's name, e.g. `halig show notebook` will
+print the 2023-04-04.age note"""
