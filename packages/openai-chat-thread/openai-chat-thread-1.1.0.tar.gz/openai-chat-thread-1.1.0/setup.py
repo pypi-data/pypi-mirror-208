12e8bd41a663cdbb086c5f2c44c947a853d90704
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:10:09 +0000

    Add package openai-chat-thread/1.1.0/openai-chat-thread-1.1.0.tar.gz

---
+++ packages/openai-chat-thread/openai-chat-thread-1.1.0.tar.gz/openai-chat-thread-1.1.0/setup.py
@@ -0,0 +1,19 @@
+
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = [line.strip() for line in f.readlines()]
+
+setup(
+    name="openai-chat-thread",
+    version="1.1.0",
+    packages=find_packages(),
+    py_modules=['openai_chat_thread'],
+    install_requires=requirements,
+    entry_points={
+        'console_scripts': [
+            'openai_chat_thread = openai_chat_thread:main',
+        ],
+    },
+    long_description=open('README.md', 'r').read(),
+    long_description_content_type='text/markdown',)
