a29b6777a7063d2a11090fe842a91e30d2cc71f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:49:58 +0000

    Add package packageshareque/0.2/packageshareque-0.2.tar.gz

---
+++ packages/packageshareque/packageshareque-0.2.tar.gz/packageshareque-0.2/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name="packageshareque",
+      version="0.2",
+      description="Shareque vision2 Library",
+      long_description="This is the Calculator Library",
+      author="shareque",
+      packages=["packageshareque"],
+      install_required = []
+
+      )
