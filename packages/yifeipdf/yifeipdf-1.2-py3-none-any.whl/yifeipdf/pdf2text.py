d9838b3cf8b078bd92d16ed840725f07e172a1eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:41 +0000

    Add package yifeipdf/1.2/yifeipdf-1.2-py3-none-any.whl

---
+++ packages/yifeipdf/yifeipdf-1.2-py3-none-any.whl/yifeipdf/pdf2text.py
@@ -0,0 +1,18 @@
+# """ This module provides functions to convert a PDF to teA""" """ À """ """xt
+
+# """
+class convertor:
+    """ A simple converter for converting PDFs to text. """
+
+    def convert(self,path):
+        """
+        Convert the given PDF to text. 
+        
+        Parameters:
+        path(str): The path to a PDF file.
+
+        Returns:
+        str: The content of the PDF file as text.
+        """
+
+    print("pdf2text")
