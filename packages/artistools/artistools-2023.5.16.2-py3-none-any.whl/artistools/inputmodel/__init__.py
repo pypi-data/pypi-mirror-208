bd8691e68b9ab909ea94af6de3eb1cfcf49544d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:56:41 +0000

    Add package artistools/2023.5.16.2/artistools-2023.5.16.2-py3-none-any.whl

---
+++ packages/artistools/artistools-2023.5.16.2-py3-none-any.whl/artistools/inputmodel/__init__.py
@@ -0,0 +1,26 @@
+import artistools.inputmodel.botyanski2017
+import artistools.inputmodel.describeinputmodel
+import artistools.inputmodel.downscale3dgrid
+import artistools.inputmodel.energyinputfiles
+import artistools.inputmodel.makeartismodel
+import artistools.inputmodel.maketardismodelfromartis
+import artistools.inputmodel.modelfromhydro
+import artistools.inputmodel.opacityinputfile
+import artistools.inputmodel.rprocess_from_trajectory
+
+from .inputmodel_misc import add_derived_cols_to_modeldata
+from .inputmodel_misc import get_2d_modeldata
+from .inputmodel_misc import get_3d_model_data_merged_model_and_abundances_minimal
+from .inputmodel_misc import get_3d_modeldata_minimal
+from .inputmodel_misc import get_cell_angle
+from .inputmodel_misc import get_dfmodel_dimensions
+from .inputmodel_misc import get_initelemabundances
+from .inputmodel_misc import get_mean_cell_properties_of_angle_bin
+from .inputmodel_misc import get_mgi_of_velocity_kms
+from .inputmodel_misc import get_modeldata
+from .inputmodel_misc import get_modeldata_tuple
+from .inputmodel_misc import save_empty_abundance_file
+from .inputmodel_misc import save_initelemabundances
+from .inputmodel_misc import save_modeldata
+from .inputmodel_misc import scale_model_to_time
+from .inputmodel_misc import sphericalaverage
