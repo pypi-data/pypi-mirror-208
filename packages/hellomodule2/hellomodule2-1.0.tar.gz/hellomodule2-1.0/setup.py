3fa488ebbc6688667e65745841b2efeaa5fc80b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:59 +0000

    Add package hellomodule2/1.0/hellomodule2-1.0.tar.gz

---
+++ packages/hellomodule2/hellomodule2-1.0.tar.gz/hellomodule2-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup, find_packages
+
+setup(name='hellomodule2',
+      version='1.0',
+      author='Karol Polit',
+      author_email='256438@student.pwr.edu.pl',
+      packages=find_packages(),
+      )
