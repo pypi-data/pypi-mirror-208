2f1dea37c1d346aec3e5c0004d30727d029343b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:13 +0000

    Add package landingai/0.0.11/landingai-0.0.11-py3-none-any.whl

---
+++ packages/landingai/landingai-0.0.11-py3-none-any.whl/landingai/io.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+
+def read_image() -> np.ndarray:
+    pass
+
+
+def sample_images(
+    video_file: str, output_path: str, sample_rate: float = None
+) -> list[str]:
+    pass
