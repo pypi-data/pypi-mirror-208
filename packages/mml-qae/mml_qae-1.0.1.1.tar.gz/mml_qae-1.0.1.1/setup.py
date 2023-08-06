063786d25648c8ecfc6b3581d213dd8e7dc3d798
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:06:41 +0000

    Add package mml-qae/1.0.1.1/mml_qae-1.0.1.1.tar.gz

---
+++ packages/mml-qae/mml_qae-1.0.1.1.tar.gz/mml_qae-1.0.1.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools 
+setuptools.setup(
+      name='mml_qae',
+      version='1.0.1.1',
+      description='The packages for the machine learning(just easily learn!) are always hard to study, and difficult to use. Now you have the MyMachineLearning_Quicker_and_Easier! It can maybe help you to use machine learning.',
+      long_description="We now have packages to deal with big data like statsmodels, sklearn, Spark, or packages to deal with nature language, such as jieba or spaCy. I admit that those packages are really useful while facing whith tons of csv data. BUT! I think we need a nicer package to deal with data, don't we? So, the mml-qae package(the whole name is My Machine Learning-Quicker and Easier) is here to help you. Alright, so, I have to say that I didn't do anything new, but I put those complex codes together into grand-new methods, so that you will be able to deal with MachineLearning and DataDealing. I started this project on 14th, May 2023 with version 1.0.1. I promise to update it regularly, but I can't promise how long the interval between two updataions. Another thing need to be attentioned, this package only works at Python3.7.2, and you MUST pip install the packages needed in mml, like statsmodels.I will tell all of those packages you need to preinstall in README.md. Then have a new day with MachineLearning and DataDealing!",
+      author='ETRO_secondleader',
+      author_email='ETRO_gfyx@163.com',
+      url='https://www.python.org', 
+      packages=setuptools.find_packages(),
+     ) 
