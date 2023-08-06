02e0c6c25b53fed5e463bd725039222f795c3f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:59:57 +0000

    Add package ironflow/0.0.7/ironflow-0.0.7.tar.gz

---
+++ packages/ironflow/ironflow-0.0.7.tar.gz/ironflow-0.0.7/setup.py
@@ -0,0 +1,48 @@
+"""
+Setuptools based setup module
+"""
+from setuptools import setup, find_packages
+import versioneer
+
+setup(
+    name='ironflow',
+    version=versioneer.get_version(),
+    description='ironflow - A visual scripting interface for pyiron.',
+    long_description='Ironflow combines ryven, ipycanvas and ipywidgets to provide a Jupyter-based visual scripting '
+                     'gui for running pyiron workflow graphs.',
+
+    url='https://github.com/pyiron/ironflow',
+    author='Max-Planck-Institut für Eisenforschung GmbH - Computational Materials Design (CM) Department',
+    author_email='liamhuber@greyhavensolutions.com',
+    license='BSD',
+
+    classifiers=['Development Status :: 3 - Alpha',
+                 'Topic :: Scientific/Engineering :: Physics',
+                 'License :: OSI Approved :: BSD License',
+                 'Intended Audience :: Science/Research',
+                 'Operating System :: OS Independent',
+                 'Programming Language :: Python :: 3.8',
+                 'Programming Language :: Python :: 3.9',
+                 'Programming Language :: Python :: 3.10'],
+
+    keywords='pyiron',
+    packages=find_packages(exclude=["*tests*", "*docs*", "*binder*", "*conda*", "*notebooks*", "*.ci_support*"]),
+    install_requires=[
+        'ipycanvas',
+        'ipython',
+        'ipywidgets >= 7,< 8',
+        'matplotlib',
+        'nglview',
+        'numpy',
+        'owlready2',
+        'pyiron_base',
+        'pyiron_atomistics',
+        'pyiron_gui >= 0.0.8',
+        'pyiron_ontology >= 0.1.3',
+        'ryvencore',
+        'seaborn',
+        'traitlets',
+    ],
+    cmdclass=versioneer.get_cmdclass(),
+
+    )
