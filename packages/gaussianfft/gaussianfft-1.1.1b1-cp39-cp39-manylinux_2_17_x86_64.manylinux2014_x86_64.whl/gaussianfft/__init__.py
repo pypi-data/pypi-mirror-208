da8579c2828e6525a76b53edcf1621472d77c988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:37 +0000

    Add package gaussianfft/1.1.1b1/gaussianfft-1.1.1b1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/gaussianfft/gaussianfft-1.1.1b1-cp39-cp39-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/gaussianfft/__init__.py
@@ -0,0 +1,36 @@
+from enum import Enum
+
+try:
+    # When using gaussianFFT in conjunction with Aspen RMS (former Emerson / Roxar)
+    # you could experience a segmentation fault if gaussianfft was imported before
+    # the roxar module.
+    import roxar
+except ImportError:
+    pass
+
+import _gaussianfft
+
+from _gaussianfft import *
+
+
+class VariogramType(Enum):
+    GAUSSIAN = 'gaussian'
+    EXPONENTIAL = 'exponential'
+    GENERAL_EXPONENTIAL = 'general_exponential'
+    SPHERICAL = 'spherical'
+    MATERN_32 = 'matern32'
+    MATERN_52 = 'matern52'
+    MATERN_72 = 'matern72'
+    CONSTANT = 'constant'
+
+
+def variogram(type, *args, **kwargs):
+    if isinstance(type, Enum):
+        type = type.value
+    return _gaussianfft.variogram(type, *args, **kwargs)
+
+
+__all__ = [
+    'variogram', 'simulate', 'seed', 'advanced', 'simulation_size',
+    'quote', 'Variogram', 'VariogramType', 'util', 'SizeTVector', 'DoubleVector',
+]
