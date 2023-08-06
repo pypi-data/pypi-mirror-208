80a29971088eacc10371da5b14f0e93891860bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:55 +0000

    Add package secretmessage/0.3/secretmessage-0.3.tar.gz

---
+++ packages/secretmessage/secretmessage-0.3.tar.gz/secretmessage-0.3/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+setuptools.setup(
+
+	name = 'secretmessage', 
+
+	version='0.3',
+	author = "E Lusifa Taehyung",
+
+	author_email = "purplebird7613@gmail.com",
+
+	description = "For generating Messages Secretly.",
+  
+	
+	packages = setuptools.find_packages(),
+ classifiers=[
+
+    "Programming Language :: Python",
+
+    "License :: OSI Approved :: MIT License",
+
+    "Operating System :: OS Independent",
+
+    ],
+
+ )
