c0cacc1deb66990512d6f1eb2265d7715a86a6ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:39 +0000

    Add package pywayne/1.0.0.3.9/pywayne-1.0.0.3.9.tar.gz

---
+++ packages/pywayne/pywayne-1.0.0.3.9.tar.gz/pywayne-1.0.0.3.9/pywayne/__init__.py
@@ -0,0 +1,14 @@
+# from . import tools, tt_api
+
+__all__ = [
+    'tools',
+    'dsp',
+    'gui',
+    'math',
+    'data_structure',
+    'plot',
+    'tt_api',
+    'ocs_api',
+]
+
+name = 'pywayne'
