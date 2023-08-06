13ca2e42f8f04c0627c96abbf3ae8e12c24cfb5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:11:35 +0000

    Add package collecting-faces/1.0.3/collecting_faces-1.0.3.tar.gz

---
+++ packages/collecting-faces/collecting_faces-1.0.3.tar.gz/collecting_faces-1.0.3/collecting_faces/preprocess/unpack_bz2.py
@@ -0,0 +1,7 @@
+import bz2
+def unpack_bz2(src_path):
+    data = bz2.BZ2File(src_path).read()
+    dst_path = src_path[:-4]
+    with open(dst_path, 'wb') as fp:
+        fp.write(data)
+    return dst_path
