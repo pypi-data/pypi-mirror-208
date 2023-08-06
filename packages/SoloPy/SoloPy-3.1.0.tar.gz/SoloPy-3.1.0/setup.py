6b37e32c44d9da7c920c2f667a8eb6d51eff93da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:26 +0000

    Add package SoloPy/3.1.0/SoloPy-3.1.0.tar.gz

---
+++ packages/SoloPy/SoloPy-3.1.0.tar.gz/SoloPy-3.1.0/setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+setup(
+  name = 'SoloPy',        
+  packages = ['SoloPy'],  
+  version = 'v3.1.0',      
+  license='gpl-3.0',      
+  description = 'PYTHON Library for Control Solo Motor',   
+  author = 'Solo',                  
+  author_email = 'support@solomotorcontrollers.com',      
+  url = 'https://github.com/Solo-FL/SoloPy',   
+  download_url = 'https://github.com/Solo-FL/SoloPy/archive/refs/tags/v3.1.0.tar.gz',    
+  keywords = ['Solo Motor controllers', 'Solo', 'SoloPy'],   
+  install_requires=[           
+          'pyserial',
+          'python-can',
+          'python-interface'
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',      
+    'Intended Audience :: Developers',    
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',   
+    'Programming Language :: Python :: 3',      
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
