077b9975232ce7ce7b55eb168250c97182953297
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:11:16 +0000

    Add package isqlab/0.3.4/isqlab-0.3.4.tar.gz

---
+++ packages/isqlab/isqlab-0.3.4.tar.gz/isqlab-0.3.4/setup.py
@@ -0,0 +1,30 @@
+# This code is part of isQ.
+# (C) Copyright ArcLight Quantum 2023.
+# This code is licensed under the MIT License.
+
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r", encoding="utf-8") as readme:
+    long_description = readme.read()
+
+requirements = [
+    # "isqopen",
+    # "pyscf",
+    # "openfermion",
+    # "openfermionpyscf",
+]
+
+setup(
+    name="isqlab",
+    version="0.3.4",
+    description="application modules for isq",
+    platforms="python 3.8+",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Yusheng Yang",
+    author_email="yangys@arclightquantum.com",
+    packages=find_packages(where="."),
+    package_data={"": ["*.txt"]},
+    install_requires=requirements,
+)
