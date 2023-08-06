53ee863241fdf18e388bdf2f37f31cc3e852cae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:45:06 +0000

    Add package maya-mock-completion/0.0.1/maya_mock_completion-0.0.1.tar.gz

---
+++ packages/maya-mock-completion/maya_mock_completion-0.0.1.tar.gz/maya_mock_completion-0.0.1/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+with open("./README.md") as f:
+    long_description = f.read()
+    
+setup(
+    name="maya_mock_completion",
+    version="0.0.1",
+    description="A mock version of the Autodesk maya libraries to run code intended for maya using a regular Python "
+                "from a virtual environment.",
+    package_dir={
+        "": ".",
+    },
+    packages=find_packages(),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mrbmp33/maya_mock_completion",
+    author="Bernat Mas Pastor",
+    author_email="bmp33@duck.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.7",
+        "Operating System :: OS Independent"
+    ],
+    extras_require={
+        "dev": "twine>=4.0.2"
+    },
+    python_requires=">=3.7"
+)
