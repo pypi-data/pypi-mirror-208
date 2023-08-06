a86c7ac325a0a8c771ee4259b8eb66f4939a3e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:14:14 +0000

    Add package ntheory/0.0.7/ntheory-0.0.7.tar.gz

---
+++ packages/ntheory/ntheory-0.0.7.tar.gz/ntheory-0.0.7/setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='ntheory',
+    packages=find_packages(include=['ntheory']),
+    version='0.1.0',
+    description='My first Python library',
+    author='Kasper Arfman',
+    license='MIT',
+    install_requires=[
+        'numpy==1.24.3',
+    ],
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest==7.3.1'],
+    test_suite='tests',
+)
