8c47e5f465c152ab41f303f1857b05823f795dff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 12:09:41 +0000

    Add package netsim-tools/1.5.3/netsim-tools-1.5.3.tar.gz

---
+++ packages/netsim-tools/netsim-tools-1.5.3.tar.gz/netsim-tools-1.5.3/setup.py
@@ -0,0 +1,40 @@
+""" redirect netsim-tools to networklab """
+import sys
+from setuptools import setup, find_packages
+
+sys.path.append('..')
+
+version="1.5.3"
+
+setup(
+  name="netsim-tools",
+  version=version,
+  packages=[],
+  author="Ivan Pepelnjak",
+  author_email="ip@ipspace.net",
+  description="CLI-based Virtual Networking Lab Abstraction Layer",
+  install_requires=[f"networklab>={version}"],
+  classifiers=[
+    "Topic :: Utilities",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3.9",
+    "Operating System :: POSIX :: Linux",
+    "Operating System :: MacOS",
+  ],
+  url="https://github.com/ipspace/netlab",
+  python_requires='>=3.7',  # Due to e.g. 'capture_output' in subprocess.run
+)
+
+print("""
+=======================================================================
+                          WARNING WARNING WARNING
+=======================================================================
+netsim-tools has been renamed to netlab in August 2022.
+
+The Python package netsim-tools has been renamed to networklab and is
+installed as a dependency of netsim-tools every time you install or
+upgrade netsim-tools, but we won't keep that dependency active foreer.
+
+Please stop using netsim-tools package and use networklab package.
+""")
