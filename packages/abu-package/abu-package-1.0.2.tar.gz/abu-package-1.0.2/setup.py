343a61aed8589595c46d0c69856107715d96e8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:20:50 +0000

    Add package abu-package/1.0.2/abu-package-1.0.2.tar.gz

---
+++ packages/abu-package/abu-package-1.0.2.tar.gz/abu-package-1.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='abu-package',
+    version='1.0.2',
+    author='Junaid Afzal',
+    description='A life changing function.',
+    py_modules=['hang'],
+)
