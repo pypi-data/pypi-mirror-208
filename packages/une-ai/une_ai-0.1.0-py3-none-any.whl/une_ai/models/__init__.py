ef5addfd4918245927af6d45f1abe3af368c01be
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:20:21 +0000

    Add package une-ai/0.1.0/une_ai-0.1.0-py3-none-any.whl

---
+++ packages/une-ai/une_ai-0.1.0-py3-none-any.whl/une_ai/models/__init__.py
@@ -0,0 +1,3 @@
+from .agent import Agent
+from .environment import Environment
+from .grid_map import GridMap
---
+++ packages/une-ai/une_ai-0.1.0-py3-none-any.whl/une_ai/models/environment.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+
+class Environment(ABC):
+
+    def __init__(self, environment_name):
+        self._name = environment_name
+    
+    def get_environment_name(self):
+        return self._name
+    
+    @abstractmethod
+    def get_percepts(self, agent):
+        pass
