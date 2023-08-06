a894b52326324df2fdce315ca84e703cac23ff91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:48 +0000

    Add package cellfinder-core/0.4.0/cellfinder_core-0.4.0-py3-none-any.whl

---
+++ packages/cellfinder-core/cellfinder_core-0.4.0-py3-none-any.whl/cellfinder_core/__init__.py
@@ -0,0 +1,7 @@
+__version__ = "0.3.1"
+__author__ = "Adam Tyson, Christian Niedworok, Charly Rousseau"
+__license__ = "BSD-3-Clause"
+
+import logging
+
+logger = logging.getLogger("cellfinder_core")
