a4ad83caf21344d0861bfc39eeb9a5e21f35579d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:14:17 +0000

    Add package testrail-api-reporter/1.3.0/testrail_api_reporter-1.3.0.tar.gz

---
+++ packages/testrail-api-reporter/testrail_api_reporter-1.3.0.tar.gz/testrail_api_reporter-1.3.0/src/testrail_api_reporter/testrail_api_reporter/publishers/__init__.py
@@ -0,0 +1,5 @@
+# Publishers
+from .confluence_sender import ConfluenceSender
+from .email_sender import EmailSender
+from .slack_sender import SlackSender
+from .gdrive_uploader import GoogleDriveUploader
