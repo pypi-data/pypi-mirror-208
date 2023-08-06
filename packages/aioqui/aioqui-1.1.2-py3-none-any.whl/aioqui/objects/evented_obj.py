bed9956bf5151d09492ba71379bb480d82bff6ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:07 +0000

    Add package aioqui/1.1.2/aioqui-1.1.2-py3-none-any.whl

---
+++ packages/aioqui/aioqui-1.1.2-py3-none-any.whl/aioqui/objects/evented_obj.py
@@ -0,0 +1,82 @@
+from PySide6.QtCore import QObject
+from PySide6.QtGui import QResizeEvent
+from loguru import logger
+from contextlib import suppress
+from typing import Any
+from PySide6.QtWidgets import (
+    QPushButton, QFrame, QLabel, QLineEdit, QTextEdit, QStackedWidget, QComboBox
+)
+
+from ..types import Applicable, Event
+from ..qasyncio import asyncSlot
+
+
+class EventedObj:
+    class Emitter:
+        def __init__(self):
+            super().__init__()
+            self.emitter = QPushButton()
+
+        def emit(self, event: Event) -> Any:
+            EventedObj.connect(self.emitter, 'clicked', event)
+            self.emitter.click()
+
+    EMITTER = Emitter()
+
+    @staticmethod
+    def Events(
+            *,
+            on_click: Event = None,
+            on_change: Event = None,
+            on_resize: Event = None,
+            on_close: Event = None
+    ) -> Applicable:
+        async def apply(obj):
+            if on_click:
+                if isinstance(obj, QPushButton):
+                    await EventedObj.connect(obj, 'clicked', on_click)
+                elif isinstance(obj, (QLabel, QFrame)):
+                    obj.mousePressEvent = lambda event: EventedObj.emit(on_click)
+                else:
+                    EventedObj._error(obj, 'on_click')
+
+            if on_change:
+                if isinstance(obj, (QLineEdit, QTextEdit)):
+                    await EventedObj.connect(obj, 'textChanged', on_change)
+                elif isinstance(obj, QStackedWidget):
+                    await EventedObj.connect(obj, 'currentChanged', on_change)
+                elif isinstance(obj, QComboBox):
+                    await EventedObj.connect(obj, 'currentTextChanged', on_change)
+                else:
+                    EventedObj._error(obj, 'on_change')
+
+            if on_close:
+                @asyncSlot
+                async def close(self) -> bool:
+                    await on_close()
+                    return self.close()
+                obj.close = lambda: EventedObj.emit(close)
+
+            if on_resize:
+                async def resizeEvent(self, event: QResizeEvent) -> None:
+                    self.resizeEvent(self, event)
+                    await on_resize()
+                obj.resizeEvent = lambda event: EventedObj.emit(lambda: resizeEvent(event))
+
+            return obj
+        return apply
+
+    @staticmethod
+    async def connect(obj: QObject, signalname: str, event: Event):
+        signal = getattr(obj, signalname)
+        with suppress(Exception):
+            signal.disconnect()
+        signal.connect(event)
+
+    @staticmethod
+    async def emit(event: Event) -> None:
+        EventedObj.EMITTER.emit(event)
+
+    @staticmethod
+    def _error(obj: QObject, event: str) -> None:
+        logger.error(f'event `{event}` is not implemented for `{obj.objectName()}\'s` type: {type(obj)}')
