078568c0d14c0e4ccae5d7702c5483e2348b695f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:10:45 +0000

    Add package pyglowmarkt/0.5.6/pyglowmarkt-0.5.6.tar.gz

---
+++ packages/pyglowmarkt/pyglowmarkt-0.5.6.tar.gz/pyglowmarkt-0.5.6/README.md
@@ -0,0 +1,271 @@
+
+# pyglowmarkt
+
+## Introduction
+
+Python API to the Bright/Glowmarkt/Hildebrand API for energy consumption.
+There is a python API and a command-line script.
+
+For instructions on use with Home Assistant, see
+https://github.com/danmed/Glow2MQTT
+
+## Install
+
+```
+pip3 install pyglowmarkt
+```
+
+## API example usage
+
+### Connect
+
+You need an account from https://glowmarkt.com/
+
+```
+from glowmarkt import *
+
+cli = BrightClient("myusername@example.org", "MyP4ssword!")
+
+```
+
+### Discover virtual entities and resources
+
+A virtual entity is e.g. your Glowmarkt device or SMETS2 smart meter.
+A virtual entity has multiple resource e.g.
+- Electricity consumption
+- Electricity cost
+- Gas consumption
+- Gas cost
+
+```
+ents = cli.get_virtual_entities()
+
+for ent in ents:
+    print("Entity:", ent.name)
+    for res in ent.get_resources():
+        print("  %s:" % res.name)
+```
+
+### Meter readings over a period of time
+
+Assuming we've got a resource from the discovery above...
+
+`get_readings` returns a list. Each element of the list is a
+`[timestamp, value]` pair which will be a `KWH` or `Pence` object.
+Use `value.value` to fetch the floating point value, or `str(value)` to
+represent as a string with the kWh/pence unit.
+```
+
+# Get time now, and 4 hours ago, this is the reading window
+now = datetime.datetime.now()
+t_from = now - datetime.timedelta(hours=4)
+t_to = now
+
+# Results will be summarised at one hour readings
+period = "PT1H"
+
+# Round times to start of period boundary
+t_from = resource.round(t_from, period)
+t_to = resource.round(t_to, period)
+
+rdgs = resource.get_readings(t_from, t_to, period)
+for r in rdgs:
+    print("    %s: %s" % (
+        r[0].astimezone().replace(tzinfo=None),
+        r[1]
+    ))
+```
+
+Timezones are managed according to t_from and t_to.  If you want to use GMT
+timezone, make sure t_from and t_to are set to use that timezone.
+
+### Tariff
+
+```
+t = res.get_tariff()
+print("    Tariff: rate=%.1f standing=%.1f" % (
+    t.current_rates.rate,
+    t.current_rates.standing_charge,
+))
+```
+
+### Not implemented / tested
+
+The API provides the means to get the current value of a resource (the last
+data point acquired) and the meter reading (the cumulative value, the number
+you would see if you go and look at the meter.
+
+I can't get these to work, maybe not implemented, or maybe only work with
+Glowmarkt hardware (I'm testing with a SMETS2 meter).
+
+## Command line
+
+### `glowmarkt-dump`
+
+Accesses the bright account and dumps out each resource's readings, and
+tariff information, human readable.
+
+```
+usage: glowmarkt-dump [-h] --username USERNAME --password PASSWORD
+                      [--minutes MINUTES] [--period PERIOD]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --username USERNAME, -u USERNAME
+                        Bright account username
+  --password PASSWORD, -p PASSWORD
+                        Bright account password
+  --minutes MINUTES, -m MINUTES
+                        Number of minutes to look back
+  --period PERIOD, -d PERIOD
+                        Summary period (default: PT1H)
+```
+
+e.g.
+
+```
+$ glowmarkt-dump -u 'username@example.org' -p 'p4ssw0rd' -m 240 -d PT1H
+Entity: DCC Sourced
+  electricity consumption:
+    2021-06-28 16:00:00: 0.000000 kWh
+    2021-06-28 17:00:00: 0.506000 kWh
+    2021-06-28 18:00:00: 2.355000 kWh
+    2021-06-28 19:00:00: 0.282000 kWh
+    2021-06-28 20:00:00: 0.000000 kWh
+    current: Not implemented.
+    meter reading: Not implemented.
+    Tariff: rate=16.3 standing=28.8
+  electricity cost:
+    2021-06-28 16:00:00: 0.000000 p
+    2021-06-28 17:00:00: 8.257920 p
+    2021-06-28 18:00:00: 38.433600 p
+    2021-06-28 19:00:00: 4.602240 p
+    2021-06-28 20:00:00: 0.000000 p
+    current: Not implemented.
+    meter reading: Not implemented.
+    Tariff: rate=16.3 standing=28.8
+```
+
+### `glowmarkt-csv`
+
+Accesses the readings for all resources with a particular classifier
+and writes out readings in CSV format.  Would be used with e.g.
+- `electricity.consumption`
+- `electricity.consumption.cost`
+- `gas.consumption`
+- `gas.consumption.cost`
+
+```
+usage: glowmarkt-csv [-h] --username USERNAME --password PASSWORD
+                     [--classifier CLASSIFIER] [--minutes MINUTES]
+                     [--period PERIOD] [--no-header]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --username USERNAME, -u USERNAME
+                        Bright account username
+  --password PASSWORD, -p PASSWORD
+                        Bright account password
+  --classifier CLASSIFIER, -c CLASSIFIER
+                        Resource classifier to use (default:
+                        electricity.consumption)
+  --minutes MINUTES, -m MINUTES
+                        Number of minutes to look back
+  --period PERIOD, -d PERIOD
+                        Summary period (default: PT1H)
+  --no-header, -n       Suppress CSV header
+```
+
+e.g.
+
+```
+$ scripts/glowmarkt-csv  -u 'username@example.org' -p 'p4ssw0rd' \
+    -m 240 -d PT30M -c electricity.consumption.cost
+entity,resource,time,value,unit
+DCC Sourced,121d3e6d-ccea-4b46-8b37-798d5cd880b3,2021-06-29T06:30:00,2.59488,pence
+DCC Sourced,121d3e6d-ccea-4b46-8b37-798d5cd880b3,2021-06-29T07:00:00,1.82784,pence
+DCC Sourced,121d3e6d-ccea-4b46-8b37-798d5cd880b3,2021-06-29T07:30:00,2.1216,pence
+DCC Sourced,121d3e6d-ccea-4b46-8b37-798d5cd880b3,2021-06-29T08:00:00,2.31744,pence
+DCC Sourced,121d3e6d-ccea-4b46-8b37-798d5cd880b3,2021-06-29T08:30:00,11.21184,pence
+DCC Sourced,121d3e6d-ccea-4b46-8b37-798d5cd880b3,2021-06-29T09:00:00,3.1008,pence
+DCC Sourced,121d3e6d-ccea-4b46-8b37-798d5cd880b3,2021-06-29T09:30:00,0,pence
+DCC Sourced,121d3e6d-ccea-4b46-8b37-798d5cd880b3,2021-06-29T10:00:00,0,pence
+```
+
+### `glowmarkt-today`
+
+Shows cumulative consumption for today (since midnight local time).
+
+Accesses the readings for all resources with a particular classifier
+and writes out readings in CSV format.  Would be used with e.g.
+- `electricity.consumption`
+- `electricity.consumption.cost`
+- `gas.consumption`
+- `gas.consumption.cost`
+
+```
+usage: glowmarkt-today [-h] --username USERNAME --password PASSWORD
+                       [--classifier CLASSIFIER]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --username USERNAME, -u USERNAME
+                        Bright account username
+  --password PASSWORD, -p PASSWORD
+                        Bright account password
+  --classifier CLASSIFIER, -c CLASSIFIER
+                        Resource classifier to use (default:
+                        electricity.consumption)
+```
+
+e.g.
+
+```
+$ scripts/glowmarkt-today  -u 'username@example.org' -p 'p4ssw0rd' \
+    -c electricity.consumption
+3.998
+```
+
+### `glowmarkt-mqtt`
+
+Gets cumulative consumption for today (since midnight local time) and pushes
+the value to an MQTT topic.
+
+Accesses the readings for all resources with a particular classifier
+and writes out readings in CSV format.  Would be used with e.g.
+- `electricity.consumption`
+- `electricity.consumption.cost`
+- `gas.consumption`
+- `gas.consumption.cost`
+
+```
+usage: glowmarkt-mqtt [-h] --username USERNAME --password PASSWORD
+                      [--classifier CLASSIFIER]
+                      [--mqtt-hostname MQTT_HOSTNAME]
+                      [--mqtt-username MQTT_USERNAME]
+                      [--mqtt-password MQTT_PASSWORD] [--topic TOPIC]
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --username USERNAME, -u USERNAME
+                        Bright account username
+  --password PASSWORD, -p PASSWORD
+                        Bright account password
+  --classifier CLASSIFIER, -c CLASSIFIER
+                        Resource classifier to use (default:
+                        electricity.consumption)
+  --mqtt-hostname MQTT_HOSTNAME, --host MQTT_HOSTNAME
+                        MQTT hostname (default: localhost)
+  --mqtt-username MQTT_USERNAME, -U MQTT_USERNAME
+                        MQTT username
+  --mqtt-password MQTT_PASSWORD, -P MQTT_PASSWORD
+                        MQTT password
+  --topic TOPIC, -t TOPIC
+                        MQTT topic to publish to (default:
+                        glowmarkt/consumption)
+```
+
+Simple test:
+- Assume Mosquitto is running (or just run `mosquitto`).
+- Run a subscriber: `mosquitto_sub -t glowmarkt/consumption`
+- Publish a reading: `glowmarkt-mqtt -u USER -p PASSWORD`
---
+++ packages/pyglowmarkt/pyglowmarkt-0.5.6.tar.gz/pyglowmarkt-0.5.6/glowmarkt/glowmarkt.py
@@ -0,0 +1,409 @@
+
+import requests
+import json
+import datetime
+
+PT1M = "PT1M"
+PT30M = "PT30M"
+PT1H = "PT1H"
+P1D = "P1D"
+P1W = "P1W"
+P1M = "P1M"
+P1Y = "P1Y"
+
+class Rate:
+    pass
+
+class Pence:
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return ("%s p" % self.value) if isinstance(self.value, str) else ("%.2f p" % self.value)
+    def unit(self):
+        return "pence"
+
+class KWH:
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return ("%s kWh" % self.value) if isinstance(self.value, str) else ("%.3f kWh" % self.value)
+    def unit(self):
+        return "kWh"
+
+class Unknown:
+    def __init__(self, value):
+        self.value = value
+    def __str__(self):
+        return "%s" % self.value
+    def unit(self):
+        return "unknown"
+
+class VirtualEntity:
+    def get_resources(self):
+        return self.client.get_resources(self.id)
+
+class Tariff:
+    pass
+
+class Resource:
+    def get_readings(self, t_from, t_to, period, func="sum", nulls=False):
+        return self.client.get_readings(self.id, t_from, t_to, period, func, nulls)
+    def get_current(self):
+        return self.client.get_current(self.id)
+    def get_meter_reading(self):
+        return self.client.get_meter_reading(self.id)
+    def get_tariff(self):
+        return self.client.get_tariff(self.id)
+    def round(self, when, period):
+        return self.client.round(when, period)
+    def catchup(self):
+        return self.client.catchup(self.id)
+
+class BrightClient:
+    def __init__(self, username, password):
+        self.username = username
+        self.password = password
+        self.application = "b0f1b774-a586-4f72-9edd-27ead8aa7a8d"
+        self.url = "https://api.glowmarkt.com/api/v0-1/"
+        self.session = requests.Session()
+
+        self.token = self.authenticate()
+
+    def authenticate(self):
+
+        headers = {
+            "Content-Type": "application/json",
+            "applicationId": self.application
+        }
+
+        data = {
+            "username": self.username,
+            "password": self.password
+        }
+
+        url = f"{self.url}auth"
+
+        resp = self.session.post(url, headers=headers, data=json.dumps(data))
+
+        if resp.status_code != 200:
+            raise RuntimeError("Authentication failed")
+
+        resp = resp.json()
+
+        if resp["valid"] == False:
+            raise RuntimeError("Expected an authentication token")
+
+        if "token" not in resp:
+            raise RuntimeError("Expected an authentication token")
+
+        return resp["token"]
+
+    def get_virtual_entities(self):
+
+        headers = {
+            "Content-Type": "application/json",
+            "applicationId": self.application,
+            "token": self.token
+        }
+
+        url = f"{self.url}virtualentity"
+
+        resp = self.session.get(url, headers=headers)
+
+        if resp.status_code != 200:
+            raise RuntimeError("Request failed")
+
+        resp = resp.json()
+
+        ves = []
+
+        for elt in resp:
+
+            ve = VirtualEntity()
+
+            ve.client = self
+
+            ve.application = elt["applicationId"]
+            ve.type_id = elt["veTypeId"]
+            ve.id = elt["veId"]
+
+            if "postalCode" in elt:
+                ve.postal_code = elt["postalCode"]
+            else:
+                ve.postal_code = None
+
+            if "name" in elt:
+                ve.name = elt["name"]
+            else:
+                ve.name = None
+
+            ves.append(ve)
+
+        return ves
+
+    def get_resources(self, ve):
+
+        headers = {
+            "Content-Type": "application/json",
+            "applicationId": self.application,
+            "token": self.token
+        }
+
+        url = f"{self.url}virtualentity/{ve}/resources"
+
+        resp = self.session.get(url, headers=headers)
+
+        if resp.status_code != 200:
+            raise RuntimeError("Request failed")
+
+        resp = resp.json()
+
+        resources = []
+
+        for elt in resp["resources"]:
+            r = Resource()
+            r.id = elt["resourceId"]
+            r.type_id = elt["resourceTypeId"]
+            r.name = elt["name"]
+            r.classifier = elt["classifier"]
+            r.description = elt["description"]
+            r.base_unit = elt["baseUnit"]
+
+            r.client = self
+
+            resources.append(r)
+            
+        return resources
+
+    def round(self, when, period):
+
+        # Work out a rounding value.  Readings seem to be more accurate if
+        # rounded to the near thing...
+        if period == "PT1M":
+            when = when.replace(second=0, microsecond=0)
+        elif period == "PT30M":
+            when = when.replace(minute=int(when.minute / 30),
+                                second=0,
+                                microsecond=0)
+        elif period == "PT1H":
+            when = when.replace(minute=0,
+                                second=0,
+                                microsecond=0)
+        elif period == "P1D":
+            when = when.replace(hour=0, minute=0,
+                                second=0,
+                                microsecond=0)
+        elif period == "P1W":
+            when = when.replace(hour=0, minute=0,
+                                second=0,
+                                microsecond=0)
+        elif period == "P1M":
+            when = when.replace(day=1, hour=0, minute=0,
+                                second=0,
+                                microsecond=0)
+        else:
+            raise RuntimeError("Period %s not known" % period)
+
+        return when
+
+    def get_readings(self, resource, t_from, t_to, period, func="sum", nulls=False):
+
+        headers = {
+            "Content-Type": "application/json",
+            "applicationId": self.application,
+            "token": self.token
+        }
+
+        utc = datetime.timezone.utc
+
+        def time_string(x):
+            """Converts a date or naive datetime instance to a timezone aware datetime instance in UTC and returns a time string in UTC. If its already timezone aware, it will still convert to UTC"""
+            if isinstance(x, datetime.datetime):
+                x = x.astimezone(utc)  # Converts local timezone with DST, or pre-specified timezones into UTC
+                return x.replace(tzinfo=None).isoformat()   # Remove TZ data without conversion (it's alreadu in UTC), then return ISO string format
+            elif isinstance(x, datetime.date):
+                x = datetime.datetime.combine(x, datetime.time()).astimezone(utc)   # Adds midnight time components (assumed in local timezone) and converts to UTC
+                return x.replace(tzinfo=None).isoformat()   # Would be similar to t_to.isoformat()[:19] where the +01:00 part is just truncated
+            else:
+                raise RuntimeError("to_from/t_to should be date/datetime")
+
+        # Convert to UTC datetimes, conversion will handle tz/dst conversions
+        t_from = time_string(t_from)
+        t_to = time_string(t_to)
+
+        params = {
+            "from": t_from,
+            "to": t_to,
+            "period": period,
+            "offset": 0,    # Enforce UTC server side, because we handle TZ/DST conversions client side
+            "function": func,
+            "nulls": 1 if nulls else 0,     # nulls=1 will give None instead of Zero for missing data so its easy to discriminate between missing data or a genuine zero usage
+        }
+
+        url = f"{self.url}resource/{resource}/readings"
+
+        resp = self.session.get(url, headers=headers, params=params)
+
+        if resp.status_code != 200:
+            raise RuntimeError("Request failed")
+
+        resp = resp.json()
+
+        if resp["units"] == "pence":
+            cls = Pence
+        elif resp["units"] == "kWh":
+            cls = KWH
+        else:
+            cls = Unknown
+
+        return [
+            [datetime.datetime.fromtimestamp(v[0], tz=utc).astimezone(),     # Timezone and DST aware datetime in local timezone converted from server side UTC
+             cls(v[1])]
+            for v in resp["data"] if v[1] is not None   # v[1] is None when nulls=1 and no data exists, so it simply wont be included in the returned list
+        ]
+
+    def get_current(self, resource):
+
+        # Tried it against the API, no data is returned
+
+        headers = {
+            "Content-Type": "application/json",
+            "applicationId": self.application,
+            "token": self.token
+        }
+
+        utc = datetime.timezone.utc
+
+        url = f"{self.url}resource/{resource}/current"
+
+        resp = self.session.get(url, headers=headers)
+
+        if resp.status_code != 200:
+            print(resp.text)
+            raise RuntimeError("Request failed")
+
+        resp = resp.json()
+
+        if len(resp["data"]) < 1:
+            raise RuntimeError("Current reading not returned")
+
+        if resp["units"] == "pence":
+            cls = Pence
+        elif resp["units"] == "kWh":
+            cls = KWH
+        else:
+            cls = Unknown
+
+        print(datetime.datetime.fromtimestamp(resp["data"][0][0]), tz=utc).astimezone()
+
+        return [
+            datetime.datetime.fromtimestamp(resp["data"][0][0], tz=utc).astimezone(),
+            cls(resp["data"][0][1])
+        ]
+
+    def catchup(self, resource):
+
+        # Tried it against the API, no data is returned
+
+        headers = {
+            "Content-Type": "application/json",
+            "applicationId": self.application,
+            "token": self.token
+        }
+
+        url = f"{self.url}resource/{resource}/catchup"
+
+        resp = self.session.get(url, headers=headers)
+
+        if resp.status_code != 200:
+            print(resp.text)
+            raise RuntimeError("Request failed")
+
+        resp = resp.json()
+
+        return resp
+
+    def get_meter_reading(self, resource):
+
+        # Tried it against the API, an error is returned
+        raise RuntimeError("Not implemented.")
+
+        headers = {
+            "Content-Type": "application/json",
+            "applicationId": self.application,
+            "token": self.token
+        }
+
+        utc = datetime.timezone.utc
+
+        url = f"{self.url}resource/{resource}/meterread"
+
+        resp = self.session.get(url, headers=headers)
+
+        if resp.status_code != 200:
+            raise RuntimeError("Request failed")
+
+        resp = resp.json()
+
+        if len(resp["data"]) < 1:
+            raise RuntimeError("Meter reading not returned")
+
+        if resp["units"] == "pence":
+            cls = Pence
+        elif resp["units"] == "kWh":
+            cls = KWH
+        else:
+            cls = Unknown
+
+        return [
+            [datetime.datetime.fromtimestamp(v[0], tz=utc).astimezone(), cls(v[1])]
+            for v in resp["data"]
+        ]
+
+    def get_tariff(self, resource):
+
+        headers = {
+            "Content-Type": "application/json",
+            "applicationId": self.application,
+            "token": self.token
+        }
+
+        url = f"{self.url}resource/{resource}/tariff"
+
+        resp = self.session.get(url, headers=headers)
+
+        if resp.status_code != 200:
+            raise RuntimeError("Request failed")
+
+        resp = resp.json()
+
+        ts = []
+
+        for elt in resp["data"]:
+
+            t = Tariff()
+            t.name = elt["name"]
+            t.commodity = elt["commodity"]
+            t.cid = elt["cid"]
+            t.type = elt["type"]
+
+            rt = Rate()
+            rt.rate = Pence(elt["currentRates"]["rate"])
+            rt.standing_charge = Pence(elt["currentRates"]["standingCharge"])
+            rt.tier = None
+            
+            t.current_rates = rt
+
+            # rts = []
+            # for elt2 in elt["structure"]:
+
+            #     rt = Rate()
+            #     rt.rate = elt2["planDetail"]["rate"]
+            #     rt.standing_charge = elt2["planDetail"]["standing"]
+            #     rt.tier = elt2["planDetail"]["tier"]
+            #     rts.append(rt)
+
+            # t.structure = rts
+        
+        return t
+
