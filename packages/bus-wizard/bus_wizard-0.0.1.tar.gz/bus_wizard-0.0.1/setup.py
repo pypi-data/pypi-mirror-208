0d766c54a14dbf3cde6f293959d117b7b067ea91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:19:55 +0000

    Add package bus-wizard/0.0.1/bus_wizard-0.0.1.tar.gz

---
+++ packages/bus-wizard/bus_wizard-0.0.1.tar.gz/bus_wizard-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import find_packages, setup
+
+with open("app/README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="bus_wizard",
+    version="0.0.1",
+    description="A package containing a bus wizard",
+    package_dir={"": "app"},
+    packages=find_packages(where="app"),
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/phanlehoang/bus_wizard",
+    author="Hoang Phan Le & An Thu Le",
+    author_email="anthule2000@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["bson >= 0.5.10"],
+    extras_require={
+        "dev": ["pytest>=7.0", "twine>=4.0.2"],
+    },
+    python_requires=">=3.10",
+)
