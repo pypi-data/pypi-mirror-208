0052ecc45c5e63bc84a4f23b3a0e9e5f81810221
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:57:39 +0000

    Add package stsf/0.1.10/stsf-0.1.10.tar.gz

---
+++ packages/stsf/stsf-0.1.10.tar.gz/stsf-0.1.10/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="stsf",
+    version="0.1.10",
+    url='https://github.com/lilocruz/solr-troubleshooting-framework',
+    author="Michael Cruz Sanchez",
+    author_email="superlinux.michael5@gmail.com",
+    license='GPLv3',
+    description="A Python module for troubleshooting Apache Solr installations",
+    keywords=["Solr", "Python", "Troubleshoot", "Framework", "lilocruz", 'Cli', 'Apache'],
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Developers",
+        "Operating System :: POSIX :: Linux",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+)
