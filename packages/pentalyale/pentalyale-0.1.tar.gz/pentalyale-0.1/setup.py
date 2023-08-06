29bff0cd5409c173033d35c692e659561a2b2bdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 06:15:52 +0000

    Add package pentalyale/0.1/pentalyale-0.1.tar.gz

---
+++ packages/pentalyale/pentalyale-0.1.tar.gz/pentalyale-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+  name='pentalyale',
+  version='0.01',
+  description='All PentalYale functions.',
+  author='PentalYale',
+  author_email='pentalyale@gmail.com',
+  url='https://github.com/PentalYale/pentalyale',
+  python_requires='>=3.6'
+)
