75030569228adf4f13bec7e1860778a8a32e088b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:20:10 +0000

    Add package claripy/9.2.51/claripy-9.2.51-py3-none-any.whl

---
+++ packages/claripy/claripy-9.2.51-py3-none-any.whl/claripy/frontend_mixins/constraint_fixer_mixin.py
@@ -0,0 +1,12 @@
+class ConstraintFixerMixin:
+    def add(self, constraints, **kwargs):
+        constraints = [constraints] if not isinstance(constraints, (list, tuple, set)) else constraints
+
+        if len(constraints) == 0:
+            return []
+
+        constraints = [BoolV(c) if isinstance(c, bool) else c for c in constraints]
+        return super().add(constraints, **kwargs)
+
+
+from .. import BoolV
