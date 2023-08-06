6a1d5dd3c6a642a8bf80d824bc5913867e9910d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:06 +0000

    Add package local-recruitment-employer-python-backend/0.0.12/local-recruitment-employer-python-backend-0.0.12.tar.gz

---
+++ packages/local-recruitment-employer-python-backend/local-recruitment-employer-python-backend-0.0.12.tar.gz/local-recruitment-employer-python-backend-0.0.12/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+     name='local-recruitment-employer-python-backend',
+     version='0.0.12',
+     author="Circles",
+     author_email="info@circles.life",
+     description="PyPI Package for Circles Local recruitment scrapers Python",
+     long_description="This is a package for sharing common Logger function used in different repositories",
+     long_description_content_type="text/markdown",
+     url="https://github.com/javatechy/dokr",
+     packages=setuptools.find_packages(),
+     classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: Other/Proprietary License",
+         "Operating System :: OS Independent",
+     ],
+ )
