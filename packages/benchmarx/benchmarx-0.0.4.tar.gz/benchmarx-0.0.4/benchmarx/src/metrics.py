66e6067ac196beab05cb9ec7d901aad3f50b75d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:41:56 +0000

    Add package benchmarx/0.0.4/benchmarx-0.0.4.tar.gz

---
+++ packages/benchmarx/benchmarx-0.0.4.tar.gz/benchmarx-0.0.4/benchmarx/src/metrics.py
@@ -0,0 +1,54 @@
+import logging
+from typing import List
+
+available_metrics = [
+    "history_x",
+    "history_f",
+    "history_df",
+    "nit",
+    "nfev",
+    "njev",
+    "nhev",
+    "errors",
+    "time"
+]
+
+
+available_metrics_to_plot = [
+    'fs',
+    'xs_norm',
+    'f_gap',       # TODO: rename to f_gap
+    'x_gap',       # TODO: rename to x_gap
+    'grads_norm'
+]
+
+nn_aval_metrics = [
+    'test_acc',
+    'train_acc',
+    'test_loss',
+    'train_loss'
+]
+
+
+def check_plot_metric(metric: str):
+    return metric in available_metrics_to_plot or metric in nn_aval_metrics
+
+
+def check_plot_metric(metric: List[str]):
+    for item in metric:
+        if item not in available_metrics_to_plot and item not in nn_aval_metrics:
+            logging.critical(f'Unsupported metric \'{item}\' to plot. Available metrics to plot: {available_metrics_to_plot}, {nn_aval_metrics}')
+            return False
+    return True
+
+
+def check_metric(metric: str):
+    return metric in available_metrics
+
+
+def check_metric(metric: List[str]):
+    for item in metric:
+        if item not in available_metrics:
+            logging.critical(f'Unsupported metric \'{item}\'. Available metrics: {available_metrics}')
+            return False
+    return True
